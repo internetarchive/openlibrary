49cbe595d7434494ef4f32b7e7fe9ca3c4874280
anand@ia301537.us.archive.org <anand@ia301537.us.archive.org>
2007-07-02 13:18:31 +0000

    adding changes in static/

--- pharos/static/js/toggle.js
+++ pharos/static/js/toggle.js
@@ -19,3 +19,24 @@ function toggle(id) {
 	}
 	return false;
 }
+
+function toggleVisibility(id, NNtype, IEtype, WC3type) {
+    if (document.getElementById) {
+        eval("document.getElementById(id).style.visibility = \"" + WC3type + "\"");
+    } else {
+        if (document.layers) {
+            document.layers[id].visibility = NNtype;
+        } else {
+            if (document.all) {
+                eval("document.all." + id + ".style.visibility = \"" + IEtype + "\"");
+            }
+        }
+    }
+}
+
+function tOn(id) {
+   toggleVisibility(id, "show", "visible", "visible") ;
+   }
+function tOff(id) {
+   toggleVisibility(id, 'hidden', 'hidden', 'hidden') ;
+   }
