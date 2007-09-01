0a40f9b81bffab7eb0b88112deb01e7adbcb85d7
Daniel B Giffin <daniel@mybuttocks.com>
2007-08-31 17:13:00 -0700

    config.sh

---
+++ catalog/onix/config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+
+export URL_CACHE_DIR=urlcache 				# a temporary directory
+export PHAROS_REPO="../.."						# the root of the Open Library repository
+export PYTHONPATH="$PHAROS_REPO"
+export PYTHON_INTERPRETER=python2.5
+
