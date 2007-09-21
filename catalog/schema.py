de892ea88d0423ee08f5e42bf954ca710a94908a
Daniel B Giffin <daniel@mybuttocks.com>
2007-09-21 13:58:34 -0700

    clean up indentation in schema.py

--- catalog/schema.py
+++ catalog/schema.py
@@ -42,19 +42,19 @@ schema_ordered = {
             'edition':
             [ 
                     ('source_record_loc', {
-		    	'title': "Source Record Locator",
+                        'title': "Source Record Locator",
                         'type': 'string',
                         'count': 'multiple',
                         'example': "marc_records_scriblio_net/part01.dat:29834:543",
                         'description': "a locator for the source record data" }),
                     ('source_record_id', {
-		    	'title': "Source Record Id",
+                        'title': "Source Record Id",
                         'type': 'string',
                         'count': 'multiple',
                         'example': "LC:DLC:00000006",
                         'description': "a record identifier that is globally unique and that also can be constructed consistently from the contents of a record and an identifier for its source catalog" }),
                     ('author_identifier', {
-		    	'title': "Author Identifier",
+                        'title': "Author Identifier",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': ['100:abcd author_id', '110:ab author_id', '111:acdn author_id'],
@@ -62,74 +62,74 @@ schema_ordered = {
                         'description': "unique author id in some catalog" }),
                     # ('authors', { 'type': 'id-ref', 'count': 'multiple', 'example': 'a/Mark_Twain' }),
                     ('contributions', {
-		    	'title': "Contributions",
+                        'title': "Contributions",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': ['700:abcde', '710:ab', '711:acdn'],
                         'example': 'Illustrated by: Steve Bjorkman' }),
                     ('title', {
-		    	'title': "Title",
+                        'title': "Title",
                         'type': 'string',
                         'marc_fields': '245:ab clean_name',
                         'example': 'The adventures of Tom Sawyer' }),
                     ('by_statement', {
-		    	'title': "By Statement",
+                        'title': "By Statement",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': '245:c',
                         'example': 'Herman Melville ; [illustrated by Barry Moser]' }),
                     ('sort_title', {
-		    	'title': "Title for sorting",
-			'type': 'string',
-			'example': 'adventures of Tom Sawyer'
-			}),
+                        'title': "Title for sorting",
+                        'type': 'string',
+                        'example': 'adventures of Tom Sawyer'
+                        }),
                     ('other_titles', {
-			'title': "Other Titles",
+                        'title': "Other Titles",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': ['246:a', '730:a-z', '740:apn' ],
                         'example': "Mark Twain's The Adventures of Tom Sawyer" }),
                     ('work_title', {
-		    	'title': "Work Title",
+                        'title': "Work Title",
                         'type': 'string',
                         'marc_fields': ['240:amnpr', '130:a-z'],
                         'description': "The 240 \"work title\" is used in the OCLC FRBR algorithm. The 130 is also used, and there should be either a 130 or a 240 in a record, but not both. It would be ideal if we could pick up either for the work title." }),
                     ('edition', {
-		    	'title': "Edition",
+                        'title': "Edition",
                         'type': 'string',
                         'marc_fields': '250:ab',
                         'example': '2nd. editon',
                         'description': 'information about this edition' }),
                     ('publisher', {
-		    	'title': "Publisher",
+                        'title': "Publisher",
                         'type': 'string',
                         'marc_fields': '260:b clean_name',
                         'example': 'W. W. Norton & Co.' }),
                     ('publish_place', {
-		    	'title': "Publish Place",
+                        'title': "Publish Place",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': '260:a clean',
                         'example': 'New York' }),
                     ('publish_date', { 
-		    	'title': "Publish Date",
+                        'title': "Publish Date",
                         'type': 'date',
                         'marc_fields': '008:7-10',
                         'example': '2006' }),
                     ('pagination', {
-		    	'title': "Pagination",
+                        'title': "Pagination",
                         'type': 'string',
                         'marc_fields': '300:a',
                         'example': "viii, 383 p. :",
                         'description': "full pagination information" }),
                     ('number_of_pages', {
-		    	'title': "Number of Pages",
+                        'title': "Number of Pages",
                         'type': 'int',
                         'example': '237',
                         'marc_fields': '300:a biggest_decimal',
                         'description': 'largest decimal found' }),
                     ('subjects', {
-		    	'title': "Subjects",
+                        'title': "Subjects",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': ['600:abcd--x--v--y--z',
@@ -138,54 +138,54 @@ schema_ordered = {
                                         '651:a--x--v--y--z'],
                         'example': 'Runaway children -- Fiction' }),
                     ('subject_place', {
-		    	'title': "Subject Places",
+                        'title': "Subject Places",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': ['651:a*', '650:z*'],
                         'example': "Venice (Italy)" }),
                     ('subject_time', {
-		    	'title': "Subject Times",
+                        'title': "Subject Times",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': ['600:y*', '650:y*'],
                         'example': '20th century' }),
                     ('genre', {
-		    	'title': "Genre",
+                        'title': "Genre",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': ['600:v*', '650:v*', '651:v*'],
                         'example': "Biography" }),
                     ('series', {
-		    	'title': "Series Information",
+                        'title': "Series Information",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': ['440:av', '490:av', '830:av' ],
                         'example': "Oxford world's classics" }),
                     ('language', {
-		    	'title': "Language",
+                        'title': "Language",
                         'type': 'string',
                         'marc_fields': '"ISO:" 008:35-37 +',
                         'example': 'ISO:tel',
                         'description': "coded or human-readable description of the text's language" }),
                     ('physical_format', {
-		    	'title': "Physical Format",
+                        'title': "Physical Format",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': '245:h' }),
                     ('notes', {
-		    	'title': "Notes",
+                        'title': "Notes",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': '500-599!505!520:a-z',
                         }),
                     ('description', {
-		    	'title': "Description",
+                        'title': "Description",
                         'type': 'text',
                         'marc_fields': '520:a'
                         }),
                     ('exerpts', { 'type': 'text', 'count': 'multiple' }),
                     ('table_of_contents', {
-		    	'title': "Table of Contents",
+                        'title': "Table of Contents",
                         'type': 'text',
                         'count': 'multiple',
                         'marc_fields': '505:art'
@@ -194,13 +194,13 @@ schema_ordered = {
                     ('scan_contributor', { 'type': 'string' }),
                     ('scan_sponsor', { 'type': 'string' }),
                     ('dewey_number', {
-		    	'title': "Dewey Decimal Classification",
+                        'title': "Dewey Decimal Classification",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': '082:a',
                         'example': '914.3' }),
                     ('LC_classification', {
-		    	'title': "Library of Congress Classification",
+                        'title': "Library of Congress Classification",
                         'type': 'string',
                         'count': 'multiple',
                         'marc_fields': '050:ab',
@@ -249,26 +249,26 @@ def print_html ():
                 print "</tbody></table>"
 
 def print_template (typename):
-	print "<dl class=\"metadata\">"
-	for (fname, fspec) in schema_ordered[typename]:
-		title = fspec.get ('title', fname)
-		description = fspec.get ('description', "")
-		multiple = (fspec.get ('count', "single") == "multiple")
+        print "<dl class=\"metadata\">"
+        for (fname, fspec) in schema_ordered[typename]:
+                title = fspec.get ('title', fname)
+                description = fspec.get ('description', "")
+                multiple = (fspec.get ('count', "single") == "multiple")
 
-		print "$if page.d.get('%s'):" % fname
-		print "\t<dt title=\"%s\"><b>%s</b></dt>" % (description, title)
-		if multiple:
-			print "\t$for v in page.d.get('%s'): <dd>$v</dd>" % fname
-		else:
-			print "\t<dd>$page.%s</dd>" % fname
-	print "</dl>"
+                print "$if page.d.get('%s'):" % fname
+                print "\t<dt title=\"%s\"><b>%s</b></dt>" % (description, title)
+                if multiple:
+                        print "\t$for v in page.d.get('%s'): <dd>$v</dd>" % fname
+                else:
+                        print "\t<dd>$page.%s</dd>" % fname
+        print "</dl>"
                 
 if __name__ == "__main__":
-	from sys import argv
-	if len (argv) == 3:
-		if argv[1] == "template":
-			typename = argv[2]
-			print_template (typename)
-	else:
-		print_html ()
+        from sys import argv
+        if len (argv) == 3:
+                if argv[1] == "template":
+                        typename = argv[2]
+                        print_template (typename)
+        else:
+                print_html ()
 
