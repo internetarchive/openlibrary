ee798723cfa9fdd72df2c0fc0c57c40b7542826d
aaronsw@frabjous-2.local <aaronsw@frabjous-2.local>
2007-09-26 15:05:15 -0700

    restore run.py

--- pharos/run.py
+++ pharos/run.py
@@ -1,24 +1,63 @@
 #!/usr/local/bin/python2.5
-import sys
+import sys, os
 import web
 import infogami
+import infogami.config
 import infogami.tdb.tdb
+olderror = infogami.config.internalerror
+def send(fr, to, message):
+   assert not fr.startswith('-') and not to.startswith('-'), 'security'
+   print >> web.debug, 'e'
+   i, o = os.popen2(["/usr/sbin/sendmail", '-f', fr, to])
+   i.write(message)
+   i.close()
+   o.close()
+   del i, o
 
-# Database parameters.
-# ** EDIT THIS **
-infogami.config.db_parameters = dict(dbn='postgres', db="pharos", user='postgres', pw='')
+def error():
+   print >> web.debug, 't'
+   olderror()
+   print >> web.debug, 't2'
+   import sys, traceback
+   tb = sys.exc_info()
+   text = """From: the bugman <bugs@openlibrary.org>
+To: the bugfixer <ol.errors@gmail.com>
+Subject: bug: %s: %s (%s)
+Content-Type: multipart/mixed; boundary="----here----"
 
+------here----
+Content-Type: text/plain
+Content-Disposition: inline
+
+%s
+
+%s
+
+------here----
+Content-Type: text/html; name="bug.html"
+Content-Disposition: attachment; filename="bug.html"
+
+""" % (tb[0], tb[1], web.ctx.path, web.ctx.method+' '+web.ctx.home+web.ctx.fullpath, ''.join(traceback.format_exception(*tb)))
+   text += str(web.djangoerror())
+   send('bugs@openlibrary.org', 'ol.errors@gmail.com', text)
+
+infogami.config.internalerror = error
+
+#web.db._hasPooling = False
+infogami.config.db_parameters = dict(dbn='postgres', host='pharosdb', db="pharos_staging", user='anand', pw='')
 infogami.config.site = 'openlibrary.org'
 infogami.config.cache_templates = True
 infogami.config.db_printing = False
-
-infogami.config.plugins += ['dump']
 infogami.config.plugin_path += ['plugins']
+infogami.config.plugins += ['search', 'dump', 'heartbeat']
+infogami.config.solr_server_address = ('pharosdb.us.archive.org', 8993)
 
-#@@ For openlibrary
-#infogami.config.plugins += ['search']
-#infogami.config.solr_server_address = ('pharosdb.us.archive.org', 8983)
-#infogami.tdb.logger.set_logfile(open("tdb.log", "a"))
+infogami.tdb.logger.set_logfile(open("tdb.log", "a"))
+
+@infogami.action
+def migrateusers():
+    import passwd
+    passwd.migrate()
 
 if __name__ == "__main__":
     infogami.run()
