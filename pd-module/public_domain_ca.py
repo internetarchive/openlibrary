00afcc9974c85caa695b9614bbc390d7bf6c28d9
phr@ia301442.us.archive.org <phr@ia301442.us.archive.org>
2007-10-15 18:43:51 +0000

    Beginning of public domain detection module.

---
+++ pd-module/public_domain_ca.py
@@ -0,0 +1,25 @@
+OLDEST_PERSON_EVER_IN_CANADA = 117
+
+def is_public_domain(edition):
+  assumptions = ["We're assuming that the data is correct."]
+  pubyear = edition.publication_year
+  maxauthordeath = None
+  for author in edition.authors:
+    death_year = None
+    if author.get('death_year'):
+      death_year = author.death_year
+    elif author.get('birth_year'):
+      death_year = author.birth_year + OLDEST_PERSON_EVER_IN_CANADA
+    if death_year:
+      if not maxauthordeath or death_year > maxauthordeath:
+        maxauthordeath = death_year
+    else:
+      assumptions.append("We're assuming that the author whose death dates are missing didn't die after those whose are available.")
+  if maxauthordeath:
+    if maxauthordeath > pubyear and pubyear < 1999:
+      pdyear = pubyear + 50            
+    else:
+      pdyear = maxauthordeath + 75
+  else:
+    pdyear = pubyear + 50
+  return { 'date': pdyear, 'assumptions': assumptions }
