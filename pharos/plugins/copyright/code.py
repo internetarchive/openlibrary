96546713f7007163e85d3f76c121b90144e04367
aaronsw@frabjous-2.local <aaronsw@frabjous-2.local>
2007-11-15 19:44:06 -0800

    make mroe robus

--- pharos/plugins/copyright/code.py
+++ pharos/plugins/copyright/code.py
@@ -1,7 +1,13 @@
+import re
 from infogami.utils import view
-
 import copyrightstatus
+
+r_year = re.compile(r'(?:[^\d]|^)(\d\d\d\d)(?:[^\d]|$)')
 
 @view.public
 def copyright_status(edition):
-    return copyrightstatus.copyright_status(edition)
+    try:
+        edition.publication_year = int(r_year.findall(edition.get('publication_date', '')))
+        return copyrightstatus.copyright_status(edition)
+    except:
+        return None # unknown
