106b574729d0316a7e6968883d32ec84a85755c3
phr@ia301442.us.archive.org <phr@ia301442.us.archive.org>
2008-02-15 01:04:14 +0000

    add plugin for copyright details page. db.py (mostly pasted from bookrev.py) is a utility function, that should really be merged.

--- pharos/plugins/copyright/code.py
+++ pharos/plugins/copyright/code.py
@@ -1,6 +1,7 @@
 import re
-from infogami.utils import view
+from infogami.utils import view, delegate, template
 import copyrightstatus
+import web,db
 
 r_year = re.compile(r'(?:[^\d]|^)(\d\d\d\d)(?:[^\d]|$)')
 
@@ -13,3 +14,15 @@ def copyright_status(edition):
         return None
     edition.publish_year = year
     return copyrightstatus.copyright_status(edition)
+
+class copyright(delegate.page):
+    def POST(self, site):
+        i = web.input(status='U',
+                      edition='zzzzzzz')
+        edition = db.get_thing(i.edition, db.get_type('type/edition'))
+        t = dict(U='unknown-copyright',
+                 PD='out-of-copyright',
+                 C='in-copyright').get(i.status)
+        assert t is not None
+        return getattr(template.render, t)(i.edition, t, edition)
+    GET = POST
