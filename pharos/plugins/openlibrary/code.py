3e2ce77c8c65d744f413d6c09e2a0110782254a1
anand@vihari.local <anand@vihari.local>
2008-03-13 19:39:18 -0700

    templates and static files for openlibrary plugin

--- pharos/plugins/openlibrary/code.py
+++ pharos/plugins/openlibrary/code.py
@@ -4,5 +4,5 @@ Open Library Plugin.
 
 from infogami.utils import types
 
-types.register_type('^a/[^/]*$', 'type/author')
-types.register_type('^b/[^/]*$', 'type/edition')
+types.register_type('^/a/[^/]*$', '/type/author')
+types.register_type('^/b/[^/]*$', '/type/edition')
