7e64724332343d70e5d9d67d79ce745ebdd10b8a
anand@vihari.local <anand@vihari.local>
2008-03-14 17:37:41 -0700

    templates and macros are no more specified as pages

--- pharos/run.py
+++ pharos/run.py
@@ -1,26 +1,37 @@
-#!/usr/bin/python2.5
-import sys, os
+#!/usr/local/bin/python2.5
+import sys
 import web
 import infogami
-import infogami.config
-import infogami.tdb.tdb
 
-infogami.config.internalerror = web.emailerrors("ol.errors@gmail.com", web.debugerror)
+# Database parameters.
+# ** EDIT THIS **
+infogami.config.db_parameters = dict(dbn='postgres', db="pharos", user='anand', pw='')
 
-infogami.config.db_parameters = dict(dbn='postgres', host='pharosdb', db=file('dbname').read().strip(), user='anand', pw='')
 infogami.config.site = 'openlibrary.org'
-infogami.config.encryption_key = eval(open('/etc/openlibrary.key').read())
+infogami.config.admin_password = "admin123"
 
 infogami.config.cache_templates = True
 infogami.config.db_printing = False
+
 infogami.config.plugin_path += ['plugins']
-infogami.config.plugins += ['openlibrary', 'search', 'pages', 'heartbeat', 'bookrev', 'upload', 'api', 'copyright', 'raw']
-infogami.config.solr_server_address = ('pharosdb.us.archive.org', 8993)
+infogami.config.plugins += ['openlibrary']
 
-infogami.tdb.logger.set_logfile(open("tdb.log", "a"))
+#@@ other openlibrary plugins
+#infogami.config.plugins += ['search']
+#infogami.config.solr_server_address = ('pharosdb.us.archive.org', 8983)
 
-infogami.config.from_address = "noreply@demo.openlibrary.org"
-infogami.config.smtp_server = "mail.archive.org"
+def createsite():
+    import web
+    from infogami.infobase.infobase import Infobase
+    web.config.db_parameters = infogami.config.db_parameters
+    web.config.db_printing = True
+    web.load()
+    Infobase().create_site(infogami.config.site, infogami.config.admin_password)
 
 if __name__ == "__main__":
-    infogami.run()
+    import sys
+    if '--createsite' in sys.argv:
+        createsite()
+    else:
+        infogami.run()
+
