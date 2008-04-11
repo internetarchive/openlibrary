1d20944988879a21cb7ddb0d553ae54ce39e1e30
anand@ia301537.us.archive.org <anand@ia301537.us.archive.org>
2008-04-11 15:03:23 +0000

    migrating heartbeat plugin

--- pharos/plugins/heartbeat/code.py
+++ pharos/plugins/heartbeat/code.py
@@ -4,7 +4,7 @@ from infogami.core import db
 import plugins.search.code as search
 
 class heartbeat(delegate.page):
-    def GET(self, site):
-        assert db.get_version(site, '').title == "Open Library"
-        assert 'b/Political_Fictions' in search.solr.search('political fictions didion').result_list
+    def GET(self):
+        assert db.get_version('/').title == "Open Library"
+        #assert 'b/Political_Fictions' in search.solr.search('political fictions didion').result_list
         print "HEALTHY"
