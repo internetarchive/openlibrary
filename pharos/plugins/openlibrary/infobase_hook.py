da5ed140777e1b6bf4fc8efcfa43bc194229f876
anand@ia301537.us.archive.org <anand@ia301537.us.archive.org>
2008-06-13 13:35:01 +0000

    Infobase hook to write book log

---
+++ pharos/plugins/openlibrary/infobase_hook.py
@@ -0,0 +1,25 @@
+"""Infobase hook for openlibrary.
+
+    * Log all modified book pages as required for the search engine.
+"""
+
+from infogami.infobase import config
+from infogami.infobase.logger import Logger
+
+import datetime
+
+root = getattr(config, 'booklogroot', 'booklog')
+
+_logger = Logger(root)
+
+def hook(object):
+    """
+    Add this hook to infobase.hooks to log all book modifications.
+    """
+    site = object._site
+    timestamp = datetime.datetime.utcnow()
+    if object.type.key == '/type/edition':
+        _logger.write('book', site.name, timestamp, object._get_data(expand=True))
+
+    #TODO: take care of author modifications
+
