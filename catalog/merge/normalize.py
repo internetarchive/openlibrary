bf21b1900aeddfd4691b3f68711ff87539b0bdc4
Edward Betts <edwardbetts@gmail.com>
2008-08-06 15:43:53 +0100

    another unicode fix

--- catalog/merge/normalize.py
+++ catalog/merge/normalize.py
@@ -6,7 +6,7 @@ re_whitespace = re.compile('[-\s,.]+')
 def normalize(s):
     if isinstance(s, unicode):
         s = s.replace(u'\u0142', u'l')
-    s = s.replace(u' & ', u' and ')
+    s = s.replace(' & ', ' and ')
     s = re_whitespace.sub(' ', s.lower())
     s = re_normalize.sub('', s.strip())
     return s
