f71391ba067948a6bc03236859305562997711f6
Edward Betts <edwardbetts@gmail.com>
2008-08-30 08:45:30 +0100

    add a bunch of code for reading MARC record from archive.org

--- catalog/get_ia.py
+++ catalog/get_ia.py
@@ -1,5 +1,7 @@
 import catalog.marc.fast_parse as fast_parse
 import catalog.marc.read_xml as read_xml
+import xml.etree.ElementTree as et
+import xml.parsers.expat
 from urllib2 import urlopen
 
 base = "http://archive.org/download/"
@@ -14,6 +16,81 @@ def get_ia(ia):
         pass
     url = base + ia + "/" + ia + "_meta.mrc"
     return fast_parse.read_edition(urlopen(url).read(), accept_electronic = True)
+
+def urlopen_keep_trying(url):
+    while True:
+        f = None
+        try:
+            f = urllib2.urlopen(url)
+        except urllib2.URLError:
+            pass
+        if f:
+            return f
+        print url, "failed"
+        sleep(5)
+        print "trying again"
+
+def files(archive_id):
+    url = archive_url + archive_id + "/" + archive_id + "_files.xml"
+    for i in range(5):
+        try:
+            tree = et.parse(urlopen_keep_trying(url))
+            break
+        except xml.parsers.expat.ExpatError:
+            sleep(2)
+    assert tree
+    for i in tree.getroot():
+        assert i.tag == 'file'
+        name = i.attrib['name']
+        if name.endswith('.mrc') or name.endswith('.marc') or name.endswith('.out') or name.endswith('.dat'):
+            yield archive_id + "/" + name
+
+def get_from_archive(locator):
+    (file, offset, length) = locator.split (":")
+    offset = int (offset)
+    length = int (length)
+
+    r0, r1 = offset, offset+length-1
+    url = 'http://www.archive.org/download/%s'% file
+
+    assert 0 < length < 100000
+
+    ureq = urllib2.Request(url, None, {'Range':'bytes=%d-%d'% (r0, r1)},)
+    return urllib2.urlopen(ureq).read(100000)
+
+def read_marc_file(part, f):
+    pos = 0
+    buf = None
+    while 1:
+        if buf:
+            length = buf[:5]
+            int_length = int(length)
+        else:
+            length = f.read(5)
+            buf = length
+        if length == "":
+            break
+        try:
+            assert length.isdigit()
+        except AssertionError:
+            print `length`
+            raise
+        int_length = int(length)
+        data = buf + f.read(int_length - len(buf))
+        buf = None
+        if data.find('\x1d') == -1:
+            data += f.read(40)
+            int_length = data.find('\x1d') + 1
+            print `data[-40:]`
+            assert int_length
+            buf = data[int_length:]
+            data = data[:int_length]
+        assert data.endswith("\x1e\x1d")
+        if len(data) < int_length:
+            break
+        loc = "%s:%d:%d" % (part, pos, int_length)
+        pos += int_length
+        yield (loc, data)
 
 def test_get_ia():
     ia = "poeticalworksoft00grayiala"
