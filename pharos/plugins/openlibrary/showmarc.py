a484760fafd0983ac9f46c22375571da5abea1da
Edward Betts <edwardbetts@gmail.com>
2008-09-25 15:58:53 +0100

    correct filename

--- pharos/plugins/openlibrary/showmarc.py
+++ pharos/plugins/openlibrary/showmarc.py
@@ -21,7 +21,7 @@ class show_ia(delegate.page):
     path = "/show-marc/ia:(.*)"
 
     def GET(self, ia):
-        filename = ia + "/" + ia + "_meta.xml"
+        filename = ia + "/" + ia + "_marc.xml"
 
         url = 'http://www.archive.org/download/%s'% filename
 
