0b918f6a795716ed69e42800bd546beb5108e989
Anand Chitipothu <anandology@gmail.com>
2008-11-06 15:57:47 +0530

    functions to run ol in server, client and standalone modes

--- pharos/ol.py
+++ pharos/ol.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
+import sys
 
 import schema
+
 import infogami
+from infogami.infobase import client
 import web
 
+config = web.storage(
+    db_parameters = None,
+    infobase_server = None,
+    writelog = None,
+    booklog = None,
+    errorlog = None,
+    memcache_servers = None,
+)
+
+
 def get_infobase():
+    """Creates infobase object."""
     from infogami.infobase import infobase, dbstore, cache
     params = infogami.config.infobase_parameters.copy()
-    params.pop('type') == 'local'
+    assert params.pop('type') == 'local'
     web.config.db_parameters = params
     infogami.config.db_parameters = params
     web.config.db_printing = True
     web.load()
-
-    print >> web.debug, web.config.db_parameters
 
     # hack to make cache work for local infobase connections
     cache.loadhook()
@@ -29,3 +41,46 @@ def run():
 
 def create(site='openlibrary.org'):
     get_infobase().create(site)
+
+def run_client():
+    """Run OpenLibrary as client.
+    Requires a separate infobase server to be running.
+    """
+    infogami.config.infobase_parameters = dict(type='remote', base_url=config.infobase_server)
+    infogami.run()
+    
+def run_server():
+    """Run Infobase server."""
+    infogami.config.infobase_parameters = dict(type='local', **config.db_parameters)
+    from infogami.infobase import server
+    server._infobase = get_infobase()
+
+    if '--create' in sys.argv:
+        server._infobase.create()
+    else:
+        server.run()
+    
+def run_standalone():
+    """Run OL in standalone mode.
+    No separate infobase server is required.
+    """
+    infogami.config.infobase_parameters = dict(type='local', **config.db_parameters)
+    web.config.db_parameters = config.db_parameters
+    web.load()
+
+    from infogami.infobase import server
+    server._infobase = get_infobase()
+    
+    if '--create' in sys.argv:
+        server._infobase.create()
+    else:
+        infogami.run()
+        
+run = run_standalone
+    
+infogami.config.site = 'openlibrary.org'
+infogami.config.admin_password = "admin123"
+infogami.config.login_cookie_name = "ol_session"
+
+infogami.config.plugin_path += ['plugins']
+infogami.config.plugins += ['openlibrary', 'i18n', 'api', 'sync', 'books', 'scod', 'search']
