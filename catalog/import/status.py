621b214377096c533b1e70ab16cd3be9336e5eff
Edward Betts <edwardbetts@gmail.com>
2008-11-15 09:30:08 +0000

    fixes to status page

--- catalog/import/status.py
+++ catalog/import/status.py
@@ -9,7 +9,39 @@ urls = (
 
 base_url = "http://0.0.0.0:9020/"
 
-done = set(['marc_western_washington_univ', 'marc_miami_univ_ohio'])
+def commify(n):
+    """
+Add commas to an integer `n`.
+ 
+>>> commify(1)
+'1'
+>>> commify(123)
+'123'
+>>> commify(1234)
+'1,234'
+>>> commify(1234567890)
+'1,234,567,890'
+>>> commify(None)
+>>>
+"""
+    if n is None: return None
+    r = []
+    for i, c in enumerate(reversed(str(n))):
+        if i and (not (i % 3)):
+            r.insert(0, ',')
+        r.insert(0, c)
+    return ''.join(r)
+
+done = ['marc_western_washington_univ', 'marc_miami_univ_ohio', 'bcl_marc', 'marc_loc_updates', 'marc_oregon_summit_records', 'CollingswoodLibraryMarcDump10-27-2008', 'hollis_marc', 'marc_laurentian', 'marc_ithaca_college', 'marc_cca']
+
+def read_book_count():
+    lines = list(open('/home/edward/book_count'))
+    t0, count0 = [int(i) for i in lines[0].split()]
+    t, count = [int(i) for i in lines[-1].split()]
+    t_delta = time() - t0
+    count_delta = count - count0
+    rec_per_sec = float(count_delta) / t_delta
+    return rec_per_sec, count
 
 files = eval(open('files').read())
 
@@ -40,6 +72,9 @@ class index:
         print "<style>th { vertical-align: bottom; text-align: left }</style>"
         print "</head><body>"
         print "<h1>Import status</h1>"
+#        print '<p style="padding: 5px; background: lightblue; border: black 1px solid; font-size:125%; font-weight: bold">MARC import is paused during the OCA conference</p>'
+        print "<b>Done:</b>", 
+        print ', '.join('<a href="http://archive.org/details/%s">%s</a>' % (ia, ia) for ia in done), '<br>'
         print "<table>"
         print "<tr><th>Archive ID</th><th>import</th><th>loaded</th><th>input<br>(rec/sec)</th>"
         print "<th>no match<br>(%)</th>"
@@ -69,11 +104,14 @@ class index:
             if k in done:
                 print '<tr bgcolor="#00ff00">'
                 broken = True
-            elif t1 > 120:
+            elif t1 > 600:
                 broken = True
                 print '<tr bgcolor="red">'
+            elif t1 > 120:
+                broken = True
+                print '<tr bgcolor="yellow">'
             else:
-                print '<tr>'
+                print '<tr bgcolor="#00ff00">'
                 total_rec_per_sec += rec_per_sec
                 total_load_per_sec += load_per_sec
                 total_recs += rec_no
@@ -92,9 +130,9 @@ class index:
                 print '<td align="right">100.0%</td>'
             else:
                 print '<td align="right">%.2f</td>' % rec_per_sec
-                print '<td align="right">%.2f%%</td>' % (float(q['load_count'] * 100.0) / q['rec_no'])
+                no_match = float(q['load_count']) / q['rec_no']
+                print '<td align="right">%.2f%%</td>' % (no_match * 100)
                 print '<td align="right">%.2f</td>' % load_per_sec
-                print '<td align="right">%.2f</td>' % t1
                 hours = q['t1'] / 3600.0
                 print '<td align="right">%.2f</td>' % hours
                 (pos, total) = progress(k, q['part'], q['pos'])
@@ -106,17 +144,24 @@ class index:
                     remaining_bytes = total - pos
                     sec = remaining_bytes / q['bytes_per_sec_total']
                     hours = sec / 3600
+                    days = hours / 24
                     print '<td align="right">%.2f</td>' % hours
+                    total_bytes = q['bytes_per_sec_total'] * q['t1']
+                    avg_bytes = total_bytes / q['rec_no']
+                    future_load = ((remaining_bytes / avg_bytes) * no_match) 
+                    total_future_load += future_load
+                    print '<td align="right">%s</td>' % commify(int(future_load))
                 else:
                     print '<td></td>'
 
             print '</tr>'
-        print '<tr><td>Total</td><td></td><td></td><td align="right">%.2f</td>' % total_rec_per_sec
+        print '<tr><td>Total</td><td align="right">%.2f</td>' % total_rec_per_sec
         if total_recs:
             print '<td align="right">%.2f%%</td>' % (float(total_load * 100.0) / total_recs)
         else:
             print '<td align="right"></td>' 
         print '<td align="right">%.2f</td>' % total_load_per_sec
+        print '<td></td>' * 3, '<td align="right">%s</td>' % commify(int(total_future_load))
         print '</tr></table>'
         print "<table>"
         print '<tr><td align="right">loading:</td><td align="right">%.1f</td><td>rec/day</td></tr>' % (total_load_per_sec * (60 * 60 * 24))
