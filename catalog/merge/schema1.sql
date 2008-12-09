d452a00391e5041a82311edde201fe722d54ace2
Edward Betts <edwardbetts@gmail.com>
2008-12-09 15:17:13 +0000

    plan to move import from dbhash to postgresql

---
+++ catalog/merge/schema1.sql
@@ -0,0 +1,19 @@
+create table isbn (
+    key varchar(16) not null,
+    value varchar(16) not null
+);
+
+create table oclc (
+    key varchar(16) not null,
+    value varchar(16) not null
+);
+
+create table title (
+    key varchar(16) not null,
+    value varchar(25) not null
+);
+
+create table lccn (
+    key varchar(16) not null,
+    value varchar(16) not null
+);
