a2a4a6be877adcfac27605af063e9726693cf467
Anand Chitipothu <anandology@gmail.com>
2008-12-09 10:01:28 +0530

    script to dump production tables

---
+++ scripts/dump_tables.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# 
+# script to dump tables of production database
+#
+# run this script as postgres user
+
+db='infobase_production'
+dir='/1/pharos/pgdumps'
+
+tables="thing datum edition_str"
+
+for t in $tables
+do
+    echo `date` -- dumping $t
+    pql $db -c "copy $t to stdout" | gzip -c > $dir/_$t.txt.gz
+    mv _$t.txt.gz $t.txt.gz
+done
+
+echo `date` -- finished
