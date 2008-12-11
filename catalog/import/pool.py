ce655776dc22a33faddf0c8dc73bcf985254b8ac
Edward Betts <edwardbetts@gmail.com>
2008-12-11 13:35:32 +0000

    switch to using postgresql for merge indexes

--- catalog/import/pool.py
+++ catalog/import/pool.py
@@ -1,21 +1,51 @@
 from urllib2 import urlopen, Request
 import cjson, web
-import dbhash
+from catalog.merge.merge_index import add_to_indexes(record)
+
+# need to use multiple databases
+# use psycopg2 to until open library is upgraded to web 3.0
+
+import psycopg2
+from catalog.read_rc import read_rc
+rc = read_rc()
+conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" \
+        % ('ol_merge', rc['user'], rc['host'], rc['pw']));
+cur = conn.cursor()
 
 index_path = '/1/pharos/edward/index/2/'
 
 pool_url = 'http://wiki-beta.us.archive.org:9020/'
 
-def build(fields):
+db_field = ('isbn', 'title', 'oclc', 'lccn')
+
+def pool_build(fields):
     params = dict((k, '_'.join(v)) for k, v in fields.iteritems() if k != 'author')
     url = pool_url + "?" + web.http.urlencode(params)
     ret = cjson.decode(urlopen(url).read())
     return ret['pool']
 
-def update(key, q):
+def pool_update(key, q):
     q['key'] = key
     req = Request(pool_url, cjson.encode(q))
     urlopen(req).read()
+
+def build(index_fields):
+    pool = {}
+    for field in db_fields:
+        if not field in index_fields:
+            continue
+        for v in index_fields[field]:
+            if field == 'isbn' and len(v) < 10:
+                continue
+            cur.execute('select key from ' + field + ' where value=%(v)s', {'v': v})
+            pool.setdefault(field, set()).update(i.key for i in cur.fetchall())
+    return dict((k, sorted(v)) for k, v in pool.iteritems())
+
+def update(key, q):
+    for field, value in merge_index(q):
+        sql = 'insert into ' + field + ' (key, value) values (%(key)s, %(value)s)'
+        cur.execute(sql, {'key': key, 'value': value })
+    q['key']
 
 def post_progress(archive_id, q):
     url = pool_url + "store/" + archive_id
---
+++ catalog/merge/merge_index.py
@@ -0,0 +1,40 @@
+# build a merge database from JSON dump
+
+import re
+from normalize import normalize
+
+def short_title(s):
+    return normalize(s)[:25]
+
+re_letters = re.compile('[A-Za-z]')
+
+def clean_lccn(lccn):
+    return re_letters.sub('', lccn).strip()
+
+def add_to_indexes(record):
+    if 'title' not in record or record['title'] is None:
+        return
+    if 'subtitle' in record and record['subtitle'] is not None:
+        title = record['title'] + ' ' + record['subtitle']
+    else:
+        title = record['title']
+    yield 'title', short_title(title)
+    if 'title_prefix' in record and record['title_prefix'] is not None:
+        title2 = short_title(record['title_prefix'] + title)
+        yield f['title'], title2
+
+    fields = [
+        ('lccn', 'lccn', clean_lccn),
+        ('oclc_numbers', 'oclc', None),
+        ('isbn_10', 'isbn', None),
+        ('isbn_13', 'isbn', None),
+    ]
+    for a, b, clean in fields:
+        if a not in record:
+            continue
+        for v in record[a]:
+            if not v or b=='isbn' and len(v) < 10:
+                continue
+            if clean:
+                v = clean(v)
+            yield f[b], v
