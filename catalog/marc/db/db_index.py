5037bc4c7ab767c295c808785239bcd11d3007c7
Edward Betts <edwardbetts@gmail.com>
2008-12-19 12:16:47 +0000

    load MARC data into DB

--- catalog/marc/db/db_index.py
+++ catalog/marc/db/db_index.py
@@ -1,9 +1,10 @@
-from catalog.get_ia import *
+#from catalog.get_ia import *
+from catalog.get_ia import read_marc_file
 from catalog.read_rc import read_rc
-from sources import sources
+#from sources import sources
 from time import time
 from catalog.marc.fast_parse import index_fields
-import dbhash
+import web, os, os.path
 
 # build an index of ISBN to MARC records
 
@@ -16,13 +17,20 @@ fields = ['title', 'oclc', 'isbn', 'lccn', 'work_title', 'other_titles']
 #db = dbhash.open(rc['index_path'] + 'isbn_to_marc.dbm', 'w')
 
 def add_to_db(isbn, loc):
+    print loc, isbn
+    return
     if isbn in db:
         db[isbn] += ' ' + loc
     else:
         db[isbn] = loc
 
 def process_record(pos, loc, data):
-    rec = index_fields(data, ['020'])
+    #rec = index_fields(data, ['020'])
+    want = ['006', '010', '020', '035', '245']
+    rec = index_fields(data, want)
+    print loc
+    print rec
+    return
     if not rec or 'isbn' not in rec:
         return
     for isbn in rec['isbn']:
@@ -50,13 +58,23 @@ rec_no = 0
 chunk = 1000
 total = 32856039
 
+def sources():
+    return ((i.archive_id, i.name) for i in web.select('marc_source'))
+
+def files(ia):
+    dir = rc['marc_path'] + ia + '/'
+    return [(i, os.path.getsize(dir + i)) for i in os.listdir(dir) if os.path.isfile(dir + i)]
+
 for ia, name in sources():
+    print
     print ia, name
     for part, size in files(ia):
-        print part, size
+        print ia, part, size
         full_part = ia + "/" + part
         filename = rc['marc_path'] + full_part
+        assert os.path.exists(filename)
         if not os.path.exists(filename):
+            print filename, 'missing'
             continue
         f = open(filename)
         for pos, loc, data in read_marc_file(full_part, f):
@@ -65,7 +83,7 @@ for ia, name in sources():
                 t = time() - t_prev
                 progress_update(rec_no, t)
                 t_prev = time()
-            print pos, loc, data
-#            process_record(pos, loc, data)
+#            print pos, loc
+            process_record(pos, loc, data)
 
-db.close()
+#db.close()
