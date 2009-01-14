710204310bd1ad039ea8142e83f7fa53804150bb
Edward Betts <edwardbetts@gmail.com>
2009-01-14 14:04:50 +0000

    doesn't do much yet

--- catalog/improve/improve_data.py
+++ catalog/improve/improve_data.py
@@ -1,7 +1,20 @@
 from catalog.infostore import get_site
-import sys
+import sys, codecs
+
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
 site = get_site()
 
-site.things({'name': sys.argv[1], 'type': '/type/author'})
+def get_authors_by_name(name):
+    return site.things({'name': name, 'type': '/type/author'})
 
+def get_books_by_author(key):
+    return site.things({'authors': key, 'type': '/type/edition'})
+
+for author_key in get_authors_by_name(sys.argv[1]):
+    print author_keys
+    book_keys = get_books_by_author(author_key)
+    for key in book_keys:
+        t = site.get(key)
+        print key, t.title
+        print '  ', t.isbn_10
