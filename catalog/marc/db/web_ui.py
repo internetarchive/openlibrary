63af42d48212cdfa7d58d1d122af99e7d1e7930f
Edward Betts <edwardbetts@gmail.com>
2009-01-14 14:05:27 +0000

    move MARC DB web formatting code to another file

--- catalog/marc/db/web_ui.py
+++ catalog/marc/db/web_ui.py
@@ -1,197 +1,33 @@
+import random
 import web #, dbhash
-from catalog.read_rc import read_rc
-from catalog.get_ia import get_data
-from catalog.marc.build_record import build_record
-from catalog.marc.fast_parse import get_all_subfields, get_tag_lines, get_first_tag, get_subfields
-from pprint import pprint
-import re, sys, os.path, random
-from catalog.marc.sources import sources
-from catalog.amazon.other_editions import find_others
+from web_marc_db import search_query, show_locs
 
-rc = read_rc()
+# too slow
+#def random_isbn():
+#    sql = "select value from isbn order by random() limit 1"
+#    return list(web.query(sql))[0].value
 
-web.config.db_parameters = dict(dbn='postgres', db='ol_merge', user=rc['user'], pw=rc['pw'], host=rc['host'])
-web.config.db_printing = False
-web.load()
-
-trans = {'&':'amp','<':'lt','>':'gt'}
-re_html_replace = re.compile('([&<>])')
-
-def marc_authors(data):
-    line = get_first_tag(data, set(['100', '110', '111']))
-    return ''.join("<b>$%s</b>%s" % (esc(k), esc(v)) for k, v in get_all_subfields(line)) if line else None
-
-def marc_publisher(data):
-    line = get_first_tag(data, set(['260']))
-    return ''.join("<b>$%s</b>%s" % (esc(k), esc(v)) for k, v in get_all_subfield)
-
-def marc_title(data):
-    line = get_first_tag(data, set(['245']))
-    return ''.join("<b>$%s</b>%s" % (esc(k), esc(v)) for k, v in get_subfields(line, set(['a', 'b']))) if line else None
-
-def find_isbn_file():
-    for p in sys.path:
-        f = p + "/catalog/isbn"
-        if os.path.exists(f):
-            return f
-
-isbn_file = find_isbn_file()
-isbn_count = os.path.getsize(isbn_file) / 11
-
-def list_to_html(l):
-    def blue(s):
-        return ' <span style="color:blue; font-weight:bold">%s</span> ' % s
-    return blue('[') + blue('|').join(l) + blue(']')
+isbn_file = '../../isbn'
+isbn_count = 9093242
 
 def random_isbn():
-    sql = "select value from isbn order by random() limit 1"
-    return list(web.query(sql))[0].value
+    f = open(isbn_file)
+    while 1:
+        f.seek(random.randrange(isbn_count) * 11)
+        isbn = f.read(10)
+        break
+        found = list(web.select('isbn', where='value=$v', vars={'v':isbn}))
+        if found > 1:
+            break
+    f.close()
+    return isbn
 
-def esc(s):
-    if not isinstance(s, basestring):
-        return s
-    return re_html_replace.sub(lambda m: "&%s;" % trans[m.group(1)], s.encode('utf8')).replace('\n', '<br>')
-
-#db_isbn = dbhash.open(rc['index_path'] + 'isbn_to_marc.dbm', 'r')
-#db_lccn = dbhash.open(rc['index_path'] + 'lccn.dbm', 'r')
-
-srcs = dict(sources())
-
-def src_list(loc):
-    return '; '.join(srcs[l[:l.find('/')]] for l in loc)
-
-data_cache = {}
-def marc_data(loc):
-    if loc not in data_cache:
-        data_cache[loc] = get_data(loc)
-    return data_cache[loc]
-
-def counts_html(v):
-    count = {}
-    lens = [len(i) for i, loc in v if i and isinstance(i, basestring)]
-    sep = '<br>' if lens and max(lens) > 20 else ' '
-    for i, loc in v:
-        count.setdefault(i, []).append(loc)
-    s = sorted(count.iteritems(), cmp=lambda x,y: cmp(len(y[1]), len(x[1]) ))
-    return sep.join('<b>%d</b>: <span title="%s">%s</span>' % (len(loc), src_list(loc), value if value else '<em>empty</em>') for value, loc in s)
-
-def list_works(this_isbn):
-    works = find_others(this_isbn, rc['amazon_other_editions'])
-    print '<a name="work">'
-    print '<h2>Other editions of the same work</h2>'
-    if not works:
-        print 'no work found'
-        return
-    print '<table>'
-    print '<tr><th>ISBN</th><th>Amazon edition</th><th></th><th>MARC titles</th></tr>'
-    for isbn, note in works:
-        if note.lower().find('audio') != -1:
-            continue
-        locs = db_isbn[isbn].split(' ') if isbn in db_isbn else []
-#        titles = [read_full_title(get_first_tag(marc_data(i), set(['245'])), accept_sound = True) for i in locs]
-        titles = [(marc_title(marc_data(i)), i) for i in locs]
-        num = len(locs)
-        #print '<tr><td><a href="/?isbn=%s">%s</a></td><td>%s</td><td>%d</td><td>%s</td></tr>' % (isbn, isbn, note, len(locs), list_to_html(titles))
-        print '<tr><td><a href="/?isbn=%s">%s</a></td><td>%s</td><td>%d</td><td>' % (isbn, isbn, note, len(locs))
-        print counts_html(titles)
-        print '</td></tr>'
-    print '</table>'
-
-def most_freq_isbn(input):
-    counts = {}
-    for a in input:
-        for b in a[0]:
-            counts[b] = counts.get(b, 0) + 1
-    return max(counts, key=counts.get)
-
-def show_locs(locs, isbn):
-    recs = [(loc, build_record(marc_data(loc))) for loc in locs]
-    keys = set()
-    print "records found from %d libraries<br>" % len(recs)
-    print '<ul>'
-    for loc, rec in recs:
-        s = srcs[loc[:loc.find('/')]]
-        print '<li><a href="http://openlibrary.org/show-marc/%s">%s</a>' % (loc, s)
-        keys.update([k for k in rec.keys()])
-        for f in 'uri':
-            if f in rec:
-                del rec[f]
-    print '</ul>'
-    keys -= set(['uri'])
-    print '<table>'
-    first_key = True
-    first = []
-    for f in ['isbn_10', 'title', 'subtitle', 'by_statement', 'authors', 'contributions']:
-        if f in keys:
-            first += [f]
-            keys -= set([f])
-    for k in first + list(keys):
-        v = [(rec.get(k, None), loc) for loc, rec in recs]
-        if k == 'isbn_10':
-            if not isbn:
-                isbn = most_freq_isbn(v)
-        if k == 'languages':
-            v = [([ i['key'][3:] for i in l ] if l else None, loc) for l, loc in v]
-        if all(i is None or (isinstance(i, list) and len(i) == 1) for i, loc in v):
-            v = [ (i[0] if i else None, loc) for i, loc in v]
-
-        print '<tr><th valign="top">%s</th><td>' % k
-        if any(isinstance(i, list) or isinstance(i, dict) for i, loc in v):
-            if k == 'authors': # easiest to switch to raw MARC display
-                v = [(marc_authors(marc_data(loc)), loc) for i, loc in v ]
-            elif k == 'isbn_10':
-                v = [ (list_to_html(sorted(i)), loc) if i else (None, loc) for i, loc in v]
-            else:
-                v = [ (list_to_html(i), loc) if i else (None, loc) for i, loc in v]
-        else:
-            v = [ (esc(i), loc) for i, loc in v]
-#        print `[i[0] for i in v]`, '<br>'
-        print counts_html(v)
-        if isbn and first_key:
-            print '<td valign="top" rowspan="%d"><img src="http://covers.openlibrary.org/b/isbn/%s-L.jpg">' % (len(first) + len(keys), isbn)
-            first_key = False
-        print '</td></tr>'
-    print '</table>'
-
-def search_oclc(oclc):
-    locs = []
-    for i in web.query('select archive_id, filename, pos, len from marc_file, marc_rec, marc_oclc, marc_source where marc_source.id=marc_file.marc_source and marc_oclc.marc_rec=marc_rec.id and marc_rec.marc_file=marc_file.id and value=$value', vars={'value': oclc}):
-        l = i.archive_id + '/' + ':'.join([i.filename, str(i.pos), str(i.len)])
-        locs.append(l)
-    if not locs:
-        print oclc, ' not found'
-        return
-
-    show_locs(locs, None)
-
-def search_lccn(lccn):
-    locs = []
-    for i in web.query('select archive_id, filename, pos, len from marc_file, marc_rec, marc_lccn, marc_source where marc_source.id=marc_file.marc_source and marc_lccn.marc_rec=marc_rec.id and marc_rec.marc_file=marc_file.id and value=$value', vars={'value': lccn}):
-        l = i.archive_id + '/' + ':'.join([i.filename, str(i.pos), str(i.len)])
-        locs.append(l)
-    if not locs:
-        print lccn, ' not found'
-        return
-
-    show_locs(locs, None)
-
-def search_isbn(isbn):
-    things = site.things({'type': '/type/edition', 'isbn_10': isbn})
-    if things:
-        print ', '.join('<a href="http://openlibrary.org%s">%s</a>' % (k, k) for k in things), '<br>'
-        
-    locs = []
-    for i in web.query('select archive_id, filename, pos, len from marc_file, marc_rec, marc_isbn, marc_source where marc_source.id=marc_file.marc_source and marc_isbn.marc_rec=marc_rec.id and marc_rec.marc_file=marc_file.id and value=$value', vars={'value': isbn}):
-        l = i.archive_id + '/' + ':'.join([i.filename, str(i.pos), str(i.len)])
-        locs.append(l)
-    if not locs:
-        print isbn, ' not found'
-        return
-
-    print '<a href="#work">skip to other editions of the same work</a><br>'
-    show_locs(locs, isbn)
-#    list_works(isbn)
-#    rec_data = dict((loc, get_data(loc)) for loc in locs)
+def search(field, value):
+    locs = search_query(field, value)
+    if locs:
+        show_locs(locs, value if field == 'isbn' else None)
+    else:
+        print value, ' not found'
 
 urls = (
     '/random', 'rand',
@@ -251,11 +87,14 @@ td { padding: 5px; background: #eee }
         print '</table>'
         print '</form>'
         if isbn:
-            search_isbn(isbn)
+            search('isbn', isbn)
         elif lccn:
-            search_lccn(lccn)
+            search('lccn', lccn)
         elif oclc:
-            search_oclc(oclc)
-        print "<body><html>"
+            search('oclc', oclc)
+        print "</body></html>"
+
 
 if __name__ == "__main__": web.run(urls, globals(), web.reloader)
+
+
