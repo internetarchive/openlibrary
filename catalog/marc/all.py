3c549a8e33da9180f281e50f8c7f5bf4a908a46d
Edward Betts <edwardbetts@gmail.com>
2009-01-14 13:21:36 +0000

    code for extracting sample of MARC records

--- catalog/marc/all.py
+++ catalog/marc/all.py
@@ -2,13 +2,28 @@ from catalog.get_ia import read_marc_file
 from catalog.read_rc import read_rc
 import web, os.path
 
+# iterate through every MARC record on disk
+
+rc = read_rc()
+
 def sources():
-    rc = read_rc()
     web.config.db_parameters = dict(dbn='postgres', db='ol_merge', user=rc['user'], pw=rc['pw'], host=rc['host'])
     web.config.db_printing = False
     web.load()
 
     return ((i.id, i.archive_id, i.name) for i in web.select('marc_source'))
+
+def files(ia):
+    endings = ['.mrc', '.marc', '.out', '.dat', '.records.utf8']
+    def good(filename):
+        return any(filename.endswith(e) for e in endings)
+
+    dir = rc['marc_path'] + ia
+    dir_len = len(dir) + 1
+    files = []
+    for dirpath, dirnames, filenames in os.walk(dir):
+        files.extend(dirpath + "/" + f for f in sorted(filenames))
+    return [(i[dir_len:], os.path.getsize(i)) for i in files if good(i)]
 
 def iter_marc():
     rec_no = 0
--- catalog/works/harry_potter_marc.py
+++
@@ -1,4 +0,0 @@
-from catalog.marc.all import iter_marc
-
-for i in iter_marc():
-    print i
