8cc50f2763d222a28c1030ff35796cb69d8594b2
Anand Chitipothu <anandology@gmail.com>
2009-04-29 11:26:30 +0000

    add webpy and infogami as submodules

--- setup.sh
+++ setup.sh
@@ -1,46 +1,11 @@
 #! /bin/bash
 
-function base() {
-    set $* $PWD
-    root=$1
-    if [ -d $1/.git ]
-    then 
-        echo $1
-    else
-        base `dirname $1`
-    fi
-}
+git submodule init
+git submodule update
 
-gitroot=`base`
+root=`dirname $0`
 
-function setup_infogami() {
-    echo "** updating infogami repository **"
+cd $root/pharos
+ln -fs ../infogami/infogami .
+ln -fs ../webpy/web .
 
-    if [ -d $gitroot/infogami ]
-    then
-       cd $gitroot/infogami && git pull
-    else
-        cd $gitroot && git clone git://github.com/infogami/infogami $gitroot/infogami
-    fi
-}
-
-function setup_webpy() {
-    if [ ! -d $gitroot/webpy ]
-    then
-        echo "** fetching web.py**"
-
-        cd $gitroot && wget http://webpy.org/static/web.py-0.31.tar.gz
-        cd $gitroot && tar xzf web.py-0.31.tar.gz
-    fi
-}
-
-function setup_symlinks() {
-    echo "**creating symlinks**"
-    cd $gitroot/pharos
-    ln -fs ../infogami/infogami .
-    ln -fs ../webpy/web .
-}
-
-setup_infogami
-setup_webpy
-setup_symlinks
