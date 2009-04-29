b8f13ac01c8cac7d4eaf766fb1c928c40276557a
Anand Chitipothu <anandology@gmail.com>
2009-04-29 17:22:52 +0530

    run git submodule commands only when the working copy is a git repository
    do nothing when the source is downloaded from a tarball/zipball.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
-git submodule init
-git submodule update
+root=`dirname $0`
 
+# run git submodule commands only when the working copy is a git repository 
+# (do nothing when the source is downloaded from a tarball/zipball).
+if [ -d $root/.git ];
+then
+    git submodule init
+    git submodule update
+fi
