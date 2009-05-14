6b26db615665279dfc128365aa5ede81767de54e
Anand Chitipothu <anandology@gmail.com>
2009-05-14 15:52:48 +0530

    script to install all required software on OL nodes

---
+++ scripts/setup_node.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+# script to install all required software on OL nodes
+
+# create OL folders
+OL_FOLDERS="/0/pharos /1/pharos /2/pharos /3/pharos"
+mkdir -p $OL_FOLDERS
+chgrp  pharos $OL_FOLDERS
+chmod g+w $OL_FOLDERS
+
+apitude install         \
+    python-setuptools   \
+    python-dev          \
+    python2.5-psycopg2  \
+    python-imaging      \
+    git-core            \
+    sqlite3
+
+easy_install -Z \
+    web.py      \
+    flup        \
+    simplejson  \
+    pymarc
+
+# setup code
+mkdir -p /0/pharos/code /0/pharos/scripts /0/pharos/services /0/pharos/etc
+cd /0/pharos/code
+git clone git://github.com/openlibrary/openlibrary.git
+
