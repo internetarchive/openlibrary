6ecae52692e8cc6d457a7bd821ab0c8faf4edaaf
Edward Betts <edwardbetts@gmail.com>
2009-06-26 09:09:59 +0000

    update Amazon crawler

--- catalog/amazon/crawl.py
+++ catalog/amazon/crawl.py
@@ -1,106 +1,189 @@
-#!/usr/local/bin/python2.5
-
-import urllib2, re
-from pprint import pprint
+from lxml.html import parse, tostring, fromstring
+import re, sys
+from urllib import unquote
+from urllib2 import urlopen
 from time import sleep
+from os.path import exists
+from datetime import date, timedelta
+import codecs
 
-# Amazon crawler
+def percent(a, b):
+    return float(a * 100.0) / b
 
-re_ul_ref = re.compile('<ul id="ref_\d+">')
-re_books_cat = re.compile('<li style="margin-left: -?\d+px">.*Books')
-re_cat_link1 = re.compile('<li style="margin-left: -?\d+px;"><a href=')
-re_cat_link2 = re.compile('<a href="(.*?)".*?>.*?class="refinementLink">(.*?)</span>')
+page_size = 12
+max_pages = 100
+max_results = page_size * max_pages
 
-re_next_page = re.compile('<span class="pagnSep">\|</span><span class="pagnNext"><a href="(.*?)"  class="pagnNext" id="pagnNextLink"')
-re_product_title = re.compile('<div class="productTitle"><a href=".*/dp/([^/]*)/.*">')
+re_product_title = re.compile('/dp/([^/]*)')
+re_result_count = re.compile('Showing (?:[\d,]+ - [\d,]+ of )?([\d,]+) Result')
+re_rh_n = re.compile('rh=n%3A(\d+)%2C')
+re_facet_count = re.compile(u'^\xa0\(([\d,]+)\)$')
 
-def read_page(f, read_cats=False):
-    state = 4
-    page = {'books': []}
-    if read_cats:
-        page['cats'] = []
-        state = 0
-    for line in f:
-        if state == 0:
-            if line.startswith("<h2>Category</h2>"):
-                state = 1
+base_url = "http://amazon.com/s?ie=UTF8&rh="
+rh = 'i:stripbooks,p_n_feature_browse-bin:618083011,p_n_date:'
+
+out_dir = '/1/edward/amazon/crawl'
+
+def get_url(params):
+    filename = 'cache/' + params
+    for i in range(10):
+        try:
+            url = base_url + rh + params
+            print url
+            req = urlopen(url)
+            page = req.read()
+            break
+        except IOError:
+            if i == 9:
+                raise
+            pass
+        sleep(2)
+    #open(filename, 'w').write(page)
+    return fromstring(page)
+
+def get_page(params):
+    return get_url(params) # no cache
+    filename = 'cache/' + params
+    if exists(filename):
+        root = parse(filename).getroot()
+    else:
+        root = get_url(params)
+    return root
+
+def get_total(root):
+    result_count = root.find(".//td[@class='resultCount']").text
+    m = re_result_count.match(result_count)
+    return int(m.group(1).replace(',', ''))
+
+def read_books(params, root):
+    # sometimes there is no link, bug at Amazaon
+    # either skip it, or reload the page
+    for i in range(5):
+        book_links = [e.find('.//a[@href]') for e in root.find_class('dataColumn')]
+        if all(a is not None for a in book_links):
+            break
+        sleep(2)
+        print 'retry:', url
+        root = get_url(params)
+
+    return [re_product_title.search(a.attrib['href']).group(1) for a in book_links]
+
+def get_cats(root):
+    cats = []
+    for div in root.find_class('narrowItemHeading'):
+        if div.text != 'Department':
             continue
-        if state == 1:
-            assert re_ul_ref.match(line)
-            state = 2
-            continue
-        if state == 2:
-            assert re.compile('<li style="margin-left: -?\d+px">.*Books')
-            state = 3
-            continue
-        if state == 3:
-            if line.startswith('</ul>'):
-                state = 4
-                continue
-            if not re_cat_link1.match(line):
-                continue
-            m = re_cat_link2.search(line)
-            assert m
-            page['cats'].append({'url': m.group(1), 'title': m.group(2)})
-            continue
-            (url, title) = m.groups()
-            title = title.replace('&amp;', '&')
-            books['cats'].append({'url': url, 'title': title})
-        if state == 4:
-            if line.startswith('<div class="header"><div class="resultCount">'):
-                state = 5
-            continue
-        if state == 5:
-            if line.startswith('<div class="sortBy">'):
-                state = 6
-            elif line.startswith('<span class="pagnSep">|</span><span class="pagnNext">'):
-                m = re_next_page.match(line)
-                assert m
-                page['link'] = m.group(1)
-                state = 6
-            continue
-        if state == 6:
-            if line.find('<div class=listView>') != -1:
-                state = 7
-            continue
-        if state == 7:
-            if line.startswith('<div id="bottomBox">'):
-                break
-            if line.startswith('<div id="sponsoredLinks">'):
-                break
-            if line.find('<div class="productTitle">') != -1:
-                m = re_product_title.search(line)
-                assert m
-                page['books'].append(m.group(1))
-            continue
-    return page
+        container = div.getparent()
+        assert container.tag == 'td' and container.attrib['class'] == 'refinementContainer'
+        break
 
-def urlopen(url):
-    request = urllib2.Request(url)
-    request.add_header('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071618 Firefox/3.0b3pre (Debian-3.0.1-1)')
-    return urllib2.urlopen(request)
+    table = container.find('table')
+    for e in table.iterfind(".//div[@class='refinement']"):
+        a = e[0]
+        assert a.tag == 'a'
+        span1 = a[0]
+        assert span1.tag == 'span' and span1.attrib['class'] == 'refinementLink'
+        span2 = a[1]
+        assert span2.tag == 'span' and span2.attrib['class'] == 'narrowValue'
+        href = a.attrib['href']
+        m1 = re_rh_n.search(href)
+        m2 = re_facet_count.search(span2.text)
+        cats.append((int(m1.group(1)), span1.text, int(m2.group(1).replace(',',''))))
+        
+    return cats
 
-out = open('out', 'w')
+    for e in container.find('table').find_class('refinementLink'):
+        a = e.getparent()
+        assert a.tag == 'a'
+        cat = { 'url': a.attrib['href'], 'title': e.text }
+        href = a.attrib['href']
+        m = re_rh_n.search(href)
+        cats.append((int(m.group(1)), e.text))
 
-def get_page(url, depth = 0):
-    f = urlopen(url)
-    page = read_page(f, read_cats=True)
-    f.close()
-    for isbn in page['books']:
-        print >> out, isbn
-    cats = page['cats']
+def read_page(cur_date):
+    params = cur_date.strftime("%Y%m%d")
+    root = get_page(params)
+    total = get_total(root)
+    grand_total = total
+    pages = (total / page_size) + 1
+    print 'total:', total, 'pages:', pages
 
-    while 'link' in page:
-        f = urlopen("http://amazon.com" + page['link'])
-        page = read_page(f, read_cats=False)
-        f.close()
-        for isbn in page['books']:
-            print >> out, isbn
+    cats = get_cats(root)
+    #return grand_total, [], cats
 
-    for cat in cats:
-        print " " * depth, cat['title']
-        get_page("http://amazon.com" + cat['url'], depth + 1)
+    books = set()
 
-root_url = 'http://www.amazon.com/s/qid=1220873277/ref=sr_ex_n_1?ie=UTF8&rs=1&sort=editionspsrank&bbn=1&rh=i%3Astripbooks%2Cp_n_feature_browse-bin%3A618083011'
-get_page(root_url)
-out.close()
+    books.update(read_books(params, root))
+    for page in range(2, min((pages, 100))+1):
+        params_with_page = params + "&page=%d" % page
+        books.update(read_books(params_with_page, get_page(params_with_page)))
+        print page, len(books)
+
+    print len(books)
+
+#    for order_by in ('-price', 'price', '-editionspsrank'):
+#        print order_by
+#        for page in range(1, min((pages, 100))+1):
+#            params_with_page = params + "&page=%d&sort=%s" % (page, order_by)
+#            cur = read_books(params_with_page, get_page(params_with_page))
+#            books.update(cur)
+#            print order_by, page, len(books), len(cur)
+#        print order_by, len(books)
+#
+#    return
+
+    cats = get_cats(root)
+    print 'cat total:', sum(i[2] for i in cats)
+    if total > max_results:
+        for n, title, count in cats:
+            print `n, title, count`
+            params_with_cat = params + ",n:" + str(n)
+            root = get_page(params_with_cat)
+            cat_total = get_total(root)
+            pages = (cat_total / page_size) + 1
+            print 'cat_total:', total, 'pages:', total / page_size
+            if cat_total > max_results:
+                print 'cat_total (%d) > max results (%d)' % (total, max_results)
+    #        assert cat_total <= max_results
+            books.update(read_books(params_with_cat, root))
+            for page in range(2, min((pages, 100)) + 1):
+                params_with_page = params_with_cat + "&page=%d" % page
+                books.update(read_books(params_with_page, get_page(params_with_page)))
+                print `n, title, page, cat_total / page_size, len(books), "%.1f%%" % percent(len(books), grand_total)`
+            print
+
+    return total, books, cats
+
+def write_books(cur_date, books):
+    out = open(out_dir + "/list." + cur_date, 'w')
+    for b in books:
+        print >> out, b
+    out.close()
+    i = 0
+
+    out = open(out_dir + '/amazon.' + cur_date, 'w')
+    index = open(out_dir + '/index.' + cur_date, 'w')
+
+    for asin in books:
+        i+= 1
+        print i, asin
+        page = urlopen('http://amazon.com/dp/' + asin).read()
+        print >> index, asin, out.tell(), len(page)
+        out.write("%10s,%d:%s" % (asin, len(page), page))
+    out.close()
+    index.close()
+
+one_day = timedelta(days=1)
+cur = date(2009, 06, 06)
+while True:
+    print cur
+    total, books, cats = read_page(cur)
+    open(out_dir + '/total.' + str(cur), 'w').write(str(total) + "\n")
+
+    out = open(out_dir + "/cats." + str(cur), 'w')
+    for i in cats:
+        print >> out, i
+    out.close()
+    print len(books)
+    write_books(str(cur), books)
+    cur -= one_day
