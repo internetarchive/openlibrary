902e52cc1fd43459e09af83f191b29271025eebd
phr <phr@phr-desktop.(none)>
2009-06-26 00:44:23 -0700

    add arg for log dir

--- scripts/start-solr.sh
+++ scripts/start-solr.sh
@@ -1,3 +1,13 @@
+# solr start script
+# start-solr.sh SOLR_HOME DATA_DIR PORTNUM LOGDIR
+#   SOLR_HOME is where all the config stuff is, i.e. solrconfig/solr-biblio
+#   DATA_DIR is where the index files are
+#   PORTNUM is the listening port
+#   LOGDIR is where the log files will go
 export SOLR=/usr/local/solr
-cd $1
-java -Xms2000m -Xmx2000m -Dsolr.solr.home=$1 -Dsolr.data.dir=$2 -Djetty.port=$3 -jar $SOLR/example/start.jar
+cd $SOLR/example
+java -Xms2000m -Xmx2000m \
+  -Djava.util.logging.config.file=logging.properties \
+  -Dsolr.solr.home=$1 -Dsolr.data.dir=$2 -Djetty.port=$3 \
+  -Djetty.logs=$4 \
+  -jar start.jar
