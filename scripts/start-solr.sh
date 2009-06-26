c678b459de74a865422ec7dc1577da17ed3a1e10
phr <phr@phr-desktop.(none)>
2009-06-26 00:04:41 -0700

    run in conf dir

--- scripts/start-solr.sh
+++ scripts/start-solr.sh
@@ -1,3 +1,3 @@
-cd /usr/local/solr/example
-sleep 5
-java -Xms2000m -Xmx2000m -Dsolr.solr.home=$1 -Dsolr.data.dir=$2 -Djetty.port=$3 -jar start.jar
+export SOLR=/usr/local/solr
+cd $1
+java -Xms2000m -Xmx2000m -Dsolr.solr.home=$1 -Dsolr.data.dir=$2 -Djetty.port=$3 -jar $SOLR/example/start.jar
