5b60513c2c5885ec939b74ac3f33eba2bcbcd71c
phr <phr@phr-desktop.(none)>
2009-06-25 23:44:34 -0700

    take solr home on command line

--- scripts/start-solr.sh
+++ scripts/start-solr.sh
@@ -1,3 +1,4 @@
-chdir /usr/local/solr
+# chdir /usr/local/solr
+chdir /home/phr/apache-solr-1.3.0/example/
 sleep 5
-java -Xms2000m -Xmx2000m -Dsolr.data.dir=$1 -Djetty.port=$2 -jar start.jar
+java -Xms2000m -Xmx2000m -Dsolr.solr.home=$1 -Dsolr.data.dir=$2 -Djetty.port=$3 -jar start.jar
