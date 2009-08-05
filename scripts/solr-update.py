c5c1566e848d740f4547193a2c543deac20cb798
Paul Rubin <phr@ia331503.us.archive.org>
2009-08-05 10:09:17 +0000

    clarify comment some more

--- scripts/solr-update.py
+++ scripts/solr-update.py
@@ -12,16 +12,19 @@
 # There are implementation reasons for the above.  I should get around
 # to fixing them, but in the normal case, importing a dump takes 12+
 # hours, so a few more minutes doesn't make much difference.
-
+#
 # To import a json dump:
 #   ./solr-update.py --dump=dumpfile.json --solr=localhost:8012
-
+# substituting your own dump filename and solr host/port of course.
+#
 # To import solr update log starting 3 days ago:
 #   ../solr-update.py --rsync --days=3  --solr=localhost:8012
-
+# substituting some other number for 3 if you want, and some
+# other host/port for your solr installation.
+#
 # You can use (for example) hours=3 or minutes=3 instead of days=3
 # but (for now) you can't combine these.  
-
+#
 # When you use the rsync option, the process will keep running and
 # listening to the update log until you kill the process.
 
