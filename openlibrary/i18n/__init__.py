3d7515d237895dbb0047adef9425ea58952fd835
Anand Chitipothu <anandology@gmail.com>
2009-09-02 17:38:10 +0530

    fix unicode error in lgettext

--- openlibrary/i18n/__init__.py
+++ openlibrary/i18n/__init__.py
@@ -172,7 +172,7 @@ class LazyObject:
         self._creator = creator
         
     def __str__(self):
-        return str(self._creator())
+        return web.safestr(self._creator())
         
     def __repr__(self):
         return repr(self._creator())
