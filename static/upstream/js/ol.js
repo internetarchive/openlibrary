03003f9e6a0cf0d55ee11cdfbb56ade5411f56bc
Anand Chitipothu <anandology@gmail.com>
2009-09-03 18:17:44 +0530

    fix search on home page

--- static/upstream/js/ol.js
+++ static/upstream/js/ol.js
@@ -8,3 +8,35 @@ function twitterOn() {
     });
 };
 
+function setupSearch() {
+  $(".optionsNoScript").hide();
+  $(".optionsScript").show();
+
+  // take alternate text from a special attribute instead of hard-coding in js. This required to support i18n.
+  var a1 = $("a#searchHead").html();
+  var a2 = $("a#searchHead").attr("text2");
+
+  $("a#searchHead").click(function(){
+    $(this).toggleClass("attn");
+    $("#headerSearch").toggleClass("darker");
+    $("#topOptions").toggle();
+    $(this).toggleText(a1, a2);
+  });
+
+  // take alternate text from a special attribute instead of hard-coding in js. This required to support i18n.
+  var b1 = $(".fullText").html();
+  var b2 = $(".fullText").attr("text2");
+
+  $(".fullText").click(function(){
+    $(this).toggleClass("attn");
+    $(this).parent().parent().next(".searchText").slideToggle();
+    $(this).toggleText(b1, b2);
+  });
+  $("a#searchFoot").click(function(){
+    $(this).toggleClass("attn");
+    $("#footerSearch").toggleClass("darker");
+    $("#bottomOptions").toggle();
+    $("#bottomText").toggle();
+    $(this).toggleText(a1, a2);
+  });
+}
