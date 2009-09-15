ffc61ba7911b5764efbd9e30fe9b571f627bf5b7
Edward Betts <edward@archive.org>
2009-09-15 10:44:17 +0100

    copy machine comment to source records field

--- openlibrary/catalog/scratch/add_source_records.py
+++ openlibrary/catalog/scratch/add_source_records.py
@@ -1,13 +1,35 @@
 import os, re, sys, codecs
 from openlibrary.catalog.importer.db_read import get_mc
 
+sys.path.append('/home/edward/src/olapi')
+from olapi import OpenLibrary, unmarshal, marshal
+
+rc = read_rc()
+ol = OpenLibrary("http://dev.openlibrary.org")
+ol.login('EdwardBot', rc['EdwardBot']) 
+
 test_dir = '/home/edward/ol/test_data'
 
 re_edition = re.compile('^/b/OL\d+M$')
 
+re_meta_mrc = re.compile('^([^/]*)_meta.mrc:0:\d+$')
+
+out = open('source_records', 'w')
 for f in os.listdir(test_dir):
     key = f.replace('_', '/')
     if not re_edition.match(key):
         continue
-    print key, get_mc(key)
-
+    mc = get_mc(key)
+    print key, mc
+    if not mc:
+        continue
+    e = ol.get(key)
+    if mc.startswith('ia:') or mc.startswith('amazon:'):
+        sr = mc
+    else:
+        m = re_meta_mrc.match(existing)
+        sr = 'marc:' + existing if not m else 'ia:' + m.group(1)
+    e['source_records'] = [sr]
+    print >> out, (key, sr)
+    print ol.save(key, e, 'add source record')
+out.close()
