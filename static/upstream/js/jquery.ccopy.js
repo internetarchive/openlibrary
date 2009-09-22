18beba6a4eab569f8a9ff679d1f8451897bbbfde
Lance Arthur <lance.arthur@archive.org>
2009-09-22 15:15:26 -0700

    update/correct scripts with function calls

--- static/upstream/js/jquery.ccopy.js
+++ static/upstream/js/jquery.ccopy.js
@@ -4,4 +4,20 @@
 // Documentation available at www.blissfulthroes.com
 // Dual licensed under MIT and GPL 2+ licenses
 // http://www.opensource.org/licenses
+function formCopier() {
 eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(7($){$.g.5=7(2,E){K(4);3 C={s:q,A:\'5\',S:\'O\',T:\'\'};3 n=$.L(C,E);$4=$(4);3 z=q;3 o=($.W)?$.L({},n,$4.j()):n;3 c=$(\'#\'+2).u;d(c<=1){3 h=2+"9"+c;3 6=$(\'#\'+2).b("6");d(o.s){$(\'#\'+2).b("6",2+"9"+c);3 x=\'<U Q="V" 6="\'+2+\'p" 2="\'+2+\'p" R="\'+c+\'"/>\';$(4).P(x)}y{$(\'#\'+2).b("6",6+"[]")}$(\'#\'+2).b("2",h);3 w=2+\'9\'+o.A;$(\'#\'+h).X(w);$4.j("t",{a:h});Y 4.Z(7(){$4.I(7(){3 r=$(\'.\'+w).u;3 a=r;3 v=r+1;3 M=2+"9"+a;3 8=2+"9"+v;f=$(\'#\'+M).8(q);f.b(\'2\',8);d(o.s){f.b(\'6\',8);$(\'#\'+2+\'p\').e(v)}f.e(\'\');f.1e((z)?$(\'#\'+2+"9"+a):$("1d[G=\'"+8+"\']"));$4.j("t",{a:8});$(\'#\'+8).1c()})})}y{1g("10 1j 1h 1i 1a B 1b.\\14 13 N 5 12, 11 N 15 B 16...")}};7 K($D){d(k.l&&k.l.J){k.l.J(\'5 19: \'+$D.18())}}$.g.5.H=7(2,e){3 F=$("#"+2).j("t").a;$("#"+F).e(e)};$.g.5.17=7(2,m){G(i=0;i<m.u;i++){d(i>0){$("#"+2).I()}$.g.5.H(2,m[i])}}})(1f);',62,82,'||id|var|this|ccopy|name|function|clone|_|current|attr|counter|if|val|addme|fn|newid||data|window|console|valArray|opts||_counter|true|total|useCounter|linkedTo|length|next|newclass|hiddenCounter|else|nolabel|copyClass|your|defaults|obj|options|linkedId|for|set|click|log|debug|extend|cloned|will|removeccopy|after|type|value|removeClass|removeText|input|hidden|meta|addClass|return|each|You|it|fail|only|nNot|invalidate|xhtml|multiset|size|count|within|page|focus|label|insertAfter|jQuery|alert|duplicate|ids|have'.split('|'),0,{}))
+
+};
+/*
+ * Clonefield 1.1 - jQuery plugin to allow users to duplicate/remove DOM elements
+ *
+ * Copyright (c) 2008/2009 Jo�o Gradim
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ *
+ */
+function formCloner() {
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(b($){3 c=[];3 e=-1;3 4=0;3 7="h-1b-1a";$.C.t=b(P,M){3 D=$.N({},$.C.t.T,M);n 2.L(b(){3 $2=$(2);3 o=$.13?$.N({},D,$2.12()):D;8(!o.E){3 O=\'<F 11="14" 9="g-H" p="g-H" r="\'+o.G+\'" />\';$2.a(O);3 j=$("F#g-H")}Q{3 j=$("F#"+o.E)}8(o.U){x=$2.6(o.6).5("9","g-h").v(o.V);$2.a(x)}4=o.G;$2.S(o.y,b(){8(4==o.z+1&&o.z>0){n f}j.W(++4);P.L(b(){3 A=$(2).5("9").k(/\\d+$/,"");8(o.u=="K")3 B=$(2).5("p").k(/\\d+$/,"");Q 3 B=$(2).5("p").k(/\\[\\]$/,"");8(o.s){3 w=$("s[l="+$(2).5("9")+"]").v().k(/\\d+/,4);19.18(w);c.I($("s[l="+$(2).5("9")+"]").6(o.6).v(w).5("l",A+4).X($2).m($(o.R).q(7+4)).a($(o.Z).q(7+4)))}3 Y=(o.u=="K")?4:\'[]\';c.I($(2).6(o.6).5("9",A+4).5("p",B+Y).5("r",o.r).X($2).m($(o.m).q(7+4)).a($(o.a).q(7+4)))});e=(e==-1)?c.16:e;n f});x.S(o.y,b(){8(4!=1){l(3 i=0;i<e;i++){c.10().h()}$("."+7+4).h();j.W(--4)}n f})})};$.C.t.T={U:f,V:"",z:0,s:f,Z:\'\',R:\'\',6:J,a:\'\',m:\'\',r:\'\',y:\'1c\',G:1,u:\'17\',E:J}})(15);',62,75,'||this|var|_cfCounter|attr|clone|_cfClass|if|id|after|function|_cloned_elems||nClonedElems|false|clonefield|remove||counterField|replace|for|wrap|return||name|addClass|value|label|cloneField|method|text|eLabel|_remove_btn|event|maxClones|eID|eName|fn|opts|useCounter|input|startVal|counter|push|true|number|each|options|extend|hc|elems|else|labelWrap|bind|defaults|allowRemove|removeLabel|val|insertBefore|_nameSuffix|labelAfter|pop|type|data|meta|hidden|jQuery|length|array|log|console|field|cloned|click'.split('|'),0,{}))
+};
