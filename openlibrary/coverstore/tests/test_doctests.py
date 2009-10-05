f324684f4b0f65dc83f1edbb9a050acd0e8acfbe
Anand Chitipothu <anandology@gmail.com>
2009-10-05 12:14:44 +0530

    report doctest errors correctly

--- openlibrary/coverstore/tests/test_doctests.py
+++ openlibrary/coverstore/tests/test_doctests.py
@@ -1,13 +1,17 @@
-import doctest
+from infogami.infobase.tests.test_doctests import find_doctests, run_doctest
 
-def run_doctest(modname):
-    mod = __import__(modname, None, None, ['x'])
-    return doctest.testmod(mod)
-    
 def test_doctests():
-    yield run_doctest, 'openlibrary.coverstore.archive'
-    yield run_doctest, 'openlibrary.coverstore.code'
-    yield run_doctest, 'openlibrary.coverstore.db'
-    yield run_doctest, 'openlibrary.coverstore.server'        
-    yield run_doctest, 'openlibrary.coverstore.utils'
-    yield run_doctest, 'openlibrary.coverstore.warc'
+    modules = [
+        'openlibrary.coverstore.archive',
+        'openlibrary.coverstore.code',
+        'openlibrary.coverstore.db',
+        'openlibrary.coverstore.server',
+        'openlibrary.coverstore.utils',
+        'openlibrary.coverstore.warc',
+    ]
+    for t in find_doctests(modules):
+        yield _run_doctest, t
+
+def _run_doctest(t):
+    # dummy function to make py.test think that test belongs in this module instead of run_doctest's module
+    run_doctest(t)
