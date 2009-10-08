a495c285a0dbabf3e2368dfcec148025fab1a207
Anand Chitipothu <anandology@gmail.com>
2009-10-08 19:57:37 +0530

    fix error in setting up wmd markdown editor

--- static/upstream/js/vendor.js
+++ static/upstream/js/vendor.js
@@ -600,22 +600,25 @@ function validateForms(){
 };
  
 
+wmd_options = {"autostart": false};
+
 // WMD setup for OpenLibrary. This script should be loaded before loading wmd.js
 
 function setup_wmd() {
     Attacklab.panels = new Attacklab.PanelCollection();
     
-    //$("#wmd-input").prepend("<div id='wmd-button-bar'></div>");
+    $("<div id='wmd-button-bar'></div>").insertBefore("#wmd-input");
 
     var previewMgr = new Attacklab.previewManager();
     var edit = new Attacklab.editor(previewMgr.refresh);
     previewMgr.refresh(true);
-	
+    
     // change title and url of help link.
     $("#wmd-help-button a")
         .attr("href", "/help/markdown")
         .attr('title', "Markdown Help");
 }
+
 var Attacklab = Attacklab || {};
 
 Attacklab.wmdBase = function(){
--- static/upstream/js/wmd-setup.js
+++ static/upstream/js/wmd-setup.js
@@ -1,10 +1,12 @@
 
+wmd_options = {"autostart": false};
+
 // WMD setup for OpenLibrary. This script should be loaded before loading wmd.js
 
 function setup_wmd() {
     Attacklab.panels = new Attacklab.PanelCollection();
     
-    //$("#wmd-input").prepend("<div id='wmd-button-bar'></div>");
+    $("<div id='wmd-button-bar'></div>").insertBefore("#wmd-input");
 
     var previewMgr = new Attacklab.previewManager();
     var edit = new Attacklab.editor(previewMgr.refresh);
