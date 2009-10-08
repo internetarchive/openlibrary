171b08b00834f403fa18ff7875bfcb8f73386e28
Lance Arthur <lance.arthur@archive.org>
2009-10-08 11:19:06 -0700

    Various and sundry changes, incl moving non-3rd party scripts out of vendor.js, removing ccopy.js from Makefile, altering others accordingly

--- static/upstream/js/jquery.form.js
+++ static/upstream/js/jquery.form.js
@@ -1,3 +1,5 @@
+// FORMS (AJAX) 
+function Forms() {
 ;(function($){$.fn.ajaxSubmit=function(options){if(!this.length){log('ajaxSubmit: skipping submit process - no element selected');return this;}
 if(typeof options=='function')
 options={success:options};var url=$.trim(this.attr('action'));if(url){url=(url.match(/^([^#]+)/)||[])[1];}
@@ -68,3 +70,4 @@ this.reset();});};$.fn.enable=function(b){if(b==undefined)b=true;return this.eac
 this.checked=select;else if(this.tagName.toLowerCase()=='option'){var $sel=$(this).parent('select');if(select&&$sel[0]&&$sel[0].type=='select-one'){$sel.find('option').selected(false);}
 this.selected=select;}});};function log(){if($.fn.ajaxSubmit.debug&&window.console&&window.console.log)
 window.console.log('[jquery.form] '+Array.prototype.join.call(arguments,''));};})(jQuery);
+};
