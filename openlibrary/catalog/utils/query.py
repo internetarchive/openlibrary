a1914ad3afdf5369501b2d97deb34d2e752a118f
Edward Betts <edward@archive.org>
2009-10-08 10:48:46 +0100

    new query function

--- openlibrary/catalog/utils/query.py
+++ openlibrary/catalog/utils/query.py
@@ -86,6 +86,16 @@ def withKey(key):
         print 'retry'
         sleep(10)
 
+def get_marc_src(e):
+    mc = get_mc(e['key'])
+    if mc:
+        yield mc
+    if not e.get('source_records', []):
+        return
+    for src in e['source_records']:
+        if src.startswith('marc:') and src != 'marc:' + mc:
+            yield src[5:]
+
 def get_mc(key): # get machine comment
     url = base_url() + key + '.json?m=history'
     ret = urllib.urlopen(url).read()
