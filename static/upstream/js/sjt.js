b089b44d8c54a87887e59e962ed61987f10a885c
Anand Chitipothu <anandology@gmail.com>
2009-10-14 19:23:51 +0530

    new javascript templating code

--- static/upstream/js/sjt.js
+++ static/upstream/js/sjt.js
@@ -1,35 +1,43 @@
-// Simple JavaScript Templating
-// John Resig - http://ejohn.org/ - MIT Licensed
-(function(){
-  var cache = {};
-  
-  this.tmpl = function tmpl(str, data){
-    // Figure out if we're getting a template, or if we need to
-    // load the template - and be sure to cache the result.
-    var fn = !/\W/.test(str) ?
-      cache[str] = cache[str] ||
-        tmpl(document.getElementById(str).innerHTML) :
-      
-      // Generate a reusable function that will serve as a template
-      // generator (and which will be cached).
-      new Function("obj",
-        "var p=[],print=function(){p.push.apply(p,arguments);};" +
+// Simple Javascript Templating 
+// Anand Chitipothu - http://anandology.com/ - MIT Licensed
+//
+// Inspired by http://ejohn.org/blog/javascript-micro-templating/
+function Template(tmpl_text) {
+    var s = [];
+    var js = ["var p=[];", "with(env) {"]
+
+    function addCode(text) {
+        js.push(text);
+    }
+    function addExpr(text) {
+        js.push("p.push(" + text + ");");
+    }
+    function addText(text) {
+        js.push("p.push(__s[" + s.length + "]);"); 
+        s.push(text);
+    }
+
+    var tokens = tmpl_text.split("<%");
+
+    addText(tokens[0]);
+    for (var i=1; i < tokens.length; i++) {
+        var t = tokens[i].split('%>')
         
-        // Introduce the data as local variables using with(){}
-        "with(obj){p.push('" +
-        
-        // Convert the template into pure JavaScript
-        str
-          .replace(/[\r\t\n]/g, " ")
-          .split("<%").join("\t")
-          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-          .replace(/\t=(.*?)%>/g, "',$1,'")
-          .split("\t").join("');")
-          .split("%>").join("p.push('")
-          .split("\r").join("\\'")
-      + "');}return p.join('');");
-    
-    // Provide some basic currying to the user
-    return data ? fn( data ) : fn;
-  };
-})();
+        if (t[0][0] == "=")
+            addExpr(t[0].substr(1));
+        else
+            addCode(t[0]);
+
+        addText(t[1]);
+    }
+    js.push("}", "return p.join('');");
+
+    var f = Function(["__s", "env"], js.join("\n"));
+    var g = function(env) {
+        return f(s, env);
+    }
+    g.toString = function() { return tmpl_text; }
+    g.toCode = function() { return f.toString(); }
+    return g;
+}
+
