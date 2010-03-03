212a675d312fb88eb33ac5f8471140b5cb6b4f0a
Edward Betts <edwardbetts@gmail.com>
2010-03-03 01:46:25 +0000

    more tests for subjects

--- openlibrary/tests/test_work_subjects.py
+++ openlibrary/tests/test_work_subjects.py
@@ -26,7 +26,23 @@ class TestWorkSubjects(unittest.TestCase):
             ('651', ' 0\x1faTexas\x1fvFiction.\x1e')
         ]]
 
-        expect = {}
+        expect = {
+            'place': {u'Texas': 1},
+            'subject': {u'Dan Rhodes (Fictitious character)': 1, u'Sheriffs': 1, u'Sheriffs in fiction': 1, u'Texas in fiction': 1, u'Fiction': 3}
+        }
+
+        self.assertEqual(find_subjects(marc), expect)
+
+        marc = [[
+            ('650', ' 0\x1faSpies\x1fzFrance\x1fzParis\x1fvFiction.\x1e'),
+            ('651', ' 0\x1faFrance\x1fxHistory\x1fyDirectory, 1795-1799\x1fvFiction.\x1e')
+        ]]
+
+        expect = {
+            'subject': {u'History': 1, u'France in fiction': 1, u'Spies': 1, u'Spies in fiction': 1, u'Fiction': 2},
+            'place': {u'Paris': 1, u'France': 2},
+            'time': {u'Directory, 1795-1799': 1}
+        }
 
         self.assertEqual(find_subjects(marc), expect)
         
