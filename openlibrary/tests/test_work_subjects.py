7138f5283109cd607e7151011517d1c6385c12a7
Edward Betts <edwardbetts@gmail.com>
2010-03-03 01:27:19 +0000

    test handling of fiction subjects

--- openlibrary/tests/test_work_subjects.py
+++ openlibrary/tests/test_work_subjects.py
@@ -1,5 +1,5 @@
 import unittest
-from openlibrary.solr.work_subject import four_types
+from openlibrary.solr.work_subject import four_types, find_subjects
 
 class TestWorkSubjects(unittest.TestCase):
     def testFourTypes(self):
@@ -19,4 +19,15 @@ class TestWorkSubjects(unittest.TestCase):
             'subject': { 'Party': 1 },
         }
         self.assertEqual(four_types(input), expect)
+
+        marc = [[
+            ('650', ' 0\x1faRhodes, Dan (Fictitious character)\x1fvFiction.\x1e'),
+            ('650', ' 0\x1faSheriffs\x1fvFiction.\x1e'),
+            ('651', ' 0\x1faTexas\x1fvFiction.\x1e')
+        ]]
+
+        expect = {}
+
+        self.assertEqual(find_subjects(marc), expect)
+        
 
