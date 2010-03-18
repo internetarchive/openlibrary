cc13c1395ba2eebd021243bd53069b00fdcb48d8
Anand Chitipothu <anandology@gmail.com>
2010-03-18 09:08:44 +0530

    special care to handle missing author_key/author_name in the solr record

--- openlibrary/plugins/worksearch/search.py
+++ openlibrary/plugins/worksearch/search.py
@@ -67,6 +67,10 @@ def work_wrapper(w):
     else:
         d.cover_id = None
 
+    # special care to handle missing author_key/author_name in the solr record
+    w.set_default('author_key', [])
+    w.set_default('author_name', [])
+    
     d.authors = [web.storage(key='/authors/' + k, name=n)
                  for k, n in zip(w['author_key'], w['author_name'])]
 
