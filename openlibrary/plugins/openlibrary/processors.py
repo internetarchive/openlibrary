1fdeba8dce05ea56a105ef840543dfca06861ac5
Anand Chitipothu <anandology@gmail.com>
2010-05-17 12:44:50 +0530

    use '301 Moved Permanently' redirect instead of '303 See Other'.

--- openlibrary/plugins/openlibrary/processors.py
+++ openlibrary/plugins/openlibrary/processors.py
@@ -34,7 +34,7 @@ class ReadableUrlProcessor:
         #@@ take care of that case here till that is fixed.
         # @@ Also, the redirection must be done only for GET requests.
         if readable_path != web.ctx.path and readable_path != urllib.quote(web.utf8(web.ctx.path)) and web.ctx.method == "GET":
-            raise web.seeother(web.safeunicode(readable_path) + web.safeunicode(web.ctx.query))
+            raise web.redirect(web.safeunicode(readable_path) + web.safeunicode(web.ctx.query))
 
         web.ctx.readable_path = readable_path
         web.ctx.path = real_path
