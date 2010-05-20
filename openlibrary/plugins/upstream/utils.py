19df78d2dab1359677c6da3cf2285bbf0f001173
Anand Chitipothu <anandology@gmail.com>
2010-05-20 14:05:27 +0530

    suuport content-encoding: gzip

--- openlibrary/plugins/upstream/utils.py
+++ openlibrary/plugins/upstream/utils.py
@@ -9,6 +9,8 @@ import random
 import urllib
 import xml.etree.ElementTree as etree
 import datetime
+import gzip
+import StringIO
 
 from infogami import config
 from infogami.utils import view, delegate
@@ -590,6 +592,48 @@ class Request:
     path = property(lambda self: web.ctx.path)
     home = property(lambda self: web.ctx.home)
     domain = property(lambda self: web.ctx.host)
+    
+class GZipMiddleware:
+    """WSGI middleware to gzip the response."""
+    def __init__(self, app):
+        self.app = app
+        
+    def __call__(self, environ, start_response):
+        accept_encoding = environ.get("HTTP_ACCEPT_ENCODING", "")
+        if not 'gzip' in accept_encoding:
+            return self.app(environ, start_response)
+        
+        response = web.storage(compress=False)
+        
+        def get_response_header(name, default=None):
+            for hdr, value in response.headers:
+                if hdr.lower() == name.lower():
+                    return value
+            return default
+            
+        def compress(text, level=9):
+            f = StringIO.StringIO()
+            gz = gzip.GzipFile(None, 'wb', level, fileobj=f)
+            gz.write(text)
+            gz.close()
+            return f.getvalue()
+        
+        def new_start_response(status, headers):
+            response.status = status
+            response.headers = headers
+            
+            if status.startswith("200") and get_response_header("Content-Type", "").startswith("text/"):
+                headers.append(("Content-Encoding", "gzip"))
+                headers.append(("Vary", "Accept-Encoding"))
+                response.compress = True
+            return start_response(status, headers)
+        
+        data = self.app(environ, new_start_response)
+        print "response", response
+        if response.compress:
+            return [compress("".join(data), 9)]
+        else:
+            return data
 
 def setup():
     """Do required initialization"""
@@ -604,6 +648,9 @@ def setup():
         'HTML': HTML,
         'request': Request()
     })
+
+    if config.get('use_gzip') == True:
+        config.middleware.append(GZipMiddleware)
     
 if __name__ == '__main__':
     import doctest
