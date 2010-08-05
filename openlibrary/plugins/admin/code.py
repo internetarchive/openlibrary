dc9674832c963c747bf44319422e74181ea92db0
Edward Betts <edwardbetts@gmail.com>
2010-08-05 20:09:27 +0000

    admin block rewrite bug fix

--- openlibrary/plugins/admin/code.py
+++ openlibrary/plugins/admin/code.py
@@ -177,7 +177,7 @@ class block:
         i = web.input()
         
         page = web.ctx.get("/admin/block") or web.ctx.site.new("/admin/block", {"key": "/admin/block", "type": "/type/object"})
-        ips = [d.strip() for d in i.ips.split('\n') if d.get('ip')]
+        ips = [d.strip() for d in i.ips.split('\r\n')]
         page.ips = ips
         page._save("update blocked IPs")
         add_flash_message("info", "Saved!")
