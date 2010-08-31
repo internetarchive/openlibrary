528ba95e8b8fae324bc239c053462744899ff67d
arielbackenroth <arielb@alumni.rice.edu>
2010-08-31 14:58:10 -0700

    more live demo of mobile site

--- openlibrary/plugins/mobile/code.py
+++ openlibrary/plugins/mobile/code.py
@@ -1,13 +1,44 @@
+import web
+from infogami.utils.view import render_template
 from infogami.utils import delegate
+
+
+def layout(page):
+    return delegate.RawText(render_template("mobile/site", page))
 
 class index(delegate.page):
     path = "/"
 
     def GET(self):
-        return "hello, world"
+        return layout(render_template("mobile/index"))
 
 class search(delegate.page):
+
     def GET(self):
-        return "search"
+        i = web.input(q="")
+        results = web.storage({"num_found": 0})
 
+        return layout(render_template("mobile/search", q=i.q, results=results))
 
+class book(delegate.page):
+    
+    path = "(/books/.*)"
+
+    def GET(self, key):
+        book = web.ctx.site.get(key)
+        return layout(render_template("mobile/book", book=book))
+
+class author(delegate.page):
+
+    path = "(/authors/.*)"
+
+    def GET(self, key):
+        author = web.ctx.site.get(key)
+        return layout(render_template("mobile/author", author=author))
+    
+#class notfound(delegate.page):
+#    
+#    path = "/.*"
+#
+#    def GET(self):
+#        raise web.NotFound()
