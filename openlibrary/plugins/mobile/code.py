f32ff95d0e645e55c45d7bd0cc2ad50a49fbffb0
arielbackenroth <arielb@alumni.rice.edu>
2010-09-09 17:28:27 -0700

    checkpoint of mobile stuff

--- openlibrary/plugins/mobile/code.py
+++ openlibrary/plugins/mobile/code.py
@@ -12,13 +12,38 @@ class index(delegate.page):
     def GET(self):
         return layout(render_template("mobile/index"))
 
+def _editions_for_works(works):
+    ocaids = set()
+
+    for work in works:
+        if work.get('ia'):
+            # just use the first one for now
+            ocaids.add(work['ia'][0])
+
+    edition_keys = web.ctx.site.things({"type": "/type/edition", "ocaid": list(ocaids)})
+    editions = web.ctx.site.get_many(edition_keys)
+
+    work_key_to_edition = {}
+    for edition in editions:
+        for work in edition.works:
+            work_key_to_edition[work.key] = edition
+
+    for work in works:
+        if work.key in work_key_to_edition:
+            edition = work_key_to_edition[work.key]
+            yield edition, work
+    
+
 class search(delegate.page):
 
     def _do_search(self, q):
-        ugly = worksearch.do_search({"q": q}, None)
-        results = web.storage({'num_found': ugly['num_found'], 'docs': []})
-        for doc in ugly['docs']:
-            results['docs'].append(worksearch.get_doc(doc))
+        ugly = worksearch.do_search({"q": q, "has_fulltext": "true"}, None)
+        results = web.storage({'num_found': ugly['num_found'], 'books': []})
+        works = [worksearch.get_doc(doc) for doc in ugly['docs']]
+        for work in works:
+            work.key = '/works/%s' % work.key
+        for edition, work in _editions_for_works(works):
+            results['books'].append((edition, work))
         return results
 
     def GET(self):
@@ -28,7 +53,7 @@ class search(delegate.page):
 
 class book(delegate.page):
     
-    path = "(/books/.*)"
+    path = r"(/books/.*)"
 
     def GET(self, key):
         book = web.ctx.site.get(key)
@@ -40,7 +65,13 @@ class author(delegate.page):
 
     def GET(self, key):
         author = web.ctx.site.get(key)
-        return layout(render_template("mobile/author", author=author))
+        books = []
+        works = author.get_books()
+        works = [work for work in works['works'] if work.get('has_fulltext')]
+        for edition, work in _editions_for_works(works):
+            books.append((edition, work))
+        
+        return layout(render_template("mobile/author", author=author, books=books))
     
 #class notfound(delegate.page):
 #    
