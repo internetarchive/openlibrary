856d4051a9c0b2308eb9ccca5048b4f69fb4ba8c
Anand Chitipothu <anandology@gmail.com>
2010-10-01 17:50:23 +0530

    fixed memcache usage.

--- openlibrary/core/lists.py
+++ openlibrary/core/lists.py
@@ -8,8 +8,7 @@ import web
 import simplejson
 
 from infogami.infobase import client
-from openlibrary.utils import olmemcache
-
+from openlibrary.plugins.openlibrary import connection
 
 SCHEMA = """
 create table seed_editions (
@@ -19,6 +18,8 @@ create table seed_editions (
     edition text,
     ebook boolean
 );
+create index seed_editions_seed_idx on seed_editions(seed);
+create index seed_editions_edition_idx on seed_editions(edition);
 
 create table seed_updates (
     id serial primary key,
@@ -64,9 +65,10 @@ class ListsEngine:
             raise Exception("Either infobase_server or db must be specified in the settings.")
         
         if "memcache_servers" in settings:
-            self.memcache = olmemcache.Client(settings["memcache_servers"])
-        else:
-            self.memcache = None
+            self.infobase_conn = connection.MemcacheMiddleware(self.infobase_conn, settings['memcache_servers'])
+            
+            # Looks like memcache still has the old data. MigrationMiddleware corrects it.
+            self.infobase_conn = connection.MigrationMiddleware(self.infobase_conn)
             
         if "lists_db" in settings:
             db_params = settings['lists_db'].copy()
@@ -80,7 +82,6 @@ class ListsEngine:
         from memcache/db and indexed them in seed_editions table.
         """
         editions = self._get_docs(edition_keys).values()
-        
         work_keys = [w['key'] 
                     for e in editions
                     for w in e.get('works', [])]
@@ -101,7 +102,9 @@ class ListsEngine:
         
         data = [dict(seed=seed, work=work, edition=edition, ebook=ebook)
             for e in editions
-            for seed, work, edition, ebook in p.process_edition(e, works)]
+            for seed, work, edition, ebook in p.process_edition(e, works)
+            if e.get("type", {}).get("key") == "/type/edition"
+            ]
             
         edition_keys = [e['key'] for e in editions]
         
@@ -120,20 +123,6 @@ class ListsEngine:
     def _get_docs(self, keys):
         """Returns docs for the specified keys as a dictionary.
         """
-        docs = self._get_docs_from_memcache(keys)
-
-        keys2 = [k for k in keys if k not in docs]
-        docs.update(self._get_docs_from_infobase(keys2))
-            
-        return docs
-    
-    def _get_docs_from_memcache(self, keys):
-        if self.memcache:
-            return self.memcache.get_multi(keys)
-        else:
-            return {}
-    
-    def _get_docs_from_infobase(self, keys):
         docs = {}
         
         for keys2 in web.group(keys, 500):
