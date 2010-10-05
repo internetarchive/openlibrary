e354faafb09371f08e61b541dcbd63af008a1276
Anand Chitipothu <anandology@gmail.com>
2010-10-05 15:54:47 +0530

    lendable_books.py: using amazon API to check availability of books on amazon.

--- scripts/2010/10/lendable_books.py
+++ scripts/2010/10/lendable_books.py
@@ -1,11 +1,22 @@
-"""Script to test a list of archive ids are lendable or not.
+"""Script to find out-of-print books from a list of archive identifiers.
+
+Availability of a book in print is found by checking OL and amazon.com for
+latest edition and checking the published date.
+
+This scripts uses [pyaws][1] library for querying amazon.com.
+
+[1]: http://github.com/IanLewis/pyaws
 """
 import simplejson
 import yaml
 import shelve
 import urllib2
+import re
 
-import xml.dom.minidom
+from xml.dom import minidom
+from ConfigParser import ConfigParser
+
+from pyaws import ecs
 
 def load_settings(settings_file):
     return yaml.safe_load(open(settings_file).read())
@@ -43,17 +54,23 @@ def load_ia(shelve_file, ia_ids_file):
         d = sh.get(ia_id, {})
         if not d.get("ia"):
             print i, "loading ia data for", ia_id
-            d['ia'] = _load_ia_data(ia)
-            sh[ia_id] = d        
-
+    
+            try:
+                d['ia'] = _load_ia_data(ia_id)
+                sh[ia_id] = d        
+            except Exception:
+                print "ERROR: failed to load ia data for", ia_id
+                import traceback
+                traceback.print_exc()
+            
 def _load_ia_data(ia):
     url = "http://www.archive.org/download/%(ia)s/%(ia)s_meta.xml" % locals()
     xml = urllib2.urlopen(url).read()
     
-    dom = xml.dom.minidom.parseString(xml)
+    dom = minidom.parseString(xml)
     
     def get_elements(name):
-        return [e.childNodes[0].data for e in dom.getElementsByTagName(name)]
+        return [e.childNodes[0].data for e in dom.getElementsByTagName(name) if e.childNodes]
 
     def get_element(name):
         try:
@@ -70,26 +87,67 @@ def _load_ia_data(ia):
         "date": get_element("date"),
         "mediatype": get_element("mediatype"),
     }            
+    
+def _setup_amazon_keys():
+    config = ConfigParser()
+    files = config.read([".amazonrc", "~/.amazonrc"])
+    if not files:
+        print >> sys.stderr, "ERROR: Unable to find .amazonrc with access keys."
+    
+    access_key = config.get("default", "access_key")
+    secret = config.get("default", "secret")
 
+    ecs.setLicenseKey(access_key)
+    ecs.setSecretAccessKey(secret)
+    
 def load_amazon(shelve_file):
+    _setup_amazon_keys()
+
     sh = shelve.open(shelve_file)
     for i, k in enumerate(sh):
         d = sh[k]
-        if 'amazon' not in d and d.get("ol", {}).get('isbn'):
-            print i, "loading amazon data for ", k, d['ol']['isbn']
-            isbns = d['ol']['isbn']
-            data = dict((isbn, _load_amazon_data(isbn)) for isbn in isbns)
-            data = dict((k, v) for k, v in d.items() if v is not None)
-            d['amazon'] = data
+        if 'amazon' not in d and 'ia' in d:
+            print i, "querying amazon for", k
+            doc = d['ia']
 
-def _load_amazon_data(isbn):
-    url = "http://www.amazon.com/gp/product/%s" % isbn
+            title = doc.get('title')
+            authors = doc.get('authors')
+            author = authors and authors[0] or ""
+            
+            d['amazon'] = _query_amazon(title, author)
+            sh[k] = d
+
+def _query_amazon(title, author):
+    # strips dates from author names
+    author = re.sub('[0-9-]+', ' ', author).strip()
     
     try:
-        return urllib2.urlopen(url).geturl()
-    except urllib2.HTTPError:
-        return None
+        res = ecs.ItemSearch(None, SearchIndex="Books", Title=title, Author=author, ResponseGroup="ItemAttributes")
+    except KeyError, e:
+        # Ignore nomathes error
+        if 'ECommerceService.NoExactMatches' in str(e):
+            return []
+        else:
+            raise
             
+    def process(x):
+        x = x.__dict__
+        
+        # remove unwanted data
+        x.pop("ItemLinks", None)
+        x.pop("DetailPageURL", None)
+        
+        return x
+        
+    return [process(x) for x in take(50, res)]
+    
+def take(n, seq):
+    """Takes first n elements from the seq."""
+    seq = iter(seq)
+
+    for i in range(n):
+        yield seq.next()
+
 def print_all(shelve_file):
     sh = shelve.open(shelve_file)
     for k in sh:
@@ -113,7 +171,6 @@ def help():
     print __doc__
 
 def main(cmd, *args):
-    print cmd, args
     if cmd == "load_ol":
         load_ol(*args)
     elif cmd == "load_ia":
