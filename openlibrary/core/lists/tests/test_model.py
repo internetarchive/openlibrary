ebb0b86f96814ef918bb51e9ea72ac2b92055f5c
Anand Chitipothu <anandology@gmail.com>
2010-11-10 15:45:48 +0530

    Fixed failing tests

--- openlibrary/core/lists/tests/test_model.py
+++ openlibrary/core/lists/tests/test_model.py
@@ -6,8 +6,8 @@ class MockCouchDB:
     def __init__(self, docs):
         self.docs = docs
     
-    def view(self, name, keys=None):
-        return [web.storage(id=doc['_id'], key=doc['_id'], value=doc) for doc in self.docs]
+    def view(self, name, keys=None, include_docs=False):
+        return [web.storage(id=doc['_id'], key=doc['_id'], value=doc, doc=doc) for doc in self.docs]
 
 class MockList(model.ListMixin):
     def __init__(self, db, seeds):
@@ -47,9 +47,9 @@ class TestListMixin:
         assert list.work_count == 6
         assert list.ebook_count == 4
         assert list.get_subjects() == [
-            {"key": "subject:love", "url": "/subjects/love", "name": "Love", "count": 4},
-            {"key": "place:san_francisco", "url": "/subjects/place:san_francisco", "name": "San Francisco", "count": 3}
+            {"key": "subject:love", "url": "/subjects/love", "name": "Love", "title": "Love", "count": 4},
+            {"key": "place:san_francisco", "url": "/subjects/place:san_francisco", "name": "San Francisco", "title": "San Francisco", "count": 3}
         ]
         assert list.get_subjects(limit=1) == [
-            {"key": "subject:love", "url": "/subjects/love", "name": "Love", "count": 4}
+            {"key": "subject:love", "url": "/subjects/love", "name": "Love", "title": "Love", "count": 4}
         ]
