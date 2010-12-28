1945b70e340987b31290b17ffcfafa8e5e14f66a
Anand Chitipothu <anandology@gmail.com>
2010-12-28 17:58:46 +0530

    Added more python dependencies to setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ def executable(path):
     st = os.stat(path)[ST_MODE]
     return (st & S_IEXEC) and not S_ISDIR(st)
 
-setup(name='openlibrary',
-      version='0.1',
-      description='OpenlibraryBot',
-      packages=find_packages(exclude=["ez_setup"]),
-      scripts=filter(executable, glob.glob('scripts/*')),
-      install_requires='web.py==0.33 Babel pyyaml psycopg2 simplejson python-memcached lxml PIL pymarc genshi couchdb'.split()
+setup(
+    name='openlibrary',
+    version='2.0',
+    description='OpenlibraryBot',
+    packages=find_packages(exclude=["ez_setup"]),
+    scripts=filter(executable, glob.glob('scripts/*')),
+    install_requires='web.py==0.33 Babel pyyaml psycopg2 simplejson python-memcached lxml PIL pymarc genshi couchdb argparse supervisor'.split(),
 )
-
