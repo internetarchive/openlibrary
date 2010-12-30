a20fb2a6ef7558da9390025d872730efc3aeddac
Anand Chitipothu <anandology@gmail.com>
2010-12-30 12:35:33 +0530

    Generate API docs as part of build_sphinx command.

--- setup.py
+++ setup.py
@@ -6,11 +6,46 @@ def executable(path):
     st = os.stat(path)[ST_MODE]
     return (st & S_IEXEC) and not S_ISDIR(st)
 
+try:
+    from sphinx.setup_command import BuildDoc
+except ImportError:
+    BuildDoc = None
+
+dependencies = """
+Babel
+PIL
+argparse
+couchdb
+genshi
+gunicorn
+lxml
+psycopg2
+pymarc
+python-memcached
+pyyaml
+simplejson
+sphinx
+supervisor
+web.py==0.33
+"""
+
+cmdclass = {}
+
+if BuildDoc:
+    class OLBuildDoc(BuildDoc):
+        def run(self):
+            print "generating API docs..."
+            os.system("python scripts/generate-api-docs.py")
+            BuildDoc.run(self)
+    cmdclass['build_sphinx'] = OLBuildDoc
+
 setup(
     name='openlibrary',
     version='2.0',
     description='OpenlibraryBot',
     packages=find_packages(exclude=["ez_setup"]),
     scripts=filter(executable, glob.glob('scripts/*')),
-    install_requires='web.py==0.33 Babel pyyaml psycopg2 simplejson python-memcached lxml PIL pymarc genshi couchdb argparse supervisor gunicorn'.split(),
+    install_requires=dependencies.split(),
+    cmdclass=cmdclass
 )
+
