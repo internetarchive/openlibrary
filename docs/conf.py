f5b5f9fc9cfd422cbf6f4c44f3130a76265299ed
Anand Chitipothu <anandology@gmail.com>
2010-12-30 12:40:19 +0530

    Enabled sphinx.ext.intersphinx plugin and linked to CouchDB docs.

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ wd = os.getcwd()
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
@@ -191,3 +191,7 @@ htmlhelp_basename = 'openlibrary'
 #latex_use_modindex = True
 
 autodoc_default_flags = ['members', 'undoc-members', 'show-inheritance']
+
+intersphinx_mapping = {
+    'CouchDB': ('http://packages.python.org/CouchDB', None),
+}
