056e46dff94228a9a0cb3b4a46d0689d211bd13c
Anand Chitipothu <anandology@gmail.com>
2011-01-19 11:36:23 +0530

    Caching seed summaries to avoid computing them again and again when computing active lists.

--- openlibrary/plugins/openlibrary/lists.py
+++ openlibrary/plugins/openlibrary/lists.py
@@ -497,10 +497,14 @@ def get_recently_modified_lists(limit, offset=0):
     
     keys = web.ctx.site.things({"type": "/type/list", "sort": "-last_modified", "limit": limit, "offset": offset})
     lists = web.ctx.site.get_many(keys)
+    
+    # Cache seed_summary, so that it can be reused later without recomputing.
+    for list in lists:
+        list.seed_summary_cached = list.seed_summary
         
     return [list.dict() for list in lists]
     
-get_recently_modified_lists = cache.memcache_memoize(get_recently_modified_lists, timeout=5*60)
+get_recently_modified_lists = cache.memcache_memoize(get_recently_modified_lists, key_prefix="get_recently_modified_lists", timeout=5*60)
     
 def _preload_lists(lists):
     """Preloads all referenced documents for each list.
@@ -538,8 +542,18 @@ def get_active_lists_in_random(limit=20, preload=True):
 
     if preload:
         _preload_lists(lists)
-            
+        
+    seed_summaries = {}
+    
+    for xlist in lists:
+        seed_summaries[xlist['key']] = xlist.pop("seed_summary_cached", None)
+    
     # convert rawdata into models.
     lists = [web.ctx.site.new(xlist['key'], xlist) for xlist in lists]
+    
+    # Initialize seed_summary to avoid recomputing it for all the lists.
+    for xlist in lists:
+        if xlist.key in seed_summaries:
+            xlist.__dict__['seed_summary'] = seed_summaries[xlist.key]
     
     return lists
