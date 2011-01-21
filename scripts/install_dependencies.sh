13c6a414f917f218299d54f3f91316d34570f2a6
Anand Chitipothu <anandology@gmail.com>
2011-01-21 16:34:21 +0530

    fixed an error and added macosx support.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,10 @@
 # Script to install dependencies for setting up dev instance
 #
 
+function log() {
+    echo -- $*
+}
+
 function install_linux_dependencies() {
     echo "installing dependencies"
     sudo apt-get -y install memcached postgresql git-core openjdk-6-jre-headless python-virtualenv python-dev libpq-dev libxslt-dev
@@ -12,14 +16,30 @@ function install_linux_dependencies() {
 }
 
 function install_macosx_dependencies() {
-    echo "this is not yet implemented"
+    if ! brew --version > /dev/null 2>&1
+    then
+        log "installing homebrew"
+        curl -fsSLk https://gist.github.com/raw/323731/install_homebrew.rb | ruby
+    else
+        log "homebrew installation found."
+    fi
+    
+    log "installing wget"
+    brew install wget
+    
+    log "installing python setuptools"
+    wget -q http://peak.telecommunity.com/dist/ez_setup.py -O /tmp/ez_setup.py
+    sudo python ez_setup.py -U setuptools
+    
+    log "installing virtualenv"
+    sudo easy_install virtualenv
 }
 
 uname=$(uname)
 
 if [ "$uname" == "Darwin" ]
 then
-    install_macosx_dependencies()
+    install_macosx_dependencies
 else
-    install_linux_dependencies()
+    install_linux_dependencies
 fi
