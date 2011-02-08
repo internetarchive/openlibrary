e6d473ed819411a53f141709dbf6c7bcba16ade8
Anand Chitipothu <anandology@gmail.com>
2011-02-08 16:10:50 +0530

    typo.

--- openlibrary/plugins/openlibrary/inlibrary.py
+++ openlibrary/plugins/openlibrary/inlibrary.py
@@ -1,12 +1,12 @@
 """Hooks for in-library lending.
 """
 import web
-from infogami.utils import filters
+from infogami.utils import features
 from openlibrary.core import inlibrary
 
 def setup():
     web.template.Template.globals.update({
-        "get_library", inlibrary.get_library
+        "get_library": inlibrary.get_library
     })
     
-    filters.register_filter("inlibrary", inlibrary.filter_inlibrary)
+    features.register_filter("inlibrary", inlibrary.filter_inlibrary)
