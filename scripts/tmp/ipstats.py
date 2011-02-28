1b383de5873ef2d47d7211c67c87974d57d4a0f2
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-02-28 17:58:48 +0530

    Added ipstats file

---
+++ scripts/tmp/ipstats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+Temporary script to store unique IPs in a single day by parsing the
+lighttpd log files directly.
+"""
+import datetime
+import subprocess
+
+
+def run_for_day(d):
+    baselog = "/var/log/lighttpd/"
+    d = datetime.datetime.now
+    basedir = "/var/log/lighttpd/%{year}d/%{month}d/%{day}d/"%dict(year = d.year, month = d.month, day = d.day)
+    counter = ["|", "awk '$2 == \"openlibrary.org\" { print $1  }'", "|", "sort -u", "|", "wc -l"]
+    if os.path.exists(basedir + "access.log.gz"):
+        cmd = ["zcat %s/access.log.gz"%basedir ] + counter
+    elif os.path.exists(basedir + "access.log"):
+        cmd = ["cat %s/access.log"%basedir] + counter
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, shell = True)
+    p.wait()
+    val = p.stdout.read()
+    return val
+    
+def main():
+    current = datetime.datetime.now()
+    for i in range(10):
+        print current
+        print run_for_day(current)
+        current = current - datetime.timedelta(days = 1)
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
+
+
+
+
