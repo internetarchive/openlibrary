d3a52c9f665d5f76e3c8d9eaab830a578e4024d7
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-03-23 11:41:49 -0700

    Load up all the services

--- openlibrary/plugins/admin/services.py
+++ openlibrary/plugins/admin/services.py
@@ -10,10 +10,19 @@ class Service(object):
     manipulate it.
     """
 
-    def __init__(self, node, name, logs):
+    def __init__(self, node, name, logs = False):
         self.node = node
         self.name = name
         self.logs = logs
 
+    def __repr__(self):
+        return "Service(name = '%s', node = '%s', logs = '%s')"%(self.name, self.node, self.logs)
+    
 
+def load_all(config):
+    """Loads all services specified in the config dictionary and returns
+    the list of Service"""
+    for node in config:
+        for service in config[node].get('services',[]):
+            yield Service(node = node, name = service)
 
--- openlibrary/plugins/admin/tests/test_services.py
+++ openlibrary/plugins/admin/tests/test_services.py
@@ -5,12 +5,13 @@ Tests for the services module used by the admin interface.
 def test_loader(serviceconfig):
     "Make sure services are loaded"
     from .. import services
-    services = services.load_all(serviceconfig)
+    services = list(services.load_all(serviceconfig))
     assert len(services) == 4
-    assert services[0].node == "ol-web0" and services[0].name == "7071-ol-gunicorn"
-    assert services[1].node == "ol-web0" and services[1].name == "7060-memcached"
-    assert services[2].node == "ol-web1" and services[2].name == "7071-ol-gunicorn"
-    assert services[3].node == "ol-web1" and services[3].name == "7060-memcached"
+    for i in services: print i
+    assert services[0].node == "ol-web1" and services[0].name == "7071-ol-gunicorn"
+    assert services[1].node == "ol-web1" and services[1].name == "7060-memcached"
+    assert services[2].node == "ol-web0" and services[2].name == "7071-ol-gunicorn"
+    assert services[3].node == "ol-web0" and services[3].name == "7060-memcached"
     
     
     
