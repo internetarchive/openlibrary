ee342a878c2181ce11a189017f1b725c0b6b3a53
Edward Betts <edwardbetts@gmail.com>
2011-03-24 17:54:31 +0000

    another approach

--- scripts/2011/03/get_titles.py
+++ scripts/2011/03/get_titles.py
@@ -1,22 +1,29 @@
-import web
+import web, psycopg2, sys
 
-db = web.database( dbn='postgres', host='ol-db', db='openlibrary')
+conn = psycopg2.connect("dbname='openlibrary' host='ol-db'")
+cur = conn.cursor()
 
-edition_id = list(db.query("select id, key from thing where key='/type/edition'"))[0].id
+cur.execute("select id, key from thing where key='/type/edition'")
+edition_id = cur.fetchone()[0]
+print edition_id
 
 get_fields = ["'%s'" % i for i in ['title','work_title','work_titles','title_prefix','subtitle']]
 sql = "select id, name from property where type=52 and name in (%s)" % ','.join(get_fields)
-properties = dict((row.id, row.name) for row in db.query(sql))
-
-print edition_id
+print sql
+cur.execute(sql)
+properties = dict(cur.fetchall())
 print properties
 
-out = open('/1/labs/titles/list', 'w')
 keys = ', '.join(`k` for k in properties.keys())
-sql = "select key, key_id, value from edition_str, thing where key_id in (%s) and thing_id=thing.id and type=%d" % (keys, edition_id)
-db_iter = db.query(sql)
+#sql = "select key, key_id, value, ordering from edition_str, thing where key_id in (%s) and thing_id=thing.id and type=%d" % (keys, edition_id)
+sql = "select thing_id, key_id, value, ordering from edition_str where key_id in (%s)" % (keys,)
+print sql
+cur.execute(sql)
 
-# slow because web.py uses fetchall (maybe)
-for row in db_iter:
-    print >> out, (row.key, properties[row.key_id], row.value)
+out = open('/1/labs/titles/list', 'w')
+rows = cur.fetchmany()
+while rows:
+    for key, key_id, value, ordering in rows:
+        print >> out, (key, properties[key_id], value, ordering)
+    rows = cur.fetchmany()
 out.close()
