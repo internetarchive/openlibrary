160a0f531b2c6c019fed3899c85aef42269248a6
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-04-06 20:06:46 +0530

    Added client library to log stats

---
+++ openlibrary/core/stats.py
@@ -0,0 +1,47 @@
+"""
+StatsD client to be used in the application to log various metrics
+
+Based on the code in http://www.monkinetic.com/2011/02/statsd.html (pystatsd client)
+
+"""
+
+# statsd.py
+
+# Steve Ivy <steveivy@gmail.com>
+# http://monkinetic.com
+
+import logging
+import socket
+import random
+
+from pystatsd import Client
+
+from infogami import config
+
+def create_stats_client():
+    "Create the client which can be used for logging statistics"
+    stats_server = config.get("admin", {}).get("statsd_server",None)
+    if stats_server:
+        host, port = stats_server.rsplit(":")
+        return = Client(host, port)
+    else:
+        return False
+
+def put(key, value):
+    "Records this ``value`` with the given ``key``. It is stored as a millisecond count"
+    global client
+    client.timing(key, value)
+
+def increment(key, n=1):
+    "Increments the value of ``key`` by ``n``"
+    global client
+    for i in range(n):
+        client.increment(key)
+
+
+client = create_stats_client()
+
+
+
+
+    
