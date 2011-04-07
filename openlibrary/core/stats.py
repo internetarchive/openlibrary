cd8b6b3bcfb4a0a04c194e6f62e9e40480a879ea
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-04-07 17:48:48 +0530

    Fixed up the stats client

--- openlibrary/core/stats.py
+++ openlibrary/core/stats.py
@@ -20,23 +20,31 @@ from infogami import config
 
 def create_stats_client():
     "Create the client which can be used for logging statistics"
-    stats_server = config.get("admin", {}).get("statsd_server",None)
-    if stats_server:
-        host, port = stats_server.rsplit(":")
-        return = Client(host, port)
-    else:
+    logger = logging.getLogger("pystatsd.client")
+    logger.addHandler(logging.StreamHandler())
+    try:
+        stats_server = config.get("admin", {}).get("statsd_server",None)
+        if stats_server:
+            host, port = stats_server.rsplit(":", 1)
+            return Client(host, port)
+        else:
+            return False
+    except Exception:
+        #TBD : Log stats error here
         return False
 
 def put(key, value):
     "Records this ``value`` with the given ``key``. It is stored as a millisecond count"
     global client
-    client.timing(key, value)
+    if client:
+        client.timing(key, value)
 
 def increment(key, n=1):
     "Increments the value of ``key`` by ``n``"
     global client
-    for i in range(n):
-        client.increment(key)
+    if client:
+        for i in range(n):
+            client.increment(key)
 
 
 client = create_stats_client()
