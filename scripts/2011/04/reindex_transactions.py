8f15e2713656f78d28719e5d09128dba1ad5d3d5
Anand Chitipothu <anandology@gmail.com>
2011-04-25 13:12:33 +0530

    Added --verify option to reindex transactions script.

--- scripts/2011/04/reindex_transactions.py
+++ scripts/2011/04/reindex_transactions.py
@@ -4,7 +4,7 @@ Some transcations/changesets in the openlibrary database have been not indexed
 because of a bug. This script fixes the old records by reindexing the given
 transactions.
 
-USAGE: 
+USAGE:
 
     $ python reindex_transactions.py transaction_id [transaction_id, ...]
 
@@ -33,17 +33,46 @@ def get_db():
 
 def reindex_transaction(id):
     logging.info("reindexing %s", id)
-    
+
     db = get_db()
-    impl = SaveImpl(db)    
-    
+    impl = SaveImpl(db)
+
     with db.transaction():
         tx = db.query("SELECT id, data FROM transaction where id=$id", vars=locals())[0]
         db.delete("transaction_index", where="tx_id=$id", vars=locals())
-        
+
+        logging.info("tx.data %s", tx.data)
         if tx.data:
             data = simplejson.loads(tx.data)
             impl._index_transaction_data(id, data)
+
+def verify_index(tx_id):
+    logging.info("verifying %s", tx_id)
+    db = get_db()
+    impl = SaveImpl(db)
+    tx = db.query("SELECT id, action, created, data FROM transaction where id=$tx_id", vars=locals())[0]
+
+    if tx.data:
+        d = db.query("SELECT * FROM transaction_index WHERE tx_id=$tx_id", vars=locals())
+        index = sorted((row.key, row.value) for row in d)
+
+        tx_data = simplejson.loads(tx.data)
+        index2 = compute_index(tx_data)
+        if index != index2:
+            print "\t".join([str(x) for x in [tx.id, tx.action, tx.created.isoformat(), index, index2]])
+
+def compute_index(tx_data):
+    d = []
+    def index(key, value):
+        if isinstance(value, (basestring, int)):
+            d.append((key, value))
+        elif isinstance(value, list):
+            for v in value:
+                index(key, v)
+
+    for k, v in tx_data.iteritems():
+        index(k, v)
+    return sorted(d)
 
 def main():
     logger.info("BEGIN")
@@ -51,6 +80,17 @@ def main():
         reindex_transaction(id)
     logger.info("END")
 
+def main_verify():
+    logger.info("BEGIN")
+    for id in sys.argv[1:]:
+        verify_index(id)
+    logger.info("END")
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
-    main()
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s")
+
+    if "--verify" in sys.argv:
+        sys.argv.remove("--verify")
+        main_verify()
+    else:
+        main()
