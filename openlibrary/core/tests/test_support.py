f48e51aafba5c3530a78e683d396c898a2a70c2c
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-05-03 21:38:07 +0530

    Implemented creation of cases

--- openlibrary/core/tests/test_support.py
+++ openlibrary/core/tests/test_support.py
@@ -4,21 +4,21 @@ def test_create_case(couchdb, sequence):
     "Tries to create a case"
     from openlibrary.core import support
     s = support.Support(db = couchdb)
-    
-    s = s.create_case(creator_name      = "Noufal Ibrahim",
+    c = s.create_case(creator_name      = "Noufal Ibrahim",
                       creator_email     = "noufal@archive.org",
                       creator_useragent = "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.18) Gecko/20110323 Iceweasel/3.5.18 (like Firefox/3.5.18)",
                       subject           = "Testing",
                       description       = "This is a test request",
                       assignee          = "anand@archive.org")
-    assert s.caseid == "case-0"
-    assert s.creator_name == "Noufal Ibrahim"
-    assert s.creator_email == "noufal@archive.org"
-    assert s.creator_useragent == "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.18) Gecko/20110323 Iceweasel/3.5.18 (like Firefox/3.5.18)"
-    assert s.subject == "Testing"
-    assert s.description == "This is a test request"
-    assert s.assignee == "anand@archive.org"
-    created_date = s.created
+
+    assert c.caseid == "case-0"
+    assert c.creator_name == "Noufal Ibrahim"
+    assert c.creator_email == "noufal@archive.org"
+    assert c.creator_useragent == "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.18) Gecko/20110323 Iceweasel/3.5.18 (like Firefox/3.5.18)"
+    assert c.subject == "Testing"
+    assert c.description == "This is a test request"
+    assert c.assignee == "anand@archive.org"
+    created_date = c.created
     current_date = datetime.datetime.utcnow()
     assert created_date.day == current_date.day
     assert created_date.month == current_date.month
