ed33278857ec8903e6b5f0635faf7bb9f3b7658d
Edward Betts <edwardbetts@gmail.com>
2011-05-05 22:35:27 +0000

    making progress towards import API

--- openlibrary/catalog/add_book/__init__.py
+++ openlibrary/catalog/add_book/__init__.py
@@ -1,3 +1,96 @@
+from openlibrary.catalog.merge.merge_marc import build_marc
+from load_book import build_query
+
+type_map = {
+    'description': 'text',
+    'notes': 'text',
+    'number_of_pages': 'int',
+}
+
+class InvalidLanguage(Exception):
+    def __init__(self, code):
+        self.code = code
+    def __str__(self):
+        return "invalid language code: '%s'" % self.code
+
+def load_data(rec):
+    loc = 'ia:' + rec['ia']
+    q = build_query(rec)
+
+    for a in q.get('authors', []):
+        if 'key' in a:
+            authors.append({'key': a['key']})
+        else:
+            try:
+                ret = ol.new(a, comment='new author')
+            except:
+                print a
+                raise
+            print 'ret:', ret
+            assert isinstance(ret, basestring)
+            authors.append({'key': ret})
+    q['source_records'] = [loc]
+    if authors:
+        q['authors'] = authors
+
+    wkey = None
+    subjects = subjects_for_work(rec)
+    subjects.setdefault('subjects', []).append('Accessible book')
+
+    if 'printdisabled' in collections:
+        subjects['subjects'].append('Protected DAISY')
+    elif 'lendinglibrary' in collections:
+        subjects['subjects'] += ['Protected DAISY', 'Lending library']
+    elif 'inlibrary' in collections:
+        subjects['subjects'] += ['Protected DAISY', 'In library']
+
+    if 'authors' in q:
+        wkey = find_matching_work(q)
+    if wkey:
+        w = ol.get(wkey)
+        need_update = False
+        for k, subject_list in subjects.items():
+            for s in subject_list:
+                if s not in w.get(k, []):
+                    w.setdefault(k, []).append(s)
+                    need_update = True
+        if need_update:
+            ol.save(wkey, w, 'add subjects from new record')
+    else:
+        w = {
+            'type': '/type/work',
+            'title': q['title'],
+        }
+        if 'authors' in q:
+            w['authors'] = [{'type':'/type/author_role', 'author': akey} for akey in q['authors']]
+        w.update(subjects)
+
+        wkey = ol.new(w, comment='initial import')
+
+    q['works'] = [{'key': wkey}]
+    for attempt in range(50):
+        if attempt > 0:
+            print 'retrying'
+        try:
+            pprint(q)
+            ret = ol.new(q, comment='initial import')
+        except httplib.BadStatusLine:
+            sleep(30)
+            continue
+        except: # httplib.BadStatusLine
+            print q
+            raise
+        break
+    print 'ret:', ret
+    assert isinstance(ret, basestring)
+    key = '/b/' + re_edition_key.match(ret).group(1)
+    pool.update(key, q)
+
+    print 'add_cover_image'
+    if 'cover' in rec:
+        add_cover_image(ret, rec['cover'])
+
+
 def load(rec):
     edition_pool = pool.build(rec)
     if not edition_pool:
