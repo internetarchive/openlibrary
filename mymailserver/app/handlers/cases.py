a23867685e15a13736e20f9b0d46b3f9d6aa07bb
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-05-30 17:49:54 +0530

    Properly create cases

--- mymailserver/app/handlers/cases.py
+++ mymailserver/app/handlers/cases.py
@@ -1,8 +1,12 @@
+from email.parser import Parser
 import logging
+
 from lamson.routing import route, route_like, stateless
-from config.settings import relay
+from config.settings import relay, support_db
 from lamson import view
 from lamson.queue import Queue
+
+from openlibrary.core import support
 
 
 @route("support\+(case)@(host)", case="[0-9]+")
@@ -12,12 +16,22 @@ def CASE(message, case=None, host=None):
     print "And I'm creating a case for it"
     q = Queue("run/cases")
     q.push(message)
-    # Create a case for it here. Don't bother forwarding it.
-    
+    # Update case here
+    try:
+        case = support_db.get_case(case)
+        m= Parser().parsestr(str(message))
+        frm = m['From']
+        payload = m.get_payload()
+        case.add_worklog_entry(frm, payload)
+    except support.InvalidCase:
+        logging.warning("Attempt to update non existing case")
+        # Send an email about this
+    except Exception:
+        logging.critical("Error while updating case", exc_info = True)
+        
 
 @route("(account)@(host)", account = "[^+]+", host=".+")
 @stateless
 def FORWARD(message, account=None, host=None):
     print "Forwarding message now %s, %s, %s"%(message, account, host)
     relay.deliver(message)
-
--- mymailserver/config/settings.py
+++ mymailserver/config/settings.py
@@ -1,5 +1,9 @@
 # This file contains python variables that configure Lamson for email processing.
+import yaml
+import couchdb
 import logging
+
+from openlibrary.core import support
 
 # You may add additional parameters such as `username' and `password' if your
 # relay server requires authentication, `starttls' (boolean) or `ssl' (boolean)
@@ -14,6 +18,17 @@ handlers = ['app.handlers.cases']
 router_defaults = {'host': '.+'}
 
 template_config = {'dir': 'app', 'module': 'templates'}
+
+# Ready the support infrastructure
+try:
+    cfg = yaml.load(open("/home/noufal/github/nibrahim/openlibrary/conf/openlibrary.yml"))
+    db = couchdb.Database(cfg['admin']['admin_db'])
+    support_db = support.Support(db)
+    logging.info("Initialised connection to support database")
+except Exception:
+    logging.critical("Couldn't initialise support database. Bailing out!", exc_info = True)
+    raise
+
 
 # the config/boot.py will turn these values into variables set in settings
 
