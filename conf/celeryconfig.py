94bb09fd0e4f03e54ebc320f0ddc4d4cfe367399
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-06-05 11:52:57 +0530

    Added postgresql backend

--- conf/celeryconfig.py
+++ conf/celeryconfig.py
@@ -4,5 +4,7 @@ BROKER_PORT = 5672
 # BROKER_PASSWORD = "mypassword"
 # BROKER_VHOST = "myvhost"
 
-CELERY_RESULT_BACKEND = "amqp"
+CELERY_RESULT_BACKEND = "database"
+CELERY_RESULT_DBURI = "postgresql://noufal@:5432/celery"
+
 CELERY_IMPORTS = ("openlibrary.tasks", )
