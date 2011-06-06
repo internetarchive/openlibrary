68d67279e34a0d23a8568f853186a529a1cfef42
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-06-06 15:01:50 +0530

    Created a simple interface on the admin panel for tombstones

--- openlibrary/plugins/admin/tasks.py
+++ openlibrary/plugins/admin/tasks.py
@@ -1,5 +1,11 @@
+import web
+from infogami.utils.view import render_template
+
 class monitor(object):
     def GET(self):
-        return "Hello"
+        db = web.database(dbn="postgres",  db="celery")
+        tasks = db.select('celery_taskmeta')
+        return render_template("admin/tasks", tasks)
+    
     
 
