4bf38262f6e17b351fb13ecce85befa049060aa7
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-06-06 22:29:40 +0530

    Changed postgres config

--- conf/celeryconfig.py
+++ conf/celeryconfig.py
@@ -5,6 +5,6 @@ BROKER_PORT = 5672
 # BROKER_VHOST = "myvhost"
 
 CELERY_RESULT_BACKEND = "database"
-CELERY_RESULT_DBURI = "postgresql://noufal@:5432/celery"
+CELERY_RESULT_DBURI = "postgresql://@:5432/celery"
 
 CELERY_IMPORTS = ("openlibrary.tasks", )
