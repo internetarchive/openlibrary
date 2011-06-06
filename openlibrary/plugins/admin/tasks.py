1777841d710287a9f97dd3eba222e302e6f76b76
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-06-06 20:02:47 +0530

    Added tab for reserved tasks

--- openlibrary/plugins/admin/tasks.py
+++ openlibrary/plugins/admin/tasks.py
@@ -8,12 +8,17 @@ from infogami import config
 def massage_taskslists(atasks):
     """Massage the output of the celery inspector into a format that
     can be printed by our template"""
+    def _start_time(task):
+        try:
+            return datetime.datetime.fromtimestamp(task['time_start'])
+        except Exception:
+            return ""
     for host,tasks in atasks.iteritems():
         for task in tasks:
-            yield dict(uuid = task['id'],
-                       started_at = datetime.datetime.fromtimestamp(task['time_start']),
-                       command = task['name'],
-                       args = task['args'] + task['kwargs'],
+            yield dict(uuid = task.get('id',""),
+                       started_at = _start_time(task),
+                       command = task.get('name'),
+                       args = task.get('args',"") + task.get('kwargs',""),
                        host = host,
                        affected_docs = 'tbd')
                        
@@ -28,8 +33,9 @@ class monitor(object):
             
             inspector = inspect()
             active_tasks = massage_taskslists(inspector.active())
-
-            return render_template("admin/tasks/index", completed_tasks, active_tasks)
+            reserved_tasks = massage_taskslists(inspector.reserved())
+            
+            return render_template("admin/tasks/index", completed_tasks, active_tasks, reserved_tasks)
         except Exception, e:
             print e
             return "Error in connecting to tombstone database"
