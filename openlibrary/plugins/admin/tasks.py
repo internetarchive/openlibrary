db8e46d86e3f046d0995a7b3cbc30beca3439a6b
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-06-06 17:45:24 +0530

    Stylised dashboard for /admin/tasks with tabs for each category

--- openlibrary/plugins/admin/tasks.py
+++ openlibrary/plugins/admin/tasks.py
@@ -4,8 +4,8 @@ from infogami.utils.view import render_template
 class monitor(object):
     def GET(self):
         db = web.database(dbn="postgres",  db="celery")
-        tasks = db.select('celery_taskmeta')
-        return render_template("admin/tasks", tasks)
+        completed_tasks = db.select('celery_taskmeta')
+        return render_template("admin/tasks/index", completed_tasks)
     
     
 
