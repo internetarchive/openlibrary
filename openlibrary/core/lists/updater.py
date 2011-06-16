02e1f153e9eae0669746372542579073e62175c8
Anand Chitipothu <anand@archive.org>
2011-06-16 18:00:39 +0530

    Removed logging.basicConfig call.

--- openlibrary/core/lists/updater.py
+++ openlibrary/core/lists/updater.py
@@ -17,10 +17,6 @@ import web
 from openlibrary.core import formats, couch
 import engine
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)-8s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S')
-
 class UpdaterContext:
     """The state of the updater.
     
