f9905d8227948304b4b51c9bdbeba7e632b1d6bc
Edward Betts <edwardbetts@gmail.com>
2011-06-22 00:23:42 +0000

    set wkey

--- openlibrary/catalog/add_book/__init__.py
+++ openlibrary/catalog/add_book/__init__.py
@@ -162,8 +162,11 @@ def load_data(rec):
             assert w_dict and isinstance(w_dict, dict)
             edits.append(w_dict)
     else:
-        edits.append(new_work(q, rec, cover_id))
+        w = new_work(q, rec, cover_id)
+        wkey = w['key']
+        edits.append(w)
 
+    assert wkey
     q['works'] = [{'key': wkey}]
     q['key'] = ekey
     assert isinstance(q, dict)
