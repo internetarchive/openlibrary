d5bd1295873a2466155c2eab6a8351626912d4c5
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-07-15 10:40:21 +0530

    Use numeric caseid to sort numerically

--- couchapps/admin/cases/views/sort-caseid/map.py
+++ couchapps/admin/cases/views/sort-caseid/map.py
@@ -1,6 +1,6 @@
 def map(doc):
     if doc.get("type","") == "case":
         yield [doc.get("status"),
-               doc.get('_id'), 
+               int(doc.get('_id').replace("case-","")),
                doc.get('history',[{}])[-1].get("at","")
                ], 1
