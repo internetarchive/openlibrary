f91c7181859a7bdcb3e4a729374da301144b10f9
Anand Chitipothu <anand@archive.org>
2011-07-22 15:54:32 +0530

    Fixed log level for celery tasks.

--- openlibrary/tasks.py
+++ openlibrary/tasks.py
@@ -24,7 +24,6 @@ def update_support_from_email():
     ol_config = formats.load_yaml(open(configfile).read())
     fetchmail(ol_config)
 
-
 @oltask
 def trigger_offline_event(event, *a, **kw):
     logger.info("trigger_offline_event %s %s %s", event, a, kw)
@@ -46,12 +45,13 @@ def on_edit(changeset):
 def update_lists(changeset):
     """Updates the lists database on edit.
     """
-    logger.info("update_lists: %s", changeset['id'])
+    logger.info("BEGIN update_lists: %s", changeset['id'])
     configfile = celeryconfig.OL_CONFIG
     ol_config = formats.load_yaml(open(configfile).read())
     
     updater = ListUpdater(ol_config.get("lists"))
     updater.process_changeset(changeset, update_seeds=True)
+    logger.info("END update_lists")
 
 @oltask
 def update_solr(changeset):
@@ -80,4 +80,11 @@ def upload_via_s3(item_id, filename, data, s3_key, s3_secret):
     f = urllib2.urlopen(req)
     logger.info('s3 upload of %s %s returned status: %d' % (item_id, filename, f.getcode()))
 
+
+def setup():
+    # make sure openlibrary logger has at least INFO level
+    ol_logger = logging.getLogger("openlibrary")
+    if ol_logger.level < logging.INFO:
+        ol_logger.level = logging.INFO
     
+setup()
