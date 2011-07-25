a6d809e1fd3ae0dbe4a7ff9926dc94636a39ec84
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-07-25 16:01:13 +0530

    Storing records properly now in couch

--- openlibrary/core/celery_couchdb.py
+++ openlibrary/core/celery_couchdb.py
@@ -1,17 +1,48 @@
+import json
+
 from celery.backends.base import BaseDictBackend
 from celery.exceptions import ImproperlyConfigured
 
+try:
+    import couchdb
+except ImportError:
+    couchdb = None
+
+
+def maybe_serialise(data):
+    """Tries to serialise the data into a JSON string. If it fails, it
+    simply returns the str() representation"""
+    try:
+        return json.dumps(data)
+    except TypeError:
+        return str(data)
+    
+
+
 class CouchDBBackend(BaseDictBackend): 
     def __init__(self, *largs, **kargs):
-        print "Hello?"
         super(CouchDBBackend, self).__init__(**kargs)
-        self.database = self.app.conf.CELERY_RESULT_DBURI
-        if not self.database:
+        self.dburi = self.app.conf.CELERY_RESULT_DBURI
+        if not self.dburi:
             raise ImproperlyConfigured(
                     "Missing connection string! Do you have "
                     "CELERY_RESULT_DBURI set to a real value?")
+
+        if not couchdb:
+            raise ImproperlyConfigured(
+                "You need to install the couchdb library to use the "
+                "CouchDB backend.")
+
+        self.database = couchdb.Database(self.dburi)
         
-        
+
+    def _store_result(self, task_id, result, status, traceback=None):
+        doc = dict(result = maybe_serialise(result),
+                   status = maybe_serialise(status),
+                   traceback = maybe_serialise(traceback))
+
+        self.database[task_id] = doc
+        self.database.save(doc)
         
         
 
