1996f2733b0d0ea21ddba37b0ea2438e63ae9b21
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-07-28 17:58:09 +0530

    Added a simple inspector interface

---
+++ openlibrary/plugins/admin/inspect_thing.py
@@ -0,0 +1,49 @@
+import datetime
+import logging
+
+import web
+import couchdb
+
+from infogami import config
+from infogami.utils.view import render_template
+
+tombstone_db = None
+logger = None
+
+class ComponentMissing(KeyError): pass
+    
+
+
+@web.memoize
+def connect_to_tombstone():
+    global tombstone_db
+    try:
+        tombstone_db_uri = config.get("celery",{})["tombstone_db"]
+        tombstone_db = couchdb.Database(tombstone_db_uri)
+    except KeyboardInterrupt,e:
+        logger.warning("Couldn't connect to tombstone database", exc_info = True)
+
+def process_task_row(task):
+    """Makes some changes to the task row from couch so that the
+    template can display it properly"""
+    task.value['started_at'] = datetime.datetime.utcfromtimestamp(task.value['started_at'])
+    return task.value
+
+def get_tasks_info(thing, tombstone_db):
+    if not tombstone_db:
+        return "Couldn't initialise connection to tombstone database. No task information available"
+    events = tombstone_db.view("history/by_key")
+    return (process_task_row(x) for x in events)
+
+
+def get_thing_info(thing):
+    global tombstone_db
+    tasks = get_tasks_info(thing, tombstone_db)
+    return render_template("admin/inspect/thing", thing, tasks)
+
+def setup():
+    global logger
+    logger = logging.getLogger("openlibrary.inspect_thing")
+    connect_to_tombstone()
+
+
