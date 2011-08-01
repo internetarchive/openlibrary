49bdeb4d6bd7859312ff3f4b4d9928572f6c4e91
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-08-01 14:46:12 +0530

    Implemented display of a single task

--- openlibrary/plugins/admin/tasks.py
+++ openlibrary/plugins/admin/tasks.py
@@ -18,32 +18,29 @@ def connect_to_taskdb():
     db_uri = config.get("celery",{})["tombstone_db"]
     return couchdb.Database(db_uri)
 
-def process_task_row(task):
+def process_task_row(taskdoc):
     """Makes some changes to the task row from couch so that the
     template can display it properly"""
-    task.value['started_at'] = datetime.datetime.utcfromtimestamp(task.value['started_at'])
-    task.value['finished_at'] = datetime.datetime.utcfromtimestamp(task.value['finished_at'])
-    task.value["keys"] = task.value['context'].get("keys",[])
-    return task.value
+    taskdoc['started_at'] = datetime.datetime.utcfromtimestamp(taskdoc['started_at'])
+    taskdoc['finished_at'] = datetime.datetime.utcfromtimestamp(taskdoc['finished_at'])
+    taskdoc["keys"] = taskdoc['context'].get("keys",[])
+    return taskdoc
 
 class tasklist(object):
     def GET(self):
         db = connect_to_taskdb()
-        completed_tasks = (process_task_row(x) for x in db.view("history/by_key"))
+        completed_tasks = (process_task_row(x.value) for x in db.view("history/by_key"))
         return render_template("admin/tasks/index", completed_tasks)
 
 class tasks(object):
     def GET(self, taskid):
         try:
             db = connect_to_taskdb()
-            q = "SELECT * FROM celery_taskmeta WHERE task_id = $taskid"
-            ret = db.query(q, vars= locals())
-            if not ret:
+            try:
+                task = db[taskid]
+            except couchdb.http.ResourceNotFound:
                 return "No such task"
-            else:
-                tsk = ret[0]
-                res = unpack_result(tsk)
-            return render_template("admin/tasks/task", tsk, res)
+            return render_template("admin/tasks/task", process_task_row(task))
         except Exception:
             logger.warning("Problem while obtaining task information '%s'", taskid, exc_info = True)
             return "Error in obtaining task information"
