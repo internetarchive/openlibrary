096f21362f3f1c6da66738ef68f6b402ce1d5659
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-08-05 10:22:34 +0530

    Fix error in couchapp that breaks on design docs

--- couchapps/celery/history/views/tasks/map.py
+++ couchapps/celery/history/views/tasks/map.py
@@ -1,5 +1,5 @@
 def map(doc):
-    if doc["type"] == "task":
+    if doc.get("type","") == "task":
         yield [doc["command"], doc["finished_at"]], 1
         yield [None, doc['finished_at']], 1
 
