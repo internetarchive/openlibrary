65faa59a832c5d65483455ae4ce6ddf8f208ecfb
Edward Betts <edwardbetts@gmail.com>
2011-08-07 19:50:12 +0000

    load ia_box_id and ia_loaded_id

--- openlibrary/catalog/add_book/__init__.py
+++ openlibrary/catalog/add_book/__init__.py
@@ -428,6 +428,22 @@ def load(rec):
         if new not in e.setdefault('source_records', []):
             e.source_records.append(new)
             need_edition_save = True
+    for f in 'ia_box_id', 'ia_loaded_id':
+        if f not in rec:
+            continue
+        if e.get(f):
+            assert isinstance(e[f], list)
+            for x in rec[f]:
+                if x not in e[f]:
+                    e[f].append(x)
+                    need_edition_save = True
+        elif isinstance(rec[f], list):
+            e[f] = rec['ia_box_id']
+            need_edition_save = True
+        else:
+            assert isinstance(rec[f], basestring)
+            e[f] = [rec['ia_box_id']]
+            need_edition_save = True
     if need_edition_save:
         reply['edition']['status'] = 'modified'
         e_dict = e.dict()
