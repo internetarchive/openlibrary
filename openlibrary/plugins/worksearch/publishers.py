d8a30d37d783d675f7863af3bf7735a6c53025de
Anand Chitipothu <anand@archive.org>
2011-08-24 18:34:31 +0530

    Failed attempt to compute ebook_count per year.

--- openlibrary/plugins/worksearch/publishers.py
+++ openlibrary/plugins/worksearch/publishers.py
@@ -1,12 +1,16 @@
 """Publisher pages
 """
-from infogami.utils import delegate
+from infogami.utils import delegate, stats
 from infogami.utils.view import render_template, safeint
 import web
 import simplejson
+import logging
+import urllib
 
 from . import subjects
 from . import search
+
+logger = logging.getLogger("openlibrary.worksearch")
 
 class publishers(subjects.subjects):
     path = '(/publishers/[^/]+)'
@@ -83,9 +87,22 @@ class publisher_search(delegate.page):
         return [process(p) for p in publisher_facets]
         
 class PublisherEngine(subjects.SubjectEngine):
+    def normalize_key(self, key):
+        return key
+    
     def get_ebook_count(self, name, value, publish_year):
-        q = 'publisher_facet:(%s)' % (subjects.re_chars.sub(r'\\\1', value).encode('utf-8'))
-        years = subjects.execute_ebook_count_query(q)
+        # TODO: compute ebook_count per year.
+        return 0
+        
+        # XXX-Anand: The following implementation is not working.
+        
+        #q = 'publisher_facet:(%s)' % (subjects.re_chars.sub(r'\\\1', value).encode('utf-8'))
+        #years = subjects.execute_ebook_count_query(q)
+        try:
+            years = self._find_ebook_count(value)
+        except:
+            logger.error("failed to find_ebook_count", exc_info=True)
+            return 0
         
         # publish_year is None when querying for all years
         # publish_year is a string when querying for a single year
@@ -101,9 +118,31 @@ class PublisherEngine(subjects.SubjectEngine):
             y = safeint(publish_year, 0)
             return years.get(y, 0)
         return 0
+    
+    def _find_ebook_count(self, publisher_name):
         
-    def normalize_key(self, key):
-        return key
+        publisher_facet = subjects.re_chars.sub(r'\\\1', publisher_name).encode('utf-8')
+        q = "publisher_facet:(%s) AND (overdrive_s:* OR ia:*)" % (publisher_facet)
+        return subjects.execute_ebook_count_query()
+        
+        url = subjects.solr_select_url + "?" + urllib.urlencode({
+            "q": q,
+            "wt": "json",
+            "rows": 0,
+            "q.op": "AND",
+            "facet": "true",
+            "facet.field": "publish_year",
+            "facet.limit": 10000,
+            "facet.mincount": 1
+        })
+
+        stats.begin("solr", url=url)
+        response = simplejson.load(urllib.urlopen(url))['response']
+        stats.end()
+                
+        facets = response['facet_counts']['facet_fields']['publish_year']
+        years = dict(web.group(facets, 2))
+        return years
 
 def setup():
     d = web.storage(name="publisher", key="publishers", prefix="/publishers/", facet="publisher_facet", facet_key="publisher_facet", engine=PublisherEngine)
