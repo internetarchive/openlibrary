5be73227b6d0a8e34ce3a3ddf70339a4c97b0a15
Anand Chitipothu <anand@archive.org>
2011-10-12 10:24:32 +0530

    Implemented save and save many.

--- openlibrary/storage/connection.py
+++ openlibrary/storage/connection.py
@@ -3,6 +3,7 @@ import logging
 import os
 import simplejson
 import re
+import web
 
 from ..plugins import openlibrary
 
@@ -19,19 +20,29 @@ class Connection(client.Connection):
         self.itemid_prefix = itemid_prefix
     
     def request(self, sitename, path, method="GET", data=None):
-        if path == '/get':
-            return self.get(sitename, data)
-        elif path == '/get_many':
-            return self.get_many(sitename, data)
-        elif path == '/things':
-            return self.things(sitename, data)
-        elif path == '/versions':
-            return self.versions(sitename, data)
-        elif path == '/_recentchanges':
-            return self.recentchanges(sitename, data)
-        elif path == "/count_editions_by_work":
-            # TODO
-            return 0
+        
+        get_functions = [
+            "get", "get_many", "things", 
+            "versions", "_recentchanges", 
+            "count_editions_by_work", 
+            "new_key"
+        ]
+        
+        post_functions = [
+            "save_many"
+        ]
+        
+        name = path[1:]
+        
+        if method == "GET" and name in get_functions:
+            f = getattr(self, name)
+            return f(sitename, data)
+        elif method == "POST" and name in post_functions:
+            f = getattr(self, name)
+            return f(sitename, data)
+        elif path.startswith("/save/"):
+            key = web.lstrips(path, "/save")
+            return self.save(sitename, key, data)
             
         # elif path == '/write':
         #     return self.write(sitename, data)
@@ -63,11 +74,18 @@ class Connection(client.Connection):
                 # the type can either be python dict or JSON
                 g = {"true": True, "false": False}
                 return eval(open(path).read(), g)
+        # elif key.startswith("/config/edition"):
+        #     path = os.path.join(os.path.dirname(openlibrary.__file__), "pages", "config_edition.page")
+        #     if os.path.exists(path):
+        #         return eval(open(path).read())
         else:
             itemid = self.key2itemid(key)
             item = itemid and self.storage.find_item(itemid)
             f = item and item.get_file(itemid + ".json")
-            return f and simplejson.loads(f.read())
+            if f:
+                return simplejson.loads(f.read())
+        
+        raise client.ClientException("404 not found", "Not Found")
         
     def key2itemid(self, key):
         if re.match("/[^/]*/OL\d+[AMW]", key):
@@ -79,6 +97,34 @@ class Connection(client.Connection):
         keys = simplejson.loads(data['keys'])
         logger.debug("get_many %s", keys)
         return [self.get(k) for k in keys]
+    
+    def permission(self, sitename, data):
+        return {"write": True, "admin": True}
+        
+    def save(self, sitename, key, data):
+        if isinstance(data, dict):
+            # save_many calls this method with data as dict
+            doc = data
+        else:
+            doc = simplejson.loads(data)
+        
+        _comment = doc.pop("_comment", None)
+        _action = doc.pop("_action", None)
+        _data = doc.pop("_data", None)
+        
+        itemid = self.key2itemid(key)
+        item = self.storage.find_item(itemid, create=True)
+        
+        f = item.get_file(itemid + ".json")
+        f.write(simplejson.dumps(doc))
+        return {"key": key, "revision": 1}
+
+    def save_many(self, sitename, data):
+        docs = simplejson.loads(data['query'])
+        return [self.save(sitename, doc['key'], doc) for doc in docs]
+        
+    def new_key(self, sitename, data):
+        return "/books/OL2M"
         
     def things(self, sitename, data):
         return []
@@ -101,8 +147,11 @@ class Connection(client.Connection):
         }
         return [dummy]
         
-    def recentchanges(self, sitename, data):
+    def _recentchanges(self, sitename, data):
         return []
+        
+    def count_editions_by_work(self, sitename, data):
+        return 0
                 
     def default_request(self, sitename, path, method, data):
         self.handle_error(404, "not found")
--- openlibrary/storage/local.py
+++ openlibrary/storage/local.py
@@ -15,10 +15,25 @@ class LocalStorage(base.BaseStorage):
         self.root = root
         self.base_url = base_url or urllib.pathname2url(root)
         
-    def find_item(self, itemid):
+    def _item(self, itemid):
         path = os.path.join(self.root, itemid)
-        if os.path.exists(path):
-            return LocalItem(itemid=itemid, path=path, base_url=self.base_url + "/" + itemid)
+        return LocalItem(itemid=itemid, path=path, base_url=self.base_url + "/" + itemid)
+        
+    def find_item(self, itemid, create=False):
+        item = self._item(itemid)
+        
+        if item.exists():
+            return item
+        elif create:
+            item.create()
+            return item
+        else:
+            return None
+            
+    def new_item(self, itemid):
+        item = self._item(itemid)
+        item.create()
+        return item
         
 class LocalItem(base.Item):
     def __init__(self, itemid, path, base_url):
@@ -26,19 +41,31 @@ class LocalItem(base.Item):
         self.path = path
         self.base_url = base_url
         
+    def exists(self):
+        return os.path.exists(self.path)
+        
+    def create(self):
+        os.makedirs(self.path)
+        
     def list_files(self):
         return os.path.listdir(self.root)
         
     def get_file(self, filename):
         path = os.path.join(self.path, filename)
-        if os.path.exists(path):
-            url = self.base_url + "/" + filename
-            return LocalFile(path, url)
-        
+        url = self.base_url + "/" + filename
+        return LocalFile(path, url)
+            
 class LocalFile(base.File):
     def __init__(self, path, url):
         self.path = path
         self.url = url
+    
+    def exists(self):
+        return os.path.exists(self.path)
         
     def read(self):
         return open(self.path).read()
+        
+    def write(self, content):
+        with open(self.path, "w") as f:
+            f.write(content)
