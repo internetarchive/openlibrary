6c9b426a517523b3bb145c28787c0a5741490a36
Anand Chitipothu <anand@archive.org>
2011-11-12 16:12:35 +0530

    Index both isbn-10 and isbn-13 even when only one is provided. #27
    This allows querying for isbn-13 even when the edition page has just
    isbn-10 defined.

---
+++ openlibrary/utils/tests/test_isbn.py
@@ -0,0 +1,11 @@
+from .. import isbn
+
+def test_isbn_13_to_isbn_10():
+    assert isbn.isbn_13_to_isbn_10("978-0-940787-08-7") == "0940787083"
+    assert isbn.isbn_13_to_isbn_10("9780940787087") == "0940787083"
+    assert isbn.isbn_13_to_isbn_10("BAD-ISBN") is None
+    
+def test_isbn_10_to_isbn_13():
+    assert isbn.isbn_10_to_isbn_13("0-940787-08-3") == "9780940787087"
+    assert isbn.isbn_10_to_isbn_13("0940787083") == "9780940787087"
+    assert isbn.isbn_10_to_isbn_13("BAD-ISBN") is None
