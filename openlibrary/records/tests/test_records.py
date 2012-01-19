14e7e3d74750016dd2447990964f3bfc5f0c4d71
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-01-19 15:01:12 +0530

    Red : Test creation of records

--- openlibrary/records/tests/test_records.py
+++ openlibrary/records/tests/test_records.py
@@ -49,15 +49,50 @@ def test_massage_search_results(mock_site):
     search_results = editions + [wkey] # This will be the input to massage_search_results
     massaged_results = massage_search_results(search_results)
     
+    # Check the fields in the main best match.
     for i in "key title".split(): # TODO: Check for the rest of the fields here
         assert massaged_results['doc'][i] == best_match[i], "Mismatch in %s field of massaged results and best match"%i
     assert massaged_results['doc']['isbn_10'] == best_match['isbn_10'], "Mismatch in ISBN10 field of massaged results and best match"
 
+    # Check the fields in the remaining matches.
     # The first two matches should be the editions and then the work match (the order of the input)
     expected_matches = [ {'edition' : editions[0], 'work' : mock_site.get(editions[0]).works[0].key},
                          {'edition' : editions[1], 'work' : mock_site.get(editions[1]).works[0].key},
                          {'edition' : None, 'work' : wkey } ]
 
     assert massaged_results['matches'] == expected_matches, "Matches field got a different value"
+
+
+def test_create_edition(mock_site):
+    "Creation of editions"
+    record = {'doc': {'isbn_10': ['1234567890'],
+                      'key': None,
+                      'publish_date': '2012',
+                      'publishers': 'Dover',
+                      'title': 'THIS IS A TEST BOOK',
+                      'type': {'key': '/type/edition'},
+                      'works': [{'key': None,
+                                 'title': 'This is a test book',
+                                 'type': {'key': '/type/work'},
+                                 'authors': [{'author': {'birth_date': '1979',
+                                                         'death_date': '2010',
+                                                         'key': None,
+                                                         'name': 'Test Author'}}]
+                    }]}}
+    
+    r = create(record)
+    new_edition = mock_site.get(r)
+    new_work = mock_site.get(new_edition.works[0]['key'])
+    new_author = mock_site.get(new_work.authors[0]['author'])
+    assert new_edition.title == "THIS IS A TEST BOOK"
+    assert new_work.title == 'This is a test book'
+    assert new_author.name == 'Test Author'
+
+
+    #TODO : Check whether the various cross references between records are correct.
+
+
+
+
 
 
