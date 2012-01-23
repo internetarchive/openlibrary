37f50662dc2ae248b02dd20da202b7d77895ba31
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-01-23 17:37:58 +0530

    Disable debug output from comparison function

--- openlibrary/records/tests/conftest.py
+++ openlibrary/records/tests/conftest.py
@@ -2,15 +2,15 @@ def pytest_funcarg__compare_results(request):
     """Returns a function to compare two objects d1 an d2 recursively
     skipping the 'key' if present parameter"""
     def compare_results(d1, d2):
-        print compare_results.depth * 2 * "--" +  "Comparing\n","1 ==> ",d1, "\n2 ==> ", d2
+        # print compare_results.depth * 2 * "--" +  "Comparing\n","1 ==> ",d1, "\n2 ==> ", d2
         if d1 == d2: # Trivially the same
             return True
         if isinstance(d1, list) and isinstance(d2, list) and len(d1) == len(d2):
             for i,j in zip(d1, d2):
-                compare_results.depth += 1
+                # compare_results.depth += 1
                 ret = compare_results(i, j)
-                compare_results.depth -= 1
-                print " ==> ",ret, "\n"
+                # compare_results.depth -= 1
+                # print " ==> ",ret, "\n"
                 if ret:
                     pass
                 else:
@@ -20,10 +20,10 @@ def pytest_funcarg__compare_results(request):
         if isinstance(d1, dict) and isinstance(d2, dict) and len(d1.keys()) == len(d2.keys()):
             for k,v in d1.iteritems():
                 if k != 'key':
-                    compare_results.depth += 1
+                    # compare_results.depth += 1
                     ret = compare_results(d1.get(k), d2.get(k))
-                    print " ==> ",ret, "\n"
-                    compare_results.depth -= 1
+                    # print " ==> ",ret, "\n"
+                    # compare_results.depth -= 1
                     if ret:
                         pass
                     else:
