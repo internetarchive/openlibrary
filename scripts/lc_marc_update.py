12fe8e9e33190e1247323d0788ebed89066e4382
Edward Betts <edwardbetts@gmail.com>
2012-08-10 17:31:54 +0000

    improved library of congress importer

---
+++ scripts/lc_marc_update.py
@@ -0,0 +1,91 @@
+from openlibrary.catalog.read_rc import read_rc
+from openlibrary import config
+from ftplib import FTP
+from time import sleep
+from lxml import etree
+import os, sys, httplib
+import argparse
+
+parser = argparse.ArgumentParser(description='Library of Congress MARC update')
+parser.add_argument('--config', default='openlibrary.yml')
+args = parser.parse_args()
+
+config_file = args.config
+config.load(config_file)
+c = config.runtime_config['lc_marc_update']
+
+def put_file(con, ia, filename, data):
+    print 'uploading %s' % filename
+    headers = {
+        'authorization': "LOW " + c['s3_key'] + ':' + c['s3_secret'],
+#        'x-archive-queue-derive': 0,
+    }
+    url = 'http://s3.us.archive.org/' + ia + '/' + filename
+    print url
+    for attempt in range(5):
+        con.request('PUT', url, data, headers)
+        try:
+            res = con.getresponse()
+        except httplib.BadStatusLine as bad:
+            print 'bad status line:', bad.line
+            raise
+        body = res.read()
+        if '<Error>' not in body:
+            return
+        print 'error'
+        print body
+        if no_bucket_error not in body and internal_error not in body:
+            sys.exit(0)
+        print 'retry'
+        sleep(5)
+    print 'too many failed attempts'
+
+
+url = 'http://archive.org/download/marc_loc_updates/marc_loc_updates_files.xml'
+
+attempts = 10
+wait = 5
+for attempt in range(attempts):
+    try:
+        root = etree.parse(url).getroot()
+        break
+    except:
+        if attempt == attempts-1:
+            raise
+        print 'error on attempt %d, retrying in %s seconds' % (attempt, wait)
+        sleep(wait)
+
+existing = set(f.attrib['name'] for f in root)
+#existing.remove("v40.i32.report") # for testing
+
+host = 'rs7.loc.gov'
+
+to_upload = set()
+
+def print_line(f):
+    if 'books.test' not in f and f not in existing:
+        to_upload.add(f)
+
+def read_block(block):
+    global data
+    data += block
+
+ftp = FTP(host)
+ftp.set_pasv(False)
+print ftp.getwelcome()
+ftp.login(c['lc_update_user'], c['lc_update_pass'])
+ftp.cwd('/emds/books/all')
+ftp.retrlines('NLST', print_line)
+
+item_id = 'marc_loc_updates'
+for f in to_upload:
+    data = ''
+    print 'downloading', f
+    ftp.retrbinary('RETR ' + f, read_block)
+    print 'done'
+    con = httplib.HTTPConnection('s3.us.archive.org')
+    con.connect()
+    put_file(con, item_id, f, data)
+    con.close()
+
+ftp.close()
