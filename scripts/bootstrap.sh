fdf43e631a0828056c69c1479402e99c9b7b201e
Anand Chitipothu <anandology@gmail.com>
2013-03-26 20:37:55 +0530

    Added code to setup OL on startup

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -50,10 +50,42 @@ eventer==0.1.1
 OL-GeoIP==1.2.4
 mockcache"
 
+function setup_database() {
+    echo "finding if posgres user vagrant already exists."
+    x=`sudo -u postgres psql -t -c "select count(*) FROM pg_catalog.pg_user where usename='vagrant'"`
+    echo "result = $x"
+    if [ "$x" -eq 0 ]; then
+        echo "setting up database..."
+        echo "  creating postgres user 'vagrant'"
+        sudo -u postgres createuser -s vagrant
+
+        echo "  creating openlibrary database"
+        sudo -u vagrant createdb openlibrary
+
+        echo " setting up openlibrary database"
+        setup_ol
+    else
+        echo "pg_user vagrant already exists. no need to setup database"
+    fi
+}
+
+function setup_ol() {
+    cd /vagrant
+    sed -e 's/hybrid/local/' -e 's/^infobase_server/# infobase_server/' conf/openlibrary.yml > conf/ol-install.yml
+    sudo -u vagrant python scripts/openlibrary-server conf/ol-install.yml install
+    #rm conf/ol-install.yml
+}
+
+function setup_nginx() {
+    ln -sf /vagrant/conf/nginx/sites-available/openlibrary.conf /etc/nginx/sites-available/
+    ln -sf /etc/nginx/sites-available/openlibrary.conf /etc/nginx/sites-enabled/
+    sudo /etc/init.d/nginx restart
+}
+
 pip install $PYTHON_PACKAGES
 
-# For some wierd error, nginx doesn't seem to be starting on boot
-/etc/init.d/nginx restart
+setup_database
+setup_nginx
 
 # change solr/tomcat port to 8983
 perl -i -pe 's/8080/8983/'  /etc/tomcat6/server.xml
