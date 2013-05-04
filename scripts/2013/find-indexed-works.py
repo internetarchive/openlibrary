ad38e169619aa3e261bc72cb2638a47a07e5dc3c
Anand Chitipothu <anand@archive.org>
2013-05-04 08:06:33 +0530

    Optimized the find-indexed-works.py script to query Solr with 1000 keys at once.
    This is way faster than the previous attempt.

--- scripts/2013/find-indexed-works.py
+++ scripts/2013/find-indexed-works.py
@@ -6,23 +6,36 @@ USAGE: zcat ol_dump_works_latest.txt.gz | cut -f2 | python scripts/2013/find-ind
 import sys
 import urllib
 import json
+import time
+import web
 
 solr_base_url = sys.argv[1]
 
-def jsonget(url):
-	print >> sys.stderr, "jsonget", url
-	return json.load(urllib.urlopen(url))
+def jsonget(url, data=None):
+	print >> sys.stderr, time.asctime(), "jsonget", url[:120], data[:50] if data else ""
+	return json.load(urllib.urlopen(url, data))
 
 def is_indexed_in_solr(key):
 	url = solr_base_url + "/select?wt=json&rows=0&q=key:" + key
 	d = jsonget(url)
 	return d['response']['numFound'] > 0
 
+def find_not_indexed(keys, chunk_size=1000):
+	for chunk in web.group(keys, chunk_size):
+		chunk = list(chunk)
+		q=" OR ".join("key:" + k for k in chunk)
+		params = urllib.urlencode({"q": q, "rows": chunk_size, "wt": "json", "fl": "key"})
+		url = solr_base_url + "/select"
+		d = jsonget(url, params)
+		found = set(doc['key'] for doc in d['response']['docs'])
+		for k in chunk:
+			if k not in found:
+				yield k
+
 def main():
-	for line in sys.stdin:
-		key = line.strip().split("/")[-1]
-		if not is_indexed_in_solr(key):
-			print line,
+	keys = (line.strip().split("/")[-1] for line in sys.stdin)
+	for k in find_not_indexed(keys):
+		print k
 
 if __name__ == '__main__':
 	main()
