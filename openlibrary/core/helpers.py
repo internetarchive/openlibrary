75211e675fbbff606f443b7c2a31b5711dd22616
Anand Chitipothu <anand@archive.org>
2013-06-27 11:24:18 +0530

    When HTML santization fails, use the original HTML instead of crashing.

--- openlibrary/core/helpers.py
+++ openlibrary/core/helpers.py
@@ -67,7 +67,12 @@ def sanitize(html):
         if BeautifulSoup:
             # Bad html. Tidy it up using BeautifulSoup
             html = str(BeautifulSoup(html))
-            html = genshi.HTML(html)
+            try:
+                html = genshi.HTML(html)
+            except genshi.ParseError:
+                # Failed to sanitize.
+                # We can't do any better than returning the original HTML, without sanitizing.
+                return html
         else:
             raise
 
