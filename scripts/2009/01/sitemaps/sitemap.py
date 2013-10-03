a7a41a85e13fcf85f9999b30f22647664cf670fe
Anand Chitipothu <anand@archive.org>
2013-10-03 17:43:59 +0530

    Simplified the sitemaps script.
    It takes dump as input and writes the sitemap files to sitemaps/ dir.

--- scripts/2009/01/sitemaps/sitemap.py
+++ scripts/2009/01/sitemaps/sitemap.py
@@ -10,9 +10,9 @@ import os
 import itertools
 import datetime
 import gzip
-import json
+#import json
 import re
-from openlibrary.core import helpers as h
+import time
 
 t_sitemap = """$def with (things)
 <?xml version="1.0" encoding="UTF-8"?>
@@ -68,7 +68,7 @@ def process_dump(dumpfile):
         last_modified = last_modified.replace(' ', 'T') + 'Z'
         sortkey = get_sort_key(key)
         if sortkey:
-            print "\t".join([sortkey, path.encode('utf-8'), last_modified])
+            yield [sortkey, path.encode('utf-8'), last_modified]
 
 re_key = re.compile("^/(authors|books|works)/OL\d+[AMW]$")
 
@@ -87,9 +87,8 @@ def get_sort_key(key):
     num = int(web.numify(key)) / 10000
     return "%s_%04d" % (prefix, num)
 
-def generate_sitemaps():
-    os.system("rm -rf sitemaps; mkdir sitemaps")
-    rows = (line.strip().split("\t") for line in sys.stdin)
+def generate_sitemaps(filename):
+    rows = (line.strip().split("\t") for line in open(filename))
     for sortkey, chunk in itertools.groupby(rows, lambda row: row[0]):
         things = [web.storage(path=path, last_modified=last_modified) for sortkey, path, last_modified in chunk]
         if things:
@@ -105,24 +104,56 @@ def generate_siteindex():
 
 def write(path, text):
     text = web.safestr(text)
-    print 'writing', path, text.count('\n')
+    log('writing', path, text.count('\n'))
     f = gzip.open(path, 'w')
     f.write(text)
     f.close()
     #os.system("gzip " + path)
 
+def write_tsv(path, rows):
+    lines = ("\t".join(row) + "\n" for row in rows)
+    f = open(path, "w")
+    f.writelines(lines)
+    f.close()
+
+def system_memory():
+    """Returns system memory in MB."""
+    try:
+        x = os.popen("cat /proc/meminfo | grep MemTotal | sed 's/[^0-9]//g'").read()
+        # proc gives memory in KB, converting it to MB
+        return int(x)/1024
+    except IOError:
+        # default to 1024MB
+        return 1024
+
+def system(cmd):
+    log("executing:", cmd)
+    status = os.system(cmd)
+    if status != 0:
+        raise Exception("%r failed with exit status: %d" % (cmd, status))
+
+def log(*args):
+    msg = " ".join(map(str, args))
+    print time.asctime(), msg
+
 def main(dumpfile):
-    cmd = "python %s '%s' --process | sort -S 1G | python %s --generate" % (sys.argv[0], dumpfile, sys.argv[0])
-    print cmd
-    os.system(cmd)
+    system("rm -rf sitemaps sitemaps_data.txt*; mkdir sitemaps")
+
+    log("processing the dump")
+    rows = process_dump(dumpfile)
+    write_tsv("sitemaps_data.txt", rows)
+
+    log("sorting sitemaps_data.txt")
+    # use half of system of 3GB whichever is smaller
+    sort_mem = min(system_memory()/2, 3072)
+    system("sort -S%dM sitemaps_data.txt > sitemaps_data.txt.sorted" % sort_mem)
+
+    log("generating sitemaps")
+    generate_sitemaps("sitemaps_data.txt.sorted") 
+    generate_siteindex()
+
+    log("done")
 
 if __name__ == "__main__":
     import sys
-    if "--process" in sys.argv:
-        sys.argv.remove("--process")
-        process_dump(sys.argv[1])
-    elif "--generate" in sys.argv:
-        sys.argv.remove("--sitemaps")
-        generate_sitemaps()
-        generate_siteindex()
-
+    main(sys.argv[1])
