6bd2462f6f67254d168e88e55c4296411686b1ae
Anand Chitipothu <anand@archive.org>
2013-10-09 07:34:56 +0530

    Fixed recaptcha on https.
    Using protocol relative URL for recaptcha.

--- openlibrary/plugins/recaptcha/recaptcha.py
+++ openlibrary/plugins/recaptcha/recaptcha.py
@@ -10,11 +10,11 @@ var RecaptchaOptions = {
 };
 </script>
 <script type="text/javascript"
-   src="http://www.google.com/recaptcha/api/challenge?k=KEY">
+   src="//www.google.com/recaptcha/api/challenge?k=KEY">
 </script>
 
 <noscript>
-   <iframe src="http://www.google.com/recaptcha/api/noscript?k=KEY"
+   <iframe src="//www.google.com/recaptcha/api/noscript?k=KEY"
        height="300" width="500" frameborder="0"></iframe><br>
    <textarea name="recaptcha_challenge_field" rows="3" cols="40">
    </textarea>
