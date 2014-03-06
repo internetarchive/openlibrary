ba5c3aafab6e5e6d07c3c3183b9131d234179265
Anand Chitipothu <anand@archive.org>
2014-03-06 11:13:13 +0530

    When editing an edition without work, transfer the authors from the edition to the work.

--- openlibrary/plugins/upstream/addbook.py
+++ openlibrary/plugins/upstream/addbook.py
@@ -588,8 +588,15 @@ class book_edit(delegate.page):
             raise web.notfound()
 
         work = edition.works and edition.works[0]
-        # HACK: create dummy work when work is not available to make edit form work
-        work = work or web.ctx.site.new('', {'key': '', 'type': {'key': '/type/work'}, 'title': edition.title})
+
+        if not work:
+            # HACK: create dummy work when work is not available to make edit form work
+            work = web.ctx.site.new('', {
+                'key': '', 
+                'type': {'key': '/type/work'}, 
+                'title': edition.title,
+                'authors': [{'type': '/type/author_role', 'author': {'key': a['key']}} for a in edition.get('authors', [])]
+            })
 
         recap_plugin_active = 'recaptcha' in config.get('plugins')
 
