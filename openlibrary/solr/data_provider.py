541ac7af11935b99afac38cfb1480d65592cf8f2
Anand Chitipothu <anandology@gmail.com>
2014-12-21 06:36:52 +0530

    Optimized data provider for solr update.

--- openlibrary/solr/data_provider.py
+++ openlibrary/solr/data_provider.py
@@ -86,6 +86,11 @@ class BetterDataProvider(LegacyDataProvider):
         self.cache = {}
         self.metadata_cache = {}
 
+        # cache for redirects
+        self.redirect_cache = {}
+
+        self.edition_keys_of_works_cache = {}
+
         from openlibrary.solr.process_stats import get_ia_db, get_db
         self.db = get_db()
         self.ia_db = get_ia_db()
@@ -95,9 +100,6 @@ class BetterDataProvider(LegacyDataProvider):
 
         infogami._setup()
         delegate.fakeload()
-
-    def get_metadata(self, identifier):
-        return ia.get_metadata(identifier)
 
     def get_metadata(self, identifier):
         """Alternate implementation of ia.get_metadata() that uses IA db directly.
@@ -132,9 +134,16 @@ class BetterDataProvider(LegacyDataProvider):
         for row in rows:
             self.metadata_cache[row.identifier] = row
 
-    def preload_documents(self, keys):
-        logger.info("preload_documents %s", keys)
+        for id in identifiers:
+            self.metadata_cache.setdefault(id, None)
 
+    def get_document(self, key):
+        #logger.info("get_document %s", key)
+        if key not in self.cache:
+            self.preload_documents([key])
+        return self.cache.get(key)
+
+    def preload_documents(self, keys):
         identifiers = [k.replace("/books/ia:", "") for k in keys if k.startswith("/books/ia:")]
         #self.preload_ia_items(identifiers)
         re_key = web.re_compile("/(books|works|authors)/OL\d+[MWA]")
@@ -144,11 +153,18 @@ class BetterDataProvider(LegacyDataProvider):
         self.preload_documents0(keys2)
         self._preload_works()
         self._preload_authors()
+        self._preload_editions()
+        self._preload_metadata_of_editions()
+
+        # for all works and authors, find redirects as they'll requested later
+        keys3 = [k for k in self.cache if k.startswith("/works/") or k.startswith("/authors/")]
+        self.preload_redirects(keys3)
 
     def preload_documents0(self, keys):
         keys = [k for k in keys if k not in self.cache]
         if not keys:
             return
+        logger.info("preload_documents0 %s", keys)
         for chunk in web.group(keys, 100):
             docs = web.ctx.site.get_many(list(chunk))
             for doc in docs:
@@ -164,6 +180,23 @@ class BetterDataProvider(LegacyDataProvider):
         # print "preload_works, found keys", keys
         self.preload_documents0(keys)
 
+    def _preload_editions(self):
+        keys = []
+        for doc in self.cache.values():
+            if doc and doc['type']['key'] == '/type/work':
+                keys.append(doc['key'])
+        self.preload_editions_of_works(keys)
+
+    def _preload_metadata_of_editions(self):
+        identifiers = []
+        for doc in self.cache.values():
+            if doc and doc['type']['key'] == '/type/edition':
+                if doc.get('ocaid'):
+                    identifiers.append(doc['ocaid'])
+                #source_records = doc.get("source_records", [])
+                #identifiers.extend(r[len("ia:"):] for r in source_records if r.startswith("ia:"))
+        self.preload_metadata(identifiers)
+
     def _preload_authors(self):
         """Preloads authors for all works in the cache.
         """
@@ -172,3 +205,77 @@ class BetterDataProvider(LegacyDataProvider):
             if doc and doc['type']['key'] == '/type/work' and doc.get('authors'):
                 keys.extend(a['author']['key'] for a in doc['authors'])
         self.preload_documents0(list(set(keys)))
+
+    def find_redirects(self, key):
+        """Returns all the keys that are redirected to this.
+        """
+        self.preload_redirects([key])
+        return self.redirect_cache[key]
+
+    def preload_redirects(self, keys):
+        keys = [k for k in keys if k not in self.redirect_cache]
+        if not keys:
+            return
+        logger.info("preload_redirects %s", keys)
+        for chunk in web.group(keys, 100):
+            self._preload_redirects0(list(chunk))
+
+    def _preload_redirects0(self, keys):
+        query = {
+            "type": "/type/redirect",
+            "location": keys,
+            "a:location": None # asking it to fill location in results
+        }
+        for k in keys:
+            self.redirect_cache.setdefault(k, [])
+
+        matches = web.ctx.site.things(query, details=True)
+        for thing in matches:
+            # we are trying to find documents that are redirecting to each of the given keys
+            self.redirect_cache[thing.location].append(thing.key)
+
+    def get_editions_of_work(self, work):
+        wkey = work['key']
+        self.preload_editions_of_works([wkey])
+        edition_keys = self.edition_keys_of_works_cache.get(wkey, [])
+        return [self.cache[k] for k in edition_keys]
+
+    def preload_editions_of_works(self, work_keys):
+        work_keys = [wkey for wkey in work_keys if wkey not in self.edition_keys_of_works_cache]
+        if not work_keys:
+            return
+        logger.info("preload_editions_of_works %s ..", work_keys[:5])
+
+        # Infobase doesn't has a way to do find editions of multiple works at once.
+        # Using raw SQL to avoid making individual infobase queries, which is very
+        # time consuming.
+        key_query = ("select id from property where name='works'" +
+                    " and type=(select id from thing where key='/type/edition')")
+
+        q = ("SELECT edition.key as edition_key, work.key as work_key" +
+            " FROM thing as edition, thing as work, edition_ref" +
+            " WHERE edition_ref.thing_id=edition.id" +
+            "   AND edition_ref.value=work.id" +
+            "   AND edition_ref.key_id=({})".format(key_query) +
+            "   AND work.key in $keys")
+        result = self.db.query(q, vars=dict(keys=work_keys))
+        for row in result:
+            self.edition_keys_of_works_cache.setdefault(row.work_key, []).append(row.edition_key)
+
+        keys = [k for _keys in self.edition_keys_of_works_cache.values()
+                  for k in _keys]
+        print "keys", keys
+        self.preload_documents0(keys)
+        return
+
+        q = {'type':'/type/edition', 'works': work_keys, '*': None}
+        print q
+        matches = web.ctx.site.things(q, details=True)
+        for doc in matches:
+            key = doc['key']
+            w = doc.get("works")
+            wkey = w and w[0].key
+            if wkey:
+                self.edition_keys_of_works_cache.setdefault(wkey, []).append(key)
+            if key not in self.cache:
+                self.cache[key] = doc
