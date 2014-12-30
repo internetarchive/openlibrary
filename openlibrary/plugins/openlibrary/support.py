e78c4f026ac4b636363c2a75fcebe1cfa46dc3b5
Anand Chitipothu <anandology@gmail.com>
2014-12-30 12:28:38 +0530

    Removed references to couch in contact form.
    Earlier we had a home-grown support system based on couchdb. Even though we
    have stopped using that long back, references to that system continued to be
    around. Deleted them in this commit.

--- openlibrary/plugins/openlibrary/support.py
+++ openlibrary/plugins/openlibrary/support.py
@@ -7,26 +7,19 @@ from infogami import config
 from infogami.utils import delegate
 from infogami.utils.view import render_template
 
-from openlibrary.core import support as S
 from openlibrary import accounts
 from openlibrary.core import stats
-
-support_db = None
 
 logger = logging.getLogger("openlibrary")
 
 class contact(delegate.page):
     def GET(self):
-        if not support_db:
-            return "The Openlibrary support system is currently offline. Please try again later."
         i = web.input(path=None)
         user = accounts.get_current_user()
         email = user and user.email
         return render_template("support", email=email, url=i.path)
 
     def POST(self):
-        # if not support_db:
-        #     return "Couldn't initialise connection to support database"
         form = web.input()
         email = form.get("email", "")
         topic = form.get("topic", "")
@@ -40,34 +33,18 @@ class contact(delegate.page):
         default_assignees = config.get("support_default_assignees",{})
         topic_key = str(topic.replace(" ","_").lower())
         if topic_key in default_assignees:
-            # This is set to False to prevent cases from being created
-            # even if there is a designated assignee. This prevents
-            # the database from being updated.
-            create_case = False 
             assignee = default_assignees.get(topic_key)
         else:
-            create_case = False
-            assignee = default_assignees.get("default", "mary@openlibrary.org")
-        if create_case:
-            c = support_db.create_case(creator_name      = user and user.get_name() or "",
-                                       creator_email     = email,
-                                       creator_useragent = useragent,
-                                       creator_username  = user and user.get_username() or "",
-                                       subject           = topic,
-                                       description       = description,
-                                       url               = url,
-                                       assignee          = assignee)
-            stats.increment("support.all")
-        else:
-            stats.increment("support.all")
-            subject = "Support case *%s*"%topic
+            assignee = default_assignees.get("default", "openlibrary@archive.org")
+        stats.increment("ol.support.all")
+        subject = "Support case *%s*"%topic
 
-            url = web.ctx.home + url
-            displayname = user and user.get_name() or ""
-            username = user and user.get_username() or ""
+        url = web.ctx.home + url
+        displayname = user and user.get_name() or ""
+        username = user and user.get_username() or ""
 
-            message = SUPPORT_EMAIL_TEMPLATE % locals()
-            sendmail(email, assignee, subject, message)
+        message = SUPPORT_EMAIL_TEMPLATE % locals()
+        sendmail(email, assignee, subject, message)
         return render_template("email/case_created", assignee)
 
 def sendmail(from_address, to_address, subject, message):
@@ -98,10 +75,5 @@ OL-username: %(username)s
 """
 
 def setup():
-    global support_db
-    try:
-        support_db = S.Support()
-    except S.DatabaseConnectionError:
-        support_db = None
-
+    pass
 
