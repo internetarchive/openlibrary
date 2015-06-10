7dd32f8e4e1f33d8eb9d10710162d2709236122a
Charles Horn <charles.horn@gmail.com>
2015-06-10 23:43:52 +1200

    correct patch result type

--- openlibrary/plugins/upstream/tests/test_forms.py
+++ openlibrary/plugins/upstream/tests/test_forms.py
@@ -12,7 +12,7 @@ class TestRegister:
 
     def test_validate(self, monkeypatch):
         monkeypatch.setattr(forms, 'find_account', lambda **kw: None)
-        monkeypatch.setattr(spamcheck, "get_spam_domains", lambda: {})
+        monkeypatch.setattr(spamcheck, "get_spam_domains", lambda: [])
 
         f = forms.Register()
         d = {
