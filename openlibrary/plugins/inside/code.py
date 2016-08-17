5f1b59d585cba82cc71eb8c60863410708307258
Brenton Cheng <brenton@archive.org>
2016-08-17 16:29:55 -0700

    Clean up Search Inside code, adding defensive checks

--- openlibrary/plugins/inside/code.py
+++ openlibrary/plugins/inside/code.py
@@ -10,43 +10,6 @@ def escape_q(q):
     """Hook for future pre-treatment of search query"""
     return q
 
-def inside_search_select(params):
-    if not hasattr(config, 'plugin_inside'):
-        return {'error': 'Unable to prepare search engine'}
-    search_endpoint = config.plugin_inside['search_endpoint']
-    search_select = search_endpoint + '?' + urllib.urlencode(params)
-
-    # TODO: Update for Elastic
-    # stats.begin("solr", url=search_select)
-
-    try:
-        json_data = urllib2.urlopen(search_select, timeout=30).read()
-        logger = logging.getLogger("openlibrary.inside")
-        logger.debug('URL: ' + search_select)
-    except:
-        return {'error': 'Unable to query search engine'}
-    finally:
-        # TODO: Update for Elastic
-        # stats.end()
-        pass
-
-    try:
-        return simplejson.loads(json_data)
-    except:
-        return {'error': 'Error converting search engine data to JSON'}
-
-@public
-def search_inside_result_count(q):
-    q = escape_q(q)
-    params = {
-        'q': web.urlquote(q)
-    }
-    results = inside_search_select(params)
-    if 'error' in results:
-        return None
-
-    return results['hits']['total']
-
 class search_inside(delegate.page):
     path = '/search/inside'
 
@@ -58,23 +21,48 @@ class search_inside(delegate.page):
             if 'error' in results:
                 return results
 
-            # TODO: This chunk seems unused
-            ekey_doc = {}
-            for doc in results['hits']['hits']:
-                ia = doc['fields']['identifier'][0]
-                q = {'type': '/type/edition', 'ocaid': ia}
-                ekeys = web.ctx.site.things(q)
-                if not ekeys:
-                    del q['ocaid']
-                    q['source_records'] = 'ia:' + ia
-                    ekeys = web.ctx.site.things(q)
-                if ekeys:
-                    ekey_doc[ekeys[0]] = doc
-            editions = web.ctx.site.get_many(ekey_doc.keys())
-            for e in editions:
-                ekey_doc[e['key']]['edition'] = e
+            # TODO: This chunk *seems* like it's not achieving anything -- try removing. If all good,
+            #       can collapse `if 'error' in results` condition above.
+            # ekey_doc = {}
+            # for doc in results['hits']['hits']:
+            #     ia = doc['fields']['identifier'][0]
+            #     q = {'type': '/type/edition', 'ocaid': ia}
+            #     ekeys = web.ctx.site.things(q)
+            #     if not ekeys:
+            #         del q['ocaid']
+            #         q['source_records'] = 'ia:' + ia
+            #         ekeys = web.ctx.site.things(q)
+            #     if ekeys:
+            #         ekey_doc[ekeys[0]] = doc
+            # editions = web.ctx.site.get_many(ekey_doc.keys())
+            # for e in editions:
+            #     ekey_doc[e['key']]['edition'] = e
 
             return results
+
+        def inside_search_select(params):
+            if not hasattr(config, 'plugin_inside'):
+                return {'error': 'Unable to prepare search engine'}
+            search_endpoint = config.plugin_inside['search_endpoint']
+            search_select = search_endpoint + '?' + urllib.urlencode(params)
+
+            # TODO: Update for Elastic
+            # stats.begin("solr", url=search_select)
+
+            try:
+                json_data = urllib2.urlopen(search_select, timeout=30).read()
+                logger = logging.getLogger("openlibrary.inside")
+                logger.debug('URL: ' + search_select)
+            except:
+                return {'error': 'Unable to query search engine'}
+
+            # TODO: Update for Elastic
+            # stats.end()
+
+            try:
+                return simplejson.loads(json_data)
+            except:
+                return {'error': 'Error converting search engine data to JSON'}
 
         def quote_snippet(snippet):
             trans = { '\n': ' ', '{{{': '<b>', '}}}': '</b>', }
@@ -93,7 +81,11 @@ class search_inside(delegate.page):
         def read_from_archive(ia):
             meta_xml = 'http://archive.org/download/' + ia + '/' + ia + '_meta.xml'
             stats.begin("archive.org", url=meta_xml)
-            xml_data = urllib2.urlopen(meta_xml, timeout=5)
+            try:
+                xml_data = urllib2.urlopen(meta_xml, timeout=5)
+            except:
+                stats.end()
+                return {}
             item = {}
             try:
                 tree = etree.parse(xml_data)
@@ -121,44 +113,53 @@ class search_inside(delegate.page):
 class snippets(delegate.page):
     path = '/search/inside/(.+)'
     def GET(self, ia):
-        def find_doc(ia, host, ia_path):
-            abbyy_gz = '_abbyy.gz'
-            files_xml = 'http://%s%s/%s_files.xml' % (host, ia_path, ia)
-            xml_data = urllib2.urlopen(files_xml, timeout=5)
-            for e in etree.parse(xml_data).getroot():
-                if e.attrib['name'].endswith(abbyy_gz):
-                    return e.attrib['name'][:-len(abbyy_gz)]
-
-        def ia_lookup(path):
-            h1 = httplib.HTTPConnection("archive.org")
-
-            for attempt in range(5):
-                h1.request("GET", path)
-                res = h1.getresponse()
-                res.read()
-                if res.status != 200:
-                    break
-            assert res.status == 302
-            new_url = res.getheader('location')
-
-            re_new_url = re.compile('^http://([^/]+\.us\.archive\.org)(/.+)$')
-
-            m = re_new_url.match(new_url)
-            return m.groups()
-
         def find_matches(ia, q):
             q = escape_q(q)
             host, ia_path = ia_lookup('/download/' + ia)
+            if host is None or ia_path is None:
+                return {'error': 'Could not lookup item info'}
             doc = find_doc(ia, host, ia_path) or ia
 
+            # TODO: Does q really need web.urlquote?
             url = 'http://' + host + '/fulltext/inside.php?item_id=' + ia + '&doc=' + doc + '&path=' + ia_path + '&q=' + web.urlquote(q)
-            ret = urllib2.urlopen(url, timeout=5).read().replace('"matches": [],\n}', '"matches": []\n}')
             try:
+                ret = urllib2.urlopen(url, timeout=5).read()
                 return simplejson.loads(ret)
             except:
-                re_h1_error = re.compile('<center><h1>(.+?)</h1></center>')
-                m = re_h1_error.search(ret)
-                # return { 'error': web.htmlunquote(m.group(1)) }
-                return { 'error': 'Error finding matches' }
+                return {'error': 'Error finding matches'}
+
+        def ia_lookup(path):
+            """Excecting a 302 redirect, retrying up to 5 times"""
+            h1 = httplib.HTTPConnection("archive.org")
+            new_url = None
+            for attempt in range(5):
+                try:
+                    h1.request("GET", path)
+                except:
+                    continue
+                res = h1.getresponse()
+                res.read()
+                if res.status == 302:
+                    new_url = res.getheader('location')
+                    break
+            if new_url is None:
+                return (None, None)
+            re_new_url = re.compile('^http://([^/]+\.us\.archive\.org)(/.+)$')
+
+            m = re_new_url.match(new_url)
+            return m.groups() if m else (None, None)
+
+        def find_doc(ia, host, ia_path):
+            abbyy_gz = '_abbyy.gz'
+            files_xml = 'http://%s%s/%s_files.xml' % (host, ia_path, ia)
+            try:
+                xml_data = urllib2.urlopen(files_xml, timeout=5)
+                xml = etree.parse(xml_data)
+            except:
+                return None
+            for e in xml.getroot():
+                if e.attrib['name'].endswith(abbyy_gz):
+                    return e.attrib['name'][:-len(abbyy_gz)]
+            return None
 
         return render_template('search/snippets.tmpl', find_matches, ia)
