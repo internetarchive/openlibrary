30b9c20a2524bee621bed5930d3c5fae828ba22d
Charles Horn <charles.horn@gmail.com>
2017-04-13 00:51:46 +1200

    get addbook test to pass

--- openlibrary/plugins/upstream/tests/test_addbook.py
+++ openlibrary/plugins/upstream/tests/test_addbook.py
@@ -1,12 +1,18 @@
 """py.test tests for addbook"""
 import web
 from .. import addbook
+from openlibrary import accounts
 
 def strip_nones(d):
     return dict((k, v) for k, v in d.items() if v is not None)
 
 class TestSaveBookHelper:
-    def test_authors(self):
+    def get_test_authors(self, monkeypatch):
+        def mock_user():
+            return type('MockUser', (object,), {'is_admin': lambda self: False})()
+
+        monkeypatch.setattr(accounts, "get_current_user", mock_user)
+
         s = addbook.SaveBookHelper(None, None)
         def f(data):
             return strip_nones(s.process_work(web.storage(data)))
