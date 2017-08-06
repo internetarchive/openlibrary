fce79017db21e0f40e9caca344b5b3865f745ed3
Michael E. Karpeles <michael.karpeles@gmail.com>
2017-08-06 22:47:14 +0000

    repairing tests

--- openlibrary/plugins/upstream/tests/test_forms.py
+++ openlibrary/plugins/upstream/tests/test_forms.py
@@ -2,13 +2,6 @@ from .. import forms
 from .. import spamcheck
 
 class TestRegister:
-    def test_leaks(self):
-        f = forms.Register()
-        assert f.displayname.value is None
-        f.displayname.value = 'Foo'
-
-        f = forms.Register()
-        assert f.displayname.value is None
 
     def test_validate(self, monkeypatch):
         monkeypatch.setattr(forms, 'find_account', lambda **kw: None)
@@ -17,10 +10,9 @@ class TestRegister:
 
         f = forms.Register()
         d = {
-            'displayname': 'Foo',
             'username': 'foo',
             'email': 'foo@example.com',
-            'email2': 'foo@example.com',
-            'password': 'foo123'
+            'password': 'foo123',
+            'password2': 'foo123'
         }
         assert f.validates(d)
