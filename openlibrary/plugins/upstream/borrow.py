64d7bb2b49c602f0ea72b11e29f8c2691fca0bb6
Michael E. Karpeles <michael.karpeles@gmail.com>
2017-11-21 06:27:44 +0000

    patching borrow-facing logic

--- openlibrary/plugins/upstream/borrow.py
+++ openlibrary/plugins/upstream/borrow.py
@@ -151,7 +151,7 @@ class borrow(delegate.page):
             if resource_type not in ['epub', 'pdf', 'bookreader']:
                 raise web.seeother(error_redirect)
 
-            user_meets_borrow_criteria = user_can_borrow_edition(user, edition, resource_type)
+            user_meets_borrow_criteria = user_can_borrow_edition(user, edition, resource_type, availability)
 
             if user_meets_borrow_criteria:
                 loan = lending.create_loan(
@@ -781,7 +781,7 @@ def resource_uses_bss(resource_id):
                 return True
     return False
 
-def user_can_borrow_edition(user, edition, _type):
+def user_can_borrow_edition(user, edition, _type, availability=None):
     """Returns True if the user can borrow this edition given their
     current loans.  Returns False if the user holds a current loan
     for the edition.
@@ -792,14 +792,21 @@ def user_can_borrow_edition(user, edition, _type):
     if user.get_loan_count() >= user_max_loans:
         return False
 
-    if edition.get_ia_waitlist_size() > 0:
+    ia_availability = availability or edition.get_ia_availability()
+    availability_status = ia_availability.get('status')
+    waitlist_size = int(ia_availability.get('num_waitlist', 0))
+
+    if waitlist_size > 0:
         # There some people are already waiting for the book,
         # it can't be borrowed unless the user is the first in the waiting list.
         waiting_loan = user.get_waiting_loan_for(edition)
         if not waiting_loan or waiting_loan['status'] != 'available':
             return False
 
-    if _type in [loan['resource_type'] for loan in edition.get_available_loans()]:
+    if availability_status.lower() in ['borrow_available', 'open']:
+        return True
+
+    elif _type in [loan['resource_type'] for loan in edition.get_available_loans()]:
         return True
 
     return False
