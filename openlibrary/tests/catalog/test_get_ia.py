a2ed80380c35f2ee93468223ba5f0f4fee4f6a5f
Charles Horn <charles.horn@gmail.com>
2017-11-22 14:41:36 +1300

    mock ia metadata response for test

--- openlibrary/tests/catalog/test_get_ia.py
+++ openlibrary/tests/catalog/test_get_ia.py
@@ -2,6 +2,7 @@ import os
 import unittest
 from _pytest.monkeypatch import monkeypatch
 from openlibrary.catalog import get_ia
+from openlibrary.core import ia
 from openlibrary.catalog.marc.marc_xml import MarcXml
 from openlibrary.catalog.marc.marc_binary import MarcBinary
 
@@ -15,10 +16,13 @@ class TestGetIA(unittest.TestCase):
     def test_get_marc_record_from_ia(self):
         """Tests the method returning MARC records form IA
         used by the import API. It should return an XML MARC if one exists."""
-        item = "1733mmoiresdel00vill"
+        xml_items = ["1733mmoiresdel00vill",     # no <?xml
+                     "0descriptionofta1682unit"] # has <?xml
         m = monkeypatch()
         m.setattr(get_ia, 'urlopen_keep_trying', return_test_marc_data)
+        m.setattr(ia, 'get_metadata', lambda itemid: {'_filenames': [itemid + "_marc.xml", itemid + "_meta.mrc"]})
 
-        result = get_ia.get_marc_record_from_ia(item)
-        self.assertIsInstance(result, MarcXml)
+        for item in xml_items:
+            result = get_ia.get_marc_record_from_ia(item)
+            self.assertIsInstance(result, MarcXml)
 
