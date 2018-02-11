5191b68d65dbaff931793a9e6335c909402ee158
Charles Horn <charles.horn@gmail.com>
2018-02-12 10:56:46 +1300

    add additional ISBN test

--- openlibrary/utils/tests/test_isbn.py
+++ openlibrary/utils/tests/test_isbn.py
@@ -9,3 +9,8 @@ def test_isbn_10_to_isbn_13():
     assert isbn.isbn_10_to_isbn_13("0-940787-08-3") == "9780940787087"
     assert isbn.isbn_10_to_isbn_13("0940787083") == "9780940787087"
     assert isbn.isbn_10_to_isbn_13("BAD-ISBN") is None
+
+def test_opposite_isbn():
+    assert isbn.opposite_isbn("0-940787-08-3") == "9780940787087"
+    assert isbn.opposite_isbn("978-0-940787-08-7") == "0940787083"
+    assert isbn.opposite_isbn("BAD-ISBN") is None
