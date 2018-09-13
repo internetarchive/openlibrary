fab51a5a6a08ef6d8fbe3e581032bc32bc3fa68c
Alakhator <alakhsethi@gmail.com>
2018-09-13 15:26:57 +0530

    Added Back-To-Top feature while scrolling

---
+++ openlibrary/plugins/openlibrary/js/scroll.js
@@ -0,0 +1,17 @@
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 200) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ 
