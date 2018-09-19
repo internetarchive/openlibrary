2f3e7e508baaa8e5f5eabafa423a828d56bc3879
Alakhator <alakhsethi@gmail.com>
2018-09-19 16:02:47 +0530

    Resolving conflicts

--- openlibrary/plugins/openlibrary/js/scroll.js
+++ openlibrary/plugins/openlibrary/js/scroll.js
@@ -1,17 +1,13 @@
+/*Scroll to top when arrow up clicked BEGIN*/
 $(window).scroll(function() {
     var height = $(window).scrollTop();
-    if (height > 200) {
-        $('#back2Top').fadeIn();
+    if (height > (window.innerHeight*2)) {
+        $('#back2Top').fadeIn();//make the back to top button visible
     } else {
-        $('#back2Top').fadeOut();
+        $('#back2Top').fadeOut();//hide the back to top button  
     }
 });
-$(document).ready(function() {
-    $("#back2Top").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
+$ (function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
         return false;
-    });
-
 });
- 
