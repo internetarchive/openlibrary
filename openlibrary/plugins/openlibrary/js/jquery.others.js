2458dab2369bf059946b0bd5b120992fd4875927
jdlrobson <jdlrobson@gmail.com>
2018-10-22 10:45:13 -0700

    Run eslint autofix to suppress warnings
    Fixes no-extra-semi warnings

--- openlibrary/plugins/openlibrary/js/jquery.others.js
+++ openlibrary/plugins/openlibrary/js/jquery.others.js
@@ -51,4 +51,4 @@ jQuery.fn.toggleText = function(a, b) {
 };
 function bookCovers(){
 $.fn.fixBroken=function(){return this.each(function(){$(this).error(function(){$(this).parent().parent().hide();$(this).parent().parent().next(".SRPCoverBlank").show();});});};
-};
+}
