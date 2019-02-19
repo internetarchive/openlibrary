bd003b0ab493d4ab3d84a838f134b1b3c3757e4b
Drini Cami <cdrini@gmail.com>
2019-02-19 16:45:05 -0500

    [solr-builder] TMP: fix partial dump query 2

--- scripts/solr_builder/sql/prod-partial-dump.sql
+++ scripts/solr_builder/sql/prod-partial-dump.sql
@@ -1,6 +1,6 @@
 COPY (
     WITH modified_things AS (
-        SELECT * FROM thing WHERE "last_modified" >= :'lo_date' AND "type" IN ('/type/author', '/type/book', '/type/work')
+        SELECT * FROM thing WHERE "last_modified" >= :'lo_date' AND "type" IN (58, 52, 17872418)
     )
 
     SELECT "type", "key", "latest_revision", "last_modified", "data" FROM modified_things
