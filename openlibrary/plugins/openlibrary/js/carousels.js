2b5a3da150e0428c976b3a8dbf83cdb0de4e41b2
Tabish Shaikh <2016ucs0014@iitjammu.ac.in>
2019-02-22 20:00:15 +0530

    single item lazy loading added

--- openlibrary/plugins/openlibrary/js/carousels.js
+++ openlibrary/plugins/openlibrary/js/carousels.js
@@ -1,6 +1,6 @@
 // used in templates/covers/add.html
-const Carousel = {
-    add: function(selector, a, b, c, d, e, f) {
+var Carousel = {
+    add: function(selector, a, b, c, d, e, f, key, loadMoreUrl) {
         a = a || 6;
         b = b || 5;
         c = c || 4;
@@ -59,6 +59,29 @@ const Carousel = {
             slidesToScroll: a,
             responsive: responsive_settings
         });
+
+        $('.carousel-'+key).on('afterChange', function(e, slick, cur) {
+            // console.log(slick.$slides.length);
+            // console.log(cur);
+            // console.log(e.currentSlide);
+            // console.log(e.slideCount);
+            if (cur === slick.$slides.length - 1)
+             {
+              //Use loadMoreUrl for loading more items
+              $('.carousel-' + key).slick('slickAdd',
+            '<div class="book carousel__item slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide00" style="width: 128px;"> \
+                <div class="book-cover"> \
+                    <a href="/works/OL2950945W" tabindex="0"> \
+                    <img class="bookcover" width="130" height="200" title="Comet by Carl Sagan" src="//covers.openlibrary.org/b/id/253762-M.jpg"> \
+                    </a> \
+                </div> \
+                <div class="book-cta"><a class="btn primary " href="/books/OL1024614M/x/borrow" data-ol-link-track="subjects" title="Borrow eBook Comet" data-key="subjects" data-ocaid="comet00saga_1" tabindex="0">Borrow eBook</a></div> \
+            </div>', $('.carousel-' + key).slick('slickCurrentSlide')-1);
+              console.log("Load more called");
+            //   cur = cur + 1;
+            //   console.log(cur);
+            }
+          });
     }
 };
 export default Carousel;
