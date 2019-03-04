d14d3a2ce09f51c81e8801b8b9f51ff84244d118
Drini Cami <cdrini@gmail.com>
2019-03-03 23:12:25 -0500

    [solr-builder] update partial dump logic/docs

--- scripts/solr_builder/sql/prod-partial-dump.sql
+++ scripts/solr_builder/sql/prod-partial-dump.sql
@@ -1,8 +1,8 @@
 COPY (
     WITH modified_things AS (
-        SELECT * FROM thing WHERE "last_modified" >= :'lo_date' AND "type" IN (58, 52, 17872418)
+        SELECT * FROM thing WHERE "last_modified" >= :'lo_date'
     )
 
-    SELECT "type", "key", "latest_revision", "last_modified", "data" FROM modified_things
+    SELECT "id", "latest_revision", "data" FROM modified_things
     LEFT JOIN data ON "thing_id" = "id" AND "revision" = "latest_revision"
 ) TO STDOUT;
