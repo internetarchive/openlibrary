30e2c37e3a8534065ba137962d08205c4827e2b1
Charles Horn <charles.horn@gmail.com>
2019-03-06 20:11:53 +1300

    use local_id if importing via API with new param provided

--- openlibrary/plugins/importapi/code.py
+++ openlibrary/plugins/importapi/code.py
@@ -212,9 +212,10 @@ class ia_importapi(importapi):
             actual_length = int(rec.leader()[:MARC_LENGTH_POS])
             edition['source_records'] = 'marc:%s/%s:%s:%d' % (ocaid, filename, offset, actual_length)
 
-            #TODO: Look up URN prefixes to support more sources, extend openlibrary/catalog/marc/sources?
-            if ocaid == 'OpenLibraries-Trent-MARCs':
-                prefix = 'trent'
+            local_id = i.get('local_id')
+            if local_id:
+                local_id_type = web.ctx.site.get('/local_ids/' + local_id)
+                prefix = local_id_type.urn_prefix
                 edition['local_id'] = ['urn:%s:%s' % (prefix, _id) for _id in rec.get_fields('001')]
 
             result = add_book.load(edition)
