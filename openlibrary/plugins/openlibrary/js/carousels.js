30073af48f450867245d50702bbbe984b61f1e47
Michael E. Karpeles <michael.karpeles@gmail.com>
2019-03-13 03:29:13 +0000

    broken integration, subjects api infinite load

--- openlibrary/plugins/openlibrary/js/carousels.js
+++ openlibrary/plugins/openlibrary/js/carousels.js
@@ -1,4 +1,5 @@
 // used in templates/covers/add.html
+var slicker;
 var Carousel = {
     add: function(selector, a, b, c, d, e, f, key, loadMoreUrl) {
         a = a || 6;
@@ -60,28 +61,45 @@ var Carousel = {
             responsive: responsive_settings
         });
 
+        var addWork = function(work) {
+            console.log(work);
+            return '<div class="book carousel__item slick-slide slick-active" aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide00" style="width: 128px;">' +
+                '<div class="book-cover">' +
+                  '<a href="' + work.key + '" tabindex="0">' +
+                    '<img class="bookcover" width="130" height="200" title="' + work.title + '" ' +
+                         'src="//covers.openlibrary.org/b/id/' + work.cover_id + '-M.jpg">' +
+                  '</a>' +
+                '</div>' +
+                '<div class="book-cta">' + // needs to be computed based on availability
+                  '<a class="btn primary " href="/books/OL1024614M/x/borrow" data-ol-link-track="subjects" ' +
+                  'title="Borrow eBook Comet" data-key="subjects" data-ocaid="comet00saga_1" tabindex="0">Borrow</a>' +
+                '</div>' +
+              '</div>';
+        }
+
         $('.carousel-'+key).on('afterChange', function(e, slick, cur) {
-            // console.log(slick.$slides.length);
-            // console.log(cur);
-            // console.log(e.currentSlide);
-            // console.log(e.slideCount);
-            if (cur === slick.$slides.length - 1)
-             {
-              //Use loadMoreUrl for loading more items
-              $('.carousel-' + key).slick('slickAdd',
-            '<div class="book carousel__item slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide00" style="width: 128px;"> \
-                <div class="book-cover"> \
-                    <a href="/works/OL2950945W" tabindex="0"> \
-                    <img class="bookcover" width="130" height="200" title="Comet by Carl Sagan" src="//covers.openlibrary.org/b/id/253762-M.jpg"> \
-                    </a> \
-                </div> \
-                <div class="book-cta"><a class="btn primary " href="/books/OL1024614M/x/borrow" data-ol-link-track="subjects" title="Borrow eBook Comet" data-key="subjects" data-ocaid="comet00saga_1" tabindex="0">Borrow eBook</a></div> \
-            </div>', $('.carousel-' + key).slick('slickCurrentSlide')-1);
-              console.log("Load more called");
-            //   cur = cur + 1;
-            //   console.log(cur);
+            var limit = slick.originalSettings.slidesToScroll;
+            var offset = slick.slickCurrentSlide() + 1;
+
+            var currentSlide = $('.slick-slider').slick('slickCurrentSlide');
+            var currentSlideNumber = currentSlide + limit;
+            var totalSlides = $('.slick-slider').slick("getSlick").$slides.length;
+
+            if (currentSlideNumber >= totalSlides) {
+                $.ajax({
+                    'url': loadMoreUrl + '?offset=' + offset + '&limit=' + limit,
+                    'type': 'GET',
+                    success: function(subject_results) {
+                        $.each(subject_results.works, function(work_idx) {
+                            var work = subject_results.works[work_idx];
+                            console.log(work);
+                            var lastSlidePos = $('.slick-slider').slick("getSlick").$slides.length - 1;
+                            $('.carousel-' + key).slick('slickAdd', addWork(work), lastSlidePos);
+                        });
+                    }
+                });
             }
-          });
+        });
     }
 };
 export default Carousel;
