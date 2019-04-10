9edf4c8dc4f1c27b483a31a8eb80ded3d0f70b1a
Charles Horn <charles.horn@gmail.com>
2019-04-10 13:28:08 +1200

    handle case if isbn_13 is None

--- openlibrary/plugins/openlibrary/api.py
+++ openlibrary/plugins/openlibrary/api.py
@@ -245,7 +245,7 @@ class price_api(delegate.page):
         if id_type == 'isbn_10' and \
            metadata['betterworldbooks'].get('price') is None:
             isbn_13 = isbn_10_to_isbn_13(id_)
-            metadata['betterworldbooks'] = get_betterworldbooks_metadata(
+            metadata['betterworldbooks'] = isbn_13 and get_betterworldbooks_metadata(
                 isbn_13) or {}
 
         # fetch book by isbn if it exists
