3aa588433f0cc5c17acb3a830015982b42fb271d
Drini Cami <cdrini@gmail.com>
2019-05-10 14:23:25 -0400

    [solr-builder] fix multiple postgres import at shard boundary

--- scripts/solr_builder/psql-import-chunk.sh
+++ scripts/solr_builder/psql-import-chunk.sh
@@ -5,7 +5,7 @@ OFFSET="$2"
 COUNT="$3"
 
 time zcat "${DUMP}" | \
-tail -n +$OFFSET | \
+tail -n +$((OFFSET+1)) | \
 head -n $COUNT | \
 sed --expression='s/\\u0000//g' | \
 psql -d postgres --user=postgres -c  "COPY test FROM STDIN with delimiter E'\t' escape '\' quote E'\b' csv" &> logs/psql-chunk-$OFFSET.txt
