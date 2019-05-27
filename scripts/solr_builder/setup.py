997a8d2664acac77cc53e9e98ab48617461458d3
Drini Cami <cdrini@gmail.com>
2019-05-27 19:13:38 -0400

    [solr-builder] move files around + more cython

--- scripts/solr_builder/setup.py
+++ scripts/solr_builder/setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("solr_builder_main.py")
+    ext_modules=cythonize("solr_builder/solr_builder_main.py")
 )
