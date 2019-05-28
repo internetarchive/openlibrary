916cde609fe4de85319eb3ccbc7c8ce73ea95644
Drini Cami <cdrini@gmail.com>
2019-05-28 15:27:00 -0400

    Create readonly postgres user for solrupdater (#2150)
    * [fix/solr-updater-user] add --debugger opt to solrupdater
    * [fix/solr-updater-user] small preparatory fixes
    * [fix/solr-updater-user] create postgres readonly user for solrupdater

--- scripts/new-solr-updater.py
+++ scripts/new-solr-updater.py
@@ -6,8 +6,6 @@ Changes:
 2013-02-25: First version
 2018-02-11: Use newer config method
 """
-from __future__ import print_function
-
 import _init_path
 
 import yaml
@@ -33,9 +31,11 @@ LOAD_IA_SCANS = False
 COMMIT = True
 args = {}
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config')
+    parser.add_argument('--debugger', action="store_true", help="Wait for a debugger to attach before beginning.")
     parser.add_argument('--state-file', default="solr-update.state")
     parser.add_argument('--ol-url', default="http://openlibrary.org/")
     parser.add_argument('--socket-timeout', type=int, default=10)
@@ -214,7 +214,17 @@ class Solr:
         update_work.solr_update(['<commit/>'])
         logger.info("END commit")
 
+
 def process_args(args):
+    if args.debugger:
+        import ptvsd
+
+        logger.info("Enabling debugger attachment (attach if it hangs here)")
+        ptvsd.enable_attach(address=('0.0.0.0', 3000))
+        logger.info("Waiting for debugger to attach...")
+        ptvsd.wait_for_attach()
+        logger.info("Debugger attached to port 3000")
+
     # Sometimes archive.org requests blocks forever.
     # Setting a timeout will make the request fail instead of waiting forever.
     socket.setdefaulttimeout(args.socket_timeout)
@@ -225,6 +235,7 @@ def process_args(args):
     global LOAD_IA_SCANS, COMMIT
     LOAD_IA_SCANS = args.load_ia_scans
     COMMIT = args.commit
+
 
 def main():
     global args
@@ -241,7 +252,7 @@ def main():
         host = web.lstrips(args.ol_url, "http://").strip("/")
         update_work.set_query_host(host)
 
-    print(str(args))
+    logger.info(str(args))
     logger.info("loading config from %s", args.config)
     load_config(args.config)
 
@@ -274,6 +285,7 @@ def main():
         if count == 0:
             logger.debug("No more log records available, sleeping...")
             time.sleep(5)
+
 
 if __name__ == "__main__":
     main()
