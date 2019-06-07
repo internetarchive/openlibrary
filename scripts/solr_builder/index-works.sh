3fb94e970f2a3760d62cade9a67bc1cefa9041bd
Drini Cami <cdrini@gmail.com>
2019-06-07 00:42:54 -0400

    [solr-builder] move bash in README to bash files

---
+++ scripts/solr_builder/index-works.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Convenience aliases/functions
+source aliases.sh
+
+set -o xtrace  # Display each line before running it
+
+WORKS_COUNT=$(time psql -c "SELECT count(*) FROM test WHERE \"Type\" = '/type/work'") # ~10min
+WORKS_INSTANCES=5
+WORKS_CHUNK_SIZE=$(pymath "ceil($WORKS_COUNT / $WORKS_INSTANCES)")
+
+# Partitions the database (~33s)
+WORKS_PARTITIONS=$(time psql -c "SELECT \"Key\" FROM test_get_partition_markers('/type/work', $WORKS_CHUNK_SIZE);")
+for key in $WORKS_PARTITIONS; do
+  RUN_SIG=works_${key//\//}_`date +%Y-%m-%d_%H-%M-%S`
+  docker_solr_builder works --start-at $key --limit $WORKS_CHUNK_SIZE -p progress/$RUN_SIG.txt
+  echo sleep 60 | tee /dev/tty | bash;
+done;
