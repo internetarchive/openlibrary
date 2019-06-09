2cc446c3d2d5edcba19a2eec3db6e9299b4cb171
Drini Cami <cdrini@gmail.com>
2019-06-09 08:14:19 -0400

    [solr-builder] solrbuilder log file opt

--- scripts/solr_builder/index-works.sh
+++ scripts/solr_builder/index-works.sh
@@ -14,6 +14,6 @@ WORKS_CHUNK_SIZE=$(pymath "ceil($WORKS_COUNT / $WORKS_INSTANCES)")
 WORKS_PARTITIONS=$(time psql -c "SELECT \"Key\" FROM test_get_partition_markers('/type/work', $WORKS_CHUNK_SIZE);")
 for key in $WORKS_PARTITIONS; do
   RUN_SIG=works_${key//\//}_`date +%Y-%m-%d_%H-%M-%S`
-  docker_solr_builder works --start-at $key --limit $WORKS_CHUNK_SIZE -p progress/$RUN_SIG.txt
+  docker_solr_builder works --start-at $key --limit $WORKS_CHUNK_SIZE -p progress/$RUN_SIG.txt -l logs/$RUN_SIG.txt
   echo sleep 60 | tee /dev/tty | bash;
 done;
