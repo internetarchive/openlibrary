8bd8c6dbbc154dd1f1b4e9b3c339ebc6e8ed6934
Tabish Shaikh <2016ucs0014@iitjammu.ac.in>
2019-07-16 01:48:41 +0200

    My sponsorship page added  (#2216)
    * fix for unintended list-style showing up
    
    * package.json updated to pass make tests
    
    * fake civi api created
    
    * civi api called and data passed to account/books
    
    * my-sponsorships nav and page created
    
    * corrected len of sponsorships

---
+++ openlibrary/core/sponsorships.py
@@ -0,0 +1,19 @@
+import requests
+from infogami import config
+
+CIVI_SPONSOR_API = '%s/internal/fake/civicrm' % config.get('servername', 'http://localhost')
+
+def get_all_sponsors():
+    """
+    Query civi for a list of all sponsorships, for all users.
+    These results will have to be summed by user for the
+    leader-board and then cached
+    """
+    pass  # todo, later
+
+def get_sponsored_editions(archive_username, limit=50, offset=0):
+    """
+    Gets a list of books from the civi API which internet archive 
+    @archive_username has sponsored
+    """
+    return requests.get(CIVI_SPONSOR_API).json()["works"]
