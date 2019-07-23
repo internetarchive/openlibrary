fa0e3752472456ea0475d7ccdf5a29039c6d3aed
Tom Morris <tfmorris@gmail.com>
2019-07-22 20:03:45 -0400

    Remove unused count. Turn on debug logging

--- scripts/solr_builder/index-orphans.sh
+++ scripts/solr_builder/index-orphans.sh
@@ -6,6 +6,6 @@ source aliases.sh
 
 set -o xtrace  # Display each line before running it
 
-ORPHANS_COUNT=$(time psql -f sql/count-orphans.sql) # ~15min
+#ORPHANS_COUNT=$(time psql -f sql/count-orphans.sql) # ~15min (~2 min on modern laptop - 3687292 for 2019-06-30)
 RUN_SIG=orphans_`date +%Y-%m-%d_%H-%M-%S`
-docker_solr_builder orphans -p progress/$RUN_SIG.txt -l logs/$RUN_SIG.txt
+docker_solr_builder orphans --log-level DEBUG -p progress/$RUN_SIG.txt -l logs/$RUN_SIG.txt
