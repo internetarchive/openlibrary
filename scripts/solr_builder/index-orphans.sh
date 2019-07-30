f83b33988b30bedaad33747c5c9ed6f5c602b481
Drini Cami <cdrini@gmail.com>
2019-07-30 03:19:55 -0400

    [solr-builder/jenkins] consolidate index-works and index-authors and simplify log filenames

--- scripts/solr_builder/index-orphans.sh
+++ scripts/solr_builder/index-orphans.sh
@@ -7,5 +7,5 @@ source aliases.sh
 set -o xtrace  # Display each line before running it
 
 ORPHANS_COUNT=$(time psql -f sql/count-orphans.sql) # ~15min
-RUN_SIG=orphans_`date +%Y-%m-%d_%H-%M-%S`
+RUN_SIG=orphans
 docker_solr_builder orphans -p progress/$RUN_SIG.txt -l logs/$RUN_SIG.txt
---
+++ scripts/solr_builder/index-type.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Convenience aliases/functions
+shopt -s expand_aliases
+source aliases.sh
+
+# Display each line before running it
+set -o xtrace
+
+# One of 'work' or 'author'
+TYPE="$1"
+INSTANCES="$2"
+COUNT=$(psql -c "SELECT count(*) FROM test WHERE \"Type\" = '${TYPE}'")
+CHUNK_SIZE=$(pymath "ceil($COUNT / $INSTANCES)")
+
+# Partitions the database (~35s)
+PARTITION=$(time psql -c "SELECT \"Key\" FROM test_get_partition_markers('/type/${TYPE}', $CHUNK_SIZE);")
+for key in $PARTITION; do
+  OLID=$(echo "${key}" | grep -oe 'OL[0-9]*[MWA]')
+  RUN_SIG="${TYPE}s_${OLID}"
+  docker_solr_builder "${TYPE}s" --start-at $key --limit $CHUNK_SIZE -p progress/$RUN_SIG.txt -l logs/$RUN_SIG.txt
+  sleep 60
+done;
