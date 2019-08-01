4b4f8a9c5477e211b3c438a4f50c6ee33cf3aca4
Tapasweni Pathak <tapaswenipathak@gmail.com>
2019-08-02 00:38:56 +0530

    Add revert access for librarians (#2228)
    * Add revert access for librarians
    * Fix checks for is_librarian() and add users check
    * Add user.is_admin() and remove use of is_admin

--- openlibrary/plugins/upstream/code.py
+++ openlibrary/plugins/upstream/code.py
@@ -190,8 +190,7 @@ class revert(delegate.mode):
             raise web.seeother(web.changequery({}))
 
         user = accounts.get_current_user()
-        is_admin = user and user.key in [m.key for m in web.ctx.site.get('/usergroup/admin').members]
-        if not (is_admin and web.ctx.site.can_write(key)):
+        if not (user and (user.is_admin() or user.is_librarian()) and web.ctx.site.can_write(key)):
             return render.permission_denied(web.ctx.fullpath, "Permission denied to edit " + key + ".")
 
         thing = web.ctx.site.get(key, i.v)
