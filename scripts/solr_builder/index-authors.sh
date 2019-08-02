069c10115a0674e28613ea337bc3535604425c26
Tom Morris <tfmorris@gmail.com>
2019-08-01 23:56:36 -0400

    Update time estimate & disable subprocess spacing

--- scripts/solr_builder/index-authors.sh
+++ scripts/solr_builder/index-authors.sh
@@ -6,7 +6,7 @@ source aliases.sh
 
 set -o xtrace  # Display each line before running it
 
-AUTHOR_COUNT=$(time psql -c "SELECT count(*) FROM entity WHERE etype = CAST('/type/author' AS type_enum)") # ~25s
+AUTHOR_COUNT=$(time psql -c "SELECT count(*) FROM entity WHERE etype = CAST('/type/author' AS type_enum)") # ~2s
 AUTHOR_COUNT=${AUTHOR_COUNT//$'\r'/}
 AUTHOR_INSTANCES=6
 AUTHORS_CHUNK_SIZE=$(pymath "ceil($AUTHOR_COUNT / $AUTHOR_INSTANCES)")
@@ -17,5 +17,4 @@ for key in $AUTHORS_PARTITIONS; do
   key=${key//$'\r'/} # get rid of embedded returns
   RUN_SIG=works_${key//\//}_`date +%Y-%m-%d_%H-%M-%S`
   docker_solr_builder authors --ol-config ../../conf/openlibrary-docker.yml --log-level DEBUG --start-at $key --limit $AUTHORS_CHUNK_SIZE -p progress/$RUN_SIG.txt -l logs/$RUN_SIG.txt
-  echo sleep 60 | tee /dev/tty | bash;
 done;
