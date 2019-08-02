cfc52bcd7cc23e9c9967f0c3ab28927f8bfb8441
jdlrobson <jdlrobson@gmail.com>
2019-08-02 15:52:02 -0700

    Revert "Add revert access for librarians (#2228)"
    This reverts commit 4b4f8a9c5477e211b3c438a4f50c6ee33cf3aca4.
    The commit makes references to undeclared variables and should
    never have been merged.
    
    Fixes: #2261

--- openlibrary/plugins/upstream/code.py
+++ openlibrary/plugins/upstream/code.py
@@ -190,7 +190,8 @@ class revert(delegate.mode):
             raise web.seeother(web.changequery({}))
 
         user = accounts.get_current_user()
-        if not (user and (user.is_admin() or user.is_librarian()) and web.ctx.site.can_write(key)):
+        is_admin = user and user.key in [m.key for m in web.ctx.site.get('/usergroup/admin').members]
+        if not (is_admin and web.ctx.site.can_write(key)):
             return render.permission_denied(web.ctx.fullpath, "Permission denied to edit " + key + ".")
 
         thing = web.ctx.site.get(key, i.v)
