f35588c3309859ec768834dfb57ca574a807c879
Drini Cami <cdrini@gmail.com>
2019-08-05 18:45:48 -0400

    [solr-builder/jenkins] index-type uses iteration in run signature Previously log files would be created with names like "works_OL123W.txt", but that was annoying because it wouldn't be sorted correctly (and it was hard to remember). Now they're named "works_1.txt", etc.

--- scripts/solr_builder/index-type.sh
+++ scripts/solr_builder/index-type.sh
@@ -16,9 +16,10 @@ CHUNK_SIZE=$(pymath "ceil($COUNT / $INSTANCES)")
 
 # Partitions the database (~35s)
 PARTITION=$(time psql -c "SELECT \"Key\" FROM test_get_partition_markers('${DB_TYPE}', $CHUNK_SIZE);")
+ITER=1
 for key in $PARTITION; do
-  OLID=$(echo "${key}" | grep -oe 'OL[0-9]*[MWA]')
-  RUN_SIG="${TYPE}s_${OLID}"
+  RUN_SIG="${TYPE}s_${ITER}"
   docker_solr_builder "${TYPE}s" --start-at $key --limit $CHUNK_SIZE -p progress/$RUN_SIG.txt -l logs/$RUN_SIG.txt
+  ((ITER++))
   sleep 60
 done;
