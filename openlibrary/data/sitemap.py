56041b3a035a6e06d169ae4ce1dc0f0ccc12bc51
Christian Clauss <cclauss@me.com>
2019-08-13 23:02:24 +0200

    Remove the unused find_path()

--- openlibrary/data/sitemap.py
+++ openlibrary/data/sitemap.py
@@ -69,15 +69,6 @@ $for path, title in docs:
 </ul>
 """)
 
-def find_path(key, type, json):
-    if type in ['/type/edition', '/type/work']:
-        data = simplejson.loads(json)
-        return key + '/' + urlsafe(data.get('title', 'untitled'))
-    elif type == '/type/author':
-        data = simplejson.loads(json)
-        return key + '/' + urlsafe(data.get('name', 'unnamed'))
-    else:
-        return key
 
 def gzwrite(path, data):
     f = gzopen(path, 'w')
