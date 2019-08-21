08ca98fb74531a67eb585e620ca2d3f765114729
Michael E. Karpeles <michael.karpeles@gmail.com>
2019-08-20 22:17:49 -0700

    book sponsorship eligibility check (#2328)
    - calls out to 3rd party library for eligibility check, gracefully falls bacl
    - enables sponsorship when not logged in, when ?sponsorship=true params present
    - adds a /sponsorship/eligibility/{isbn} json endpoint for archive.org/donate
    - DRYs archive.org url, moves to config

--- openlibrary/core/sponsorships.py
+++ openlibrary/core/sponsorships.py
@@ -1,18 +1,32 @@
 import json
 import urllib
 import requests
+import web
 from infogami import config
 from infogami.utils.view import public
 from openlibrary import accounts
-from openlibrary.core.lending import get_work_availability
-from openlibrary.core.vendors import get_betterworldbooks_metadata
+from openlibrary.core import models
+from openlibrary.core.lending import get_work_availability, config_ia_domain
+from openlibrary.core.vendors import (
+    get_betterworldbooks_metadata, create_edition_from_amazon_metadata)
 from openlibrary.accounts import get_internet_archive_id
 from openlibrary.core.lending import config_ia_civicrm_api
+from openlibrary.utils.isbn import to_isbn_13
+
+try:
+    from booklending_utils.sponsorship import eligibility_check
+except ImportError:
+    def eligibility_check(edition):
+        """For testing if Internet Archive book sponsorship check unavailable"""
+        work = edition.works[0]
+        authors = [w.author.name for w in work.authors]
+        if authors:
+            return True
 
 CIVI_ISBN = 'custom_52'
 CIVI_USERNAME = 'custom_51'
 CIVI_CONTEXT = 'custom_53'
-PRICE_LIMIT = 50.00
+PRICE_LIMIT_CENTS = 5000
 
 
 def get_sponsored_editions(user):
@@ -78,27 +92,67 @@ def get_sponsorships_by_contact_id(contact_id, isbn=None):
     } for t in txs]
 
 
+def isbn_qualifies_for_sponsorship(isbn):
+    """Checks possible isbn10 + isbn13 variations to"""
+    edition = models.Edition.get_by_isbn(isbn)
+    if edition:
+        return qualifies_for_sponsorship(edition)
+
 @public
 def qualifies_for_sponsorship(edition):
-    # User must be logged in and in /usergroup/sponsors list
+    # defaults
+    dwhi = None
+    eligibility = False
+    price = None
+
     work = edition.works and edition.works[0]
+    edition.isbn13 = to_isbn_13(edition.isbn_13 and edition.isbn_13[0] or
+                              edition.isbn_10 and edition.isbn_10[0])
     req_fields = all(edition.get(x) for x in [
-        'publishers', 'title', 'publish_date', 'covers', 'number_of_pages'
+        'publishers', 'title', 'publish_date', 'covers',
+        'number_of_pages', 'isbn13'
     ])
-    isbn = (edition.isbn_13 and edition.isbn_13[0] or
-             edition.isbn_10 and edition.isbn_10[0])
-    if work and req_fields and isbn:
-        work_id = work.key.split("/")[-1]
-        num_pages = int(edition.get('number_of_pages'))
-        availability = get_work_availability(work_id)
-        dwwi = availability.get(work_id, {}).get('status', 'error') == 'error'
-        if dwwi:
-            bwb_price = get_betterworldbooks_metadata(isbn).get('price_amt')
-            if bwb_price:
-                scan_price = 3.0 + (.12 * num_pages)
-                total_price = scan_price + float(bwb_price)
-                return total_price <= PRICE_LIMIT
-    return False
+    if not (work and req_fields and edition.isbn13):
+        return {
+            'do_we_have_it': dwhi,
+            'is_eligibile': eligibility,
+            'price': price,
+            'error': ("Open Library is missing book metadata "
+                      "necessary for sponsorship"),
+        }
+
+    work_id = work.key.split("/")[-1]
+    num_pages = int(edition.get('number_of_pages'))
+    availability = get_work_availability(work_id)
+    dwhi = (availability.get(work_id, {}).get('status', 'error') != 'error') and availability
+    if not dwhi:
+        bwb_price = get_betterworldbooks_metadata(
+            edition.isbn13).get('price_amt')
+        if bwb_price:
+            SETUP_COST_CENTS = 300
+            PAGE_COST_CENTS = 12
+            scan_price_cents = SETUP_COST_CENTS + (PAGE_COST_CENTS * num_pages)
+            book_cost_cents = int(float(bwb_price) * 100)
+            total_price_cents = scan_price_cents + book_cost_cents
+            price = {
+                'book_cost_cents': book_cost_cents,
+                'scan_price_cents': scan_price_cents,
+                'total_price_cents': total_price_cents
+            }
+            if total_price_cents <= PRICE_LIMIT_CENTS:
+                eligibility = eligibility_check(edition)
+    params = {
+        'campaign': 'pilot',
+        'type': 'sponsorship',
+        'context': 'ol',
+        'isbn': edition.isbn13
+    }
+    return {
+        'is_eligible': eligibility,
+        'do_we_have_it': dwhi,
+        'price': price,
+        'url': config_ia_domain + '/donate?' + urllib.urlencode(params)
+    }
 
 
 def get_all_sponsors():
