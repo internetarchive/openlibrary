dabd7b8c0c42e3ac2700779da9f303a6344073f6
Charles Horn <charles.horn@gmail.com>
2019-09-07 13:12:15 +1200

    Experimental amazon lookup API and existing API refactor (#2342)
    * new experimental api endpoint
    * split out product serialization
    * handle older dates, strformat has issues with pre 1900 dates
    * privileged experimental endpoint
    * handle no results from amazon
    * expose more offers data that is already retrieved
    * explicit check for not None
    * fix up indentation
    * document new and existing vendor API code
    * rename method and inline kwargs
    * use amazon terminology for Product Advertising API 'operation'

--- openlibrary/core/vendors.py
+++ openlibrary/core/vendors.py
@@ -2,6 +2,7 @@ import re
 import web
 import urllib2
 import simplejson
+from amazon.api import SearchException
 from infogami import config
 from infogami.utils.view import public
 from . import lending, cache, helpers as h
@@ -11,35 +12,58 @@ from openlibrary.utils.isbn import (
 from openlibrary.catalog.add_book import load
 from openlibrary import accounts
 
+
 BETTERWORLDBOOKS_API_URL = 'http://products.betterworldbooks.com/service.aspx?ItemId='
+
 
 @public
 def get_amazon_metadata(id_, id_type='isbn'):
+    """Main interface to Amazon LookupItem API. Will cache results.
+
+    :param str id_: The item id: isbn (10/13), or Amazon ASIN.
+    :param str id_type: 'isbn' or 'asin'.
+    :return: A single book item's metadata, or None.
+    :rtype: dict or None
+    """
+
     try:
         if id_:
             return cached_get_amazon_metadata(id_, id_type=id_type)
     except Exception:
         return None
 
-def _get_amazon_metadata(id_=None, id_type='isbn'):
-    # TODO: extend this to work with
-    # isbn=, asin=, title=, authors=, etc
-    kwargs = {}
-    if id_type == 'isbn':
-        id_ = normalize_isbn(id_)
-        kwargs = {'SearchIndex': 'Books', 'IdType': 'ISBN'}
-    kwargs['ItemId'] = id_
-    try:
-        if not lending.amazon_api:
-            raise Exception
-        product = lending.amazon_api.lookup(**kwargs)
-        # sometimes more than one product can be returned, choose first
-        if isinstance(product, list):
-            product = product[0]
-    except Exception as e:
-        return None
 
-    price_fmt, price, qlt = (None, None, None)
+def search_amazon(title='', author=''):
+    """Uses the Amazon Product Advertising API ItemSearch operation to search for
+    books by author and/or title.
+    https://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemSearch.html
+
+    :param str title: title of book to search for.
+    :param str author: author name of book to search for.
+    :return: dict of "results", a list of one or more found books, with metadata.
+    :rtype: dict
+    """
+
+    results = lending.amazon_api.search(Title=title, Author=author, SearchIndex='Books')
+    data = {'results': []}
+    try:
+        for product in results:
+            data['results'].append(_serialize_amazon_product(product))
+    except SearchException:
+        data = {'error': 'no results'}
+    return data
+
+
+def _serialize_amazon_product(product):
+    """Takes a full Amazon product Advertising API returned AmazonProduct
+    with multiple ResponseGroups, and extracts the data we are interested in.
+
+    :param amazon.api.AmazonProduct product:
+    :return: Amazon metadata for one product
+    :rtype: dict
+    """
+
+    price_fmt = price = qlt = None
     used = product._safe_get_element_text('OfferSummary.LowestUsedPrice.Amount')
     new = product._safe_get_element_text('OfferSummary.LowestNewPrice.Amount')
 
@@ -57,7 +81,7 @@ def _get_amazon_metadata(id_=None, id_type='isbn'):
 
     data = {
         'url': "https://www.amazon.com/dp/%s/?tag=%s" % (
-            id_, h.affiliate_id('amazon')),
+            product.asin, h.affiliate_id('amazon')),
         'price': price_fmt,
         'price_amt': price,
         'qlt': qlt,
@@ -65,13 +89,31 @@ def _get_amazon_metadata(id_=None, id_type='isbn'):
         'authors': [{'name': name} for name in product.authors],
         'source_records': ['amazon:%s' % product.asin],
         'number_of_pages': product.pages,
-        'languages': list(product.languages),  # needs to be normalized
+        'languages': list(product.languages),
         'cover': product.large_image_url,
         'product_group': product.product_group,
     }
+    if product._safe_get_element('OfferSummary') is not None:
+        data['offer_summary'] = {
+            'total_new': int(product._safe_get_element_text('OfferSummary.TotalNew')),
+            'total_used': int(product._safe_get_element_text('OfferSummary.TotalUsed')),
+            'total_collectible': int(product._safe_get_element_text('OfferSummary.TotalCollectible')),
+        }
+        collectible = product._safe_get_element_text('OfferSummary.LowestCollectiblePrice.Amount')
+        if new:
+            data['offer_summary']['lowest_new'] = int(new)
+        if used:
+            data['offer_summary']['lowest_used'] = int(used)
+        if collectible:
+            data['offer_summary']['lowest_collectible'] = int(collectible)
+        amazon_offers = product._safe_get_element_text('Offers.TotalOffers')
+        if amazon_offers:
+            data['offer_summary']['amazon_offers'] = int(amazon_offers)
+
     if product.publication_date:
         # TODO: Don't populate false month and day for older products
-        data['publish_date'] = product.publication_date.strftime('%b %d, %Y')
+        data['publish_date'] = (product.publication_date.strftime('%b %d, %Y') if product.publication_date.year > 1900
+                               else str(product.publication_date.year))
     if product.binding:
         data['physical_format'] = product.binding.lower()
     if product.edition:
@@ -89,11 +131,47 @@ def _get_amazon_metadata(id_=None, id_type='isbn'):
                 data['isbn_10'] = [isbn_13_to_isbn_10(isbn)]
     return data
 
+
+def _get_amazon_metadata(id_=None, id_type='isbn'):
+    """Uses the Amazon Product Advertising API ItemLookup operation to locatate a
+    specific book by identifier; either 'isbn' or 'asin'.
+    https://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemLookup.html
+
+    :param str id_: The item id: isbn (10/13), or Amazon ASIN.
+    :param str id_type: 'isbn' or 'asin'.
+    :return: A single book item's metadata, or None.
+    :rtype: dict or None
+    """
+
+    kwargs = {}
+    if id_type == 'isbn':
+        id_ = normalize_isbn(id_)
+        kwargs = {'SearchIndex': 'Books', 'IdType': 'ISBN'}
+    kwargs['ItemId'] = id_
+    kwargs['MerchantId'] = 'Amazon'  # Only affects Offers Response Group, does Amazon sell this directly?
+    try:
+        if not lending.amazon_api:
+            raise Exception
+        product = lending.amazon_api.lookup(**kwargs)
+        # sometimes more than one product can be returned, choose first
+        if isinstance(product, list):
+            product = product[0]
+    except Exception as e:
+        return None
+
+    return _serialize_amazon_product(product)
+
+
 def clean_amazon_metadata_for_load(metadata):
     """This is a bootstrapping helper method which enables us to take the
-    results of plugins.upstream.code.get_amazon_metadata and create an
-    OL book catalog record
+    results of get_amazon_metadata() and create an
+    OL book catalog record.
+
+    :param dict metadata: Metadata representing an Amazon product.
+    :return: A dict representing a book suitable for importing into OL.
+    :rtype: dict
     """
+
     # TODO: convert languages into /type/language list
     conforming_fields = [
         'title', 'authors', 'publish_date', 'source_records',
@@ -109,11 +187,18 @@ def clean_amazon_metadata_for_load(metadata):
         conforming_metadata['identifiers'] = {'amazon': [asin]}
     return conforming_metadata
 
+
 def create_edition_from_amazon_metadata(id_, id_type='isbn'):
-    """Fetches amazon metadata by isbn from affiliates API, attempts to
-    create OL edition from metadata, and returns the resulting edition key
-    `/key/OL..M` if successful or None otherwise
+    """Fetches Amazon metadata by id from Amazon Product Advertising API, attempts to
+    create OL edition from metadata, and returns the resulting edition
+    key `/key/OL..M` if successful or None otherwise.
+
+    :param str id_: The item id: isbn (10/13), or Amazon ASIN.
+    :param str id_type: 'isbn' or 'asin'.
+    :return: Edition key '/key/OL..M' or None
+    :rtype: str or None
     """
+
     md = get_amazon_metadata(id_, id_type=id_type)
     if md and md.get('product_group') == 'Book':
         # Save token of currently logged in user (or no-user)
@@ -136,14 +221,6 @@ def create_edition_from_amazon_metadata(id_, id_type='isbn'):
         if reply and reply.get('success'):
             return reply['edition'].get('key')
 
-@public
-def get_betterworldbooks_metadata(isbn):
-    isbn = normalize_isbn(isbn)
-    try:
-        if isbn:
-            return _get_betterworldbooks_metadata(isbn)
-    except Exception:
-        return {}
 
 def cached_get_amazon_metadata(*args, **kwargs):
     """If the cached data is `None`, likely a 503 throttling occurred on
@@ -154,6 +231,7 @@ def cached_get_amazon_metadata(*args, **kwargs):
     be cached as to not trigger a re-cache (only the value `None`
     will cause re-cache)
     """
+
     # fetch/compose a cache controller obj for
     # "upstream.code._get_amazon_metadata"
     memoized_get_amazon_metadata = cache.memcache_memoize(
@@ -167,7 +245,32 @@ def cached_get_amazon_metadata(*args, **kwargs):
         result = memoized_get_amazon_metadata.update(*args, **kwargs)[0]
     return result
 
+
+@public
+def get_betterworldbooks_metadata(isbn):
+    """
+    :param str isbn: Unormalisied ISBN10 or ISBN13
+    :return: Metadata for a single BWB book, currently listed on their catalog, or error dict.
+    :rtype: dict
+    """
+
+    isbn = normalize_isbn(isbn)
+    try:
+        if isbn:
+            return _get_betterworldbooks_metadata(isbn)
+    except Exception:
+        return {}
+
+
 def _get_betterworldbooks_metadata(isbn):
+    """Returns price and other metadata (currently minimal)
+    for a book currently available on betterworldbooks.com
+
+    :param str isbn: Normalised ISBN10 or ISBN13
+    :return: Metadata for a single BWB book currently listed on their catalog, or error dict.
+    :rtype: dict
+    """
+
     url = BETTERWORLDBOOKS_API_URL + isbn
     try:
         req = urllib2.Request(url)
@@ -180,7 +283,7 @@ def _get_betterworldbooks_metadata(isbn):
         used_price = re.findall("<LowestUsedPrice>\$([0-9.]+)</LowestUsedPrice>", response)
         used_qty = re.findall("<TotalUsed>([0-9]+)</TotalUsed>", response)
 
-        price_fmt, price, qlt = None, None, None
+        price_fmt = price = qlt = None
 
         if used_qty and used_qty[0] and used_qty[0] != '0':
             price = used_price[0] if used_price else ''
@@ -210,6 +313,7 @@ def _get_betterworldbooks_metadata(isbn):
         except simplejson.decoder.JSONDecodeError:
             return {'error': e.read(), 'code': e.code}
         return simplejson.loads(response)
+
 
 cached_get_betterworldbooks_metadata = cache.memcache_memoize(
     _get_betterworldbooks_metadata, "upstream.code._get_betterworldbooks_metadata", timeout=dateutil.HALF_DAY_SECS)
--- openlibrary/plugins/openlibrary/api.py
+++ openlibrary/plugins/openlibrary/api.py
@@ -17,7 +17,8 @@ from openlibrary.plugins.worksearch.subjects import get_subject
 from openlibrary.core import ia, db, models, lending, helpers as h
 from openlibrary.core.vendors import (
     get_amazon_metadata, create_edition_from_amazon_metadata,
-    get_betterworldbooks_metadata)
+    search_amazon, get_betterworldbooks_metadata)
+
 
 class book_availability(delegate.page):
     path = "/availability/v2"
@@ -49,6 +50,7 @@ class book_availability(delegate.page):
             else []
         )
 
+
 class browse(delegate.page):
     path = "/browse"
     encoding = "json"
@@ -73,6 +75,7 @@ class browse(delegate.page):
         return delegate.RawText(
             simplejson.dumps(result),
             content_type="application/json")
+
 
 class ratings(delegate.page):
     path = "/works/OL(\d+)W/ratings"
@@ -201,6 +204,7 @@ class work_editions(delegate.page):
             "entries": editions
         }
 
+
 class author_works(delegate.page):
     path = "(/authors/OL\d+A)/works"
     encoding = "json"
@@ -242,12 +246,42 @@ class author_works(delegate.page):
             "entries": works
         }
 
+
+class amazon_search_api(delegate.page):
+    """Librarian + admin only endpoint to check for books
+    avaialable on Amazon via the Product Advertising API
+    ItemSearch operation.
+
+    https://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemSearch.html
+
+    Currently experimental to explore what data is avaialable to affiliates.
+
+    :return: JSON {"results": []} containing Amazon product metadata
+             for items matching the title and author search parameters.
+    :rtype: str
+    """
+
+    path = '/_tools/amazon_search'
+
+    @jsonapi
+    def GET(self):
+        user = accounts.get_current_user()
+        if not (user and (user.is_admin() or user.is_librarian())):
+            return web.HTTPError('403 Forbidden')
+        i = web.input(title='', author='')
+        if not (i.author or i.title):
+            return simplejson.dumps({
+                'error': 'author or title required'
+            })
+        results = search_amazon(title=i.title, author=i.author)
+        return simplejson.dumps(results)
+
+
 class price_api(delegate.page):
     path = r'/prices'
 
     @jsonapi
     def GET(self):
-        # TODO: add: title='', authors='' for lookup
         i = web.input(isbn='', asin='')
         if not (i.isbn or i.asin):
             return simplejson.dumps({
