f940d473d75c0ddcbcb5404fa102ecd01e542e1c
Charles Horn <charles.horn@gmail.com>
2019-09-30 14:08:03 +1300

    tests for specific titles that did not find work matches

--- openlibrary/tests/catalog/test_utils.py
+++ openlibrary/tests/catalog/test_utils.py
@@ -116,3 +116,12 @@ mk_norm_conversions = [
 @pytest.mark.parametrize('title,expected', mk_norm_conversions)
 def test_mk_norm(title, expected):
     assert mk_norm(title) == expected
+
+mk_norm_matches = [
+        ("Your Baby's First Word Will Be DADA",
+         "Your baby's first word will be DADA"),
+        ]
+
+@pytest.mark.parametrize('a,b', mk_norm_matches)
+def test_mk_norm_equality(a, b):
+    assert mk_norm(a) == mk_norm(b)
