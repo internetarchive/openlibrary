7ce46a815fd7345299def618139b8351cf5b8a35
Michael E. Karpeles <michael.karpeles@gmail.com>
2019-10-04 03:00:13 +0000

    incorporates promises, sponsorships in dwwi decision

--- openlibrary/core/sponsorships.py
+++ openlibrary/core/sponsorships.py
@@ -2,26 +2,26 @@ import json
 import urllib
 import requests
 import web
+import logging
 from infogami import config
 from infogami.utils.view import public
 from openlibrary import accounts
 from openlibrary.core import models
-from openlibrary.core.lending import get_work_availability, config_ia_domain
+from openlibrary.core.lending import (
+    get_work_availability, config_ia_domain)
 from openlibrary.core.vendors import (
     get_betterworldbooks_metadata, create_edition_from_amazon_metadata)
 from openlibrary.accounts import get_internet_archive_id
 from openlibrary.core.lending import config_ia_civicrm_api
 from openlibrary.utils.isbn import to_isbn_13
-
 try:
     from booklending_utils.sponsorship import eligibility_check
 except ImportError:
     def eligibility_check(edition):
         """For testing if Internet Archive book sponsorship check unavailable"""
-        work = edition.works[0]
-        authors = [w.author.name for w in work.authors]
-        if authors:
-            return True
+        return False
+
+logger = logging.getLogger("openlibrary.sponsorship")
 
 CIVI_ISBN = 'custom_52'
 CIVI_USERNAME = 'custom_51'
@@ -91,6 +91,38 @@ def get_sponsorships_by_contact_id(contact_id, isbn=None):
         'total_amount': t.pop('total_amount')
     } for t in txs]
 
+def do_we_want_it(isbn, work_id):
+    """Returns True if we don't have this edition (or other editions of
+    the same work), if the isbn has not been promised to us, has not
+    yet been sponsored, and is not already in our possession.
+
+    Args:
+        isbn - str isbn10 or 13
+        work_id - str openlibrary work id
+    """
+    availability = get_work_availability(work_id)  # checks all editions
+    if availability and availability.get(work_id, {}).get('status', 'error') != 'error':
+        return False, availability
+
+    # We don't have any of these work's editions available to borrow
+    # Let's confirm this edition hasn't already been sponsored or promised
+    params = {
+        'search_field': 'isbn',
+        'include_promises': 'true',  # include promises and sponsored books
+        'search_id': isbn
+    }
+    url = '%s/book/marc/ol_dedupe.php?%s' % (config_ia_domain,  urllib.urlencode(params))
+    r = requests.get(url)
+    try:
+        data = r.json()
+        dwwi = data.get('response', 0)
+        return dwwi, data.get('books')
+    except:
+        logger.error("DWWI Failed for isbn %s" % isbn, exc_info=True)
+    # err on the side of false negative
+    return False
+
+
 
 def isbn_qualifies_for_sponsorship(isbn):
     """Checks possible isbn10 + isbn13 variations to"""
@@ -100,32 +132,29 @@ def isbn_qualifies_for_sponsorship(isbn):
 
 @public
 def qualifies_for_sponsorship(edition):
-    # defaults
-    dwhi = None
-    eligibility = False
-    price = None
+    resp = {
+        'is_eligible': False,
+        'price': None
+    }
 
     work = edition.works and edition.works[0]
     edition.isbn13 = to_isbn_13(edition.isbn_13 and edition.isbn_13[0] or
                               edition.isbn_10 and edition.isbn_10[0])
-    req_fields = all(edition.get(x) for x in [
+    req_fields = [edition.get(x) for x in [
         'publishers', 'title', 'publish_date', 'covers',
         'number_of_pages', 'isbn13'
-    ])
-    if not (work and req_fields and edition.isbn13):
-        return {
-            'do_we_have_it': dwhi,
-            'is_eligibile': eligibility,
-            'price': price,
-            'error': ("Open Library is missing book metadata "
-                      "necessary for sponsorship"),
+    ]]
+    if not (work and all(req_fields) and edition.isbn13):
+        resp['error'] = {
+            'reason': 'Open Library is missing book metadata necessary for sponsorship',
+            'values': req_fields
         }
+        return resp
 
     work_id = work.key.split("/")[-1]
     num_pages = int(edition.get('number_of_pages'))
-    availability = get_work_availability(work_id)
-    dwhi = (availability.get(work_id, {}).get('status', 'error') != 'error') and availability
-    if not dwhi:
+    dwwi, matches = do_we_want_it(edition.isbn13, work_id)
+    if dwwi:
         bwb_price = get_betterworldbooks_metadata(
             edition.isbn13).get('price_amt')
         if bwb_price:
@@ -134,25 +163,32 @@ def qualifies_for_sponsorship(edition):
             scan_price_cents = SETUP_COST_CENTS + (PAGE_COST_CENTS * num_pages)
             book_cost_cents = int(float(bwb_price) * 100)
             total_price_cents = scan_price_cents + book_cost_cents
-            price = {
+            resp['price'] = {
                 'book_cost_cents': book_cost_cents,
                 'scan_price_cents': scan_price_cents,
                 'total_price_cents': total_price_cents
             }
             if total_price_cents <= PRICE_LIMIT_CENTS:
-                eligibility = eligibility_check(edition)
-    params = {
-        'campaign': 'pilot',
-        'type': 'sponsorship',
-        'context': 'ol',
-        'isbn': edition.isbn13
-    }
-    return {
-        'is_eligible': eligibility,
-        'do_we_have_it': dwhi,
-        'price': price,
-        'url': config_ia_domain + '/donate?' + urllib.urlencode(params)
-    }
+                resp['is_eligible'] = eligibility_check(edition)
+            else:
+                resp['error'] = {
+                    'reason': 'cost exceeds %s' % PRICE_LIMIT_CENTS,
+                    'values': total_price_cents
+                }
+    else:
+        resp['error'] = {
+            'reason': 'matches',
+            'values': maches
+        }
+    resp.update({
+        'url': config_ia_domain + '/donate?' + urllib.urlencode({
+            'campaign': 'pilot',
+            'type': 'sponsorship',
+            'context': 'ol',
+            'isbn': edition.isbn13
+        })
+    })
+    return resp
 
 
 def get_all_sponsors():
