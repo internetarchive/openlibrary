d3d4ff70f7f30fff2c262e7b8c9ce48a9259c6b6
Drini Cami <cdrini@gmail.com>
2020-01-02 11:38:45 -0500

    [feature/barcodescanner] appease js/less lint gods

--- openlibrary/plugins/openlibrary/js/page-scan/LazyBookCard.js
+++ openlibrary/plugins/openlibrary/js/page-scan/LazyBookCard.js
@@ -2,14 +2,16 @@
  * Class for displaying a card for a book, and having it
  * update as it makes requests for information about the book.
  */
+import Promise from 'promise-polyfill'; // polyfill Promise support for IE11
+
 export default class LazyBookCard {
     /**
-     * @param {LazyBookCardState} state 
+     * @param {LazyBookCardState} state
      */
     constructor(state) {
-      this.state = state;
-      /** @type {JQuery} */
-      this.ui = null;
+        this.state = state;
+        /** @type {JQuery} */
+        this.ui = null;
     }
 
     /**
@@ -29,96 +31,97 @@ export default class LazyBookCard {
             </div>
           </a>
         `);
-        
+
         // Reset state
         const state = this.state;
         this.state = {};
         this.updateState(state);
-        
+
         return this.ui;
-      }
+    }
 
     /**
      * Update the state and the UI
-     * @param {LazyBookCardState} newState 
+     * @param {LazyBookCardState} newState
      */
     updateState(newState) {
-      const oldState = this.state;
-      newState = Object.assign({}, oldState, newState);
+        const oldState = this.state;
+        newState = Object.assign({}, oldState, newState);
 
-      if (this.ui) {
-        if (oldState.link != newState.link) {
-          this.ui.attr('href', newState.link);
+        if (this.ui) {
+            if (oldState.link != newState.link) {
+                this.ui.attr('href', newState.link);
+            }
+
+            if (oldState.coverSrc != newState.coverSrc) {
+                this.ui.find('.cover img').attr('src', newState.coverSrc);
+            }
+
+            const textFields = ['title', 'byline', 'identifier'];
+            for (let field of textFields) {
+                if (oldState[field] != newState[field]) {
+                    this.ui.find(`.${field}`).text(newState[field]);
+                }
+            }
+
+            const classFields = ['loading', 'errored'];
+            for (let field of classFields) {
+                if (oldState[field] != newState[field]) {
+                    this.ui.toggleClass(field, newState[field]);
+                }
+            }
         }
-        
-        if (oldState.coverSrc != newState.coverSrc) {
-          this.ui.find('.cover img').attr('src', newState.coverSrc);
-        }
-        
-        const textFields = ['title', 'byline', 'identifier'];
-        for (let field of textFields) {
-          if (oldState[field] != newState[field]) {
-            this.ui.find('.' + field).text(newState[field]);
-          }
-        }
-        
-        const classFields = ['loading', 'errored'];
-        for (let field of classFields) {
-          if (oldState[field] != newState[field]) {
-            this.ui.toggleClass(field, newState[field]);
-          }
-        }
-      }
-  
-      this.state = newState;
+
+        this.state = newState;
     }
-    
+
     /**
      * @param {string} isbn
      * @return {LazyBookCard}
      */
     static fromISBN(isbn) {
-      const cardEl = new LazyBookCard({
-        title: isbn,
-        loading: true,
-        link: `https://openlibrary.org/isbn/${isbn}`,
-      });
-
-      fetch(`https://openlibrary.org/isbn/${isbn}.json`).then(r => r.json())
-      .then(editionRecord => {
-        cardEl.updateState({
-          title: editionRecord.title,
-          identifier: isbn,
-          link: `https://openlibrary.org${editionRecord.key}`,
+        const cardEl = new LazyBookCard({
+            title: isbn,
+            loading: true,
+            link: `https://openlibrary.org/isbn/${isbn}`,
         });
-        
-        if (editionRecord.covers) {
-          const coverId = editionRecord.covers.find(x => x != -1);
-          if (coverId) {
-            cardEl.updateState({
-              coverSrc: `http://covers.openlibrary.org/b/id/${coverId}-M.jpg`,
-            });
-          }
-        }
 
-        return fetch(`https://openlibrary.org${editionRecord.works[0].key}.json`).then(r => r.json())
-      }).then(workRecord => {
-        return Promise.all(
-          workRecord.authors.map(a => `https://openlibrary.org${a.author.key}.json`)
-          .map(link => fetch(link).then(r => r.json()))
-        );
-      }).then(authorRecords => {
-        cardEl.updateState({byline: authorRecords.map(a => a.name).join(', ')})
-        cardEl.updateState({loading: false});
-      })
-      .catch(err => {
-        cardEl.updateState({loading: false, errored: true});
-      });
+        fetch(`https://openlibrary.org/isbn/${isbn}.json`).then(r => r.json())
+            .then(editionRecord => {
+                cardEl.updateState({
+                    title: editionRecord.title,
+                    identifier: isbn,
+                    link: `https://openlibrary.org${editionRecord.key}`,
+                });
 
-      return cardEl;
+                if (editionRecord.covers) {
+                    const coverId = editionRecord.covers.find(x => x != -1);
+                    if (coverId) {
+                        cardEl.updateState({
+                            coverSrc: `http://covers.openlibrary.org/b/id/${coverId}-M.jpg`,
+                        });
+                    }
+                }
+
+                return fetch(`https://openlibrary.org${editionRecord.works[0].key}.json`).then(r => r.json())
+            }).then(workRecord => {
+                return Promise.all(
+                    workRecord.authors.map(a => `https://openlibrary.org${a.author.key}.json`)
+                        .map(link => fetch(link).then(r => r.json()))
+                );
+            }).then(authorRecords => {
+                cardEl.updateState({
+                    loading: false,
+                    byline: authorRecords.map(a => a.name).join(', '),
+                });
+            })
+            // eslint-disable-next-line no-unused-vars
+            .catch(err => cardEl.updateState({loading: false, errored: true}));
+
+        return cardEl;
     }
 }
-  
+
 /**
  * @typedef {object} LazyBookCardState
  * @property {string} [coverSrc] url of the cover image
