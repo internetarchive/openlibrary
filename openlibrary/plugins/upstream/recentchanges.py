a850617db3c9986abe48b9f3913cc2f8738cfaff
Christian Clauss <cclauss@me.com>
2020-01-10 11:19:07 +0100

    from infogami.utils.flash import add_flash_message

--- openlibrary/plugins/upstream/recentchanges.py
+++ openlibrary/plugins/upstream/recentchanges.py
@@ -7,7 +7,8 @@ import simplejson
 import yaml
 
 from infogami.utils import delegate
-from infogami.utils.view import public, render, render_template, add_flash_message, safeint
+from infogami.utils.flash import add_flash_message
+from infogami.utils.view import public, render, render_template, safeint
 from infogami.utils import features
 
 from openlibrary.utils import dateutil
