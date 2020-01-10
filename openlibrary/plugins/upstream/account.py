c1db712503500b96256b0c941cbd40f9cbce08ee
Christian Clauss <cclauss@me.com>
2020-01-10 10:51:30 +0100

    from infogami.utils.flash import add_flash_message

--- openlibrary/plugins/upstream/account.py
+++ openlibrary/plugins/upstream/account.py
@@ -10,9 +10,8 @@ import simplejson
 
 from infogami.utils import delegate
 from infogami import config
-from infogami.utils.view import (
-    require_login, render, render_template, add_flash_message
-)
+from infogami.utils.flash import add_flash_message
+from infogami.utils.view import require_login, render, render_template
 
 from infogami.infobase.client import ClientException
 from infogami.utils.context import context
