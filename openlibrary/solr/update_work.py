09fab8cb535978d781c2f2a52c9e48a5535cb13b
Christian Clauss <cclauss@me.com>
2020-01-20 09:24:30 +0100

    from six.moves.http_client import HTTPConnection

--- openlibrary/solr/update_work.py
+++ openlibrary/solr/update_work.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import datetime
-import httplib
 import logging
 import os
 import re
@@ -12,6 +11,7 @@ from unicodedata import normalize
 import simplejson as json
 import six
 from six.moves import urllib
+from six.moves.http_client import HTTPConnection
 import web
 from lxml.etree import tostring, Element, SubElement
 
@@ -815,7 +815,7 @@ def solr_update(requests, debug=False, commitWithin=60000):
     :param bool debug:
     :param int commitWithin: Solr commitWithin, in ms
     """
-    h1 = httplib.HTTPConnection(get_solr())
+    h1 = HTTPConnection(get_solr())
     url = 'http://%s/solr/update' % get_solr()
 
     logger.info("POSTing update to %s", url)
