2bbfbc0d58839fe1a564a27fb331cfda935a61d4
Drini Cami <cdrini@gmail.com>
2020-01-27 19:13:17 -0500

    fix edit page tabs not defaulting to edition

--- openlibrary/plugins/openlibrary/js/tabs.js
+++ openlibrary/plugins/openlibrary/js/tabs.js
@@ -1,8 +1,29 @@
 const TABS_OPTIONS = { fx: { opacity: 'toggle' } };
 
+/**
+ * @param {JQuery} $node one or more tab containers
+ */
 export default function initTabs($node) {
-    $node.tabs(TABS_OPTIONS);
-    $node.filter('.autohash').bind('tabsselect', function(event, ui) {
-        document.location.hash = ui.panel.id;
-    });
+    if ($node.length > 1) {
+        $node.each((i, el) => initTabs($(el)));
+        return;
+    }
+
+    const $container = $node;
+    const autohash = $container.hasClass('autohash');
+    const opts = TABS_OPTIONS;
+    const panelIdToIndex = {};
+    $container.children(':not(:first-child)')
+        .each((i, el) => panelIdToIndex[el.id] = i);
+
+    if (autohash) {
+        if (location.hash) {
+            // skip the hash symbol
+            const panelName = location.hash.slice(1);
+            opts.active = panelIdToIndex[panelName];
+        }
+        $container.on('tabsselect', (event, ui) => location.hash = ui.panel.id);
+    }
+
+    $container.tabs(opts);
 }
