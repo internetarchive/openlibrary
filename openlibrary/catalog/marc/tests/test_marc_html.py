28a7926a0772262662c38e7340e879d6d40ff572
Christian Clauss <cclauss@me.com>
2020-02-20 14:07:26 +0100

    Fix indentation

--- openlibrary/catalog/marc/tests/test_marc_html.py
+++ openlibrary/catalog/marc/tests/test_marc_html.py
@@ -23,9 +23,9 @@ def test_html_line():
         ('245', ('10\x1faDbu ma la \xca\xbejug pa\xca\xbei kar t\xcc\xa3i\xcc\x84k '
                  ':\x1fbDwags-brgyud grub pa\xca\xbei s\xcc\x81in\xcc\x87 rta /\x1f'
                  'cKarma-pa Mi-bskyod-rdo-rje.\x1e'),
-                 (u'10 <b>$a</b>Dbu ma la \u02bejug pa\u02bei kar \u1e6d\u012bk :<b>'
-                  u'$b</b>Dwags-brgyud grub pa\u02bei \u015bi\u1e45 rta /<b>$c</b>Ka'
-                  u'rma-pa Mi-bskyod-rdo-rje.')),
+                (u'10 <b>$a</b>Dbu ma la \u02bejug pa\u02bei kar \u1e6d\u012bk :<b>'
+                 u'$b</b>Dwags-brgyud grub pa\u02bei \u015bi\u1e45 rta /<b>$c</b>Ka'
+                 u'rma-pa Mi-bskyod-rdo-rje.')),
     ]
     hr = html_record("00053This is the leader.Now we are beyond the leader.")
     for tag, input, output in samples:
