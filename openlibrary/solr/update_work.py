d7ccb1b8c33f8e49761ff774a2dea302ca9b7759
Christian Clauss <cclauss@me.com>
2020-02-21 09:07:20 +0100

    six.ensure_text()

--- openlibrary/solr/update_work.py
+++ openlibrary/solr/update_work.py
@@ -977,7 +977,7 @@ class UpdateRequest:
         node = dict2element(self.doc)
         root = Element("add")
         root.append(node)
-        return tostring(root)
+        return six.ensure_text(tostring(root))
 
     def tojson(self):
         """
