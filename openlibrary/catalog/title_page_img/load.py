b0794f656e272db907b50c7f6ea2962edaf30c8b
Charles Horn <charles.horn@gmail.com>
2020-02-24 10:17:44 +1300

    Revert "remove catalog/title_page_img/"
    This reverts commit f64ef6195e0819e1056d2f284bf3d8f287f0d6a0.

---
+++ openlibrary/catalog/title_page_img/load.py
@@ -0,0 +1,31 @@
+from openlibrary.catalog.read_rc import read_rc
+import httplib
+import json
+
+from six.moves import urllib
+
+
+rc = read_rc()
+
+def add_cover_image(ekey, ia):
+    h1 = httplib.HTTPConnection('openlibrary.org')
+    body = json.dumps(dict(username='ImportBot', password=rc['ImportBot']))
+    headers = {'Content-Type': 'application/json'}
+    h1.request('POST', 'http://openlibrary.org/account/login', body, headers)
+
+    res = h1.getresponse()
+
+    res.read()
+    assert res.status == 200
+    cookies = res.getheader('set-cookie').split(',')
+    cookie =  ';'.join([c.split(';')[0] for c in cookies])
+
+    cover_url = 'http://www.archive.org/download/' + ia + '/page/' + ia + '_preview.jpg'
+    body = urllib.parse.urlencode({"url": cover_url})
+    assert ekey.startswith('/books/')
+    add_cover_url = 'http://openlibrary.org' + ekey + '/add-cover.json'
+    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Cookie': cookie}
+    h1.request('POST', add_cover_url, body, headers)
+    res = h1.getresponse()
+    res.read()
+    return
