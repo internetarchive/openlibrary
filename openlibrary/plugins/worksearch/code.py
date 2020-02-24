9acaec8eb8fc9c339755542993f9c894efc4a786
cclauss <cclauss@me.com>
2020-02-24 21:42:36 +0100

    worksearch: Use absolute imports

--- openlibrary/plugins/worksearch/code.py
+++ openlibrary/plugins/worksearch/code.py
@@ -803,10 +803,10 @@ class search_json(delegate.page):
         return delegate.RawText(json.dumps(response, indent=True))
 
 def setup():
-    import searchapi
+    from openlibrary.plugins.worksearch import searchapi
     searchapi.setup()
 
-    from . import subjects
+    from openlibrary.plugins.worksearch import subjects
 
     # subjects module needs read_author_facet and solr_select_url.
     # Importing this module to access them will result in circular import.
@@ -817,7 +817,7 @@ def setup():
 
     subjects.setup()
 
-    from . import publishers, languages
+    from openlibrary.plugins.worksearch import languages, publishers
     publishers.setup()
     languages.setup()
 
