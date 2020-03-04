3d6c55cdff63eb847e345ee0a7bbde7d3bfc39e3
Charles Horn <charles.horn@gmail.com>
2020-03-04 17:58:56 +1300

    line wrapping

--- openlibrary/catalog/add_book/test_load_book.py
+++ openlibrary/catalog/add_book/test_load_book.py
@@ -1,6 +1,7 @@
 import pytest
 from openlibrary.catalog.add_book import load_book
-from openlibrary.catalog.add_book.load_book import import_author, build_query, InvalidLanguage
+from openlibrary.catalog.add_book.load_book import (
+    import_author, build_query, InvalidLanguage)
 
 
 @pytest.fixture
@@ -23,7 +24,8 @@ def test_import_author_personal_name(monkeypatch):
     """Name order is only flipped to natural order if 'personal_name' is present.
     """
     monkeypatch.setattr(load_book, 'find_entity', lambda a: None)
-    result = import_author({'personal_name': 'Surname, Forename', 'name': 'Surname, Forename'})
+    result = import_author({'personal_name': 'Surname, Forename',
+                            'name': 'Surname, Forename'})
     assert result['name'] == 'Forename Surname'
 
 
