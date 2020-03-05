6b28e7b8474bd017a332229d6c97eb7a98ceb909
Charles Horn <charles.horn@gmail.com>
2020-03-05 13:31:48 +1300

    make name flipping the default without personal_name, while retaining desired past behaviour

--- openlibrary/catalog/add_book/test_load_book.py
+++ openlibrary/catalog/add_book/test_load_book.py
@@ -26,18 +26,22 @@ def new_import(monkeypatch):
 
 
 # These authors will be imported with natural name order
-# The presence of 'personal_name' is what triggers the name swap
+# i.e. => Forename Surname
 natural_names = [
      {'name': 'Forename Surname'},
      {'name': 'Surname, Forename', 'personal_name': 'Surname, Forename'},
+     {'name': 'Surname, Forename'},
+     {'name': 'Surname, Forename', 'entity_type': 'person'},
      ]
 
 
 # These authors with be imported with 'name' unchanged
 unchanged_names = [
      {'name': 'Forename Surname'},
-     {'name': 'Surname, Forename'},
-     {'name': 'Surname, Forename', 'entity_type': 'person'},
+     {'name': 'Smith, John III, King of Coats, and Bottles',
+         'personal_name': 'Smith, John'},
+     {'name': 'Smith, John III, King of Coats, and Bottles'},
+     {'name': 'Harper, John Murdoch, 1845-'},
      {'entity_type': 'org', 'name': 'Organisation, Place'},
      ]
 
@@ -48,10 +52,11 @@ def test_import_author_name_natural_order(author, new_import):
     assert result['name'] == 'Forename Surname'
 
 
-@pytest.mark.parametrize('author', natural_names)
+@pytest.mark.parametrize('author', unchanged_names)
 def test_import_author_name_unchanged(author, new_import):
+    expect = author['name']
     result = import_author(author)
-    assert result['name'] == author['name']
+    assert result['name'] == expect
 
 
 def test_build_query(add_languages):
@@ -63,7 +68,7 @@ def test_build_query(add_languages):
     }
     q = build_query(rec)
     assert q['title'] == 'magic'
-    assert q['authors'][0]['name'] == 'Surname, Forename'
+    assert q['authors'][0]['name'] == 'Forename Surname'
     assert q['description'] == {'type': '/type/text', 'value': 'test'}
     assert q['type'] == {'key': '/type/edition'}
     assert q['languages'] == [{'key': '/languages/eng'}, {'key': '/languages/fre'}]
