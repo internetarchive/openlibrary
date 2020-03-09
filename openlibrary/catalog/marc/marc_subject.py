48ff11e40a74a4ee59b00baa78be8add56fbdfde
Charles Horn <charles.horn@gmail.com>
2020-03-09 13:47:11 +1300

    remove unused import in test and deprecate

--- openlibrary/catalog/marc/marc_subject.py
+++ openlibrary/catalog/marc/marc_subject.py
@@ -141,22 +141,9 @@ def get_work_subjects(w, do_get_mc=True):
     return combine_subjects(subjects)
 
 
-@deprecated
+@deprecated('Use openlibrary.catalog.marc.get_subjects.tidy_subject() instead.')
 def tidy_subject(s):
-    s = s.strip()
-    if len(s) >= 2:
-        s = s[0].upper() + s[1:]
-    m = re_etc.search(s)
-    if m:
-        return m.group(1)
-    s = remove_trailing_dot(s)
-    m = re_fictitious_character.match(s)
-    if m:
-        return m.group(2) + ' ' + m.group(1) + m.group(3)
-    m = re_comma.match(s)
-    if m:
-        return m.group(3) + ' ' + m.group(1) + m.group(2)
-    return s
+    return get_subjects.tidy_subject(s)
 
 
 re_aspects = re.compile(' [Aa]spects$')
@@ -177,96 +164,9 @@ def find_aspects(f):
     return x + ' of ' + flip_subject(a)
 
 
-@deprecated
+@deprecated('Use openlibrary.catalog.marc.get_subjects.read_subject() instead.')
 def read_subjects(rec):
-    subjects = defaultdict(lambda: defaultdict(int))
-    for tag, field in rec.read_fields(subject_fields):
-        f = rec.decode_field(field)
-        aspects = find_aspects(f)
-
-        if tag == '600': # people
-            name_and_date = []
-            for k, v in f.get_subfields(['a', 'b', 'c', 'd']):
-                v = '(' + v.strip('.() ') + ')' if k == 'd' else v.strip(' /,;:')
-                if k == 'a':
-                    m = re_flip_name.match(v)
-                    if m:
-                        v = flip_name(v)
-                name_and_date.append(v)
-            name = remove_trailing_dot(' '.join(name_and_date)).strip()
-            if name != '':
-                subjects['person'][name] += 1
-        elif tag == '610': # org
-            v = ' '.join(f.get_subfield_values('abcd'))
-            v = v.strip()
-            if v:
-                v = remove_trailing_dot(v).strip()
-            if v:
-                v = tidy_subject(v)
-            if v:
-                subjects['org'][v] += 1
-
-            for v in f.get_subfield_values('a'):
-                v = v.strip()
-                if v:
-                    v = remove_trailing_dot(v).strip()
-                if v:
-                    v = tidy_subject(v)
-                if v:
-                    subjects['org'][v] += 1
-        elif tag == '611': # event
-            v = ' '.join(j.strip() for i, j in f.get_all_subfields() if i not in 'vxyz')
-            if v:
-                v = v.strip()
-            v = tidy_subject(v)
-            if v:
-                subjects['event'][v] += 1
-        elif tag == '630': # work
-            for v in f.get_subfield_values(['a']):
-                v = v.strip()
-                if v:
-                    v = remove_trailing_dot(v).strip()
-                if v:
-                    v = tidy_subject(v)
-                if v:
-                    subjects['work'][v] += 1
-        elif tag == '650': # topical
-            for v in f.get_subfield_values(['a']):
-                if v:
-                    v = v.strip()
-                v = tidy_subject(v)
-                if v:
-                    subjects['subject'][v] += 1
-        elif tag == '651': # geo
-            for v in f.get_subfield_values(['a']):
-                if v:
-                    subjects['place'][flip_place(v).strip()] += 1
-
-        for v in f.get_subfield_values(['y']):
-            v = v.strip()
-            if v:
-                subjects['time'][remove_trailing_dot(v).strip()] += 1
-        for v in f.get_subfield_values(['v']):
-            v = v.strip()
-            if v:
-                v = remove_trailing_dot(v).strip()
-            v = tidy_subject(v)
-            if v:
-                subjects['subject'][v] += 1
-        for v in f.get_subfield_values(['z']):
-            v = v.strip()
-            if v:
-                subjects['place'][flip_place(v).strip()] += 1
-        for v in f.get_subfield_values(['x']):
-            v = v.strip()
-            if not v:
-                continue
-            if aspects and re_aspects.search(v):
-                continue
-            v = tidy_subject(v)
-            if v:
-                subjects['subject'][v] += 1
-    return dict((k, dict(v)) for k, v in subjects.items())
+    return get_subjects.read_subject(s)
 
 
 @deprecated
