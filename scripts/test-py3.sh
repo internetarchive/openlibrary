36f3fa4000a29a61b1937db3739725a6dcf267d9
cclauss <cclauss@me.com>
2020-03-10 19:09:03 +0100

    Promote two passing tests

--- scripts/test-py3.sh
+++ scripts/test-py3.sh
@@ -4,6 +4,7 @@ pytest openlibrary/mocks openlibrary/olbase openlibrary/utils scripts/tests \
     openlibrary/coverstore/tests/test_code.py \
     openlibrary/coverstore/tests/test_webapp.py \
     openlibrary/plugins/admin/tests/test_services.py \
+    openlibrary/plugins/books/tests/test_doctests.py \
     openlibrary/plugins/importapi/tests/test_code_ils.py \
     openlibrary/plugins/importapi/tests/test_import_edition_builder.py \
     openlibrary/plugins/openlibrary/tests/test_borrow_home.py \
@@ -14,6 +15,7 @@ pytest openlibrary/mocks openlibrary/olbase openlibrary/utils scripts/tests \
     openlibrary/plugins/upstream/tests/test_utils.py \
     openlibrary/plugins/worksearch/tests/test_worksearch.py \
     openlibrary/tests/core/test_cache.py \
+    openlibrary/tests/core/test_connections.py \
     openlibrary/tests/core/test_helpers.py \
     openlibrary/tests/core/test_i18n.py \
     openlibrary/tests/core/test_init.py \
@@ -53,8 +55,7 @@ pytest openlibrary/coverstore/tests/test_coverstore.py \
     openlibrary/coverstore/tests/test_doctests.py || true
 
 # plugins: All failing tests run in allow failures (|| true) mode
-pytest openlibrary/plugins/books/tests/test_doctests.py \
-    openlibrary/plugins/books/tests/test_dynlinks.py \
+pytest openlibrary/plugins/books/tests/test_dynlinks.py \
     openlibrary/plugins/openlibrary/tests/test_home.py \
     openlibrary/plugins/openlibrary/tests/test_stats.py \
     openlibrary/plugins/upstream/tests/test_merge_authors.py \
@@ -62,6 +63,5 @@ pytest openlibrary/plugins/books/tests/test_doctests.py \
 
 # openlibrary/tests: All failing tests run in allow failures (|| true) mode
 pytest openlibrary/tests/accounts/test_models.py \
-    openlibrary/tests/core/test_connections.py \
     openlibrary/tests/core/test_ia.py \
     openlibrary/tests/solr/test_update_work.py || true
