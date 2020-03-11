2b0c4f880dfc22d56aaacdff5a2c0f651a19cc3a
cclauss <cclauss@me.com>
2020-03-11 21:34:12 +0100

    int(bytes(header.data_length).decode('utf-8'))

--- openlibrary/coverstore/warc.py
+++ openlibrary/coverstore/warc.py
@@ -42,7 +42,7 @@ class WARCReader:
             if header is None:
                 break
             yield LazyWARCRecord(self._file, self._file.tell(), header)
-            self._file.seek(int(ensure_text(header.data_length)), 1)
+            self._file.seek(int(bytes(header.data_length).decode('utf-8')), 1)
             consume_crlf()
             consume_crlf()
 
