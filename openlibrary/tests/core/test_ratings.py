ac35aaf5bed538d22591b5913561841f80427c70
Shubham Kumar <shubhamkrai123@gmail.com>
2020-03-14 14:57:21 +0530

    removed unused imports

--- openlibrary/tests/core/test_ratings.py
+++ openlibrary/tests/core/test_ratings.py
@@ -1,9 +1,3 @@
-from openlibrary.core import ratings
-
-# this should be moved to openlibrary.core
-from openlibrary.plugins.upstream.models import UnitParser
-
-
 class TestRating:
     def test_rate(self):
         pass
