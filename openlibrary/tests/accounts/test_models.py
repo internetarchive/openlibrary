d0ca4cb5526ad6e9f4af15a1663415ae86f2f877
cclauss <cclauss@me.com>
2020-03-24 19:41:31 +0100

    Fix indentation in two Python files

--- openlibrary/tests/accounts/test_models.py
+++ openlibrary/tests/accounts/test_models.py
@@ -5,24 +5,24 @@ from requests.models import Response
 def test_verify_hash():
     secret_key = "aqXwLJVOcV"
     hash = model.generate_hash(secret_key, "foo")
-    assert model.verify_hash(secret_key, "foo", hash) == True
+    assert model.verify_hash(secret_key, "foo", hash)
 
 
 class TestInternetArchiveAccount:
     def test_xauth_http_error_without_json(self, monkeypatch):
         xauth = InternetArchiveAccount.xauth
-
         resp = Response()
         resp.status_code = 500
         resp._content = b'Internal Server Error'
         monkeypatch.setattr(model.requests, 'post', lambda url, **kwargs: resp)
-        assert xauth('create', s3_key='_', s3_secret='_') == {'code': 500, 'error': 'Internal Server Error'}
+        assert xauth('create', s3_key='_', s3_secret='_') == {'code': 500, 'error':
+                                                              'Internal Server Error'}
 
     def test_xauth_http_error_with_json(self, monkeypatch):
         xauth = InternetArchiveAccount.xauth
-
         resp = Response()
         resp.status_code = 400
         resp._content = b'{"error": "Unknown Parameter Blah"}'
         monkeypatch.setattr(model.requests, 'post', lambda url, **kwargs: resp)
-        assert xauth('create', s3_key='_', s3_secret='_') == {"error": "Unknown Parameter Blah"}
+        assert xauth('create', s3_key='_', s3_secret='_') == {"error":
+                                                              "Unknown Parameter Blah"}
