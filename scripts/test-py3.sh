19d09e69ee566025b99a541c0127002e14cee4e0
cclauss <cclauss@me.com>
2020-04-08 23:39:49 +0200

    Prepare coverstore/warc.py for Python 3 again

--- scripts/test-py3.sh
+++ scripts/test-py3.sh
@@ -2,7 +2,17 @@
 
 pytest openlibrary/mocks openlibrary/olbase openlibrary/utils scripts/tests openlibrary/tests \
     openlibrary/catalog/add_book/tests/test_add_book.py \
+    openlibrary/catalog/add_book/tests/test_load_book.py \
+    openlibrary/catalog/add_book/tests/test_match.py \
+    openlibrary/catalog/marc/tests/test_marc_binary.py \
+    openlibrary/catalog/marc/tests/test_marc_html.py \
+    openlibrary/catalog/merge/test_amazon.py \
+    openlibrary/catalog/merge/test_merge.py \
+    openlibrary/catalog/merge/test_merge_marc.py \
+    openlibrary/catalog/merge/test_names.py \
+    openlibrary/catalog/merge/test_normalize.py \
     openlibrary/coverstore/tests/test_code.py \
+    openlibrary/coverstore/tests/test_coverstore.py \
     openlibrary/coverstore/tests/test_webapp.py \
     openlibrary/plugins/admin/tests/test_services.py \
     openlibrary/plugins/books/tests/test_doctests.py \
@@ -18,16 +28,7 @@ pytest openlibrary/mocks openlibrary/olbase openlibrary/utils scripts/tests open
     openlibrary/plugins/upstream/tests/test_merge_authors.py \
     openlibrary/plugins/upstream/tests/test_related_carousels.py \
     openlibrary/plugins/upstream/tests/test_utils.py \
-    openlibrary/plugins/worksearch/tests/test_worksearch.py \
-    openlibrary/catalog/add_book/tests/test_load_book.py \
-    openlibrary/catalog/add_book/tests/test_match.py \
-    openlibrary/catalog/marc/tests/test_marc_binary.py \
-    openlibrary/catalog/marc/tests/test_marc_html.py \
-    openlibrary/catalog/merge/test_amazon.py \
-    openlibrary/catalog/merge/test_merge.py \
-    openlibrary/catalog/merge/test_merge_marc.py \
-    openlibrary/catalog/merge/test_names.py \
-    openlibrary/catalog/merge/test_normalize.py
+    openlibrary/plugins/worksearch/tests/test_worksearch.py
 
 # The following sections allow us to quickly spot tests that are fixed
 
@@ -40,11 +41,7 @@ pytest \
     openlibrary/tests/catalog/test_utils.py || true
 
 # coverstore: All failing tests run in allow failures (|| true) mode
-pytest \
-    openlibrary/coverstore/tests/test_coverstore.py \
-    openlibrary/coverstore/tests/test_doctests.py || true
+pytest openlibrary/coverstore/tests/test_doctests.py || true
 
 # plugins: All failing tests run in allow failures (|| true) mode
-pytest openlibrary/plugins/books/tests/test_dynlinks.py \
-    openlibrary/plugins/openlibrary/tests/test_home.py \
-    openlibrary/plugins/upstream/tests/test_merge_authors.py || true
+pytest openlibrary/plugins/openlibrary/tests/test_home.py || true
