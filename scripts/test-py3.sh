4e670b52bc7b1df02b9a536ed824937643a3caab
cclauss <cclauss@me.com>
2020-04-14 11:13:46 +0200

    Deterministic dicthash regardless of key order

--- scripts/test-py3.sh
+++ scripts/test-py3.sh
@@ -7,8 +7,7 @@ pytest openlibrary/catalog openlibrary/coverstore openlibrary/mocks openlibrary/
        --ignore=openlibrary/catalog/marc/tests/test_parse.py \
        --ignore=openlibrary/tests/catalog/test_get_ia.py \
        --ignore=openlibrary/coverstore/tests/test_doctests.py \
-       --ignore=openlibrary/plugins/openlibrary/tests/test_home.py \
-       --ignore=openlibrary/plugins/upstream/tests/test_merge_authors.py
+       --ignore=openlibrary/plugins/openlibrary/tests/test_home.py
 RETURN_CODE=$?
        
 pytest openlibrary/catalog/marc/tests/test_get_subjects.py || true
@@ -17,6 +16,5 @@ pytest openlibrary/catalog/marc/tests/test_parse.py || true
 pytest openlibrary/tests/catalog/test_get_ia.py || true
 pytest openlibrary/coverstore/tests/test_doctests.py || true
 pytest openlibrary/plugins/openlibrary/tests/test_home.py || true
-pytest openlibrary/plugins/upstream/tests/test_merge_authors.py || true
 
 exit ${RETURN_CODE}
