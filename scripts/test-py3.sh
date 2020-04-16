b702c0680c3d84572b592ec99318227e955ffd00
cclauss <cclauss@me.com>
2020-04-16 18:02:22 +0200

    test_merge_authors.py must now pass

--- scripts/test-py3.sh
+++ scripts/test-py3.sh
@@ -7,16 +7,14 @@ pytest openlibrary/catalog openlibrary/coverstore openlibrary/mocks openlibrary/
        --ignore=openlibrary/catalog/marc/tests/test_parse.py \
        --ignore=openlibrary/tests/catalog/test_get_ia.py \
        --ignore=openlibrary/coverstore/tests/test_doctests.py \
-       --ignore=openlibrary/plugins/openlibrary/tests/test_home.py \
-       --ignore=openlibrary/plugins/upstream/tests/test_merge_authors.py
+       --ignore=openlibrary/plugins/openlibrary/tests/test_home.py
 RETURN_CODE=$?
        
 pytest openlibrary/catalog/marc/tests/test_get_subjects.py || true
 pytest openlibrary/catalog/marc/tests/test_marc.py || true
 pytest openlibrary/catalog/marc/tests/test_parse.py || true
 pytest openlibrary/tests/catalog/test_get_ia.py || true
-pytest openlibrary/coverstore/tests/test_doctests.py || true
+pytest --show-capture=all openlibrary/coverstore/tests/test_doctests.py || true
 pytest --show-capture=all openlibrary/plugins/openlibrary/tests/test_home.py || true
-pytest openlibrary/plugins/upstream/tests/test_merge_authors.py || true
 
 exit ${RETURN_CODE}
