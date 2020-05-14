6f351a5cdad231ad36b7e39885615968d9dd4cb1
Tabish Shaikh <2016ucs0014@iitjammu.ac.in>
2020-05-14 21:02:56 +0530

    text move to the side

--- openlibrary/plugins/openlibrary/js/realtime_account_validation.js
+++ openlibrary/plugins/openlibrary/js/realtime_account_validation.js
@@ -8,7 +8,7 @@ export function initRealTimeValidation() {
                 success: function(result) {
                     console.log(result);
                     if(result.output == true){
-                        $('#validScreenname').removeClass().addClass('darkgreen').text("Screen name is Valid").css('font-weight','700');
+                        $('#usernameMessage').removeClass().addClass('darkgreen').html("Screen name is Valid <br/>").css('font-weight','700');
                         $('#userUrl').removeClass().addClass('darkgreen').text(value).css('font-weight','700');  
                         $("label[for='username']").removeClass();
                         $(document.getElementById('username')).removeClass().addClass('required');
@@ -16,7 +16,7 @@ export function initRealTimeValidation() {
                     else{
                         $(document.getElementById('username')).removeClass().addClass('required invalid');
                         $("label[for='username']").removeClass().addClass('invalid');
-                        $('#validScreenname').removeClass().addClass('invalid').text("Screen name is not Valid").css('font-weight','700');
+                        $('#usernameMessage').removeClass().addClass('invalid').html("Screen name is not Valid <br/>").css('font-weight','700');
                         $('#userUrl').removeClass().addClass('hidden');
                     }
                 }
@@ -25,7 +25,7 @@ export function initRealTimeValidation() {
         else{
             $("label[for='username']").removeClass();
             $(document.getElementById('username')).removeClass().addClass('required');
-            $('#validScreenname').removeClass().text("");
+            $('#usernameMessage').removeClass().html("<br/>");
             $('#userUrl').removeClass().text("");
         }
     });
@@ -34,20 +34,20 @@ export function initRealTimeValidation() {
         var value2 = document.getElementById("password2").value;
         if(!value2=='' && !value==''){
             if(value2==value){
-                $('#passwordMessage').removeClass().addClass('darkgreen').text("Passwords Match").css('font-weight','700');
+                $('#password2Message').removeClass().addClass('darkgreen').text("Passwords Match").css('font-weight','700');
                 $("label[for='password2']").removeClass();
                 $(document.getElementById('password2')).removeClass().addClass('required');
             }
             else{
                 $(document.getElementById('password2')).removeClass().addClass('required invalid');
                 $("label[for='password2']").removeClass().addClass('invalid');
-                $('#passwordMessage').removeClass().addClass('invalid').text("Passwords dont match");
+                $('#password2Message').removeClass().addClass('invalid').text("Passwords didnt match");
             }
         }
         else{
             $("label[for='password2']").removeClass();
             $(document.getElementById('password2')).removeClass().addClass('required');
-            $('#passwordMessage').removeClass().text("");
+            $('#password2Message').removeClass().text("");
         }
     });
     $('#emailAddr').on('blur', function(){
@@ -59,14 +59,14 @@ export function initRealTimeValidation() {
                 success: function(result) {
                     if(result.output == true){
                         console.log(result);
-                        $('#eMatch').removeClass().addClass('darkgreen').text("Email id is valid").css('font-weight','700');
+                        $('#emailAddrMessage').removeClass().addClass('darkgreen').text("Email id is valid").css('font-weight','700');
                         $("label[for='emailAddr']").removeClass();
                         $(document.getElementById('emailAddr')).removeClass().addClass('required');
                     }
                     else{
                         $(document.getElementById('emailAddr')).removeClass().addClass('required invalid');
                         $("label[for='emailAddr']").removeClass().addClass('invalid');
-                        $('#eMatch').removeClass().addClass('invalid').text("Email id is not valid");
+                        $('#emailAddrMessage').removeClass().addClass('invalid').text("Email id is not valid");
                     }
                 }
             });
@@ -74,7 +74,7 @@ export function initRealTimeValidation() {
         else{
             $("label[for='emailAddr']").removeClass();
             $(document.getElementById('emailAddr')).removeClass().addClass('required');
-            $('#eMatch').removeClass().text("");
+            $('#emailAddrMessage').removeClass().text("");
         }
     });
 }
