9c69ed82ba653208eacbacbba0128c69d63beae6
Tabish Shaikh <2016ucs0014@iitjammu.ac.in>
2020-05-16 21:29:27 +0530

    corrected js to make test pass

--- openlibrary/plugins/openlibrary/js/realtime_account_validation.js
+++ openlibrary/plugins/openlibrary/js/realtime_account_validation.js
@@ -7,79 +7,79 @@ export function initRealTimeValidation() {
 
     $('#username').on('blur', function(){
         var value = $(this).val();
-        if(!value==""){
+        if (!value=='') {
         $.ajax({
-                url: "/account/sceenname/rtverify?name="+value,
+                url: '/account/sceenname/rtverify?name='+value,
                 type: 'GET',
                 success: function(result) {
                     console.log(result);
-                    if(result.output == true){
-                        $('#usernameMessage').removeClass().addClass('darkgreen').html("Screen name is Valid <br/>").css('font-weight','700');
-                        $("label[for='username']").removeClass();
+                    if (result.output == true) {
+                        $('#usernameMessage').removeClass().addClass('darkgreen').html('Screen name is Valid <br/>').css('font-weight','700');
+                        $('label[for="username"]').removeClass();
                         $(document.getElementById('username')).removeClass().addClass('required');
                     }
-                    else{
+                    else {
                         $(document.getElementById('username')).removeClass().addClass('required invalid');
-                        $("label[for='username']").removeClass().addClass('invalid');
-                        $('#usernameMessage').removeClass().addClass('invalid').html("Screen name is not Valid <br/>").css('font-weight','700');
+                        $('label[for="username"]').removeClass().addClass('invalid');
+                        $('#usernameMessage').removeClass().addClass('invalid').html('Screen name is not Valid <br/>').css('font-weight','700');
                     }
                 }
             });
         }
-        else{
-            $("label[for='username']").removeClass();
+        else {
+            $('label[for="username"]').removeClass();
             $(document.getElementById('username')).removeClass().addClass('required');
-            $('#usernameMessage').removeClass().html("<br/>");
+            $('#usernameMessage').removeClass().html('<br/>');
         }
     });
 
     $('#password, #password2').on('blur', function(){
-        var value = document.getElementById("password").value;
-        var value2 = document.getElementById("password2").value;
-        if(!value2=='' && !value==''){
-            if(value2==value){
-                $('#password2Message').removeClass().addClass('darkgreen').text("Passwords Match").css('font-weight','700');
-                $("label[for='password2']").removeClass();
+        var value = document.getElementById('password').value;
+        var value2 = document.getElementById('password2').value;
+        if (!value2=='' && !value=='') {
+            if (value2==value) {
+                $('#password2Message').removeClass().addClass('darkgreen').text('Passwords Match').css('font-weight','700');
+                $('label[for="password2"]').removeClass();
                 $(document.getElementById('password2')).removeClass().addClass('required');
             }
-            else{
+            else {
                 $(document.getElementById('password2')).removeClass().addClass('required invalid');
-                $("label[for='password2']").removeClass().addClass('invalid');
-                $('#password2Message').removeClass().addClass('invalid').text("Passwords didnt match");
+                $('label[for="password2"]').removeClass().addClass('invalid');
+                $('#password2Message').removeClass().addClass('invalid').text('Passwords didnt match');
             }
         }
-        else{
-            $("label[for='password2']").removeClass();
+        else {
+            $('label[for="password2"]').removeClass();
             $(document.getElementById('password2')).removeClass().addClass('required');
-            $('#password2Message').removeClass().text("");
+            $('#password2Message').removeClass().text('');
         }
     });
     
     $('#emailAddr').on('blur', function(){
         var value_email = $(this).val();
-        if(!value_email==''){
+        if (!value_email=='') {
         $.ajax({
-                url: "/account/email/rtverify?email="+value_email,
+                url: '/account/email/rtverify?email='+value_email,
                 type: 'GET',
                 success: function(result) {
-                    if(result.output == true){
+                    if (result.output == true) {
                         console.log(result);
-                        $('#emailAddrMessage').removeClass().addClass('darkgreen').text("Email id is valid").css('font-weight','700');
-                        $("label[for='emailAddr']").removeClass();
+                        $('#emailAddrMessage').removeClass().addClass('darkgreen').text('Email id is valid').css('font-weight','700');
+                        $('label[for="emailAddr"]').removeClass();
                         $(document.getElementById('emailAddr')).removeClass().addClass('required');
                     }
-                    else{
+                    else {
                         $(document.getElementById('emailAddr')).removeClass().addClass('required invalid');
-                        $("label[for='emailAddr']").removeClass().addClass('invalid');
-                        $('#emailAddrMessage').removeClass().addClass('invalid').text("Email id is not valid");
+                        $('label[for="emailAddr"]').removeClass().addClass('invalid');
+                        $('#emailAddrMessage').removeClass().addClass('invalid').text('Email id is not valid');
                     }
                 }
             });
         }
-        else{
-            $("label[for='emailAddr']").removeClass();
+        else {
+            $('label[for="emailAddr"]').removeClass();
             $(document.getElementById('emailAddr')).removeClass().addClass('required');
-            $('#emailAddrMessage').removeClass().text("");
+            $('#emailAddrMessage').removeClass().text('');
         }
     });
 }
