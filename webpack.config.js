81694cbd4f24308639f118c37439508fa004258f
Drini Cami <cdrini@gmail.com>
2020-06-19 12:00:45 -0400

    Revert changes to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,106 +8,102 @@ const
     // output.path.
     distDir = path.resolve(__dirname, 'static/build');
 
-function buildEntryPoint(entry, output) {
-    return {
-        // Apply the rule of silence: https://wikipedia.org/wiki/Unix_philosophy.
-        stats: {
-            all: false,
-            // Output a timestamp when a build completes. Useful when watching files.
-            builtAt: true,
-            errors: true,
-            warnings: true
-        },
+module.exports = {
+    // Apply the rule of silence: https://wikipedia.org/wiki/Unix_philosophy.
+    stats: {
+        all: false,
+        // Output a timestamp when a build completes. Useful when watching files.
+        builtAt: true,
+        errors: true,
+        warnings: true
+    },
 
-        // Fail on the first build error instead of tolerating it for prod builds. This seems to
-        // correspond to optimization.noEmitOnErrors.
-        bail: prod,
+    // Fail on the first build error instead of tolerating it for prod builds. This seems to
+    // correspond to optimization.noEmitOnErrors.
+    bail: prod,
 
-        // Specify that all paths are relative the Webpack configuration directory not the current
-        // working directory.
-        context: __dirname,
+    // Specify that all paths are relative the Webpack configuration directory not the current
+    // working directory.
+    context: __dirname,
 
-        // A map of ResourceLoader module / entry chunk names to JavaScript files to pack.
-        entry: { [output]: entry },
+    // A map of ResourceLoader module / entry chunk names to JavaScript files to pack.
+    entry: {
+        all: './openlibrary/plugins/openlibrary/js/index.js'
+    },
 
-        resolve: {
-            alias: {}
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery'
-            })
-        ],
-        module: {
-            rules: [ {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        // Beware of https://github.com/babel/babel-loader/issues/690.
-                        // Changes to browsers require manual invalidation.
-                        cacheDirectory: true
-                    }
+    resolve: {
+        alias: {}
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ],
+    module: {
+        rules: [ {
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    // Beware of https://github.com/babel/babel-loader/issues/690.
+                    // Changes to browsers require manual invalidation.
+                    cacheDirectory: true
                 }
-            }, {
-                test: /\.less$/,
-                loader: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader' // compiles Less to CSS
-                ]
-            } ]
+            }
+        }, {
+            test: /\.less$/,
+            loader: [
+                'style-loader',
+                'css-loader',
+                'less-loader' // compiles Less to CSS
+            ]
+        } ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                // Turn off webpack's default 'vendors' cache group. If this is desired
+                // later on, we can explicitly turn this on for clarity.
+                // https://webpack.js.org/plugins/split-chunks-plugin/#optimization-splitchunks
+                vendors: false,
+
+            }
         },
-        optimization: {
-            splitChunks: {
-                cacheGroups: {
-                    // Turn off webpack's default 'vendors' cache group. If this is desired
-                    // later on, we can explicitly turn this on for clarity.
-                    // https://webpack.js.org/plugins/split-chunks-plugin/#optimization-splitchunks
-                    vendors: false,
+        // Don't produce production output when a build error occurs.
+        noEmitOnErrors: prod
+    },
 
-                }
-            },
-            // Don't produce production output when a build error occurs.
-            noEmitOnErrors: prod
-        },
+    output: {
+        // Specify the destination of all build products.
+        path: distDir,
+        // base path for build products when referenced from production
+        // (see https://webpack.js.org/guides/public-path/)
+        publicPath: '/static/build/',
 
-        output: {
-            // Specify the destination of all build products.
-            path: distDir,
-            // base path for build products when referenced from production
-            // (see https://webpack.js.org/guides/public-path/)
-            publicPath: '/static/build/',
+        // Store outputs per module in files named after the modules. For the JavaScript entry
+        // itself, append .js to each ResourceLoader module entry name.
+        filename: '[name].js',
 
-            // Store outputs per module in files named after the modules. For the JavaScript entry
-            // itself, append .js to each ResourceLoader module entry name.
-            filename: '[name].js',
+        // This option determines the name of **non-entry** chunk files.
+        chunkFilename: '[name].[contenthash].js',
 
-            // This option determines the name of **non-entry** chunk files.
-            chunkFilename: '[name].[contenthash].js',
+        // Expose the module.exports of each module entry chunk through the global
+        // ol (open library)
+        library: [ 'ol' ],
+        libraryTarget: 'this'
+    },
 
-            // Expose the module.exports of each module entry chunk through the global
-            // ol (open library)
-            library: [ 'ol' ],
-            libraryTarget: 'this'
-        },
+    // Accurate source maps at the expense of build time.
+    // The source map is intentionally exposed
+    // to users via sourceMapFilename for prod debugging.
+    devtool: 'source-map',
+    mode: prod,
 
-        // Accurate source maps at the expense of build time.
-        // The source map is intentionally exposed
-        // to users via sourceMapFilename for prod debugging.
-        devtool: 'source-map',
-        mode: prod,
-
-        performance: {
-            maxAssetSize: 703 * 1024,
-            maxEntrypointSize: 703 * 1024,
-            // Size violations for prod builds fail; development builds are unchecked.
-            hints: prod ? 'error' : false
-        }
+    performance: {
+        maxAssetSize: 703 * 1024,
+        maxEntrypointSize: 703 * 1024,
+        // Size violations for prod builds fail; development builds are unchecked.
+        hints: prod ? 'error' : false
     }
-}
-
-module.exports = [
-    buildEntryPoint('./openlibrary/plugins/openlibrary/js/index.js', 'all'),
-];
+};
