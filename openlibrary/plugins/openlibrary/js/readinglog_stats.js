721f5d67babebed359a9032779ab9e91f0e41266
Drini Cami <cdrini@gmail.com>
2020-06-19 11:56:20 -0400

    Fix eslint for readinglog_stats.js

--- openlibrary/plugins/openlibrary/js/readinglog_stats.js
+++ openlibrary/plugins/openlibrary/js/readinglog_stats.js
@@ -6,11 +6,11 @@ import entries from 'lodash/entries';
 import groupBy from 'lodash/groupBy';
 import uniq from 'lodash/uniq';
 import uniqBy from 'lodash/uniqBy';
-import 'chart.js';
+import Chart from 'chart.js';
 import 'chartjs-plugin-datalabels';
 
 /**
- * @param {object} config 
+ * @param {object} config
  * @param {object[]} config.works
  * @param {object[]} config.authors
  * @param {string} config.lang
@@ -38,7 +38,7 @@ export function init(config) {
         const bars = orderBy(entries(grouped), x => x[1].length, 'desc').slice(0, 20);
         canvas.height = bars.length * 20 + 5;
         canvas.width= 400;
-        const chart = new Chart(canvas.getContext('2d'), {
+        new Chart(canvas.getContext('2d'), {
             type: 'horizontalBar',
             data: {
                 labels: bars.map(b => b[0]),
@@ -59,7 +59,7 @@ export function init(config) {
                 onClick: (e, [chartEl]) => {
                     if (chartEl) {
                         const bar = bars[chartEl._index];
-                        document.querySelector('.selected-works--list').innerHTML = render_works_list(bar[1]);
+                        document.querySelector('.selected-works--list').innerHTML = window.render_works_list(bar[1]);
                     } else {
                         document.querySelector('.selected-works--list').innerHTML = '';
                     }
@@ -75,7 +75,7 @@ export function init(config) {
             }
         });
 
-        $(render_excluded_works_list(excluded, config.works.length)).appendTo(container);
+        $(window.render_excluded_works_list(excluded, config.works.length)).appendTo(container);
     }
 
     const SPARQL_FIELDS = [
@@ -90,61 +90,61 @@ export function init(config) {
         return `
             SELECT DISTINCT ?x ?xLabel ?olid
                 ${
-                    SPARQL_FIELDS.map(f => `?${f.name} ` + (f.type == 'uri' ? `?${f.name}Label ` : '')).join('')
-                }
+    SPARQL_FIELDS.map(f => `?${f.name} ${f.type == 'uri' ? `?${f.name}Label ` : ''}`).join('')
+}
             WHERE {
               VALUES ?olids { ${authors.map(a => `"${a.key.split('/')[2]}"`).join(' ')} }
               ?x wdt:P648 ?olids;
                  wdt:P648 ?olid.
 
               ${
-                SPARQL_FIELDS.map(f => `OPTIONAL { ?x ${f.relation} ?${f.name}. }`)
-                .join('\n')
-              }
+    SPARQL_FIELDS.map(f => `OPTIONAL { ?x ${f.relation} ?${f.name}. }`)
+        .join('\n')
+}
 
               SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],${config.lang},en". }
             }
         `;
     }
 
-    document.getElementById('wd-query-sample').href = 'https://query.wikidata.org/#' + encodeURIComponent(buildSparql(config.authors.slice(0, 20)));
+    document.getElementById('wd-query-sample').href = `https://query.wikidata.org/#${encodeURIComponent(buildSparql(config.authors.slice(0, 20)))}`;
 
     const wdPromise = fetch('https://query.wikidata.org/sparql?format=json', {
         method: 'POST',
         body: new URLSearchParams({query: buildSparql(config.authors)})
     })
-    .then(r => r.json())
-    .then(resp => {
-        const bindings = resp.results.bindings;
-        const grouped = groupBy(bindings, o => o.x.value.split('/')[4]);
-        const records = entries(grouped).map(([qid, bindings]) => {
-            const record = { qid, olids: uniq(bindings.map(x => x.olid.value)) };
-            // { qid: Q123, olids: [ { value: }, {value: }], blah: [ {value:}, {value:} ], blahLabel: [{value:}, {value:},
-            for (const {name, type} of SPARQL_FIELDS) {
-                if (type == 'uri') {
+        .then(r => r.json())
+        .then(resp => {
+            const bindings = resp.results.bindings;
+            const grouped = groupBy(bindings, o => o.x.value.split('/')[4]);
+            const records = entries(grouped).map(([qid, bindings]) => {
+                const record = { qid, olids: uniq(bindings.map(x => x.olid.value)) };
+                // { qid: Q123, olids: [ { value: }, {value: }], blah: [ {value:}, {value:} ], blahLabel: [{value:}, {value:},
+                for (const {name, type} of SPARQL_FIELDS) {
+                    if (type == 'uri') {
                     // need to dedupe whilst keeping labels in mind
-                    const deduped = uniqBy(
-                        bindings
-                        .filter(x => x[name])
-                        .map(x => ({ [name]: x[name], [name + 'Label']: x[name + 'Label'] })),
-                        x => x[name].value)
-                    record[name] = deduped.map(x => x[name]);
-                    record[name + 'Label'] = deduped.map(x => x[name + 'Label']);
-                } else {
-                    record[name] = uniqBy(bindings.map(x => x[name]), 'value');
+                        const deduped = uniqBy(
+                            bindings
+                                .filter(x => x[name])
+                                .map(x => ({ [name]: x[name], [`${name}Label`]: x[`${name}Label`] })),
+                            x => x[name].value)
+                        record[name] = deduped.map(x => x[name]);
+                        record[`${name}Label`] = deduped.map(x => x[`${name}Label`]);
+                    } else {
+                        record[name] = uniqBy(bindings.map(x => x[name]), 'value');
+                    }
                 }
-            }
-            return record;
-        });
+                return record;
+            });
 
-        for (const record of records) {
-            for (const olid of record.olids) {
-                if (`/authors/${olid}` in authors_by_id) {
-                    authors_by_id[`/authors/${olid}`].wd = record;
+            for (const record of records) {
+                for (const olid of record.olids) {
+                    if (`/authors/${olid}` in authors_by_id) {
+                        authors_by_id[`/authors/${olid}`].wd = record;
+                    }
                 }
             }
-        }
-    });
+        });
 
     // Add full authors to the works objects for easy reference
     for (const work of config.works) {
@@ -165,8 +165,8 @@ export function init(config) {
 }
 
 /**
- * @param {object} obj 
- * @param {string} key 
+ * @param {object} obj
+ * @param {string} key
  * @return {any}
  */
 function getPath(obj, key) {
