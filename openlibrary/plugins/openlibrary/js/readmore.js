dc191cda381266ac0aec2516b1140b04682c267d
Michael E. Karpeles (mek) <michael.karpeles@gmail.com>
2020-07-05 08:52:34 +0000

    fixing css + js lint

--- openlibrary/plugins/openlibrary/js/readmore.js
+++ openlibrary/plugins/openlibrary/js/readmore.js
@@ -1,31 +1,29 @@
-export function initReadMoreButton() { 
-    var el, p, up;
-    $('.read-more-button').on("click",function(){
-        el = $(this);
-        p  = el.parent();
-        up = p.parent();
-        $('.'+up.attr('class')+'-content').removeClass("restricted-height", 300);
-        $('.'+up.attr('class')+'.read-more').addClass("hidden");
-        $('.'+up.attr('class')+'.read-less').removeClass("hidden");
-    });
-    $('.read-less-button').on("click",function(){
-        el = $(this);
-        p  = el.parent();
-        up = p.parent();
-        $('.'+up.attr('class')+'-content').addClass("restricted-height", 300);
-        $('.'+up.attr('class')+'.read-more').removeClass("hidden");
-        $('.'+up.attr('class')+'.read-less').addClass("hidden");
-    });
+export function initReadMoreButton() {
+    var el, p, up, height;
+    var cls; // eslint-disable-line no-unused-vars
 
-    var all, height;
-    $(".restricted-view").each(function() {
+    $('.read-more-button').on('click',function(){
+        el = $(this);
+        p  = el.parent();
+        up = p.parent();
+        cls =  up.attr('class'); // eslint-disable-line no-unused-vars
+        $('.${cls}-content').removeClass('restricted-height', 300);
+        $('.${cls}.read-more').addClass('hidden');
+        $('.${cls}.read-less').removeClass('hidden');
+    });
+    $('.read-less-button').on('click',function(){
+        el = $(this);
+        p  = el.parent();
+        up = p.parent();
+        cls =  up.attr('class'); // eslint-disable-line no-unused-vars
+        $('.${cls}-content').addClass('restricted-height', 300);
+        $('.${cls}.read-more').removeClass('hidden');
+        $('.${cls}.read-less').addClass('hidden');
+    });
+    $('.restricted-view').each(function() {
         height = $(this).outerHeight();
-        if(height<50){
-            var parent;
-            parent = $(this).parent();
-        }
-        else{
-            $(this).addClass("restricted-height");
+        if (height>50){
+            $(this).addClass('restricted-height');
         }
     });
 }
