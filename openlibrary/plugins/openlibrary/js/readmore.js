1275ba183f366a499776ada079c221bb5135cec4
Mek <michael.karpeles@gmail.com>
2020-07-05 10:28:04 -0700

    fix hide readmore if height below 50px & rm spaces lint

--- openlibrary/plugins/openlibrary/js/readmore.js
+++ openlibrary/plugins/openlibrary/js/readmore.js
@@ -6,21 +6,23 @@ export function initReadMoreButton() {
         el = $(this);
         p  = el.parent();
         up = p.parent();
-        $(`.${ up.attr('class') }-content`).removeClass('restricted-height', 300);
-        $(`.${ up.attr('class') }.read-more`).addClass('hidden');
-        $(`.${ up.attr('class') }.read-less`).removeClass('hidden');
+        $(`.${up.attr('class')}-content`).removeClass('restricted-height', 300);
+        $(`.${up.attr('class')}.read-more`).addClass('hidden');
+        $(`.${up.attr('class')}.read-less`).removeClass('hidden');
     });
     $('.read-less-button').on('click',function(){
         el = $(this);
         p  = el.parent();
         up = p.parent();
-        $(`.${ up.attr('class') }-content`).addClass('restricted-height', 300);
-        $(`.${ up.attr('class') }.read-more`).removeClass('hidden');
-        $(`.${ up.attr('class') }.read-less`).addClass('hidden');
+        $(`.${up.attr('class')}-content`).addClass('restricted-height', 300);
+        $(`.${up.attr('class')}.read-more`).removeClass('hidden');
+        $(`.${up.attr('class')}.read-less`).addClass('hidden');
     });
     $('.restricted-view').each(function() {
         height = $(this).outerHeight();
-        if (height>50){
+        if (height<50) {
+            $(`.${$(this).parent().attr('class')}.read-more`).addClass("hidden");
+        } else {
             $(this).addClass('restricted-height');
         }
     });
