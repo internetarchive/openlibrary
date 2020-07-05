b4279e3e1ac90e894290bfbdba0008d58acfb452
Michael E. Karpeles (mek) <michael.karpeles@gmail.com>
2020-07-05 11:17:21 +0000

    unbreaking readmore js

--- openlibrary/plugins/openlibrary/js/readmore.js
+++ openlibrary/plugins/openlibrary/js/readmore.js
@@ -6,19 +6,17 @@ export function initReadMoreButton() {
         el = $(this);
         p  = el.parent();
         up = p.parent();
-        cls =  up.attr('class'); // eslint-disable-line no-unused-vars
-        $('.${cls}-content').removeClass('restricted-height', 300);
-        $('.${cls}.read-more').addClass('hidden');
-        $('.${cls}.read-less').removeClass('hidden');
+        $(`.${ up.attr('class') }-content`).removeClass('restricted-height', 300);
+        $(`.${ up.attr('class') }.read-more`).addClass('hidden');
+        $(`.${ up.attr('class') }.read-less`).removeClass('hidden');
     });
     $('.read-less-button').on('click',function(){
         el = $(this);
         p  = el.parent();
         up = p.parent();
-        cls =  up.attr('class'); // eslint-disable-line no-unused-vars
-        $('.${cls}-content').addClass('restricted-height', 300);
-        $('.${cls}.read-more').removeClass('hidden');
-        $('.${cls}.read-less').addClass('hidden');
+        $(`.${ up.attr('class') }-content`).addClass('restricted-height', 300);
+        $(`.${ up.attr('class') }.read-more`).removeClass('hidden');
+        $(`.${ up.attr('class') }.read-less`).addClass('hidden');
     });
     $('.restricted-view').each(function() {
         height = $(this).outerHeight();
