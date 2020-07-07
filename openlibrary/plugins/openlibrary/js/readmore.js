8e1f1e50a308334df9b5f7ce930b5d671616ca5d
Michael E. Karpeles <michael.karpeles@gmail.com>
2020-07-07 16:48:49 -0400

    Small fixes + CR comments

--- openlibrary/plugins/openlibrary/js/readmore.js
+++ openlibrary/plugins/openlibrary/js/readmore.js
@@ -1,23 +1,18 @@
 export function initReadMoreButton() {
     $('.read-more-button').on('click',function(){
-        const el = $(this);
-        const p = el.parent();
-        const up = p.parent();
+        const up = $(this).parent().parent();
         $(`.${up.attr('class')}-content`).removeClass('restricted-height', 300);
         $(`.${up.attr('class')}.read-more`).addClass('hidden');
         $(`.${up.attr('class')}.read-less`).removeClass('hidden');
     });
     $('.read-less-button').on('click',function(){
-        const el = $(this);
-        const p = el.parent();
-        const up = p.parent();
+        const up = $(this).parent().parent();
         $(`.${up.attr('class')}-content`).addClass('restricted-height', 300);
         $(`.${up.attr('class')}.read-more`).removeClass('hidden');
         $(`.${up.attr('class')}.read-less`).addClass('hidden');
     });
     $('.restricted-view').each(function() {
-        const height = $(this).outerHeight();
-        if (height<50) {
+        if ($(this).outerHeight()<50) {
             $(`.${$(this).parent().attr('class')}.read-more`).addClass('hidden');
         } else {
             $(this).addClass('restricted-height');
