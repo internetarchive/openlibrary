c33f24971514e21eb5013a4eeb7e151d8dbd6ba9
Michael E. Karpeles (mek) <michael.karpeles@gmail.com>
2020-07-07 00:43:40 +0000

    adding WebPackChunk + adding filesize limit for readmore

--- openlibrary/plugins/openlibrary/js/readmore.js
+++ openlibrary/plugins/openlibrary/js/readmore.js
@@ -1,24 +1,22 @@
 export function initReadMoreButton() {
-    var el, p, up, height;
-
     $('.read-more-button').on('click',function(){
-        el = $(this);
-        p  = el.parent();
-        up = p.parent();
+        const el = $(this);
+        const p = el.parent();
+        const up = p.parent();
         $(`.${up.attr('class')}-content`).removeClass('restricted-height', 300);
         $(`.${up.attr('class')}.read-more`).addClass('hidden');
         $(`.${up.attr('class')}.read-less`).removeClass('hidden');
     });
     $('.read-less-button').on('click',function(){
-        el = $(this);
-        p  = el.parent();
-        up = p.parent();
+        const el = $(this);
+        const p = el.parent();
+        const up = p.parent();
         $(`.${up.attr('class')}-content`).addClass('restricted-height', 300);
         $(`.${up.attr('class')}.read-more`).removeClass('hidden');
         $(`.${up.attr('class')}.read-less`).addClass('hidden');
     });
     $('.restricted-view').each(function() {
-        height = $(this).outerHeight();
+        const height = $(this).outerHeight();
         if (height<50) {
             $(`.${$(this).parent().attr('class')}.read-more`).addClass('hidden');
         } else {
