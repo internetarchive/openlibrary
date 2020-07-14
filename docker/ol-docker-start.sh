1f712b7bd9619e38b06e09c5f789c8f277415bde
Drini Cami <cdrini@gmail.com>
2020-07-14 01:32:36 -0400

    Fix pyenv not working inside su

--- docker/ol-docker-start.sh
+++ docker/ol-docker-start.sh
@@ -6,9 +6,9 @@
 CONFIG=conf/openlibrary.yml
 COVER_CONFIG=conf/coverstore.yml
 
-python global "${PYTHON:-2.7.6}"
+python --version
 
-if [[ "$PYTHON" = python3* || -n $INFOGAMI ]]; then
+if [[ "$PYENV_VERSION" = 3* || -n $INFOGAMI ]]; then
   pushd vendor/infogami
   # Use Python 3 compatible infogami
   git pull origin "${INFOGAMI:-master}"
@@ -36,23 +36,30 @@ echo "Starting ol services."
 # postgres
 su postgres -c "/etc/init.d/postgresql start"
 
+# su doesn't forward any environment variables, which kinda breaks pyenv
+# So we include the variables pyenv needs here to forward
+read -r -d '' PY_ENV_VARS << EOM
+PATH="$PATH"
+PYENV_VERSION="$PYENV_VERSION"
+EOM
+
 # infobase
-su openlibrary -c "scripts/infobase-server conf/infobase.yml 7000" &
+su openlibrary -c "$PY_ENV_VARS && scripts/infobase-server conf/infobase.yml 7000" &
 
 # wait unit postgres is ready, then reindex solr
 export -f reindex-solr
-su openlibrary -c "until pg_isready; do sleep 5; done && reindex-solr localhost $CONFIG" &
+su openlibrary -c "$PY_ENV_VARS && until pg_isready; do sleep 5; done && reindex-solr localhost $CONFIG" &
 
 # solr updater
-su solrupdater -c "python scripts/new-solr-updater.py \
+su solrupdater -c "$PY_ENV_VARS && python scripts/new-solr-updater.py \
   -c $CONFIG \
   --state-file solr-update.offset \
   --ol-url http://web/" &
 
 # In dev mode, run the coverstore locally (in the background)
-su openlibrary -c "scripts/coverstore-server $COVER_CONFIG \
+su openlibrary -c "$PY_ENV_VARS && scripts/coverstore-server $COVER_CONFIG \
     --gunicorn --workers 1 --max-requests 250 --bind :8081" &
 
 # ol server, running in the foreground to avoid exiting container
-su openlibrary -c "authbind --deep scripts/openlibrary-server $CONFIG \
+su openlibrary -c "$PY_ENV_VARS && authbind --deep scripts/openlibrary-server $CONFIG \
                      --gunicorn --reload --workers 4 --timeout 180 --bind :80"
