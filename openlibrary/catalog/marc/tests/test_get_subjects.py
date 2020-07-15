8664d7c5301c3da6e5d06c0a1af4234bd86220f6
Charles Horn <charles.horn@gmail.com>
2020-07-15 18:14:09 +1200

    stop repairing broken MARC records on the fly in tests, fix the records properly

--- openlibrary/catalog/marc/tests/test_get_subjects.py
+++ openlibrary/catalog/marc/tests/test_get_subjects.py
@@ -114,10 +114,7 @@ class TestSubjects:
     @pytest.mark.parametrize('item,expected', bin_samples)
     def test_subjects_bin(self, item, expected):
         filename = os.path.dirname(__file__) + '/test_data/bin_input/' + item
-
         data = open(filename).read()
-        if len(data) != int(data[:5]):
-            data = data.decode('utf-8').encode('raw_unicode_escape')
         rec = MarcBinary(data)
         assert read_subjects(rec) == expected
 
