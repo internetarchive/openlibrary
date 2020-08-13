b82491051213a487b49fa07c7201d639fe3915a6
Charles Horn <charles.horn@gmail.com>
2020-08-13 15:14:50 +1200

    update py3 test script

--- scripts/test_py3.sh
+++ scripts/test_py3.sh
@@ -3,7 +3,7 @@
 pytest . \
        --ignore=openlibrary/catalog/marc/tests/test_get_subjects.py \
        --ignore=openlibrary/catalog/marc/tests/test_parse.py \
-       --ignore=openlibrary/tests/catalog/test_get_ia.py \
+       --ignore=openlibrary/catalog/add_book/tests/test_add_book.py \
        --ignore=openlibrary/plugins/openlibrary/tests/test_home.py \
        --ignore=openlibrary/records/tests \
        --ignore=tests/integration \
@@ -12,9 +12,9 @@ pytest . \
        --ignore=vendor
 RETURN_CODE=$?
        
-pytest --show-capture=all -v openlibrary/catalog/marc/tests/test_get_subjects.py || true  # internetarchive/openlibrary#3584
-pytest --show-capture=all -v openlibrary/catalog/marc/tests/test_parse.py || true  # internetarchive/openlibrary#3584
-pytest --show-capture=all -v openlibrary/tests/catalog/test_get_ia.py || true  # internetarchive/openlibrary#3584
+pytest --show-capture=all -v openlibrary/catalog/marc/tests/test_get_subjects.py || true
+pytest --show-capture=all -v openlibrary/catalog/marc/tests/test_parse.py || true
+pytest --show-capture=all -v openlibrary/catalog/add_book/tests/test_add_book.py || true
 pytest --show-capture=all -v openlibrary/plugins/openlibrary/tests/test_home.py || true  # internetarchive/openlibrary#3670
 flake8 --exit-zero --count --select=E722 --show-source  # Show all the bare exceptions
 safety check || true  # Show any insecure dependencies
