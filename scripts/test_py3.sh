57a79d07e7d554d971e1c6c6932768e4cb513676
cclauss <cclauss@me.com>
2020-08-14 18:04:32 +0200

    Fix-pytests-for-openlibrary/records-again

--- scripts/test_py3.sh
+++ scripts/test_py3.sh
@@ -4,8 +4,6 @@ pytest . \
        --ignore=openlibrary/catalog/marc/tests/test_get_subjects.py \
        --ignore=openlibrary/catalog/marc/tests/test_parse.py \
        --ignore=openlibrary/tests/catalog/test_get_ia.py \
-       --ignore=openlibrary/plugins/openlibrary/tests/test_home.py \
-       --ignore=openlibrary/records/tests \
        --ignore=tests/integration \
        --ignore=scripts/2011 \
        --ignore=infogami \
@@ -15,8 +13,7 @@ RETURN_CODE=$?
 pytest --show-capture=all -v openlibrary/catalog/marc/tests/test_get_subjects.py || true  # internetarchive/openlibrary#3584
 pytest --show-capture=all -v openlibrary/catalog/marc/tests/test_parse.py || true  # internetarchive/openlibrary#3584
 pytest --show-capture=all -v openlibrary/tests/catalog/test_get_ia.py || true  # internetarchive/openlibrary#3584
-pytest --show-capture=all -v openlibrary/plugins/openlibrary/tests/test_home.py || true  # internetarchive/openlibrary#3670
-flake8 --exit-zero --count --select=E722 --show-source  # Show all the bare exceptions
+flake8 --exit-zero --count --select=E722,F403 --show-source --statistics  # Show bare exceptions and wildcard (*) imports
 safety check || true  # Show any insecure dependencies
 
 exit ${RETURN_CODE}
