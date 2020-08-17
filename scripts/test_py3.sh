b96e71102a76b629cba5aaf114ad829f1ce9dd62
Christian Clauss <cclauss@me.com>
2020-08-17 12:21:59 +0200

    flake8 --select=E722,F403 --statistics

--- scripts/test_py3.sh
+++ scripts/test_py3.sh
@@ -16,7 +16,7 @@ pytest --show-capture=all -v openlibrary/catalog/marc/tests/test_get_subjects.py
 pytest --show-capture=all -v openlibrary/catalog/marc/tests/test_parse.py || true  # internetarchive/openlibrary#3584
 pytest --show-capture=all -v openlibrary/tests/catalog/test_get_ia.py || true  # internetarchive/openlibrary#3584
 pytest --show-capture=all -v openlibrary/plugins/openlibrary/tests/test_home.py || true  # internetarchive/openlibrary#3670
-flake8 --exit-zero --count --select=E722,F403 --show-source  # Show bare exceptions and wildcard (*) imports
+flake8 --exit-zero --count --select=E722,F403 --show-source --statistics  # Show bare exceptions and wildcard (*) imports
 safety check || true  # Show any insecure dependencies
 
 exit ${RETURN_CODE}
