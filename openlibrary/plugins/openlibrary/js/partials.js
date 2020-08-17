8872706472228464857a76eaa2d67344cd2c9595
Tabish Shaikh <tabish.shaikh91@gmail.com>
2020-08-17 13:55:54 +0530

    Carousel made to work with partials.js

--- openlibrary/plugins/openlibrary/js/partials.js
+++ openlibrary/plugins/openlibrary/js/partials.js
@@ -1,3 +1,7 @@
+import 'slick-carousel';
+import '../../../../static/css/components/carousel--js.less';
+import Carousel from './carousel/Carousel';
+
 export function initPartials() {
     jQuery(window).load(function () {
         $.ajax({
@@ -10,6 +14,15 @@ export function initPartials() {
             datatype: 'json',
             success: function (response) {
                 $('.RelatedWorksCarousel').append(response[0]);
+                const $carouselElements = $('.RelatedWorksCarousel .carousel--progressively-enhanced');
+                if ($carouselElements.length) {
+                    $carouselElements.each(function (_i, carouselElement) {
+                        Carousel.add.apply(
+                            Carousel,
+                            JSON.parse(carouselElement.dataset.config)
+                        );
+                    });
+                }
             }
         });
     });
