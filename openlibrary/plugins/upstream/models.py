0e53f4b81b533233ab2d1bac4e35a729eb30fcfe
Christian Clauss <cclauss@me.com>
2020-08-24 22:31:35 +0200

    editions.sort(key=lambda ed: ed.get_publish_year() or -sys.maxsize, reverse=True)
    https://docs.python.org/3/library/sys.html#sys.maxsize

--- openlibrary/plugins/upstream/models.py
+++ openlibrary/plugins/upstream/models.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import logging
 import re
+import sys
 import simplejson
 import web
 
@@ -678,7 +679,7 @@ class Work(models.Work):
             edition_keys = web.ctx.site.things(db_query)
 
         editions = web.ctx.site.get_many(edition_keys)
-        editions.sort(key=lambda ed: ed.get_publish_year() or 0, reverse=True)
+        editions.sort(key=lambda ed: ed.get_publish_year() or -sys.maxsize, reverse=True)
 
         availability = lending.get_availability_of_ocaids([
             ed.ocaid for ed in editions if ed.ocaid
