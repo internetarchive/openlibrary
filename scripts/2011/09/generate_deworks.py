5a52ef5a3e7fccab0676ac7692a52c34e36ebac6
cclauss <cclauss@me.com>
2020-08-29 20:06:06 +0200

    Python 3 compatibility for four files in /scripts

--- scripts/2011/09/generate_deworks.py
+++ scripts/2011/09/generate_deworks.py
@@ -13,47 +13,43 @@ Each row contains a JSON document with the following fields:
 * editions - List of editions that belong to this work
 * authors - All the authors of this work
 * ia - IA metadata for all the ia items referenced in the editions as a list
-* duplicates - dictionary of duplicates (key -> it's duplicates) of work and edition docs mentioned above
+* duplicates - dictionary of duplicates (key -> it's duplicates) of work and edition
+    docs mentioned above
 
 CHANGELOG:
 
-2013-04-26: Changed the format of denormalized documents and included duplicates/redirects.
+2020-08-29: Python 3 compatibility
+2013-04-26: Changed the format of denormalized documents and included
+            duplicates/redirects.
 2012-10-31: include IA metadata in the response
 2012-03-15: print the output to stdout so that it can be piped though gzip
 2011-10-04: first version
 
 """
-import time
-import sys
+
 import gzip
-import simplejson
-import itertools
-import os
 import logging
-#import bsddb
-import zlib
-import subprocess
-import web
-from collections import defaultdict
+import os
 import re
 import shutil
+import sys
+import time
+from collections import defaultdict
 
-import _init_path
+import simplejson
+import web
+
+import _init_path  # noqa: F401
 from openlibrary.data import mapreduce
 from openlibrary.utils import compress
 
-
-try:
-    import bsddb
-except ImportError:
-    #import bsddb3 as bsddb
-    pass
-
 logger = logging.getLogger(None)
+
 
 class DenormalizeWorksTask(mapreduce.Task):
     """Map reduce task to generate denormalized works dump from OL dump.
     """
+
     def __init__(self, ia_metadata):
         mapreduce.Task.__init__(self)
         self.ia = ia_metadata
@@ -69,7 +65,7 @@ class DenormalizeWorksTask(mapreduce.Task):
         else:
             # the requested identifier is not found.
             # returning a fake metadata
-            return {"collection":[]}
+            return {"collection": []}
 
     def close(self):
         """Removes all the temp files created for running map-reduce.
@@ -77,37 +73,37 @@ class DenormalizeWorksTask(mapreduce.Task):
         shutil.rmtree(self.tmpdir, ignore_errors=True)
 
     def map(self, key, value):
-        """Takes key and json as inputs and emits (work-key, work-json) or (work-key, edition-json)
-        for work and edition records respectively.
+        """Takes key and json as inputs and emits (work-key, work-json) or
+        (work-key, edition-json) for work and edition records respectively.
         """
         doc = simplejson.loads(value)
-        type_key = doc['type']['key']
+        type_key = doc["type"]["key"]
 
-        if type_key == '/type/edition':
-            if doc.get('works'):
-                yield doc['works'][0]['key'], value
+        if type_key == "/type/edition":
+            if doc.get("works"):
+                yield doc["works"][0]["key"], value
             else:
-                yield doc['key'], value
-        elif type_key == '/type/work':
-            yield doc['key'], value
-        elif type_key == '/type/author':
+                yield doc["key"], value
+        elif type_key == "/type/work":
+            yield doc["key"], value
+        elif type_key == "/type/author":
             # Store all authors for later use
             self.authors[key] = doc
-        elif type_key == '/type/redirect':
+        elif type_key == "/type/redirect":
             self.duplicates[doc.get("location")].append(key)
 
     def process_edition(self, e):
         if "ocaid" in e:
             ia = self.get_ia_metadata(e["ocaid"])
             if ia is not None:
-                e['_ia_meta'] = ia
+                e["_ia_meta"] = ia
 
     def reduce(self, key, values):
         docs = {}
         for json in values:
             doc = simplejson.loads(json)
             self.process_edition(doc)
-            docs[doc['key']] = doc
+            docs[doc["key"]] = doc
 
         if key.startswith("/works/"):
             work = docs.pop(key, {})
@@ -116,22 +112,30 @@ class DenormalizeWorksTask(mapreduce.Task):
             work = {}
 
         doc = {}
-        doc['work'] = work
-        doc['editions'] = docs.values()
-        doc['authors'] = [self.get_author_data(a['author']) for a in work.get('authors', []) if 'author' in a]
+        doc["work"] = work
+        doc["editions"] = docs.values()
+        doc["authors"] = [
+            self.get_author_data(a["author"])
+            for a in work.get("authors", [])
+            if "author" in a
+        ]
 
-        ia_ids = [e['ocaid'] for e in doc['editions'] if e.get('ocaid') and e['ocaid'] in self.ia]
-        doc['ia'] = [self.get_ia_metadata(ia_id) for ia_id in ia_ids]
+        ia_ids = [
+            e["ocaid"]
+            for e in doc["editions"]
+            if e.get("ocaid") and e["ocaid"] in self.ia
+        ]
+        doc["ia"] = [self.get_ia_metadata(ia_id) for ia_id in ia_ids]
 
         keys = set()
         keys.add(work.get("key"))
-        keys.update(e.get("key") for e in doc['editions'])
+        keys.update(e.get("key") for e in doc["editions"])
         if None in keys:
             keys.remove(None)
 
         duplicates = [(k, self.get_duplicates(k)) for k in keys]
         duplicates = [(k, dups) for k, dups in duplicates if dups]
-        doc['duplicates'] = dict(duplicates)
+        doc["duplicates"] = dict(duplicates)
         return key, simplejson.dumps(doc)
 
     def get_duplicates(self, key, result=None, level=0):
@@ -143,8 +147,8 @@ class DenormalizeWorksTask(mapreduce.Task):
             return result
 
         for k in self.duplicates[key]:
-            result.add(k) # add the duplicate and all it's duplicates
-            self.get_duplicates(k, result, level+1)
+            result.add(k)  # add the duplicate and all it's duplicates
+            self.get_duplicates(k, result, level + 1)
 
         return result
 
@@ -152,19 +156,21 @@ class DenormalizeWorksTask(mapreduce.Task):
         def get(key):
             return self.authors.get(key, {"key": key})
 
-        if 'key' in author:
-            key = author['key']
+        if "key" in author:
+            key = author["key"]
             return get(key)
-        elif 'author' in author and 'key' in author['author']:
-            key = author['author']['key']
+        elif "author" in author and "key" in author["author"]:
+            key = author["author"]["key"]
             return {"author": get(key)}
         else:
             return author
 
+
 class AuthorsDict:
     """Dictionary for storing author records in memory very efficiently.
     """
-    re_author = re.compile("/authors/OL(\d+)A")
+
+    re_author = re.compile(r"/authors/OL(\d+)A")
     chunk_size = 100
 
     def __init__(self):
@@ -172,7 +178,7 @@ class AuthorsDict:
         self.z = compress.Compressor('{"name": "", "personal_name": ""}')
 
     def __len__(self):
-        return len(d)
+        return len(self.d)
 
     def get(self, key, default=None):
         try:
@@ -184,7 +190,7 @@ class AuthorsDict:
         try:
             m = self.re_author.match(key)
         except TypeError:
-            print repr(key)
+            print(repr(key))
             raise
         if m:
             index = int(m.group(1))
@@ -199,7 +205,7 @@ class AuthorsDict:
         else:
             value = self.d[key]
         doc = self.decode(value)
-        doc['key'] = key
+        doc["key"] = key
         return doc
 
     def __setitem__(self, key, value):
@@ -213,16 +219,24 @@ class AuthorsDict:
             self.d[key] = self.encode(value)
 
     def encode(self, doc):
-        for k in ['type', 'key', 'latest_revision', 'revision', 'last_modified', 'created']:
+        for k in [
+            "type",
+            "key",
+            "latest_revision",
+            "revision",
+            "last_modified",
+            "created",
+        ]:
             doc.pop(k, None)
         return self.z.compress(simplejson.dumps(doc))
 
     def decode(self, text):
         doc = simplejson.loads(self.z.decompress(text))
-        doc['type'] = {'key': '/type/author'}
+        doc["type"] = {"key": "/type/author"}
         return doc
 
-def xopen(filename, mode='r'):
+
+def xopen(filename, mode="r"):
     if filename.endswith(".gz"):
         return gzip.open(filename, mode)
     elif filename == "-":
@@ -233,9 +247,9 @@ def xopen(filename, mode='r'):
     else:
         return open(filename, mode)
 
+
 def read_ia_metadata(filename):
     logger.info("BEGIN reading " + filename)
-    t0 = time.time()
     N = 500000
     d = {}
     for i, line in enumerate(xopen(filename)):
@@ -246,22 +260,25 @@ def read_ia_metadata(filename):
     logger.info("END reading " + filename)
     return d
 
+
 def read_dump(filename):
     t0 = time.time()
     N = 50000
     for i, line in enumerate(xopen(filename)):
         if i % N == 0:
             t1 = time.time()
-            dt = t1-t0
-            rate = dt and (N/dt)
+            dt = t1 - t0
+            rate = dt and (N / dt)
             t0 = t1
             logger.info("reading %d (%d docs/sec)", i, rate)
         _type, key, _revision, _last_modified, jsondata = line.strip().split("\t")
         yield key, jsondata
 
+
 def mkdir_p(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
 
 def main(dumpfile, ia_dumpfile):
     logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
@@ -271,9 +288,10 @@ def main(dumpfile, ia_dumpfile):
     task = DenormalizeWorksTask(ia_metadata)
 
     for key, json in task.process(records):
-        print key + "\t" + json
+        print(key + "\t" + json)
 
     task.close()
+
 
 def make_author_db(author_dump_file):
     logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
@@ -282,7 +300,7 @@ def make_author_db(author_dump_file):
     db = web.database(dbn="sqlite", db="authors.sqlite")
     db.printing = False
     db.query("CREATE TABLE docs (key text, value blog)")
-    db.query("PRAGMA cachesize=%d" % (1024*1024*100))
+    db.query("PRAGMA cachesize=%d" % (1024 * 1024 * 100))
     db.query("PRAGMA synchronous=OFF")
     db.query("PRAGMA journal_mode=OFF")
 
@@ -296,30 +314,32 @@ def make_author_db(author_dump_file):
     db.query("CREATE UNIQUE INDEX key_index ON docs(key)")
     logger.log("END create index")
 
+
 def make_ia_db(editions_dump_file):
-    #logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
-    #global logger
-    #logger = logging.getLogger("openlibrary")
+    # logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
+    # global logger
+    # logger = logging.getLogger("openlibrary")
     logger.info("BEGIN make_ia_db %s", editions_dump_file)
 
-    #db = bsddb.hashopen("ia.db", cachesize=1024*1024*500)
+    # db = bsddb.hashopen("ia.db", cachesize=1024*1024*500)
 
-    #from openlibrary.core import ia
+    # from openlibrary.core import ia
 
     f = open("ocaids.txt", "w")
 
     for key, json in read_dump(editions_dump_file):
         if "ocaid" in json:
             doc = simplejson.loads(json)
-            ocaid = doc.get('ocaid')
-            print >> f, ocaid
+            ocaid = doc.get("ocaid")
+            print(ocaid, file=f)
             """
             if ocaid:
                 metaxml = ia.get_metadata(ocaid)
                 db[ocaid] = simplejson.dumps(metaxml)
             """
     f.close()
-    #db.close()
+    # db.close()
+
 
 def test_AuthorsDict():
     d = AuthorsDict()
@@ -331,12 +351,13 @@ def test_AuthorsDict():
     ]
 
     for doc in docs:
-        d[doc['key']] = dict(doc)
+        d[doc["key"]] = dict(doc)
 
     for doc in docs:
-        assert d[doc['key']] == doc
+        assert d[doc["key"]] == doc
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # the --authordb and --iadb options are not in use now.
     if "--authordb" in sys.argv:
         sys.argv.remove("--authordb")
