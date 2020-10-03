2f7281bce466e886610091cb9ef9808bbdf1d8a5
cclauss <cclauss@me.com>
2020-10-03 21:17:53 +0200

    fixup! docker/ol-web1-start.sh

--- docker/ol-web1-start.sh
+++ docker/ol-web1-start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 python --version
-ln --verbose --symbolic opt/olsystem/etc/nginx/nginx.conf      /etc/nginx/nginx.conf
-ln --verbose --symbolic opt/olsystem/etc/nginx/sites-available /etc/nginx/sites-available
-ln --verbose --symbolic opt/olsystem/etc/haproxy/haproxy.cfg   /etc/haproxy/haproxy.cfg
+
+sudo rm -f /etc/haproxy/haproxy.cfg
+sudo ln --verbose --symbolic opt/olsystem/etc/haproxy/haproxy.cfg   /etc/haproxy/haproxy.cfg
+
+sudo mkdir /etc/nginx || true
+sudo ln --verbose --symbolic opt/olsystem/etc/nginx/nginx.conf      /etc/nginx/nginx.conf
+sudo ln --verbose --symbolic opt/olsystem/etc/nginx/sites-available /etc/nginx/sites-available
+
+ls -l /etc/haproxy /etc/nginx
+
 authbind --deep \
   scripts/openlibrary-server "$OL_CONFIG" \
   --gunicorn \
