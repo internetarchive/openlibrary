097ad90174cc5302867ae56e6375a7bec90e7b5c
cclauss <cclauss@me.com>
2020-10-07 22:24:01 +0200

    Setup ol-web1 again

--- docker/ol-web-start.sh
+++ docker/ol-web-start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ "$USE_NGINX" ]]; then
+  sudo mkdir /etc/nginx || true
+  sudo ln --verbose --symbolic olsystem/etc/nginx/nginx.conf      /etc/nginx/nginx.conf
+  sudo ln --verbose --symbolic olsystem/etc/nginx/sites-available /etc/nginx/sites-available
+  sudo systemctl start nginx
+fi
+
 python --version
 authbind --deep \
   scripts/openlibrary-server "$OL_CONFIG" \
