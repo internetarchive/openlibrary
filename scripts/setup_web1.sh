8817f31e749483c1b2cb6400b7d471bd4d215cec
cclauss <cclauss@me.com>
2020-10-11 17:25:55 +0200

    fixup! Clone infogami

--- scripts/setup_web1.sh
+++ scripts/setup_web1.sh
@@ -20,14 +20,17 @@ sudo useradd --no-log-init --system --gid openlibrary --create-home openlibrary
 cd /opt
 ls -l  # nothing
 
-sudo git clone https://github.com/internetarchive/openlibrary
-sudo git clone https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/internetarchive/olsystem
+# sudo git clone https://github.com/internetarchive/openlibrary
+sudo git clone https://github.com/cclauss/openlibrary
+# sudo git clone https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/internetarchive/olsystem
+sudo git pull https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/internetarchive/olsystem.git master
 sudo chown openlibrary /opt/*
 ls -l  # openlibrary, olsystem owned by openlibrary
 
 cd /opt/openlibrary
-make git
-cd infogami && sudo git pull origin master && cd ..
+sudo git checkout Setup-ol-web1-again
+sudo make git
+cd vendor/infogami && sudo git pull origin master && cd ../..
 
 sudo docker-compose down
 USE_NGINX=true sudo docker-compose -f docker-compose.yml -f docker-compose.infogami-local.yml -f docker-compose.production.yml up --no-deps -d web
