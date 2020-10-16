e61d56ac1b45c7706d2fce5c77af96188442b1ae
cclauss <cclauss@me.com>
2020-10-16 20:18:58 +0200

    sudo is not available inside the docker container

--- docker/ol-web-start.sh
+++ docker/ol-web-start.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-echo "USE_NGINX is ${USE_NGINX:false}"
-echo "pwd" ; pwd
-
 if [[ "$USE_NGINX" ]]; then
-  sudo mkdir /etc/nginx || true
-  sudo ln --verbose --symbolic /opt/olsystem/etc/nginx/nginx.conf      /etc/nginx/nginx.conf
-  sudo ln --verbose --symbolic /opt/olsystem/etc/nginx/sites-available /etc/nginx/sites-available
-  sudo systemctl start nginx
+  mkdir /etc/nginx || true
+  ln --verbose --symbolic /opt/olsystem/etc/nginx/nginx.conf      /etc/nginx/nginx.conf
+  ln --verbose --symbolic /opt/olsystem/etc/nginx/sites-available /etc/nginx/sites-available
+  systemctl start nginx
 fi
 
 python --version
-echo "pwd" ; pwd
-echo "ls -l" ; ls -l /
-echo "ls -l /olsystem" ; ls -l /olsystem || true
-echo "ls -l /opt/openlibrary/olsystem" ; ls -l /opt/openlibrary/olsystem || true
 authbind --deep \
   scripts/openlibrary-server "$OL_CONFIG" \
   --gunicorn \
