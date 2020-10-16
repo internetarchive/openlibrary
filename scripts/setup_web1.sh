99116b585df7c68f29acef7c380f16f00ec1750a
cclauss <cclauss@me.com>
2020-10-16 20:18:58 +0200

    sudo is not available inside the docker container

--- scripts/setup_web1.sh
+++ scripts/setup_web1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # CAUTION: To git clone olsystem, environment variables must be set...
+# Set $GITHUB_USERNAME or $USER will be used.
+# Set $GITHUB_TOKEN or this script will halt.
 if [[ -z ${GITHUB_TOKEN} ]]; then
     echo "FATAL: Can not git clone olsystem" ;
     exit 1 ;
@@ -22,15 +24,18 @@ ls -l  # nothing
 
 # sudo git clone https://github.com/internetarchive/openlibrary
 sudo git clone https://github.com/cclauss/openlibrary
-# sudo git clone https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/internetarchive/olsystem
-sudo git pull https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/internetarchive/olsystem.git master
+sudo git clone https://${GITHUB_USERNAME:-$USER}:${GITHUB_TOKEN}@github.com/internetarchive/olsystem
+# sudo git pull https://${GITHUB_USERNAME:-$USER}:${GITHUB_TOKEN}@github.com/internetarchive/olsystem.git master
 sudo chown openlibrary /opt/*
-ls -l  # openlibrary, olsystem owned by openlibrary
+ls -l  # containerd, olsystem, openlibrary owned by openlibrary
 
 cd /opt/openlibrary
 sudo git checkout Setup-ol-web1-again
 sudo make git
-cd vendor/infogami && sudo git pull origin master && cd ../..
+cd /opt/openlibrary/vendor/infogami && sudo git pull origin master
 
+cd /opt/openlibrary
 sudo docker-compose down
-USE_NGINX=true sudo docker-compose -f docker-compose.yml -f docker-compose.infogami-local.yml -f docker-compose.production.yml up --no-deps -d web
+export DOCKER_CLIENT_TIMEOUT=500
+export COMPOSE_HTTP_TIMEOUT=500
+sudo docker-compose -f docker-compose.yml -f docker-compose.infogami-local.yml -f docker-compose.production.yml up --no-deps -d web ; sudo docker-compose logs --tail=100 -f web
