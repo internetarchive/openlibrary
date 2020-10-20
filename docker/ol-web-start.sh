29cc9f968866410a0dcb140cb1982ed6e5bf149f
Christian Clauss <cclauss@me.com>
2020-10-20 16:02:29 +0200

    Move nginx logic into docker-compose.production.yml

--- docker/ol-web-start.sh
+++ docker/ol-web-start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [[ "$USE_NGINX" ]]; then
-  mkdir /etc/nginx || true
-  rm -f /etc/nginx/nginx.conf || true
-  ln -sfv /olsystem/etc/nginx/nginx.conf      /etc/nginx/nginx.conf
-  ln -sfv /olsystem/etc/nginx/sites-available /etc/nginx/sites-available
-  systemctl start nginx
-fi
-
 python --version
 authbind --deep \
   scripts/openlibrary-server "$OL_CONFIG" \
