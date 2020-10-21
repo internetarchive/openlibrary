befc72dd18c6e92eac78aa9f1cbd0c4a0591f717
Christian Clauss <cclauss@me.com>
2020-10-21 15:54:57 +0200

    fixup! Run nginx outside of Docker

--- scripts/setup_web1.sh
+++ scripts/setup_web1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # This script is used to provision an ol-webX node _before_ docker gets on it.
+# Assumption: nginx is up-and-running on the platform
+sudo systemctl start nginx
+
 # CAUTION: To git clone olsystem, environment variables must be set...
 # Set $GITHUB_USERNAME or $USER will be used.
 # Set $GITHUB_TOKEN or this script will halt.
@@ -10,7 +13,7 @@ fi
 
 # apt list --installed
 sudo apt-get update
-sudo apt-get install -y docker.io docker-compose
+sudo apt-get install -y docker.io docker-compose nginx
 docker --version        # 19.03.8
 docker-compose version  #  1.25.0
 sudo systemctl start docker
@@ -24,13 +27,16 @@ ls -Fla  # nothing
 
 sudo git clone https://${GITHUB_USERNAME:-$USER}:${GITHUB_TOKEN}@github.com/internetarchive/olsystem
 # sudo git pull https://${GITHUB_USERNAME:-$USER}:${GITHUB_TOKEN}@github.com/internetarchive/olsystem.git master
-sudo mkdir /etc/nginx
-sudo ln -sfv /opt/olsystem/etc/nginx/nginx.conf /etc/nginx/nginx.conf
-sudo ln -sfv /opt/olsystem/etc/nginx/sites-available /etc/nginx/sites-available
-ls -Fla /etc/nginx  # symlinks in place
+# sudo mkdir /etc/nginx
+# sudo ln -sfv /opt/olsystem /olsystem
+# sudo ln -sfv /opt/olsystem/etc/nginx/nginx.conf /etc/nginx/nginx.conf
+# sudo ln -sfv /opt/olsystem/etc/nginx/sites-available /etc/nginx/sites-available
+# ls -Fla /etc/nginx  # symlinks in place
+# sudo systemctl start nginx
+# sudo systemctl enable nginx
 
-OL_REPO=${OL_REPO:-internetarchive}
-sudo git clone https://github.com/$OL_REPO/openlibrary
+OL_DOMAIN=${OL_DOMAIN:-internetarchive}
+sudo git clone https://github.com/$OL_DOMAIN/openlibrary
 ls -Fla  # containerd, olsystem, openlibrary owned by openlibrary
 
 cd /opt/openlibrary
@@ -41,6 +47,7 @@ cd /opt/openlibrary/vendor/infogami && sudo git pull origin master
 
 cd /opt/openlibrary
 sudo docker-compose down
+sleep 2
 export DOCKER_CLIENT_TIMEOUT=500
 export COMPOSE_HTTP_TIMEOUT=500
 sudo docker-compose -f docker-compose.yml -f docker-compose.infogami-local.yml -f docker-compose.production.yml up --no-deps -d web
