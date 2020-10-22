303e7a9e205656db81d9bcf30f266f760f64463c
Drini Cami <cdrini@gmail.com>
2020-10-22 17:32:55 -0400

    Fix logging not setting stderr in solr_builder runs

--- scripts/solr_builder/index-type.sh
+++ scripts/solr_builder/index-type.sh
@@ -10,6 +10,7 @@ set -o xtrace
 # One of 'work' or 'author'
 TYPE="$1"
 INSTANCES="$2"
+LOG_DIR="$3"
 DB_TYPE="/type/${TYPE}"
 COUNT=$(psql -c "SELECT count(*) FROM test WHERE \"Type\" = '${DB_TYPE}'")
 CHUNK_SIZE=$(pymath "ceil($COUNT / $INSTANCES)")
@@ -18,10 +19,11 @@ CHUNK_SIZE=$(pymath "ceil($COUNT / $INSTANCES)")
 PARTITION=$(time psql -c "SELECT \"Key\" FROM test_get_partition_markers('${DB_TYPE}', $CHUNK_SIZE);")
 ITER=1
 for key in $PARTITION; do
-  RUN_SIG="${TYPE}s_${ITER}"
+  RUN_SIG="ol_run_${TYPE}s_${ITER}"
   # Create log files so they're acceissble sync for querying
-  touch {logs,progress}/$RUN_SIG.txt
-  docker_solr_builder "${TYPE}s" --start-at $key --limit $CHUNK_SIZE -p progress/$RUN_SIG.txt -l logs/$RUN_SIG.txt
+  mkdir -p {logs,progress}/$LOG_DIR
+  touch {logs,progress}/$LOG_DIR/$RUN_SIG.txt
+  DOCKER_IMAGE_NAME=$RUN_SIG docker_solr_builder "${TYPE}s" --start-at $key --limit $CHUNK_SIZE -p "progress/$LOG_DIR/$RUN_SIG.txt"
   ((ITER++))
   sleep 60
 done;
