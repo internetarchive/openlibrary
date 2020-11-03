6abf17a438413e45f1e24aaf850c1e475985c377
Christian Clauss <cclauss@me.com>
2020-11-03 16:44:53 +0100

    Add release to Sentry init

--- openlibrary/plugins/openlibrary/sentry.py
+++ openlibrary/plugins/openlibrary/sentry.py
@@ -1,4 +1,5 @@
 import logging
+from subprocess import PIPE, Popen, STDOUT
 
 import sentry_sdk
 
@@ -12,6 +13,11 @@ def is_enabled():
     return hasattr(infogami.config, 'sentry') and infogami.config.sentry.enabled
 
 
+def get_software_version():  # -> str:
+    cmd = "git rev-parse --short HEAD --".split()
+    return str(Popen(cmd, stdout=PIPE, stderr=STDOUT).stdout.read().decode().strip())
+
+
 def setup():
     logger.info("Setting up sentry (enabled={})".format(is_enabled()))
 
@@ -19,5 +25,6 @@ def setup():
         return
 
     sentry_sdk.init(dsn=infogami.config.sentry.dsn,
-                    environment=infogami.config.sentry.environment)
+                    environment=infogami.config.sentry.environment,
+                    release=get_software_version())
     delegate.add_exception_hook(lambda: sentry_sdk.capture_exception())
