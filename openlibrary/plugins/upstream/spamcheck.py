f7cc665382293e504125354ea55a2409d90612aa
SouthGoingZax <72705998+SouthGoingZax@users.noreply.github.com>
2020-11-21 12:00:21 -0800

    Add regular expression support to the spamcheck library.

--- openlibrary/plugins/upstream/spamcheck.py
+++ openlibrary/plugins/upstream/spamcheck.py
@@ -1,3 +1,4 @@
+import re
 import web
 
 def get_spam_words():
@@ -42,7 +43,7 @@ def is_spam(i=None):
     if i is None:
         i = web.input()
     text = str(dict(i)).lower()
-    return any(w.lower() in text for w in spamwords)
+    return any(re.search(w.lower(),text) for w in spamwords)
 
 def is_spam_email(email):
     domain = email.split("@")[-1].lower()
