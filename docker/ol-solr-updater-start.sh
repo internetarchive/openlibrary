d76f0f3843eeaf3a1cbbfc53de87a61b10d6ed97
Christian Clauss <cclauss@me.com>
2020-12-11 21:36:16 +0100

    fixup! Review feedback

--- docker/ol-solr-updater-start.sh
+++ docker/ol-solr-updater-start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 python --version
-scripts/new-solr-updater.py
-    --config $OL_CONFIG
-    --state-file $SOLR_UPDATER_STATE_FILE
-    --exclude-edits-containing $SOLR_UPDATER_EXCLUDE_EDITS_CONTAINING
-    --socket-timeout $SOLR_UPDATER_SOCKET_TIMEOUT
+python scripts/new-solr-updater.py \
+    -c $OL_CONFIG \
+    --state-file solr-update.offset \
+    --ol-url http://web/
