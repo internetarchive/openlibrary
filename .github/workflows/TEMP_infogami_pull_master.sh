8e9b9ff9891aec46627758733ca53c5e594e5788
Christian Clauss <cclauss@me.com>
2020-12-12 09:18:18 +0100

    Update TEMP_infogami_pull_master.sh

--- .github/workflows/TEMP_infogami_pull_master.sh
+++ .github/workflows/TEMP_infogami_pull_master.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ -z ${CI} ]] ; then
-    echo "Only runs under continuous integration"
-    exit 1
-fi
+# if [[ -z ${CI} ]] ; then
+#    echo "Only runs under continuous integration"
+#    exit 1
+# fi
 
 pushd /openlibrary/vendor/infogami
 git pull origin master
