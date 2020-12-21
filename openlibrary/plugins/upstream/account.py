49603f38042f98699c05716d3946d0172f5e2d59
SouthGoingZax <72705998+SouthGoingZax@users.noreply.github.com>
2020-12-21 11:07:40 -0800

    4271/feature/add goodreads import unit test (#4315)
    * Add unit test for goodreads csv import.
    
    * Lint fixes and remove unnecessary TestGoodReadsImport class.
    
    * Allow legacy Python to understand that this file contains Unicode characters
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Specify the encoding when decode()'ing csv_data.
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Separate out utf-8 and bytes test cases into separate tests.  Skip the utf-8 test for Python versions below 3.0.
    
    * Update openlibrary/plugins/upstream/account.py
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * Refactor csv parsing code in account.py
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- openlibrary/plugins/upstream/account.py
+++ openlibrary/plugins/upstream/account.py
@@ -865,24 +865,7 @@ class fetch_goodreads(delegate.page):
 
     @require_login
     def POST(self):
-        import csv
-        i = web.input(csv={})
-        csv_payload = i.csv.value if isinstance(i.csv.value, str) else i.csv.value.decode()
-        csv_file = csv.reader(csv_payload.splitlines(), delimiter=',', quotechar='"')
-        header = next(csv_file)
-        books = {}
-        books_wo_isbns = {}
-        for book in list(csv_file):
-            _book = dict(zip(header, book))
-            _book['ISBN'] = _book['ISBN'].replace('"','').replace('=','')
-            _book['ISBN13'] = _book['ISBN13'].replace('"','').replace('=','')
-            if _book['ISBN'] != '':
-                books[_book['ISBN']] = _book
-            elif _book['ISBN13'] != '':
-                books[_book['ISBN13']] = _book
-                books[_book['ISBN13']]['ISBN'] = _book['ISBN13']
-            else:
-                books_wo_isbns[_book['Book Id']] = _book
+        books, books_wo_isbns = process_goodreads_csv(web.input())
         return render['account/import'](books, books_wo_isbns)
 
 class export_books(delegate.page):
@@ -959,3 +942,24 @@ def as_admin(f):
         finally:
             web.ctx.headers = []
     return g
+
+
+def process_goodreads_csv(i):
+    import csv
+    csv_payload = i.csv if isinstance(i.csv, str) else i.csv.decode()
+    csv_file = csv.reader(csv_payload.splitlines(), delimiter=',', quotechar='"')
+    header = next(csv_file)
+    books = {}
+    books_wo_isbns = {}
+    for book in list(csv_file):
+        _book = dict(zip(header, book))
+        isbn = _book['ISBN'] = _book['ISBN'].replace('"', '').replace('=', '')
+        isbn_13 = _book['ISBN13'] = _book['ISBN13'].replace('"', '').replace('=', '')
+        if isbn != '':
+            books[isbn] = _book
+        elif isbn_13 != '':
+            books[isbn_13] = _book
+            books[isbn_13]['ISBN'] = isbn_13
+        else:
+            books_wo_isbns[_book['Book Id']] = _book
+    return books, books_wo_isbns
