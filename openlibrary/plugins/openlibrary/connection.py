2ba6765c99c5f7d54b40821ee73feadffa2f5b0c
SouthGoingZax <72705998+SouthGoingZax@users.noreply.github.com>
2020-12-31 10:25:42 -0800

    /api/save_many: Stop POST'ing byte literals in JSON values to Infogami (#4338)
    * Convert request data to bytes and HTTP errors to strings.
    
    * Replace urllib with requests module in openlibrary/api.py.  Add workaround to openlibrary/plugins/openlibrary/connection.py to avoid POSTing JSON values as bytes to Infobase.
    
    * Convert request data to bytes and HTTP errors to strings.
    
    * Replace urllib with requests module in openlibrary/api.py.  Add workaround to openlibrary/plugins/openlibrary/connection.py to avoid POSTing JSON values as bytes to Infobase.
    
    * Code review clean-up.
    
    * Default --recursive flag to True in scripts/copydocs.py

--- openlibrary/plugins/openlibrary/connection.py
+++ openlibrary/plugins/openlibrary/connection.py
@@ -86,6 +86,11 @@ class ConnectionMiddleware:
         return self.conn.request(sitename, path, 'POST', data)
 
     def save_many(self, sitename, data):
+        # Work-around for https://github.com/internetarchive/openlibrary/issues/4285
+        # Infogami seems to read encoded bytes as a string with a byte literal inside
+        # of it, which is invalid JSON and also can't be decode()'d.
+        if isinstance(data.get('query'), bytes):
+            data['query'] = data['query'].decode()
         return self.conn.request(sitename, '/save_many', 'POST', data)
 
     def store_get(self, sitename, path):
