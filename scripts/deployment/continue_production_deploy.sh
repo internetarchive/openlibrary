0601b2f76ea1d4de18a3e1c544e056f71c41caee
Christian Clauss <cclauss@me.com>
2021-01-14 19:47:17 +0100

    Update based on actual deploy

--- scripts/deployment/continue_production_deploy.sh
+++ scripts/deployment/continue_production_deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+set -o xtrace
+
 # https://github.com/internetarchive/openlibrary/wiki/Deployment-Scratchpad
 
 # This script must be run on each Open Library host to continue a new deployment.
 
-# ~2min
-time docker image prune
 
 export HOSTNAME="${HOSTNAME:-$HOST}"
-if [[ $HOSTNAME != "ol-home0" ]]; then
+if [[ $HOSTNAME != ol-home0.* ]]; then
+    # ~2 - 4 min
+    time docker image prune -f
     # Decompress the .tar.gz image that was transfered from ol-home0
     cd /opt/olimages
     # ~4min
@@ -16,7 +18,7 @@ if [[ $HOSTNAME != "ol-home0" ]]; then
 
     # `sudo git pull origin master` the core Open Library repos:
     # 1. https://github.com/internetarchive/olsystem
-    # 2. https://git.archive.org/jake/booklending_utils
+    # 2. https://git.archive.org/jake/booklending_utils -- ONLY ON WEB NODES!!!
     # 3. https://github.com/internetarchive/openlibrary
     # 4. https://github.com/internetarchive/infogami
     REPO_DIRS="/opt/olsystem /opt/booklending_utils /opt/openlibrary /opt/openlibrary/vendor/infogami"
@@ -25,6 +27,8 @@ if [[ $HOSTNAME != "ol-home0" ]]; then
         cd $REPO_DIR
         sudo git pull origin master
     done
+    cd /opt/openlibrary
+    echo "FROM oldev:latest" | docker build -t "oldev:$(git rev-parse HEAD)" -
 fi
 
 cd /opt/openlibrary
@@ -32,10 +36,10 @@ export COMPOSE_FILE="docker-compose.yml:docker-compose.production.yml"
 # WARNING! Moment of downtime 😬 
 docker-compose down
 docker volume rm openlibrary_ol-vendor openlibrary_ol-build openlibrary_ol-nodemodules
-if [[ $HOSTNAME == "ol-covers0" ]]; then
+if [[ $HOSTNAME == ol-covers0.* ]]; then
     docker-compose up -d --scale covers=2 covers_nginx memcached
-elif [[ $HOSTNAME == "ol-home0" ]]; then
-    docker-compose up -d infobase infobase_nginx affiliate-server  # cronjobs importbot solr-updater
+elif [[ $HOSTNAME == ol-home0.* ]]; then
+    docker-compose up -d --no-deps infobase infobase_nginx affiliate-server  # cronjobs importbot solr-updater
 else  # start a web node
     docker-compose run -uroot --rm home make i18n
     docker-compose up --no-deps -d web
