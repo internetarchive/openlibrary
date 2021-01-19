7d8bcaf1eefbb54f049f6abd4976b6214c57725f
Christian Clauss <cclauss@me.com>
2021-01-19 17:19:05 +0100

    start continue finish

--- scripts/deployment/continue_production_deploy.sh
+++ scripts/deployment/continue_production_deploy.sh
@@ -6,7 +6,6 @@ set -o xtrace
 
 # This script must be run on each Open Library host to continue a new deployment.
 
-
 export HOSTNAME="${HOSTNAME:-$HOST}"
 if [[ $HOSTNAME != ol-home0.* ]]; then
     # ~2 - 4 min
@@ -15,32 +14,8 @@ if [[ $HOSTNAME != ol-home0.* ]]; then
     cd /opt/olimages
     # ~4min
     time docker load < /opt/olimages/oldev_latest.tar.gz
-
-    # `sudo git pull origin master` the core Open Library repos:
-    # 1. https://github.com/internetarchive/olsystem
-    # 2. https://git.archive.org/jake/booklending_utils -- ONLY ON WEB NODES!!!
-    # 3. https://github.com/internetarchive/openlibrary
-    # 4. https://github.com/internetarchive/infogami
-    REPO_DIRS="/opt/olsystem /opt/booklending_utils /opt/openlibrary /opt/openlibrary/vendor/infogami"
-    for REPO_DIR in $REPO_DIRS
-    do
-        cd $REPO_DIR
-        sudo git pull origin master
-    done
-    cd /opt/openlibrary
-    echo "FROM oldev:latest" | docker build -t "oldev:$(git rev-parse HEAD)" -
 fi
 
-cd /opt/openlibrary
-export COMPOSE_FILE="docker-compose.yml:docker-compose.production.yml"
-# WARNING! Moment of downtime 😬 
-docker-compose down
-docker volume rm openlibrary_ol-vendor openlibrary_ol-build openlibrary_ol-nodemodules
-if [[ $HOSTNAME == ol-covers0.* ]]; then
-    docker-compose up -d --scale covers=2 covers_nginx memcached
-elif [[ $HOSTNAME == ol-home0.* ]]; then
-    docker-compose up -d --no-deps infobase infobase_nginx affiliate-server  # cronjobs importbot solr-updater
-else  # start a web node
-    docker-compose run -uroot --rm home make i18n
-    docker-compose up --no-deps -d web
-fi
+# Add a git SHA tag to the Docker image to facilitate rapid rollback
+echo "FROM oldev:latest" | docker build -t "oldev:$(git rev-parse HEAD)" -
+docker image ls
--- scripts/deployment/start_production_deploy.sh
+++ scripts/deployment/start_production_deploy.sh
@@ -6,6 +6,7 @@ set -o xtrace
 
 # This script must be run on ol-home0 to start a new deployment.
 
+echo "Starting production deployment at $(date)"
 export HOSTNAME="${HOSTNAME:-$HOST}"
 if [[ $HOSTNAME != ol-home0.* ]]; then
     echo "FATAL: Must only be run on ol-home0" ;
@@ -40,21 +41,53 @@ export COMPOSE_FILE="docker-compose.yml:docker-compose.production.yml"
 time docker-compose build --pull web
 docker-compose run -uroot --rm home make i18n
 
-# Add a git SHA tag to the Docker image to facilitate rapid rollback
-echo "FROM oldev:latest" | docker build -t "oldev:$(git rev-parse HEAD)" -
-docker image ls
-
 # Compress the image in a .tar.gz file for transfer to other hosts
 cd /opt/olimages
 time docker save oldev:latest | gzip > oldev_latest.tar.gz
 
-# Transfer the .tar.gz image to other hosts
+# Transfer the .tar.gz image and four repo dirs to other hosts
 REMOTE_HOSTS="ol-covers0 ol-web1 ol-web2"
 for REMOTE_HOST in $REMOTE_HOSTS
 do
     echo "Starting rsync of oldev_latest.tar.gz to $REMOTE_HOST..."
     time rsync -a --no-owner --group --verbose oldev_latest.tar.gz "$REMOTE_HOST:/opt/olimages/"
-    echo -e "Finished rsync of oldev_latest.tar.gz to $REMOTE_HOST...\n"
+    if [[ $HOSTNAME == ol-web.* ]]; then
+        REPO_DIRS="/opt/olsystem /opt/booklending_utils /opt/openlibrary /opt/openlibrary/vendor/infogami"
+    else
+        REPO_DIRS="/opt/olsystem /opt/openlibrary /opt/openlibrary/vendor/infogami"
+    fi
+    for REPO_DIR in $REPO_DIRS
+    do
+        echo "Starting rsync of $REPO_DIR to $REMOTE_HOST..."
+        time rsync -a -r --no-owner --group --verbose $REPO_DIR "$REMOTE_HOST:$REPO_DIR"
+    done
+    echo -e "Finished rsync to $REMOTE_HOST...\n"
 done
 
-echo "Please run continue_production_deploy.sh on ol-covers0, ol-home0, ol-web1, ol-web2"
+# Uncompress and tag oldev_latest.tar.gz that we have rsynced over
+CONTINUE="/opt/openlibrary/scripts/deployment/continue_production_deployment.sh"
+bash $CONTINUE
+for REMOTE_HOST in $REMOTE_HOSTS
+do
+    ssh $REMOTE_HOST $CONTINUE
+done
+
+echo "Press return to continue the deployment..."
+read
+
+# Reboot all hosts EXCEPT ol-web2
+FINISH="/opt/openlibrary/scripts/deployment/finish_production_deployment.sh"
+bash $FINISH
+REMOTE_HOSTS="ol-covers0 ol-web1"
+for REMOTE_HOST in $REMOTE_HOSTS
+do
+    ssh $REMOTE_HOST $FINISH
+done
+
+echo "Press return to finish the deployment..."
+read
+
+# Reboot all hosts EXCEPT ol-web2
+ssh ol-web2 $FINISH
+
+echo "Finished production deployment at $(date)"
