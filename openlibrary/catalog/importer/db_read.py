0852185348a66516af6d3b6dc99c74b734a3590c
Darrel Herbst <dherbst@gmail.com>
2021-02-04 09:12:47 -0500

    Refactor db_read.py to replace urlopen with requests.

--- openlibrary/catalog/importer/db_read.py
+++ openlibrary/catalog/importer/db_read.py
@@ -3,8 +3,9 @@ import web
 import json
 from time import sleep
 
+import requests
+
 from six.moves.urllib.parse import urlencode
-from six.moves.urllib.request import urlopen
 
 
 staging = False
@@ -12,25 +13,26 @@ staging = False
 db = web.database(dbn='postgres', db='marc_index', host='ol-db')
 db.printing = False
 
-def find_author(name): # unused
-    iter = web.query('select key from thing, author_str where thing_id = id and key_id = 1 and value = $name', {'name': name })
+
+def find_author(name):  # unused
+    iter = web.query('select key from thing, author_str where thing_id = id and key_id = 1 and value = $name', {'name': name})
     return [row.key for row in iter]
 
+
 def read_from_url(url):
-    data = None
+    response = None
     for i in range(20):
         try:
-            data = urlopen(url).read()
-            if data:
-                break
-            print('data == None')
-        except IOError:
-            print('IOError')
+            response = requests.get(url)
+            response.raise_for_status()
+            break
+        except requests.HTTPError:
+            print('HTTPError')
             print(url)
         sleep(10)
-    if not data:
+    if not response:
         return None
-    ret = json.loads(data)
+    ret = response.json()
     if ret['status'] == 'fail' and ret['message'].startswith('Not Found: '):
         return None
     if ret['status'] != 'ok':
@@ -38,24 +40,30 @@ def read_from_url(url):
     assert ret['status'] == 'ok'
     return ret['result']
 
+
 def set_staging(v):
     global staging
     staging = v
 
+
 def api_url():
     return "https://openlibrary.org%s/api/" % (':8080' if staging else '')
+
 
 def api_versions(): return api_url() + "versions?"
 def api_things(): return api_url() + "things?"
 def api_get(): return api_url() + "get?key="
 
-def get_versions(q): # unused
+
+def get_versions(q):  # unused
     url = api_versions() + urlencode({'query': json.dumps(q)})
     return read_from_url(url)
+
 
 def get_things(q):
     url = api_things() + urlencode({'query': json.dumps(q)})
     return read_from_url(url)
+
 
 def get_mc(key):
     """
@@ -67,6 +75,7 @@ def get_mc(key):
     """
     found = list(db.query('select v from machine_comment where k=$key', {'key': key}))
     return found[0].v if found else None
+
 
 def withKey(key):
     def process(key):
