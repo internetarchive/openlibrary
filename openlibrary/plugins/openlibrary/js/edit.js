5d35431cf748a81c035ee9f22751f02a26bc292a
nynalaalekhya <nynalaalekhya@gmail.com>
2021-02-12 10:50:50 -0800

    Removed inline js from excerpts.html

---
+++ openlibrary/plugins/openlibrary/js/edit.js
@@ -0,0 +1,57 @@
+function error(errordiv, input, message) {
+    $(errordiv).show().html(message);
+    $(input).focus();
+    return false;
+}
+
+function update_len() {
+    var len = $('#excerpts-excerpt').val().length;
+    var color;
+    if (len > 2000) {
+        color = '#e44028';
+    } else {
+        color = 'gray';
+    }
+    $('#excerpts-excerpt-len').html(2000 - len).css('color', color);
+}
+
+function show_hide_title() {
+    if ($('#excerpts-display .repeat-item').length > 1) {
+        $('#excerpts-so-far').show();
+    } else {
+        $('#excerpts-so-far').hide();
+    }
+}
+
+
+export function initEdit() {
+
+    $('#excerpts').repeat({
+        vars: {
+            prefix: 'work--excerpts',
+        },
+        validate: function(data) {
+            if (!data.excerpt) {
+                return error('#excerpts-errors', '#excerpts-excerpt', 'Please provide an excerpt.');
+            }
+            if (data.excerpt.length > 2000) {
+                return error('#excerpts-errors', '#excerpts-excerpt', 'That excerpt is too long.')
+            }
+            $('#excerpts-errors').hide();
+            return true;
+        }
+    });
+
+    // update length on every keystroke
+    $('#excerpts-excerpt').keyup(update_len);
+
+    // update length on add.
+    $('#excerpts')
+        .bind('repeat-add', update_len)
+        .bind('repeat-add', show_hide_title)
+        .bind('repeat-remove', show_hide_title);
+
+    // update length on load
+    update_len();
+    show_hide_title();
+}
