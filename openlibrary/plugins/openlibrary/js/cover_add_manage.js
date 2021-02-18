f742064ebce9ae0a63d065a85ae8ccdc46e6988a
Noah <drakene@g.cofc.edu>
2021-02-18 13:19:34 -0500

    Further fixes for js syntax

--- openlibrary/plugins/openlibrary/js/cover_add_manage.js
+++ openlibrary/plugins/openlibrary/js/cover_add_manage.js
@@ -4,52 +4,54 @@ export default function(){
         parent.closeThrobber();
     }
     $('#form.addcover-form').submit(function(event) {
+        var file;
+        var url;
+        var coverid;
+        function val(selector) {
+            return $.trim($(selector).val());
+        }
+        function error(message) {
+            $('#errors').show().html(message);
+            event.preventDefault();
+        }
+        file = val('#coverFile');
+        url = val('#imageUrl');
+        coverid = val('#coverid');
 
-         function val(selector) {
-             return $.trim($(selector).val());
-         }
-         function error(message) {
-             $('#errors').show().html(message);
-             event.preventDefault();
-         }
-         file = val('#coverFile');
-         url = val('#imageUrl');
-         coverid = val('#coverid');
+        if (file == '' && (url == '' || url == 'http://') && coverid == '') {
+           return error('Please choose an image or provide a URL.');
+        }
 
-         if (file == '' && (url == '' || url == 'http://') && coverid == '') {
-             return error('Please choose an image or provide a URL.');
-         }
+        function test_url(url) {
+            var obj = {
+                optional: function() { return false; }
+            }
+            return $.validator.url.apply(obj, [url, null]);
+        }
 
-         function test_url(url) {
-             var obj = {
-                 optional: function() { return false; }
-             }
-             return $.validator.url.apply(obj, [url, null]);
-         }
+        if (url != '' && url != 'http://' && !test_url(url)) {
+            return error('Please provide a valid URL.');
+        }
+    });
 
-         if (url != '' && url != 'http://' && !test_url(url)) {
-             return error('Please provide a valid URL.');
-         }
-     });
+    // Clicking a cover should set the form value to the data-id of that cover
+    $('#popcovers .book').on('click', function() {
+        $(this).toggleClass('selected').siblings().removeClass('selected');
+        coverid = '';
+        if ($(this).hasClass('selected')) {
+            coverid = $(this).data('id');
+        }
+        $('#coverid').val(coverid);
+    });
 
-     // Clicking a cover should set the form value to the data-id of that cover
-     $('#popcovers .book').on('click', function() {
-         $(this).toggleClass('selected').siblings().removeClass('selected');
-         coverid = '';
-         if ($(this).hasClass('selected')) {
-             coverid = $(this).data('id');
-         }
-         $('#coverid').val(coverid);
-     });
-
-     // covers/manage.html
-     $('.column').sortable({
-         connectWith: '.trash'
-     });
-     $('.trash').sortable({
-         connectWith: '.column'
-     });
-     $('.column').disableSelection();
-     $('.trash').disableSelection();
-     $('#topNotice').hide();
+    // covers/manage.html
+    $('.column').sortable({
+        connectWith: '.trash'
+    });
+    $('.trash').sortable({
+        connectWith: '.column'
+    });
+    $('.column').disableSelection();
+    $('.trash').disableSelection();
+    $('#topNotice').hide();
 }
