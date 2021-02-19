cffa364d7fcb312444a9fa01cdd8c11524aa1425
Noah <drakene@g.cofc.edu>
2021-02-19 13:36:29 -0500

    Fix for js syntax

--- openlibrary/plugins/openlibrary/js/cover_add_manage.js
+++ openlibrary/plugins/openlibrary/js/cover_add_manage.js
@@ -3,7 +3,7 @@ export default function () {
     function val(selector) {
         return $.trim($(selector).val());
     }
-    function error(message) {
+    function error(message, event) {
         $('#errors').show().html(message);
         event.preventDefault();
     }
@@ -13,12 +13,12 @@ export default function () {
         parent.closeThrobber();
     }
     $('#form.addcover-form').on('submit', function(event) {
-        var file = val('#coverFile');;
-        var url = val('#imageUrl');;
-        var coverid = val('#coverid');;
+        var file = val('#coverFile');
+        var url = val('#imageUrl');
+        var coverid = val('#coverid');
 
         if (file === '' && (url === '' || url === 'http://') && coverid === '') {
-            return error('Please choose an image or provide a URL.');
+            return error('Please choose an image or provide a URL.', event);
         }
 
         function test_url(url) {
