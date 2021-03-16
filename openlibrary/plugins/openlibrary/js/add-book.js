fc5f48659e4744e79e6e53d92c18657f34d84db6
Clifton McCallum <clifton.mccallum@gmail.com>
2021-03-16 10:47:41 -0400

    Fixed more linting issues and syntax errors

--- openlibrary/plugins/openlibrary/js/add-book.js
+++ openlibrary/plugins/openlibrary/js/add-book.js
@@ -2,10 +2,10 @@ export function initAddBookImport () {
     window.q.push(function() {
         $('.list-books a').click(function() {
             var li = $(this).parents('li:first');
-            $('input#work').val('/works/' + $(li.attr('id')));
+            var attributeId = li.attr('id')
+            $('input#work').val('/works/' + $(attributeId));
             $('form#addbook').submit();
         });
-    
         $('#bookAddCont').click(function() {
             $('input#work').val('none-of-these');
             $('form#addbook').submit();
