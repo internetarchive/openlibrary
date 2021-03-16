bdfa8ee785b8b574f1b5f113ef2220c697992cab
Clifton McCallum <clifton.mccallum@gmail.com>
2021-03-16 16:45:42 -0400

    added semicolon after variable declaration

--- openlibrary/plugins/openlibrary/js/add-book.js
+++ openlibrary/plugins/openlibrary/js/add-book.js
@@ -2,7 +2,7 @@ export function initAddBookImport () {
     window.q.push(function() {
         $('.list-books a').click(function() {
             var li = $(this).parents('li:first');
-            var attributeId = li.attr('id')
+            var attributeId = li.attr('id');
             $('input#work').val('/works/' + $(attributeId));
             $('form#addbook').submit();
         });
