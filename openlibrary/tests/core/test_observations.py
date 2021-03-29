afe23a8622fae0de950fc77d4340e6e6c1245009
James Champ <jameschamp@acm.org>
2021-03-29 18:29:29 -0400

    Update sort values test

--- openlibrary/tests/core/test_observations.py
+++ openlibrary/tests/core/test_observations.py
@@ -1,16 +1,21 @@
-from openlibrary.core.observations import ObservationValue, _sort_values
+from openlibrary.core.observations import _sort_values
 
 def test_sort_values():
-    unsorted_list = [
-      ObservationValue(1, 'two', 3),
-      ObservationValue(2, 'four', 4),
-      ObservationValue(3, 'one', None),
-      ObservationValue(4, 'three', 1),
-      ObservationValue(5, 'five', 2)
+    orders_list = [3, 4, 2, 1]
+    values_list = [
+      {'id': 1, 'name': 'order'},
+      {'id': 2, 'name': 'in'},
+      {'id': 3, 'name': 'this'},
+      {'id': 4, 'name': 'is'}
     ]
 
     # sorted values returned given unsorted list
-    assert _sort_values(unsorted_list) == ['one', 'two', 'three', 'four', 'five']
+    assert _sort_values(orders_list, values_list) == ['this', 'is', 'in', 'order']
 
-    # an empty list is returned if an empty list is passed
-    assert _sort_values([]) == []
+    # no errors thrown when orders list contains an ID not found in the values list
+    orders_list.insert(0, 5)
+    assert _sort_values(orders_list, values_list) == ['this', 'is', 'in', 'order']
+
+    # value with ID that is not in orders list will not be included in sorted list
+    values_list.append({'id': 100, 'name': 'impossible!'})
+    assert _sort_values(orders_list, values_list) == ['this', 'is', 'in', 'order']
