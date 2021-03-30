dd2fe36c32cf1bbb5874cd8fd8aaba9566bbbb0d
Noah <drakene@g.cofc.edu>
2021-03-30 15:03:08 -0400

    Fix syntax?

--- openlibrary/plugins/openlibrary/js/admin.js
+++ openlibrary/plugins/openlibrary/js/admin.js
@@ -7,15 +7,13 @@ export function initAdmin() {
     $('a.tag').on('click', function () {
         var action;
         var tag;
-        const action_property_str = 'action'
-        const tag_property_str = 'tag'
 
         $(this).toggleClass('active');
         action = $(this).hasClass('active') ? 'add_tag': 'remove_tag';
         tag = $(this).text();
         $.post(window.location.href, {
-            action_property_str : action,
-            tag_property_str : tag
+            action: action,
+            tag: tag
         });
     });
 
