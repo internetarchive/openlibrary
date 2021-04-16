c412459ae15e4ef3a846c7d928160d556d8b102c
Drini Cami <cdrini@gmail.com>
2021-04-15 22:58:50 -0400

    Add solr8-updater to docker-compose.production.yml

--- docker/ol-solr-updater-start.sh
+++ docker/ol-solr-updater-start.sh
@@ -2,7 +2,8 @@
 
 python --version
 python scripts/new-solr-updater.py $OL_CONFIG \
-    --state-file /solr-updater-data/solr-update.offset \
+    --state-file /solr-updater-data/$STATE_FILE \
     --ol-url "$OL_URL" \
     --exclude-edits-containing 'Bot' \
-    --socket-timeout 1800
+    --socket-timeout 1800 \
+    $EXTRA_OPTS
