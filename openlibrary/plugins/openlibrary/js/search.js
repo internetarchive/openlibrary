f03373af704b7ac20186d91795a3d289328ff3f9
lephemere <73437497+lephemere@users.noreply.github.com>
2021-04-19 19:09:45 +0200

    Add searchFacets javascript file

---
+++ openlibrary/plugins/openlibrary/js/search.js
@@ -0,0 +1,45 @@
+function more(header, start_facet_count, facet_inc) {
+    const div_header = "div." + header
+    const facetEntry = div_header + " div.facetEntry"
+    const shown = $(facetEntry + ":not(:hidden)").length
+    const total = $(facetEntry).length
+    if (shown == start_facet_count) {
+        $("#" + header + "_less").show();
+        $("#" + header + "_bull").show();
+    }
+    if (shown + facet_inc >= total) {
+        $("#" + header + "_more").hide();
+        $("#" + header + "_bull").hide();
+    }
+    $(facetEntry + ":hidden").slice(0, facet_inc).removeClass('ui-helper-hidden');
+}
+
+function less(header, start_facet_count, facet_inc) {
+    const div_header = "div." + header
+    const facetEntry = div_header + " div.facetEntry"
+    const shown = $(facetEntry + ":not(:hidden)").length
+    const total = $(facetEntry).length
+    if (shown - facet_inc == start_facet_count) {
+        $("#" + header + "_less").hide();
+        $("#" + header + "_bull").hide();
+    }
+    if (shown == total) {
+        $("#" + header + "_more").show();
+        $("#" + header + "_bull").show();
+    }
+    $(facetEntry + ":not(:hidden)").slice(shown - facet_inc, shown).addClass('ui-helper-hidden');
+}
+
+export function initSearchFacets() {
+    const data_config_json = $('#searchFacets').data('config');
+    const start_facet_count = data_config_json['start_facet_count'];
+    const facet_inc = data_config_json['facet_inc'];
+
+    $(".header_bull").hide();
+    $('.header_more').on('click', function(){
+        more($(this).data('header'), start_facet_count, facet_inc);
+    });
+    $('.header_less').on('click', function(){
+        less($(this).data('header'), start_facet_count, facet_inc);
+    });
+}
