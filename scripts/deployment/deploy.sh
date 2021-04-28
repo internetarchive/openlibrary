f09e04efb80fe800e73ea7abd103014c07de4a8f
Drini Cami <cdrini@gmail.com>
2021-04-28 19:10:46 -0400

    Small tweaks to deploy scripts

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -20,23 +20,24 @@ fi
 echo "Starting production deployment at $(date)"
 
 # `sudo git pull origin master` the core Open Library repos:
-parallel --quote -v ssh {1} "cd {2} && sudo git pull origin master" ::: $SERVERS ::: /opt/olsystem /opt/openlibrary
+parallel --quote ssh {1} "echo -e '\n\n{}'; cd {2} && sudo git pull origin master" ::: $SERVERS ::: /opt/olsystem /opt/openlibrary
 
 # booklending utils requires login
 for SERVER in $SERVERS; do
-  ssh $SERVER 'if [ -d /opt/booklending_utils ]; then cd /opt/booklending_utils && sudo git pull origin master; fi'
+  echo -e '\n\n'$SERVER
+  ssh -t $SERVER 'if [ -d /opt/booklending_utils ]; then cd /opt/booklending_utils && sudo git pull origin master; fi'
 done
 
 # Prune old images now ; this should remove any unused images
-parallel --quote -v ssh {} "docker image prune -f" ::: $SERVERS
+parallel --quote ssh {} "echo -e '\n\n{}'; docker image prune -f" ::: $SERVERS
 
 # Pull the latest docker images
-parallel --quote -v ssh {} "cd /opt/openlibrary && COMPOSE_FILE=\"$COMPOSE_FILE\" docker-compose --profile {} pull" ::: $SERVERS
+parallel --quote ssh {} "echo -e '\n\n{}'; cd /opt/openlibrary && COMPOSE_FILE=\"$COMPOSE_FILE\" docker-compose --profile {} pull --quiet" ::: $SERVERS
 
 # Add a git SHA tag to the Docker image to facilitate rapid rollback
 cd /opt/openlibrary
 CUR_SHA=$(git rev-parse HEAD | head -c7)
-parallel --quote -v ssh {} "echo 'FROM openlibrary/olbase:latest' | docker build -t 'openlibrary/olbase:$CUR_SHA' -" ::: $SERVERS
+parallel --quote ssh {} "echo -e '\n\n{}'; echo 'FROM openlibrary/olbase:latest' | docker build -t 'openlibrary/olbase:$CUR_SHA' -" ::: $SERVERS
 
 # And tag the deploy!
 DEPLOY_TAG="deploy-$(date +%Y-%m-%d)"
