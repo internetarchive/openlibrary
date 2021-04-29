1fb6f4df9a245d47c18ca8870ec626204186dfb0
Drini Cami <cdrini@gmail.com>
2021-04-28 20:46:26 -0400

    Small tweaks to deploy scripts (#5107)
    * Small tweaks to deploy scripts
    
    * Update are_servers_in_sync for olbase

--- scripts/deployment/are_servers_in_sync.sh
+++ scripts/deployment/are_servers_in_sync.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+SERVERS="ol-home0 ol-covers0 ol-web1 ol-web2"
 REPO_DIRS="/opt/olsystem /opt/openlibrary /opt/openlibrary/vendor/infogami /opt/booklending_utils"
+
 for REPO_DIR in $REPO_DIRS; do
     echo $REPO_DIR
-    SERVERS="ol-home0 ol-covers0 ol-web1 ol-web2"
+    
     for SERVER in $SERVERS; do
-	#ssh $SERVER "cd $REPO_DIR; hostname ; sudo git checkout master ; sudo git pull ; git rev-parse HEAD"
-	ssh $SERVER "cd $REPO_DIR; hostname ; git rev-parse HEAD"
+	ssh $SERVER "cd $REPO_DIR; echo -ne $SERVER'\t'; git rev-parse HEAD"
     done
     echo "---"
 done
 
 for SERVER in $SERVERS; do
-    ssh $SERVER "hostname | docker image ls | grep oldev | grep latest"
+    ssh $SERVER "hostname | docker image ls | grep olbase | grep latest"
 done
