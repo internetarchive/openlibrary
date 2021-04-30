1c089c6f8d72fcd6d3e94557740022a0952e236b
Jon Robson <jdlrobson@gmail.com>
2021-04-30 11:36:43 -0700

    Update add-book.js
    No need to run window.q inside jQuery.ready - they are the same.

--- openlibrary/plugins/openlibrary/js/add-book.js
+++ openlibrary/plugins/openlibrary/js/add-book.js
@@ -1,13 +1,11 @@
 export function initAddBookImport () {
-    window.q.push(function() {
-        $('.list-books a').click(function() {
-            var li = $(this).parents('li:first');
-            $('input#work').val(`/works/${li.attr('id')}`);
-            $('form#addbook').submit();
-        });
-        $('#bookAddCont').click(function() {
-            $('input#work').val('none-of-these');
-            $('form#addbook').submit();
-        });
+    $('.list-books a').click(function() {
+        var li = $(this).parents('li:first');
+        $('input#work').val(`/works/${li.attr('id')}`);
+        $('form#addbook').submit();
+    });
+    $('#bookAddCont').click(function() {
+        $('input#work').val('none-of-these');
+        $('form#addbook').submit();
     });
 }
