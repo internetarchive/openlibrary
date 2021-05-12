98faca9380e65da8db4ba1eaf666fde3d08c11da
Ray Berger <RayBB@users.noreply.github.com>
2021-05-11 19:33:36 -1000

    clearer variable names and docstrings

--- openlibrary/components/AuthorIdentifiers.vue
+++ openlibrary/components/AuthorIdentifiers.vue
@@ -3,8 +3,8 @@
         <span class="box">
             <select v-model="selectedIdentifier" name="name">
                 <option disabled value="">Select one</option>
-                <option v-for="identifier in allPossibleIdentifiers" :key="identifier.name" :value="identifier.name">
-                  {{identifier.label}}
+                <option v-for="idConfig in identifierConfigsByKey" :key="idConfig.name" :value="idConfig.name">
+                  {{idConfig.label}}
                 </option>
             </select>
         </span>
@@ -14,7 +14,7 @@
         <span class="box">
             <button type="button" name="set" :disabled="!setButtonEnabled" @click=setIdentifier>Set</button>
         </span>
-        <template v-for="identifier in identifiersWithValues">
+        <template v-for="identifier in assignedIdentifiersWithConfigs">
             <div class="box" :key="identifier.name">{{ identifier.label }}</div>
             <div class="box" :key="identifier.name">{{ identifier.value }}</div>
             <div class="box" :key="identifier.name">
@@ -29,17 +29,16 @@ export default {
     // Props are for external options; if a subelement of this is modified,
     // the view automatically re-renders
     props: {
-        // this is the list of remote ids currently associated with the book in the database
-        // It is passed as a string and then converted to json
-        remote_ids_string: {
+        /** The list of ids currently associated with the entity in the database in string form */
+        assigned_ids_string: {
             type: String,
             //default: () =>  "{'wikidata': 'Q10000'}"
         },
-        // everything from https://openlibrary.org/config/author
+        /** everything from https://openlibrary.org/config/author */
         author_config: {
             type: Object
         },
-        // see createHiddenInputs function for usage
+        /** see createHiddenInputs function for usage */
         output_selector: {
             type: String
         }
@@ -51,20 +50,19 @@ export default {
         return {
             selectedIdentifier: '', // Which identifier is selected in dropdown
             inputValue: '', // What user put into input
-            remoteIds: {}, // IDs assigned to object in client
-            allPossibleIdentifiers: [], // A list of every identifier that can be set
+            assignedIdentifiers: {}, // IDs assigned to the entity
         }
     },
 
     computed: {
-        // Merges the key/value with the config data about identifiers
-        identifiersWithValues: function(){
-            return Object.entries(this.remoteIds)
-                .map(([key, value]) => Object.assign({value: value}, this.allIdentifiersByKey[key] || {}));
+        /** A list of assigned identifiers, merged with their respective configs */
+        assignedIdentifiersWithConfigs: function(){
+            return Object.entries(this.assignedIdentifiers)
+                .map(([key, value]) => Object.assign({value: value}, this.identifierConfigsByKey[key] || {}));
         },
-        // allows for lookup of identifier in O(1) time
-        allIdentifiersByKey: function(){
-            return Object.fromEntries(this.allPossibleIdentifiers.map(e => [e.name, e]));
+        identifierConfigsByKey: function(){
+            const parsedConfigs = JSON.parse(decodeURIComponent(this.author_config))['identifiers'];
+            return Object.fromEntries(parsedConfigs.map(e => [e.name, e]));
         },
         setButtonEnabled: function(){
             return this.selectedIdentifier !== '' && this.inputValue !== '';
@@ -78,27 +76,27 @@ export default {
 
             // We use $set otherwise we wouldn't get the reactivity desired
             // See https://vuejs.org/v2/guide/reactivity.html#Change-Detection-Caveats
-            this.$set(this.remoteIds, this.selectedIdentifier, this.inputValue)
+            this.$set(this.assignedIdentifiers, this.selectedIdentifier, this.inputValue)
             this.inputValue = '';
             this.createHiddenInputs()
         },
-        // Removes an identifier with value from memory and it will be deleted from database on save
+        /** Removes an identifier with value from memory and it will be deleted from database on save */
         removeIdentifier: function(identifierName){
-            this.$set(this.remoteIds, identifierName, '')
+            this.$set(this.assignedIdentifiers, identifierName, '')
             this.createHiddenInputs()
         },
         createHiddenInputs: function(){
-            // Right now, we have a vue component embedded as a small part of a larger form
-            // As far as I can tell, there is no way for that parent form to automatically detect the inputs in a component without JS
-            // This is because the vue component is in a shadow dom
-            // So for now this just drops the hidden inputs into the the parent form anytime there is a change
-            const html = this.identifiersWithValues.map(identifier=>`<input type="hidden" name="author--remote_ids--${identifier.name}" value="${identifier.value}"/>`).join('');
+            /** Right now, we have a vue component embedded as a small part of a larger form
+              * There is no way for that parent form to automatically detect the inputs in a component without JS
+              * This is because the vue component is in a shadow dom
+              * So for now this just drops the hidden inputs into the the parent form anytime there is a change
+              */
+            const html = this.assignedIdentifiersWithConfigs.map(identifier=>`<input type="hidden" name="author--remote_ids--${identifier.name}" value="${identifier.value}"/>`).join('');
             document.querySelector(this.output_selector).innerHTML = html;
         }
     },
     mounted: function(){
-        this.remoteIds = JSON.parse(decodeURIComponent(this.remote_ids_string));
-        this.allPossibleIdentifiers = JSON.parse(decodeURIComponent(this.author_config))['identifiers'];
+        this.assignedIdentifiers = JSON.parse(decodeURIComponent(this.assigned_ids_string));
     }
 }
 </script>
