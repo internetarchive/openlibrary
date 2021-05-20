55ab89b7f8bd1d9a3000b80c6b1a48d2253323b1
Ray Berger <RayBB@users.noreply.github.com>
2021-05-19 16:59:46 -1000

    remove extra $ call

--- openlibrary/plugins/openlibrary/js/merge.js
+++ openlibrary/plugins/openlibrary/js/merge.js
@@ -1,38 +1,36 @@
 export function initAuthorMergePage() {
-    $(function () {
-        $('#save').click(function () {
-            const n = $('#mergeForm input[type=radio]:checked').length;
-            if (n === 0) {
-                $('#noMaster').dialog('open');
+    $('#save').click(function () {
+        const n = $('#mergeForm input[type=radio]:checked').length;
+        if (n === 0) {
+            $('#noMaster').dialog('open');
+        } else {
+            $('#confirmMerge').dialog('open');
+        }
+        return false;
+    });
+    $('div.radio:first input[type=radio]').prop('checked', true);
+    $('div.checkbox:first input[type=checkbox]').prop('checked', true);
+    $('div.author:first').addClass('master');
+    $('#include input[type=radio]').mouseover(function () {
+        $(this).parent().parent().addClass('mouseoverHighlight', 300);
+    });
+    $('#include input[type=radio]').mouseout(function () {
+        $(this).parent().parent().removeClass('mouseoverHighlight', 100);
+    });
+    $('#include input[type=radio]').click(function () {
+        const previousMaster = $('.merge').find('div.master');
+        previousMaster.removeClass('master mergeSelection');
+        previousMaster.find('input[type=checkbox]').prop('checked', false);
+        $(this).parent().parent().addClass('master');
+        $(this).parent().parent().find('input[type=checkbox]').prop('checked', true);
+    });
+    $('#include input[type=checkbox]').on('change', function () {
+        if (!$(this).parent().parent().hasClass('master')) {
+            if ($(this).is(':checked')) {
+                $(this).parent().parent().addClass('mergeSelection');
             } else {
-                $('#confirmMerge').dialog('open');
+                $(this).parent().parent().removeClass('mergeSelection');
             }
-            return false;
-        });
-        $('div.radio:first input[type=radio]').prop('checked', true);
-        $('div.checkbox:first input[type=checkbox]').prop('checked', true);
-        $('div.author:first').addClass('master');
-        $('#include input[type=radio]').mouseover(function () {
-            $(this).parent().parent().addClass('mouseoverHighlight', 300);
-        });
-        $('#include input[type=radio]').mouseout(function () {
-            $(this).parent().parent().removeClass('mouseoverHighlight', 100);
-        });
-        $('#include input[type=radio]').click(function () {
-            const previousMaster = $('.merge').find('div.master');
-            previousMaster.removeClass('master mergeSelection');
-            previousMaster.find('input[type=checkbox]').prop('checked', false);
-            $(this).parent().parent().addClass('master');
-            $(this).parent().parent().find('input[type=checkbox]').prop('checked', true);
-        });
-        $('#include input[type=checkbox]').on('change', function () {
-            if (!$(this).parent().parent().hasClass('master')) {
-                if ($(this).is(':checked')) {
-                    $(this).parent().parent().addClass('mergeSelection');
-                } else {
-                    $(this).parent().parent().removeClass('mergeSelection');
-                }
-            }
-        });
+        }
     });
 }
