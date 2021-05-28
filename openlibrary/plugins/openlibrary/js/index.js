bb7079ea0b4828a3303debe23cd4ea94a0d957c7
Drini Cami <cdrini@gmail.com>
2021-05-28 16:22:39 -0400

    Delete unused read api call

--- openlibrary/plugins/openlibrary/js/index.js
+++ openlibrary/plugins/openlibrary/js/index.js
@@ -214,12 +214,7 @@ jQuery(function () {
 
     if (document.getElementById('searchFacets')) {
         import(/* webpackChunkName: "search" */ './search')
-            .then((module) => {
-                module.initSearchFacets();
-                if (document.getElementById('adminTiming')) {
-                    module.initAdminTiming();
-                }
-            });
+            .then((module) => module.initSearchFacets());
     }
 
     if ($('#cboxPrevious').length) {
