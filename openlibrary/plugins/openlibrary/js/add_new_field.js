cb470da3e1f847af9dfbd9b6c7f86b24a6309faf
Yash Saravgi <f20191530@hyderabad.bits-pilani.ac.in>
2021-06-02 20:10:15 +0530

    Fix jQuery deprecated methods (#5244)
    * Fix jQuery deprecated methods flagged by eslint

--- openlibrary/plugins/openlibrary/js/add_new_field.js
+++ openlibrary/plugins/openlibrary/js/add_new_field.js
@@ -41,8 +41,9 @@ export default function($){
                 }
             });
 
+            const $href = $(options.href);
             // handle cancel
-            $(options.href).bind('cbox_closed', function() {
+            $href.on('cbox_closed', function() {
 
                 if ($this.val() == '__add__') {
                     $this.val('');
@@ -54,7 +55,7 @@ export default function($){
             });
 
             // handle submit
-            $('form:first', $(options.href)).on('submit', function(event) {
+            $('form').first().add($href).on('submit', function(event) {
                 var array, d, i, data;
                 event.preventDefault();
 
@@ -63,7 +64,7 @@ export default function($){
                 d = {};
 
                 for (i in array) {
-                    d[array[i].name] = $.trim(array[i].value);
+                    d[array[i].name] = array[i].value.trim();
                 }
 
                 // validate
@@ -78,7 +79,7 @@ export default function($){
                 $('<option/>')
                     .html(d.label || d.value)
                     .attr('value', d.value)
-                    .insertBefore($this.find('option:last').prev()) // insert before ---
+                    .insertBefore($this.find('option').last().prev()) // insert before ---
                     .parent().val(d.value);
 
                 // add JSON to hidden field
