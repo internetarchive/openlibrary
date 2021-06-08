9084616c050e2290f1b6c014b9f1be31836e74d3
Yash Saravgi <f20191530@hyderabad.bits-pilani.ac.in>
2021-06-08 22:21:09 +0530

    delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("SW Registered!");
-        console.log(registration);
-    }).catch(error => {
-        console.log("SW Failed");
-        console.log(error);
-    });
-}
