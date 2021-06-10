b6bd490a0bbc8625d27c3b778c28aa3dfc679c68
Yash Saravgi <f20191530@hyderabad.bits-pilani.ac.in>
2021-06-10 09:50:02 +0530

    lint fix

--- static/sw.js
+++ static/sw.js
@@ -1,14 +1,14 @@
-self.addEventListener("install", e => {
-    console.log("Install!!!");
+self.addEventListener('install', e => {
+    console.log('Install!!!');
     e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["/"]);
+        caches.open('static').then(cache => {
+            return cache.addAll(['/']);
         })
     );
 });
 
-self.addEventListener("fetch", e => {
-    console.log("SW fetch",e);
+self.addEventListener('fetch', e => {
+    console.log('SW fetch',e);
     e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch(e.request);
