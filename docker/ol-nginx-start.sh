bf8b0dc1706fabeb9ef699b861380366df0c5811
Drini Cami <cdrini@gmail.com>
2021-06-30 20:20:58 +0000

    fix for logrotate

--- docker/ol-nginx-start.sh
+++ docker/ol-nginx-start.sh
@@ -2,4 +2,11 @@
 
 crontab /etc/cron.d/archive-webserver-logs
 service cron start
+
+# logrotate comes from olsystem which is volume mounted
+# logrotate requires files to be 644
+# expect conflicts writing to file
+chmod 644 /etc/logrotate.d/nginx
+logrotate --verbose /etc/logrotate.d/nginx
+
 nginx -g "daemon off;"
