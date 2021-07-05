80807226ef49a6fd058f530edf27a6f401ebafb7
Drini Cami <cdrini@gmail.com>
2021-07-05 11:46:42 -0400

    Make initTabs non-default

--- openlibrary/plugins/openlibrary/js/tabs.js
+++ openlibrary/plugins/openlibrary/js/tabs.js
@@ -1,7 +1,7 @@
 const TABS_OPTIONS = { fx: { opacity: 'toggle' } };
 import 'jquery-ui/ui/widgets/tabs';
 
-export default function initTabs($node) {
+export function initTabs($node) {
     $node.tabs(TABS_OPTIONS);
     $node.filter('.autohash').on('tabsselect', function(event, ui) {
         document.location.hash = ui.panel.id;
