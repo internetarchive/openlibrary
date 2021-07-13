8924cad9fb8933b9297bfe1ee8aef599438d9f66
Yash Saravgi <yashsaravgi911@gmail.com>
2021-07-13 21:32:18 +0530

    Remove JS which hides the bar after 3 sec

--- openlibrary/plugins/openlibrary/js/automatic.js
+++ openlibrary/plugins/openlibrary/js/automatic.js
@@ -8,9 +8,6 @@ export default function($) {
     // validate forms
     $('form.validate').ol_validate();
 
-    // hide info flash messages
-    $('.flash-messages .info').fadeTo(3000, 1).slideUp();
-
     // hide all images in .no-img
     $('.no-img img').hide();
 
