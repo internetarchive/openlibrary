f27a42a6e4f1d679377ea86599a26ee0dfc9cec0
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-07-17 23:15:55 +0530

    Have two independent scripts to generate ratings and reading log dumps

---
+++ scripts/dump-reading-log.sh
@@ -0,0 +1,12 @@
+psql -U openlibrary -d openlibrary -c "COPY (
+  SELECT
+  concat('OL', work_id, 'W') AS work_id,
+  CASE
+    WHEN (ratings.edition_id IS NULL) THEN NULL
+    ELSE concat('OL', ratings.edition_id, 'M')
+  END AS edition_id,
+  DATE_TRUNC('day', ratings.created) AS created,
+  ratings.rating AS rating
+  FROM ratings
+  WHERE ratings.created <= '$till_date'
+) TO stdout WITH (format csv, header, delimiter E'\t')"
