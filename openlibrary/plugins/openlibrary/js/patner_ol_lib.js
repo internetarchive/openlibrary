c6a75207daf472f5f9486398ca9a3799d05dbe84
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-07-30 12:28:19 +0530

    simplify script by removing addBorrowButtons fn

--- openlibrary/plugins/openlibrary/js/patner_ol_lib.js
+++ openlibrary/plugins/openlibrary/js/patner_ol_lib.js
@@ -20,17 +20,6 @@ export const getAvailabilityDataFromArchiveOrg = async isbnList => {
     return jsonResponse.responses;
 };
 
-const addBorrowButtons = async (bookContainer, btnParent) => {
-    const isbnElementMap = getIsbnToElementMap(bookContainer);
-    const data = await getAvailabilityDataFromArchiveOrg(Object.keys(isbnElementMap))
-    return {
-        container: bookContainer,
-        btn: btnParent,
-        isbnElementMap: isbnElementMap,
-        results: data
-    }
-};
-
 export const addOpenLibraryButtons = async options => {
     const {bookContainer, btnParent} = options
     if (bookContainer === undefined || btnParent === undefined) {
@@ -38,14 +27,13 @@ export const addOpenLibraryButtons = async options => {
             'book container and button parent must be specified in options for open library buttons to populate!'
         )
     }
-    const data = await addBorrowButtons(bookContainer, btnParent)
-    const results = data.results
-    const foundIsbnElementsMap = data.isbnElementMap
+    const foundIsbnElementsMap = getIsbnToElementMap(bookContainer);
+    const results = await getAvailabilityDataFromArchiveOrg(Object.keys(foundIsbnElementsMap))
     Object.keys(foundIsbnElementsMap).map((isbn) => {
         const availability = results[isbn];
         if (availability && availability.status !== 'error') {
             let e = foundIsbnElementsMap[isbn];
-            let buttons = e.getElementsByClassName(data.btn)[0];
+            let buttons = e.getElementsByClassName(btnParent)[0];
             buttons.innerHTML = `${buttons.innerHTML}<div><a class="openlibrary-btn" href="https://openlibrary.org/borrow/ia/${availability.identifier}?ref=">Open Library</a></div>`;
         }
     })
