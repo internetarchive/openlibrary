50b9d1ce380a6bbf2b9fc3a3e0d8037a7cc2792f
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-07-31 00:30:20 +0530

    Using a map instead of a for and other minor code refactoring

--- openlibrary/plugins/openlibrary/js/patner_ol_lib.js
+++ openlibrary/plugins/openlibrary/js/patner_ol_lib.js
@@ -2,20 +2,19 @@ const getIsbnToElementMap = container => {
     const reISBN = /((978)?[0-9][0-9]{10}[0-9xX])|((978)?[0-9]{9}[0-9Xx])/;
     const elements = document.getElementsByClassName(container);
     let isbnElementMap = {};
-    for (let i = 0; i < elements.length; i++) {
-        let e = elements.item(i);
-        let match = e.innerHTML.match(reISBN);
-        if (match) {
-            isbnElementMap[match[0]] = e;
+    Array.from(elements).map((e) => {
+        const isbnMatches = e.innerHTML.match(reISBN);
+        if (isbnMatches) {
+            isbnElementMap[isbnMatches[0]] = e;
         }
-    }
+    })
     return isbnElementMap;
 };
 
 export const getAvailabilityDataFromArchiveOrg = async isbnList => {
-    const apiUrl = 'https://archive.org/services/availability'
-    const url = `${apiUrl}?isbn=${isbnList.join(',')}`;
-    const response = await fetch(url);
+    const apiBaseUrl = 'https://archive.org/services/availability'
+    const apiUrl = `${apiBaseUrl}?isbn=${isbnList.join(',')}`;
+    const response = await fetch(apiUrl);
     const jsonResponse = await response.json();
     return jsonResponse.responses;
 };
@@ -28,9 +27,9 @@ export const addOpenLibraryButtons = async options => {
         )
     }
     const foundIsbnElementsMap = getIsbnToElementMap(bookContainer);
-    const results = await getAvailabilityDataFromArchiveOrg(Object.keys(foundIsbnElementsMap))
+    const availabilityResults = await getAvailabilityDataFromArchiveOrg(Object.keys(foundIsbnElementsMap))
     Object.keys(foundIsbnElementsMap).map((isbn) => {
-        const availability = results[isbn];
+        const availability = availabilityResults[isbn];
         if (availability && availability.status !== 'error') {
             const e = foundIsbnElementsMap[isbn];
             const buttons = e.getElementsByClassName(selectorToPlaceBtnIn)[0];
