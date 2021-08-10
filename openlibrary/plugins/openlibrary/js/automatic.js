2a332d9c4a7688d4cc85c7999076daa0129b4cc4
Drini Cami <cdrini@gmail.com>
2021-08-10 20:50:41 +0200

    Make flash messages visible by default to avoid layout shift

--- openlibrary/plugins/openlibrary/js/automatic.js
+++ openlibrary/plugins/openlibrary/js/automatic.js
@@ -2,9 +2,6 @@
  * Setup actions on document.ready for standard classNames.
  */
 export default function($) {
-    // Flash messages are hidden by default so that CSS is not on the critical path.
-    $('.flash-messages').show();
-
     // validate forms
     $('form.validate').ol_validate();
 
