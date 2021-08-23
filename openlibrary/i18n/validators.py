c417c1589e017f332bdc332cfd882ab24df59810
James Champ <jameschamp@acm.org>
2021-08-23 19:01:00 -0400

    Fix linting errors

--- openlibrary/i18n/validators.py
+++ openlibrary/i18n/validators.py
@@ -10,34 +10,35 @@ def validate(message, catalog):
 
 
 def _validate_format(message, catalog):
-        """Returns an error list if the format strings are mismatched.
+    """Returns an error list if the format strings are mismatched.
 
-        Relies on Babel's built-in python format checker.
-        """
-        errors = []
+    Relies on Babel's built-in python format checker.
+    """
+    errors = []
 
-        if message.python_format:
-            try:
-                python_format(catalog, message)
-            except TranslationError as e:
-                errors.append(f'    {e}')
+    if message.python_format:
+        try:
+            python_format(catalog, message)
+        except TranslationError as e:
+            errors.append(f'    {e}')
 
-        return errors
+    return errors
+
 
 def _validate_fuzzy(message):
-        """Returns an error list if the message is fuzzy.
+    """Returns an error list if the message is fuzzy.
 
-        If a fuzzy flag is found above the header of a `.po`
-        file, the message will have `None` as its line number.
-        """
-        errors = []
-        if message.fuzzy:
-            if message.lineno:
-                errors.append('    Is fuzzy')
-            else:
-                errors.append(
-                    'File is fuzzy.  Remove line containing "#, fuzzy"'
-                    ' found near the beginning of the file.'
-                )
+    If a fuzzy flag is found above the header of a `.po`
+    file, the message will have `None` as its line number.
+    """
+    errors = []
+    if message.fuzzy:
+        if message.lineno:
+            errors.append('    Is fuzzy')
+        else:
+            errors.append(
+                'File is fuzzy.  Remove line containing "#, fuzzy"'
+                ' found near the beginning of the file.'
+            )
 
-        return errors
+    return errors
