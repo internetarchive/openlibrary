f9759f050a5545587549d26566324bb267c18f03
Charles Horn <charles.horn@gmail.com>
2021-09-01 15:56:17 +1200

    remove legacy sql

--- openlibrary/catalog/merge/schema2.sql
+++
@@ -1,8 +0,0 @@
-alter table isbn add primary key (key, value);
-create index isbn_index on isbn (value);
-alter table oclc add primary key (key, value);
-create index oclc_index on oclc (value);
-alter table title add primary key (key, value);
-create index title_index on title (value);
-alter table lccn add primary key (key, value);
-create index lccn_index on lccn (value);
