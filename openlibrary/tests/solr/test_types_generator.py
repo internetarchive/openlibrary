7b5a811466a939d393204a92b2e19708f1eaee49
James Champ <jameschamp@acm.org>
2021-10-26 21:02:30 -0400

    Update test

--- openlibrary/tests/solr/test_types_generator.py
+++ openlibrary/tests/solr/test_types_generator.py
@@ -7,7 +7,7 @@ root = os.path.dirname(__file__)
 
 
 def test_up_to_date():
-    types_path = os.path.join(root, '..', '..', 'solr', 'types.py')
+    types_path = os.path.join(root, '..', '..', 'solr', 'solr_types.py')
     assert generate().strip() == open(types_path).read().strip(), """
     This auto-generated file is out-of-date. Run:
     ./openlibrary/solr/types_generator.py > ./openlibrary/solr/types.py`
