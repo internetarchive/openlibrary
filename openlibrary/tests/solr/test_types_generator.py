4edb141a384e8bff73246bd39f285c6286f0938f
jimman2003 <jimman2003@hotmail.com>
2021-11-10 06:22:20 +0200

    pyupgrade + black on `/tests` (#5338)
    * Ran pyupgrade on tests
    
    * Update openlibrary/tests/catalog/test_utils.py
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- openlibrary/tests/solr/test_types_generator.py
+++ openlibrary/tests/solr/test_types_generator.py
@@ -8,7 +8,9 @@ root = os.path.dirname(__file__)
 
 def test_up_to_date():
     types_path = os.path.join(root, '..', '..', 'solr', 'solr_types.py')
-    assert generate().strip() == open(types_path).read().strip(), """
+    assert (
+        generate().strip() == open(types_path).read().strip()
+    ), """
     This auto-generated file is out-of-date. Run:
     ./openlibrary/solr/types_generator.py > ./openlibrary/solr/types.py`
     """
