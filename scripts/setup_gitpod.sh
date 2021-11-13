4007ea8d3ddd8db78e624deaa3445662b16372a3
Raymond Berger <RayBB@users.noreply.github.com>
2021-11-13 12:57:58 -0500

    add pre-commit run

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -9,3 +9,4 @@ pyenv install
 pip install pre-commit
 # PIP_USER false because https://github.com/gitpod-io/gitpod/issues/4886#issuecomment-963665656
 env PIP_USER=false pre-commit install
+env PIP_USER=false pre-commit run
