d58f3df31b55aa2e660ca8e4680c86ea231509c8
Raymond Berger <RayBB@users.noreply.github.com>
2021-11-13 13:18:33 -0500

    move docker-compose up to bottom

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # This script does the setup needed for gitpod
 
-# Builds the docker images so when user opens env this step is cached
-docker-compose up --no-start
-
 # Setup pre-commit hooks
 pyenv install
 pip install pre-commit
 # PIP_USER false because https://github.com/gitpod-io/gitpod/issues/4886#issuecomment-963665656
 env PIP_USER=false pre-commit install
 env PIP_USER=false pre-commit run
+
+# Builds the docker images so when user opens env this step is cached.
+# We do this last because it occasionally fails on gitpod
+docker-compose up --no-start
