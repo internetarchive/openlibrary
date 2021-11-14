d4aa2debf28a8b9bbff88585678953148ed86365
Christian Clauss <cclauss@me.com>
2021-11-14 18:25:27 +0100

    requests params are a dict of key / value pairs.

--- openlibrary/solr/update_work.py
+++ openlibrary/solr/update_work.py
@@ -1371,16 +1371,16 @@ def update_author(
 
     reply = requests.get(
         base_url,
-        params=[
-            ('wt', 'json'),
-            ('json.nl', 'arrarr'),
-            ('q', 'author_key:%s' % author_id),
-            ('sort', 'edition_count desc'),
-            ('row', 1),
-            ('fl', 'title,subtitle'),
-            ('facet', 'true'),
-            ('facet.mincount', 1),
-        ]
+        params={
+            'wt': 'json',
+            'json.nl': 'arrarr',
+            'q': 'author_key:%s' % author_id,
+            'sort': 'edition_count desc',
+            'row': 1,
+            'fl': 'title,subtitle',
+            'facet': 'true',
+            'facet.mincount': 1,
+        }
         + [('facet.field', '%s_facet' % field) for field in facet_fields],
     ).json()  # type: ignore
     work_count = reply['response']['numFound']
