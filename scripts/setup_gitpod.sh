c6cf18a85ded2bb6ef4e09a699ef8fcd93a25d6d
Raymond Berger <RayBB@users.noreply.github.com>
2021-11-15 12:58:35 -0500

    run pyenv install in background for gitpod

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -2,11 +2,14 @@
 # This script does the setup needed for gitpod
 
 # Setup pre-commit hooks
-pyenv install
-pip install pre-commit
-# PIP_USER false because https://github.com/gitpod-io/gitpod/issues/4886#issuecomment-963665656
-env PIP_USER=false pre-commit install
-env PIP_USER=false pre-commit run
+# This is in {} so the pyenv and pre-commit steps can run in the background while docker starts
+{
+    pyenv install
+    pip install pre-commit
+    # PIP_USER false because https://github.com/gitpod-io/gitpod/issues/4886#issuecomment-963665656
+    env PIP_USER=false pre-commit install
+    env PIP_USER=false pre-commit run
+} &
 
 # Builds the docker images so when user opens env this step is cached.
 # We do this last because it occasionally fails on gitpod
