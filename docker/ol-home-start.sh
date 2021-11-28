928e45eccefcbd226f8a80e8a996bd881f25683d
Raymond Berger <RayBB@users.noreply.github.com>
2021-11-28 14:23:19 -0500

    add http to terminal echo
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- docker/ol-home-start.sh
+++ docker/ol-home-start.sh
@@ -8,4 +8,4 @@ python --version
 echo "Waiting for postgres..."
 until pg_isready --host db; do sleep 5; done
 time make reindex-solr
-echo "You may now open to localhost:8080"
+echo "You may now open http://localhost:8080"
