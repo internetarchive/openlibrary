80ffe23cf11de0160380018791ae49080a0ed76c
Mek <michael.karpeles@gmail.com>
2022-02-02 23:14:55 -0800

    rm trailing whitespace

--- openlibrary/plugins/openlibrary/lists.py
+++ openlibrary/plugins/openlibrary/lists.py
@@ -470,7 +470,7 @@ class export(delegate.page):
             return delegate.RawText(formats.dump_yaml(data))
         else:
             raise web.notfound()
-    
+
     def get_exports(self, lst: list, raw: bool = False) -> dict[str, list]:
         export_data = lst.get_export_list()
         if "editions" in export_data:
