36646bf4eca694b861445d6333f75b6f8be88459
Mek <michael.karpeles@gmail.com>
2022-02-02 23:04:21 -0800

    Update lists.py

--- openlibrary/plugins/openlibrary/lists.py
+++ openlibrary/plugins/openlibrary/lists.py
@@ -471,8 +471,7 @@ class export(delegate.page):
         else:
             raise web.notfound()
     
-    def get_exports(self, lst: list, raw: bool = False) -> d
-    ict[str, list]:
+    def get_exports(self, lst: list, raw: bool = False) -> dict[str, list]:
         export_data = lst.get_export_list()
         if "editions" in export_data:
             export_data["editions"] = sorted(
