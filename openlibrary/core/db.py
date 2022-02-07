c2157cf0acda36110b0b55a96c360f15175b84d0
Michael E. Karpeles <michael.karpeles@gmail.com>
2022-02-07 05:11:10 +0000

    API to resolve redirects for single work
    - Each feature type (i.e. bookshelves, ratings, observations, booknotes) inherits from db.CommonExtras
    - Result shows occurrences of redirect works per type (bookshelves, ratings, observations, booknotes)
    - Handles chains of redirects
    - _test=True / ?test=true flag to db rollback/abort transaction

--- openlibrary/core/db.py
+++ openlibrary/core/db.py
@@ -1,8 +1,8 @@
 """Interface to access the database of openlibrary.
 """
 import web
+from psycopg2.errors import UniqueViolation
 from infogami.utils import stats
-
 
 @web.memoize
 def _get_db():
@@ -15,6 +15,58 @@ def get_db():
     The database object is cached so that one object is used everywhere.
     """
     return _get_db()
+
+
+class CommonExtras:
+    """
+    A set of methods used by bookshelves, booknotes, ratings, and observations tables
+    """
+
+    @classmethod
+    def update_work_id(cls, current_work_id, new_work_id, _test=False):
+        """This method allows all instances of a work_id (such as that of a
+        redirect) to be updated to a new work_id.
+        """
+        oldb = get_db()
+        t = oldb.transaction()
+        rows_changed = 0
+        rows_deleted = 0
+        try:
+            rows_changed = oldb.update(
+                cls.TABLENAME,
+                where="work_id=$work_id",
+                work_id=new_work_id,
+                vars={"work_id": current_work_id})
+        except UniqueViolation:
+            try:
+                # get records with old work_id
+                rows = oldb.select(
+                    cls.TABLENAME,
+                    where="work_id=$work_id",
+                    vars={"work_id": current_work_id})
+                for row in rows:
+                    where = " AND ".join([
+                        f"{k}='{v}'"
+                        for k, v in row.items()
+                        if k in cls.PRIMARY_KEY
+                    ])
+                    web.debug(where)
+                    try:
+                        # try to update the row to new_work_id
+                        oldb.query(f"UPDATE {cls.TABLENAME} set work_id={new_work_id} where {where}")
+                        rows_changed += 1
+                    except UniqueViolation:
+                        # otherwise, delete row with current_work_id if failed
+                        oldb.query(f"DELETE FROM {cls.TABLENAME} WHERE {where}")
+                        rows_deleted += 1
+            except:
+                t.rollback()
+                raise
+        except:
+            t.rollback()
+            raise
+        t.rollback() if _test else t.commit()
+        return rows_changed, rows_deleted
 
 
 def _proxy(method_name):
