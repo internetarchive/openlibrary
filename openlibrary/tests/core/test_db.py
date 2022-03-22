d94690900da0f161475253d7d89c60bf512cda31
Michael E. Karpeles (Mek) <michael.karpeles@gmail.com>
2022-03-22 23:33:21 +0000

    adding collision test case

--- openlibrary/tests/core/test_db.py
+++ openlibrary/tests/core/test_db.py
@@ -1,12 +1,14 @@
 import web
-from openlibrary.core.db import CommonExtras, get_db
+from openlibrary.core.db import get_db
+from openlibrary.core.bookshelves import Bookshelves
 
 class TestUpdateWorkID:
 
-    def setup_method(self, method):
+    @classmethod
+    def setup_class(cls):
         web.config.db_parameters = dict(dbn="sqlite", db=":memory:")
-        self.db = get_db()
-        self.db.query("""
+        db = get_db()
+        db.query("""
         CREATE TABLE bookshelves_books (
         username text NOT NULL,
         work_id integer NOT NULL,
@@ -16,22 +18,45 @@ class TestUpdateWorkID:
         );
         """)
 
-    def test_update(self):
-        CommonExtras.TABLENAME = "bookshelves_books"
-        book1 = {
+    def setup_method(self, method):
+        self.db = get_db()
+        self.source_book = {
             "username": "@cdrini",
             "work_id": "1",
             "edition_id": "1",
             "bookshelf_id": "1"
         }
-        assert not len(list(self.db.select("bookshelves_books", where=book1)))
-        self.db.insert("bookshelves_books", **book1)
-        assert len(list(self.db.select("bookshelves_books", where=book1))) == 1
+        assert not len(list(self.db.select("bookshelves_books")))
+        self.db.insert("bookshelves_books", **self.source_book)
 
-        # [x] 1 -> 2 (no 2 exists in db) + many case
-        #     1 -> 2 (2 does exist in db) + many case
-        #        1 -> 2 (2 has different edition than 1)
-        CommonExtras.update_work_id("1", "2")
+    def teardown_method(self):
+        self.db.query("delete from bookshelves_books;")
+
+    def test_update_collision(self):
+        existing_book = {
+            "username": "@cdrini",
+            "work_id": "2",
+            "edition_id": "2",
+            "bookshelf_id": "1"
+        }
+        self.db.insert("bookshelves_books", **existing_book)
+        assert len(list(self.db.select("bookshelves_books"))) == 2
+        Bookshelves.update_work_id(self.source_book['work_id'], existing_book['work_id'])
+        assert len(list(self.db.select("bookshelves_books", where={
+            "username": "@cdrini",
+            "work_id": "2",
+            "edition_id": "2"
+        }))), "failed to update 1 to 2"
+        assert not len(list(self.db.select("bookshelves_books", where={
+            "username": "@cdrini",
+            "work_id": "1",
+            "edition_id": "1"
+        }))), "old work_id 1 present"
+
+
+    def test_update_simple(self):
+        assert len(list(self.db.select("bookshelves_books"))) == 1
+        Bookshelves.update_work_id(self.source_book['work_id'], "2")
         assert len(list(self.db.select("bookshelves_books", where={
             "username": "@cdrini",
             "work_id": "2",
