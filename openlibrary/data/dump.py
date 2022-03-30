48f58283ed584ce7d30555d16ce24775deba5221
Drini Cami <cdrini@gmail.com>
2022-03-30 16:14:47 -0400

    Fix data dump having wrong keys (eg /a/OL1A)
    * Partially revert 9403742e7a7509b2b0cb9aad24727e39a7850fae to make sure the process records is json.dumps and printed.
    * Place filter in the righter ordering from 9403742e7a7509b2b0cb9aad24727e39a7850fae
    * Add unit tests!

--- openlibrary/data/dump.py
+++ openlibrary/data/dump.py
@@ -30,17 +30,14 @@ logger = logging.getLogger(__file__)
 logger.setLevel(logging.DEBUG)
 
 
-def print_dump(json_records, filter=None):
+def print_dump(json_records, filter=None, print=print):
     """Print the given json_records in the dump format."""
-    for i, json_data in enumerate(json_records):
+    for i, raw_json_data in enumerate(json_records):
         if i % 1_000_000 == 0:
             log(f"{i:,}")
-        d = json.loads(json_data)
+        d = json.loads(raw_json_data)
         d.pop("id", None)
         d = _process_data(d)
-
-        if filter and filter(d) is False:
-            continue
 
         key = web.safestr(d["key"])
 
@@ -53,8 +50,12 @@ def print_dump(json_records, filter=None):
         if key.startswith(("/b/", "/scan", "/old/")) or not key.startswith("/"):
             continue
 
+        if filter and filter(d) is False:
+            continue
+
         type_key = d["type"]["key"]
         timestamp = d["last_modified"]["value"]
+        json_data = json.dumps(d)
 
         print("\t".join([type_key, key, str(d["revision"]), timestamp, json_data]))
 
