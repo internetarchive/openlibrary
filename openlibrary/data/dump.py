87ddb1694c03b375f2ab4a487bd42cd7ab653530
jimman2003 <jimman2003@hotmail.com>
2022-04-02 04:00:02 +0300

    Remove flatten_dict,unused

--- openlibrary/data/dump.py
+++ openlibrary/data/dump.py
@@ -19,7 +19,6 @@ import time
 import web
 
 from infogami import config
-from infogami.infobase.utils import flatten_dict
 from openlibrary.config import load_config
 from openlibrary.data import db
 from openlibrary.data.sitemap import generate_html_index, generate_sitemaps
