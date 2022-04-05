0f9f90a69338b7db9dca77df1efd6a28880411e5
jimman2003 <jimman2003@hotmail.com>
2022-04-05 19:48:14 +0300

    Replace SpyPrint with capsys fixure (#6381)
    * Replace SpyPrint with capsys fixure

--- openlibrary/tests/data/test_dump.py
+++ openlibrary/tests/data/test_dump.py
@@ -3,16 +3,8 @@ import json
 from openlibrary.data.dump import print_dump
 
 
-class SpyPrint:
-    def __init__(self):
-        self.output = []
-
-    def __call__(self, msg: str):
-        self.output.append(msg)
-
-
 class TestPrintDump:
-    def test_fixes_prefixes(self):
+    def test_fixes_prefixes(self, capsys):
         records = [
             {
                 "key": "/b/OL1M",
@@ -21,41 +13,35 @@ class TestPrintDump:
                 "last_modified": {"value": "2019-01-01T00:00:00.000"},
             },
         ]
-        spy_print = SpyPrint()
 
-        print_dump(map(json.dumps, records), print=spy_print)
-        assert spy_print.output == [
-            "\t".join([
-                "/type/edition",
-                "/books/OL1M",
-                "1",
-                "2019-01-01T00:00:00.000",
-                json.dumps({
-                    "key": "/books/OL1M",
-                    "type": {"key": "/type/edition"},
-                    "revision": 1,
-                    "last_modified": {"value": "2019-01-01T00:00:00.000"},
-                }),
-            ]),
-        ]
+        print_dump(map(json.dumps, records))
+        assert capsys.readouterr().out.strip() == "\t".join([
+            "/type/edition",
+            "/books/OL1M",
+            "1",
+            "2019-01-01T00:00:00.000",
+            json.dumps({
+                "key": "/books/OL1M",
+                "type": {"key": "/type/edition"},
+                "revision": 1,
+                "last_modified": {"value": "2019-01-01T00:00:00.000"},
+            }),
+        ])
 
-    def test_excludes_sensitive_pages(self):
+    def test_excludes_sensitive_pages(self, capsys):
         records = [
             {"key": "/people/foo"},
             {"key": "/user/foo"},
             {"key": "/admin/foo"},
         ]
-        spy_print = SpyPrint()
+        print_dump(map(json.dumps, records))
+        assert capsys.readouterr().out == ""
 
-        print_dump(map(json.dumps, records), print=spy_print)
-        assert spy_print.output == []
-
-    def test_excludes_obsolete_pages(self):
+    def test_excludes_obsolete_pages(self, capsys):
         records = [
             {"key": "/scan_record/foo"},
             {"key": "/old/what"},
         ]
-        spy_print = SpyPrint()
 
-        print_dump(map(json.dumps, records), print=spy_print)
-        assert spy_print.output == []
+        print_dump(map(json.dumps, records))
+        assert capsys.readouterr().out == ""
