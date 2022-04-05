fb6b398f8d29138c4f866d54e9f40e2516be2c21
Mark F. Heiman <mark.heiman@gmail.com>
2022-04-05 15:35:42 -0500

    Added doctests

--- openlibrary/utils/__init__.py
+++ openlibrary/utils/__init__.py
@@ -130,6 +130,14 @@ def dicthash(d):
 author_olid_embedded_re = re.compile(r'OL\d+A', re.IGNORECASE)
 
 def find_author_olid_in_string(s):
+    """
+    >>> find_author_olid_in_string("ol123a")
+    'OL123A'
+    >>> find_author_olid_in_string("/authors/OL123A/edit")
+    'OL123A'
+    >>> find_author_olid_in_string("some random string")
+    ''
+    """
     found = re.search(author_olid_embedded_re, s)
     return found.group(0).upper() if found else ""
 
@@ -137,6 +145,14 @@ def find_author_olid_in_string(s):
 work_olid_embedded_re = re.compile(r'OL\d+W', re.IGNORECASE)
 
 def find_work_olid_in_string(s):
+    """
+    >>> find_work_olid_in_string("ol123w")
+    'OL123W'
+    >>> find_work_olid_in_string("/works/OL123A/Title_of_book")
+    'OL123W'
+    >>> find_work_olid_in_string("some random string")
+    ''
+    """
     found = re.search(work_olid_embedded_re, s)
     return found.group(0).upper() if found else ""
 
