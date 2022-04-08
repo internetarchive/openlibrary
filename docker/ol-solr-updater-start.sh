64d38179a6d3aa8baa5039e1ffb99d69a1bce938
Drini Cami <cdrini@gmail.com>
2022-04-07 20:02:49 -0400

    Rename new-solr-updater -> solr_updater

--- docker/ol-solr-updater-start.sh
+++ docker/ol-solr-updater-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python --version
-python scripts/new-solr-updater.py $OL_CONFIG \
+python scripts/solr_updater.py $OL_CONFIG \
     --state-file /solr-updater-data/$STATE_FILE \
     --ol-url "$OL_URL" \
     --socket-timeout 1800 \
