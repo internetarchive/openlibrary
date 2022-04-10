b6a123a7380b56ab0d8e2f3f5eeda585ebc5d1c5
Drini Cami <cdrini@gmail.com>
2022-04-10 18:40:07 -0400

    Hotfix nginx logrotate not working
    The logrotate file has to be owned by root for some inexplicable reason.

--- docker/ol-nginx-start.sh
+++ docker/ol-nginx-start.sh
@@ -6,9 +6,10 @@ if [ -n "$CRONTAB_FILES" ] ; then
 fi
 
 # logrotate comes from olsystem which is volume mounted
-# logrotate requires files to be 644
+# logrotate requires files to be 644 and owned by root??? (WHAT)
 # expect conflicts writing to file
 chmod 644 /etc/logrotate.d/nginx
+chown root:root /etc/logrotate.d/nginx
 logrotate --verbose /etc/logrotate.d/nginx
 
 nginx -g "daemon off;"
