d3f07fa403544696062d2d47f0fa2b7b1e20fea9
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-04-17 15:52:21 +0300

    format js code

--- openlibrary/plugins/openlibrary/js/readmore.js
+++ openlibrary/plugins/openlibrary/js/readmore.js
@@ -1,18 +1,18 @@
 export function initReadMoreButton() {
-    $('.read-more-button').on('click',function(){
+    $('.read-more-button').on('click', function () {
         const up = $(this).parent().parent();
         $(`.${up.attr('class')}-content`).removeClass('restricted-height', 300);
         $(`.${up.attr('class')}.read-more`).addClass('hidden');
         $(`.${up.attr('class')}.read-less`).removeClass('hidden');
     });
-    $('.read-less-button').on('click',function(){
+    $('.read-less-button').on('click', function () {
         const up = $(this).parent().parent();
         $(`.${up.attr('class')}-content`).addClass('restricted-height', 300);
         $(`.${up.attr('class')}.read-more`).removeClass('hidden');
         $(`.${up.attr('class')}.read-less`).addClass('hidden');
     });
-    $('.restricted-view').each(function() {
-        if ($(this).outerHeight()<50) {
+    $('.restricted-view').each(function () {
+        if ($(this).outerHeight() < 50) {
             $(`.${$(this).parent().attr('class')}.read-more`).addClass('hidden');
         } else {
             $(this).addClass('restricted-height');
@@ -29,24 +29,24 @@ export function initClampers(clampers) {
                 Clamper shows used to show more/less by toggling `hidden`
                 style on parent .clamp tag
             */
-            $(clamper).on('click', function(event) {
+            $(clamper).on('click', function (event) {
                 const up = $(this);
 
-            // prevent the subjects from collapsing/expanding when the <a> link is being clicked 
-            if (event.target.nodeName == "A"){
-                return
-            }
-            else{
-                
-                if (up.hasClass('clamp')) {
-                    up.css({display: up.css('display') === '-webkit-box' ? 'unset' : '-webkit-box'});
-
-                    if (up.attr('data-before') === '\u25BE ') {
-                        up.attr('data-before', '\u25B8 ')
-                    } else {
-                        up.attr('data-before', '\u25BE ')
-                    }
+                // prevent the subjects from collapsing/expanding when the <a> link is being clicked 
+                if (event.target.nodeName == "A") {
+                    return
                 }
+                else {
+
+                    if (up.hasClass('clamp')) {
+                        up.css({ display: up.css('display') === '-webkit-box' ? 'unset' : '-webkit-box' });
+
+                        if (up.attr('data-before') === '\u25BE ') {
+                            up.attr('data-before', '\u25B8 ')
+                        } else {
+                            up.attr('data-before', '\u25BE ')
+                        }
+                    }
                 }
             })
         }
