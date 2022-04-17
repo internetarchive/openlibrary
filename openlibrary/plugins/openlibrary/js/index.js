753a932c7eccba84f6aff3d656811cb7a7ee670c
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-04-18 01:06:54 +0300

    delete old js script

--- openlibrary/plugins/openlibrary/js/index.js
+++ openlibrary/plugins/openlibrary/js/index.js
@@ -123,6 +123,36 @@ jQuery(function () {
             .then(module => module.initEditionsTable());
     }
 
+
+    //js code for the Share module in the book page
+    const viewBtn = document.querySelector(".view-modal"),
+    popup = document.querySelector(".popup"),
+    close = popup.querySelector(".close"),
+    field = popup.querySelector(".copy-field"),
+    input = field.querySelector("input"),
+    copy = field.querySelector("button");
+
+    viewBtn.onclick = ()=>{
+    popup.classList.toggle("show");
+    }
+    close.onclick = ()=>{
+    viewBtn.click();
+    }
+
+    copy.onclick = ()=>{
+        input.select(); //select input value
+        if(document.execCommand("copy")){ //if the selected text copy
+        field.classList.add("active");
+        copy.innerText = "Copied";
+        setTimeout(()=>{
+            window.getSelection().removeAllRanges(); //remove selection from document
+            field.classList.remove("active");
+            copy.innerText = "Copy";
+        }, 3000);
+        }
+    }
+
+
     const edition = document.getElementById('tabsAddbook');
     const autocompleteAuthor = document.querySelector('.multi-input-autocomplete--author');
     const addRowButton = document.getElementById('add_row_button');
