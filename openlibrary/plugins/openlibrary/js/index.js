2c2092393d04078d6887248a185dfd8a06b33bf5
constantinazouni <t8190046@aueb.gr>
2022-04-20 00:17:49 +0300

    Add comments

--- openlibrary/plugins/openlibrary/js/index.js
+++ openlibrary/plugins/openlibrary/js/index.js
@@ -373,27 +373,17 @@ jQuery(function () {
     }
     
     
-    
-    var mq = window.matchMedia( "(max-width: 570px)" );
-if (mq.matches) {
-    // window width is at less than 570px
-    $(".editionAll").remove().insertBefore($(".editionCover"));;
-    $(".work-line").remove().insertBefore($(".cta-button-group"));;
-    $(".edition-byline").remove().insertBefore($(".cta-button-group"));;
-    $(".work-title").remove().insertBefore($(".cta-button-group"));;
-    $(".work-subtitle").remove().insertBefore($(".cta-button-group"));;
-    $(".readers-stats").remove().insertBefore($(".cta-button-group"));;
-}
-
-   
-     
-
-    
-     
-     
-        
-    
-
-
+    // Change book header in mobile 
+    var mq = window.matchMedia( "(max-width: 960px)" );
+    if (mq.matches) {
+        // window width is less than 960px
+        // replace divs inside book cover
+        $(".editionAll").remove().insertBefore($(".editionCover"));;
+        $(".work-line").remove().insertBefore($(".cta-button-group"));;
+        $(".edition-byline").remove().insertBefore($(".cta-button-group"));;
+        $(".work-title").remove().insertBefore($(".cta-button-group"));;
+        $(".work-subtitle").remove().insertBefore($(".cta-button-group"));;
+        $(".readers-stats").remove().insertBefore($(".cta-button-group"));;
+    }
 
 });
