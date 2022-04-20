5d8fe5a6dc7496478667c76df4abede6b0358d96
Drini Cami <cdrini@gmail.com>
2022-04-20 15:28:20 -0400

    Fix accidental case sensitive filenames!
    These break windows development

--- tests/unit/js/readmore.test.js
+++ tests/unit/js/readmore.test.js
@@ -1,4 +1,25 @@
-import { initClampers } from '../../../openlibrary/plugins/openlibrary/js/readmore';
+import { initClampers, resetReadMoreButtons } from '../../../openlibrary/plugins/openlibrary/js/readmore';
+import $ from 'jquery';
+
+describe('resetReadMoreButtons', () => {
+    const $dummyEl = $('<div class="a-parent"><div class="restricted-view"></div></div>');
+    $dummyEl.appendTo(document.body);
+
+    test('restricted view if big scroll height', () => {
+        const restrictedViewEl = $dummyEl.find('.restricted-view')[0];
+        jest.spyOn(restrictedViewEl, 'scrollHeight', 'get').mockImplementation(() => 100);
+        resetReadMoreButtons();
+        expect(restrictedViewEl.classList.contains('restricted-height')).toBe(true);
+    });
+
+    test('no restricted view if small scroll height', () => {
+        const restrictedViewEl = $dummyEl.find('.restricted-view')[0];
+        jest.spyOn(restrictedViewEl, 'scrollHeight', 'get').mockImplementation(() => 50);
+        resetReadMoreButtons();
+        expect(restrictedViewEl.classList.contains('restricted-height')).toBe(false);
+        $dummyEl.remove();
+    });
+});
 
 describe('initClampers', () => {
     test('clamp removed if not needed', () => {
@@ -28,8 +49,4 @@ describe('initClampers', () => {
         expect(clamper.classList.contains('clamp')).toBe(true);
 
     });
-
-
-
-
 });
