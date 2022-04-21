96c94036be270139c71c711a69990edefaaae5e8
Philippos Priovolos <prio_01@hotmail.com>
2022-04-21 12:56:26 +0300

    Update Unit test for initClampers function in the readmore.js

--- tests/unit/js/readmore.test.js
+++ tests/unit/js/readmore.test.js
@@ -49,4 +49,41 @@ describe('initClampers', () => {
         expect(clamper.classList.contains('clamp')).toBe(true);
 
     });
+    const DUMMY_HTML = 
+    `<span class="clamp" data-before="▾  " style="display: unset;">
+         <h6>Subjects</h6>
+        <a>Ghosts</a>,
+        <a>Monsters</a>,
+        <a>Vampires</a>,
+        <a>Witches</a>, 
+        <a>Challenges and Overcoming Obstacles</a>, 
+        <a>Magic and Supernatural</a>,     
+        <a>Cleverness</a>, 
+        <a>School Life</a>, 
+        <a>school stories</a>,     
+        <a>Wizards</a>, 
+        <a>Magic</a>,
+        <a>MAGIA</a>, 
+        <a>MAGOS</a>,
+        <a>Juvenile fiction</a>,
+        <a>Fiction</a>,  
+        <a>NOVELAS INGLESAS</a>, 
+        <a>Schools</a>,  
+        <a>orphans</a>,
+        <a>fantasy fiction</a>, 
+        <a>England in fiction</a>
+        </span>`;
+    test("Clicking anchor tag does not expand", () => {
+        const $clamper = $(DUMMY_HTML);
+        initClampers($clamper);
+        $clamper.find("a").first().trigger("click");
+        expect($clamper.css("display")).toBe("unset");
+    });
+    test("Clicking non-anchor tag does clamp", () => {
+        const $clamper = $(DUMMY_HTML);
+        initClampers($clamper);
+        $clamper.find("h6").first().trigger("click");
+        expect($clamper.css("display")).not.toBe("unset");
+    });
+    
 });
