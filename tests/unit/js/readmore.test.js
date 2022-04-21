5c77cc10f991175bae07eb56fed5cfac63c13f88
Philippos Priovolos <prio_01@hotmail.com>
2022-04-22 00:17:49 +0300

    Add a new unit test for initClampers function in the readmore.js file

--- tests/unit/js/readmore.test.js
+++ tests/unit/js/readmore.test.js
@@ -50,40 +50,52 @@ describe('initClampers', () => {
 
     });
     const DUMMY_HTML = 
-    `<span class="clamp" data-before="▾  " style="display: unset;">
-         <h6>Subjects</h6>
-        <a>Ghosts</a>,
-        <a>Monsters</a>,
-        <a>Vampires</a>,
-        <a>Witches</a>, 
-        <a>Challenges and Overcoming Obstacles</a>, 
-        <a>Magic and Supernatural</a>,     
-        <a>Cleverness</a>, 
-        <a>School Life</a>, 
-        <a>school stories</a>,     
-        <a>Wizards</a>, 
-        <a>Magic</a>,
-        <a>MAGIA</a>, 
-        <a>MAGOS</a>,
-        <a>Juvenile fiction</a>,
-        <a>Fiction</a>,  
-        <a>NOVELAS INGLESAS</a>, 
-        <a>Schools</a>,  
-        <a>orphans</a>,
-        <a>fantasy fiction</a>, 
+    `<span class='clamp' data-before='▾  ' style='display: unset;'>
+        <h6>Subjects</h6>
+        <a>Ghosts</a>
+        <a>Monsters</a>
+        <a>Vampires</a>
+        <a>Witches</a> 
+        <a>Challenges and Overcoming Obstacles</a> 
+        <a>Magic and Supernatural</a> 
+        <a>Cleverness</a>
+        <a>School Life</a> 
+        <a>school stories</a>     
+        <a>Wizards</a>
+        <a>Magic</a>
+        <a>MAGIA</a> 
+        <a>MAGOS</a>
+        <a>Juvenile fiction</a>
+        <a>Fiction</a>
+        <a>NOVELAS INGLESAS</a> 
+        <a>Schools</a>
+        <a>orphans</a>
+        <a>fantasy fiction</a> 
         <a>England in fiction</a>
-        </span>`;
+      </span>`;
     test("Clicking anchor tag does not expand", () => {
         const $clamper = $(DUMMY_HTML);
+        jest
+            .spyOn($clamper[0], 'scrollHeight', 'get')
+            .mockImplementation(() => 100);
+        jest
+            .spyOn($clamper[0], 'clientHeight', 'get')
+            .mockImplementation(() => 10);
         initClampers($clamper);
-        $clamper.find("a").first().trigger("click");
-        expect($clamper.css("display")).toBe("unset");
+        $($clamper).find('a').first().trigger('click');
+        expect($clamper.css("display")).toBe('unset');
     });
     test("Clicking non-anchor tag does clamp", () => {
         const $clamper = $(DUMMY_HTML);
+        jest
+            .spyOn($clamper[0], 'scrollHeight', 'get')
+            .mockImplementation(() => 100);
+        jest
+            .spyOn($clamper[0], 'clientHeight', 'get')
+            .mockImplementation(() => 10);
         initClampers($clamper);
-        $clamper.find("h6").first().trigger("click");
-        expect($clamper.css("display")).not.toBe("unset");
+        $($clamper).find('h6').first().trigger('click');
+        expect($clamper.css('display')).not.toBe('unset');
     });
     
 });
