b46fd7f718fe3d138cee324e497eecde27484602
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-04-22 15:50:25 +0300

    minor js styling modifications

--- openlibrary/plugins/openlibrary/js/index.js
+++ openlibrary/plugins/openlibrary/js/index.js
@@ -126,30 +126,30 @@ jQuery(function () {
 
     /* js code for the Share module in the book page
      * Coding By CodingNepal - youtube.com/codingnepal */
-    const viewBtn = document.querySelector(".view-modal"),
-    popup = document.querySelector(".popup"),
-    close = popup.querySelector(".close"),
-    field = popup.querySelector(".copy-field"),
-    input = field.querySelector("input"),
-    copy = field.querySelector("button");
+    const viewBtn = document.querySelector('.view-modal'),
+        popup = document.querySelector('.popup'),
+        close = popup.querySelector('.close'),
+        field = popup.querySelector('.copy-field'),
+        input = field.querySelector('input'),
+        copy = field.querySelector('button');
 
     viewBtn.onclick = ()=>{
-    popup.classList.toggle("show");
+        popup.classList.toggle('show');
     }
     close.onclick = ()=>{
-    viewBtn.click();
+        viewBtn.click();
     }
 
     copy.onclick = ()=>{
         input.select(); //select input value
-        if(document.execCommand("copy")){ //if the selected text copy
-        field.classList.add("active");
-        copy.innerText = "Copied";
-        setTimeout(()=>{
-            window.getSelection().removeAllRanges(); //remove selection from document
-            field.classList.remove("active");
-            copy.innerText = "Copy";
-        }, 3000);
+        if(document.execCommand('copy')){ //if the selected text copy
+            field.classList.add('active');
+            copy.innerText = 'Copied';
+            setTimeout(()=>{
+                window.getSelection().removeAllRanges(); //remove selection from document
+                field.classList.remove('active');
+                copy.innerText = 'Copy';
+            }, 3000);
         }
     }
 
