4399d18485bf691b046731ae3579b143d43ac793
Dimitris Bouris <72794144+dbouris@users.noreply.github.com>
2022-05-01 12:51:57 +0300

    reorganize the js and create the share modal

--- openlibrary/plugins/openlibrary/js/index.js
+++ openlibrary/plugins/openlibrary/js/index.js
@@ -124,35 +124,7 @@ jQuery(function () {
     }
 
 
-    /* js code for the Share module in the book page
-     * Coding By CodingNepal - youtube.com/codingnepal */
-    const viewBtn = document.querySelector('.view-modal'),
-        popup = document.querySelector('.popup'),
-        close = popup.querySelector('.close'),
-        field = popup.querySelector('.copy-field'),
-        input = field.querySelector('input'),
-        copy = field.querySelector('button');
-
-    viewBtn.onclick = ()=>{
-        popup.classList.toggle('show');
-    }
-    close.onclick = ()=>{
-        viewBtn.click();
-    }
-
-    copy.onclick = ()=>{
-        input.select(); //select input value
-        if (document.execCommand('copy')) { //if the selected text copy
-            field.classList.add('active');
-            copy.innerText = 'Copied';
-            setTimeout(()=>{
-                window.getSelection().removeAllRanges(); //remove selection from document
-                field.classList.remove('active');
-                copy.innerText = 'Copy';
-            }, 3000);
-        }
-    }
-
+    
 
     const edition = document.getElementById('tabsAddbook');
     const autocompleteAuthor = document.querySelector('.multi-input-autocomplete--author');
