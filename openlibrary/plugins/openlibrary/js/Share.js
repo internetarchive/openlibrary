abd2d2d9d8e395a308e0eb94dc5916a94c23b97a
Philippos Priovolos <prio_01@hotmail.com>
2022-05-03 21:06:17 +0300

    format javascript in Share.js

--- openlibrary/plugins/openlibrary/js/Share.js
+++ openlibrary/plugins/openlibrary/js/Share.js
@@ -1,17 +1,17 @@
-const popup = document.querySelector(".popup"),
-  field = popup.querySelector(".copy-field"),
-  input = field.querySelector("input"),
-  copy = field.querySelector("button");
+const popup = document.querySelector('.popup'),
+    field = popup.querySelector('.copy-field'),
+    input = field.querySelector('input'),
+    copy = field.querySelector('button');
 copy.onclick = () => {
-  input.select(); //select input value
-  if (document.execCommand("copy")) {
-    //if the selected text copy
-    field.classList.add("active");
-    copy.innerText = "Copied";
-    setTimeout(() => {
-      window.getSelection().removeAllRanges(); //remove selection from document
-      field.classList.remove("active");
-      copy.innerText = "Copy";
-    }, 3000);
-  }
+    input.select(); //select input value
+    if (document.execCommand('copy')) {
+        //if the selected text copy
+        field.classList.add('active');
+        copy.innerText = 'Copied';
+        setTimeout(() => {
+            window.getSelection().removeAllRanges(); //remove selection from document
+            field.classList.remove('active');
+            copy.innerText = 'Copy';
+        }, 3000);
+    }
 };
