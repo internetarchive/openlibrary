ba6183407fe9c6ad9236c59c0339f13ec5b6279f
Mark F. Heiman <mark.heiman@gmail.com>
2022-05-25 14:06:17 -0500

    Converted from table to inline display

--- openlibrary/components/MergeUI/ExcerptsTable.vue
+++ openlibrary/components/MergeUI/ExcerptsTable.vue
@@ -1,28 +1,22 @@
 <template>
-    <table>
-        <thead>
-            <tr>
-            <th></th>
-            <th v-for="field in fields" :key="field">{{field}}</th>
-            </tr>
-        </thead>
-    <tbody>
-        <tr v-for="(excerpt, index) in excerpts" :key="index">
-            <td>{{index}}</td>
-            <td v-for="field in fields" :key="field">
-            <div v-if="excerpt[field] == 'excerpt'" :title="excerpt[field]">
-                {{excerpt[field].value || excerpt[field]}}
-            </div>
-            <div v-if="excerpt[field] == 'author'">
-                <a :href="excerpt[field].key" target="_blank">
-                    {{excerpt[field].key.slice("/people/".length)}}
-                </a>
-            </div>
-            <div v-else>{{excerpt[field]}}</div>
-        </td>
-        </tr>
-    </tbody>
-    </table>
+    <ul class="reset">
+        <li class="excerpt-item" v-for="(excerpt, index) in excerpts" :key="index">
+                <span v-if="excerpt['excerpt']" :title="`excerpt-${index}`">
+                    {{excerpt['excerpt'].value || excerpt['excerpt']}}
+                </span>
+                <span v-if="excerpt['pages']">
+                    (page: {{excerpt['pages']}})
+                </span>
+                <span v-if="excerpt['page']">
+                    (page: {{excerpt['page']}})
+                </span>
+                <span v-if="excerpt['author']">
+                    <a :href="excerpt['author'].key" target="_blank">
+                        {{excerpt['author'].key.slice("/people/".length)}}
+                    </a>
+                </span>
+        </li>
+    </ul>
 </template>
 
 <script>
