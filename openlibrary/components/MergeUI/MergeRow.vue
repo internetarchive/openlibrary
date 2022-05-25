fb42bef75e8b33a203c647dc4639bbe05a39ad07
Mark F. Heiman <mark.heiman@gmail.com>
2022-05-25 14:07:11 -0500

    Added support for editions and references pseudofields

--- openlibrary/components/MergeUI/MergeRow.vue
+++ openlibrary/components/MergeUI/MergeRow.vue
@@ -12,6 +12,8 @@
         :record="record"
         :merged="merged"
         :cellSelected="cellSelected"
+        :class="{ [`wrap-${field.replace(/\|/g, '--')}`]: true }"
+        :show_diffs="show_diffs"
       />
       <MergeRowField
         v-else-if="field in record"
@@ -19,20 +21,40 @@
         :value="record[field]"
         :merged="merged"
         :class="{ selected: cellSelected && cellSelected(record, field) }"
+        :show_diffs="show_diffs"
+      />
+      <MergeRowEditionField
+        v-else-if="field == 'editions'"
+        :editions="editions"
+        :record="record"
+        :merged="merged"
+        :class="{ selected: cellSelected && cellSelected(record, field) }"
+      />
+      <MergeRowReferencesField
+        v-else-if="field == 'references'"
+        :lists="lists"
+        :bookshelves="bookshelves"
+        :ratings="ratings"
+        :record="record"
+        :merged="merged"
+        :class="{ selected: cellSelected && cellSelected(record, field) }"
       />
     </td>
-    <slot name="post"></slot>
   </tr>
 </template>
 
 <script>
 import MergeRowField from './MergeRowField.vue';
 import MergeRowJointField from './MergeRowJointField.vue';
+import MergeRowEditionField from './MergeRowEditionField.vue';
+import MergeRowReferencesField from './MergeRowReferencesField.vue';
 
 export default {
     components: {
         MergeRowField,
-        MergeRowJointField
+        MergeRowJointField,
+        MergeRowEditionField,
+        MergeRowReferencesField
     },
     props: {
         record: {
@@ -43,12 +65,27 @@ export default {
             type: Array,
             required: true
         },
+        editions: {
+            type: Array,
+        },
+        lists: {
+            type: Object,
+        },
+        bookshelves: {
+            type: Object,
+        },
+        ratings: {
+            type: Object,
+        },
         cellSelected: {
             type: Function
         },
         merged: {
             type: Object,
             required: false
+        },
+        show_diffs: {
+            type: Boolean
         }
     },
     data() {
