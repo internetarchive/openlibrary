69c6d3fec655ab2ec80ce4b96674917be8d629cd
James Champ <jameschamp@acm.org>
2022-06-17 17:52:19 -0400

    Restructure table and add styling

--- openlibrary/components/MergeUI.vue
+++ openlibrary/components/MergeUI.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
     <MergeTable :olids="olids" ref="mergeTable"/>
-
-    <div class="btn-group">
-        <button class="merge-btn" @click="doMerge" :disabled="mergeStatus == 'Saving...'">Do Merge</button>
-        <button class="reject-btn" v-if="mrid" @click="rejectMerge">Reject Merge</button>
-    </div>
-    <div class="comment-input" v-if="mrid">
-        <label for="comment">Comment: </label>
-        <input name="comment" v-model="comment" type="text">
+    <div class="action-bar">
+        <div class="comment-input" v-if="mrid">
+            <label for="comment">Comment: </label>
+            <input name="comment" v-model="comment" type="text">
+        </div>
+        <div class="btn-group">
+            <button class="merge-btn" @click="doMerge" :disabled="mergeStatus == 'Saving...'">Do Merge</button>
+            <button class="reject-btn" v-if="mrid" @click="rejectMerge">Reject Merge</button>
+        </div>
     </div>
     <pre v-if="mergeStatus">{{mergeStatus}}</pre>
   </div>
@@ -93,10 +94,16 @@ export default {
 }
 
 .comment-input {
+    display: flex;
+    flex-direction: column;
     padding: 0 5px 5px;
 
     input {
         width: 90%;
     }
+}
+
+.action-bar {
+    margin: 5px;
 }
 </style>
