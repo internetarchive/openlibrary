390a58997db40e5b8b083e22e58ea8ef31e0d682
James Champ <jameschamp@acm.org>
2022-06-21 21:40:57 -0400

    Reformat file

--- openlibrary/plugins/openlibrary/js/merge-request-table/index.js
+++ openlibrary/plugins/openlibrary/js/merge-request-table/index.js
@@ -1,84 +1,83 @@
 import { FadingToast } from '../Toast';
 
 export function initCloseLinks(elems) {
-  for (const elem of elems) {
-    elem.addEventListener('click', function() {
-      const mrid = elem.dataset.mrid
-      onCloseClick(mrid, elem.parentNode.parentNode)
-    })
-  }
+    for (const elem of elems) {
+        elem.addEventListener('click', function () {
+            const mrid = elem.dataset.mrid
+            onCloseClick(mrid, elem.parentNode.parentNode)
+        })
+    }
 }
 
 export function initCommentLinks(elems) {
-  for (const elem of elems) {
-    console.log(elem)
-    elem.addEventListener('click', function() {
-      const mrid = elem.dataset.mrid
-      onCommentClick(mrid)
-    })
-  }
+    for (const elem of elems) {
+        elem.addEventListener('click', function () {
+            const mrid = elem.dataset.mrid
+            onCommentClick(mrid)
+        })
+    }
 }
 
 async function onCloseClick(mrid, parentRow) {
-  const comment = promptForComment('(Optional) Why are you closing this request?')
-  if (comment !== null) {
-    await closeRequest(mrid, comment)
-      .then(result => result.json())
-      .then(data => {
-        if (data.status === 'ok') {
-          removeRow(parentRow)
-        }
-      })
-      .catch(e => {
-        throw e
-      });
-  }
+    const comment = promptForComment('(Optional) Why are you closing this request?')
+    if (comment !== null) {
+        await closeRequest(mrid, comment)
+            .then(result => result.json())
+            .then(data => {
+                if (data.status === 'ok') {
+                    removeRow(parentRow)
+                }
+            })
+            .catch(e => {
+                throw e
+            });
+    }
 }
 
 async function onCommentClick(mrid) {
-  const comment = promptForComment('Please enter your response.')
-  if (comment !== null) {
-    await commentOnRequest(mrid, comment)
-      .then(result => result.json())
-      .then(data => {
-        if (data.status === 'ok') {
-          // We did it!
-          // Tell the submitter somehow
-          new FadingToast('Comment updated!').show()
-        }
-      })
-      .catch(e => {
-        throw e
-      })
-  }
+    const comment = promptForComment('Please enter your response.')
+    if (comment !== null) {
+        await commentOnRequest(mrid, comment)
+            .then(result => result.json())
+            .then(data => {
+                if (data.status === 'ok') {
+                    // We did it!
+                    // Tell the submitter somehow
+                    new FadingToast('Comment updated!').show()
+                }
+            })
+            .catch(e => {
+                throw e
+            })
+    }
 }
 
 function promptForComment(msg) {
-  return prompt(msg)
+    return prompt(msg)
 }
 
-async function updateRequest(action, mrid, comment=null) {
-  const formData = new FormData();
-  formData.set('mrid', mrid)
-  formData.set('action', action)
-  if (comment) {
-    formData.set('comment', comment)
-  }
+async function updateRequest(action, mrid, comment = null) {
+    const formData = new FormData();
+    formData.set('mrid', mrid)
+    formData.set('action', action)
+    if (comment) {
+        formData.set('comment', comment)
+    }
 
-  return fetch('/merges', {
-    method: 'POST',
-    body: formData
-  })
+    return fetch('/merges', {
+        method: 'POST',
+        body: formData
+    })
 }
 
 async function closeRequest(mrid, comment) {
-  return updateRequest('decline', mrid, comment)
+    return updateRequest('decline', mrid, comment)
 }
 
 async function commentOnRequest(mrid, comment) {
-  return updateRequest('comment', mrid, comment)
+    return updateRequest('comment', mrid, comment)
 }
 
 function removeRow(row) {
-  row.parentNode.removeChild(row)
+    row.parentNode.removeChild(row)
 }
