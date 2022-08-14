287cb1ed7f824e704bfd72758441af4ebab4a9b1
Christian Clauss <cclauss@me.com>
2022-08-14 22:52:15 +0200

    Use enumerate() for counting

--- openlibrary/solr/db_load_works.py
+++ openlibrary/solr/db_load_works.py
@@ -8,8 +8,8 @@ db.printing = False
 re_work_key = re.compile(r'^/works/OL(\d+)W$')
 
 total = 13941626
-num = 0
-for line in open('work_file'):
+
+for num, line in enumerate(open('work_file')):
     num += 1
     if num % 10000 == 0:
         print("%d %d %.2f%%" % (num, total, (float(num) * 100.0) / total))
