f400c443c676308f531eade99b65258e0690324d
Jim Man <58356745+jimman2003@users.noreply.github.com>
2022-08-19 01:01:57 +0300

    Simpify jest config + style mock (#6864)

--- tests/unit/js/styleMock.js
+++ tests/unit/js/styleMock.js
@@ -1,8 +1,2 @@
-// Jest transformer to handle .less imports
-// See https://stackoverflow.com/a/46184841/2317712
 // eslint-disable-next-line no-undef
-module.exports = {
-    process() {
-        return '';
-    },
-};
+module.exports = {};
