b2cb10c51f4916f73b2f51b31bfec1adff04ab35
Christian Clauss <cclauss@me.com>
2022-09-15 16:52:52 +0200

    Add pre-deploy.sh

--- scripts/deployment/pre-deploy.sh
+++ scripts/deployment/pre-deploy.sh
@@ -7,11 +7,7 @@ for REPO_DIR in $REPO_DIRS; do
     echo $REPO_DIR
 
     for SERVER in $SERVERS; do
-	    ssh $SERVER "cd $REPO_DIR; echo -ne $SERVER'\t'; sudo git status ; sudo git rev-parse HEAD"
+	    ssh $SERVER "cd $REPO_DIR; echo -ne $SERVER'\t'; sudo git status
     done
     echo "---"
-done
-
-for SERVER in $SERVERS; do
-    ssh $SERVER "hostname | docker image ls | grep olbase | grep latest"
 done
