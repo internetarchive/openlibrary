0456acf1767708e5e9c02ec4ffc46209b7ace160
Drini Cami <cdrini@gmail.com>
2022-09-29 19:01:42 -0400

    Remove no longer used utils

--- openlibrary/utils/tests/test_utils.py
+++ openlibrary/utils/tests/test_utils.py
@@ -1,5 +1,4 @@
 from openlibrary.utils import (
-    escape_bracket,
     extract_numeric_id_from_olid,
     finddict,
     str_to_key,
@@ -17,12 +16,6 @@ def test_str_to_key():
 def test_finddict():
     dicts = [{'x': 1, 'y': 2}, {'x': 3, 'y': 4}]
     assert finddict(dicts, x=1) == {'x': 1, 'y': 2}
-
-
-def test_escape_bracket():
-    assert escape_bracket('test') == 'test'
-    assert escape_bracket('this [is a] test') == 'this \\[is a\\] test'
-    assert escape_bracket('aaa [10 TO 500] bbb') == 'aaa [10 TO 500] bbb'
 
 
 def test_extract_numeric_id_from_olid():
