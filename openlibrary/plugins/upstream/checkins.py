ce691060c9ab77293d844ccacd0b580568cac398
James Champ <jameschamp@acm.org>
2022-11-01 19:18:03 -0700

    Prevent `make_date_string` from returning None
    Date inputs are validated before this function is called.

--- openlibrary/plugins/upstream/checkins.py
+++ openlibrary/plugins/upstream/checkins.py
@@ -13,20 +13,35 @@ from openlibrary.core.bookshelves_events import BookshelvesEvents
 from openlibrary.utils.decorators import authorized_for
 
 
-def make_date_string(year: int, month: Optional[int], day: Optional[int]) -> str | None:
-    """Creates a date string in 'YYYY-MM-DD' format, given the year, month, and day.
+def make_date_string(year: int, month: Optional[int], day: Optional[int]) -> str:
+    """Creates a date string in the expected format, given the year, month, and day.
 
-    Month and day can be None.  If the month is None, only the year is returned.
-    If there is a month but day is None, the year and month are returned.
+    Event dates can take one of three forms:
+    "YYYY"
+    "YYYY-MM"
+    "YYYY-MM-DD"
     """
-    if not year:
-        return None
     result = f'{year}'
     if month:
         result += f'-{month:02}'
         if day:
             result += f'-{day:02}'
     return result
+
+
+def is_valid_date(day, month, year):
+    """Validates dates.
+
+    Dates are considered valid if there is:
+    1. A year
+    2. A year and a month
+    3. A year, month, and day
+    """
+    if not year:
+        return False
+    if day and not month:
+        return False
+    return True
 
 
 class patron_check_ins(delegate.page):
@@ -63,33 +78,18 @@ class patron_check_ins(delegate.page):
     def validate_data(self, data):
         """Validates data submitted from check-in dialog."""
 
-        # There must be a year:
-        if 'year' not in data:
-            return False
-
         # Event type must exist:
         if 'event_type' not in data:
             return False
 
-        # Validate start and end dates
-        if not self.is_valid_date(
+        # Date must be valid:
+        if not is_valid_date(
             data.get('day', None),
             data.get('month', None),
             data.get('year', None),
         ):
             return False
 
-        return True
-
-    def is_valid_date(self, day, month, year):
-        """Validates dates using the following logic:
-        If a month is present, there must be a year.
-        If a day is present, there must be a month and year.
-        """
-        if day and (not month or not year):
-            return False
-        if month and not year:
-            return False
         return True
 
 
