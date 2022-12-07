e1a6cbce63cef3444a1e56ba85c409420baab8fd
Christian Clauss <cclauss@me.com>
2022-12-07 07:13:18 +0100

    Remove scripts/flake8-diff.sh

--- scripts/flake8-diff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# https://flake8.readthedocs.io/en/latest/user/error-codes.html
-# https://pycodestyle.readthedocs.io/en/latest/intro.html#error-codes
-# E226 missing whitespace around arithmetic operator
-# F401 '._init_path' imported but unused
-# W504 line break after binary operator
-# flake8 --diff --filename=*.py --ignore=E203,E226,F401,W503 --max-line-length=88 --statistics
-flake8 --diff --filename=*.py --select=E9,F63,F7,F82  --max-line-length=256 --statistics
