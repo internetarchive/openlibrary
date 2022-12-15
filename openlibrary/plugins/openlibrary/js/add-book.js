8729b45741667d7bdf0e0d4fb93163c96cb66cc1
Russell Warlow <52016758+russell-warlow@users.noreply.github.com>
2022-12-15 15:05:54 -0800

    fix bug with stripping potentially faulty isbn's, improved naming, made sure to pass linting. ready to review.

--- openlibrary/plugins/openlibrary/js/add-book.js
+++ openlibrary/plugins/openlibrary/js/add-book.js
@@ -10,19 +10,19 @@ export function initAddBookImport () {
         $('input#work').val('none-of-these');
         $('form#addbook').trigger('submit');
     });
-    $('#addbook').on('submit', validateIsbn);
-    $('#id_value').on('input', clearError);
-    $('#id_name').on('change', clearError);
+    $('#addbook').on('submit', parseAndValidateIsbn);
+    $('#id_value').on('input', clearIsbnError);
+    $('#id_name').on('change', clearIsbnError);
 }
 
 const isbn_invalid_checksum = 'Invalid ISBN checksum digit';
 const isbn10_wrong_length_or_chars = 'ID must be exactly 10 characters [0-9 or X]. For example 0-19-853453-1 or 0198534531';
 const isbn13_wrong_length_or_chars = 'ID must be exactly 13 characters [0-9]. For example 978-3-16-148410-0 or 9783161484100';
 
-// a flag to make raiseError perform differently upon subsequent calls
+// a flag to make raiseIsbnError perform differently upon subsequent calls
 let addBookWithIsbnErrors = false;
 
-function raiseError(event, errorMessage) {
+function raiseIsbnError(event, errorMessage) {
     if (!addBookWithIsbnErrors) {
         addBookWithIsbnErrors = true;
         const errorDiv = document.getElementById('id-errors');
@@ -33,10 +33,13 @@ function raiseError(event, errorMessage) {
         const isbnInput = document.getElementById('id_value');
         isbnInput.focus({focusVisible: true});
         event.preventDefault();
+        return;
     }
+    // parsing potentially invalid ISBN
+    document.getElementById('id_value').value = parseIsbn(document.getElementById('id_value').value);
 }
 
-function clearError() {
+function clearIsbnError() {
     addBookWithIsbnErrors = false;
     const errorDiv = document.getElementById('id-errors');
     errorDiv.classList.add('hidden');
@@ -44,24 +47,25 @@ function clearError() {
     confirm.classList.add('hidden');
 }
 
-function validateIsbn(event) {
+function parseAndValidateIsbn(event) {
     const fieldName = document.getElementById('id_name').value;
     const isbn = parseIsbn(document.getElementById('id_value').value);
     if (fieldName === 'isbn_10') {
         if (!isFormatValidIsbn10(isbn)) {
-            return raiseError(event, isbn10_wrong_length_or_chars);
+            return raiseIsbnError(event, isbn10_wrong_length_or_chars);
         }
         if (!isChecksumValidIsbn10(isbn)) {
-            return raiseError(event, isbn_invalid_checksum);
+            return raiseIsbnError(event, isbn_invalid_checksum);
         }
     }
     else if (fieldName === 'isbn_13') {
         if (!isFormatValidIsbn13(isbn)) {
-            return raiseError(event, isbn13_wrong_length_or_chars);
+            return raiseIsbnError(event, isbn13_wrong_length_or_chars);
         }
         if (!isChecksumValidIsbn13(isbn)) {
-            return raiseError(event, isbn_invalid_checksum);
+            return raiseIsbnError(event, isbn_invalid_checksum);
         }
     }
+    // parsing valid ISBN that passes checks
     document.getElementById('id_value').value = isbn;
 }
