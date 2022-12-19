e1e5c1ed278a612ffe032361d11b312ec5513be9
Drini Cami <cdrini@gmail.com>
2022-12-19 14:19:58 -0500

    Make MoreLikeThis carousels require user language
    This makes more sense for browse-ish carousels

--- openlibrary/plugins/upstream/utils.py
+++ openlibrary/plugins/upstream/utils.py
@@ -1181,6 +1181,15 @@ def convert_iso_to_marc(iso_639_1: str) -> str | None:
 def get_identifier_config(identifier: Literal['work', 'edition', 'author']) -> Storage:
     return _get_identifier_config(identifier)
 
+@public
+def get_user_lang_marc() -> str:
+    return convert_iso_to_marc(web.ctx.lang or 'en') or 'eng'
+
+
+@public
+def get_author_config():
+    return _get_author_config()
+
 
 @web.memoize
 def _get_identifier_config(identifier: Literal['work', 'edition', 'author']) -> Storage:
