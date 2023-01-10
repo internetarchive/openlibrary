8faae90c28058b78aa2053b8f184f13f26388cbc
Drini Cami <cdrini@gmail.com>
2023-01-10 14:16:00 -0500

    [solrbuilder] Fix ratings psql-chunk causing infinite loop

--- scripts/solr_builder/psql-import-simple.sh
+++ scripts/solr_builder/psql-import-simple.sh
@@ -4,4 +4,4 @@ DUMP="$1"
 DESTINATION="$2"
 
 zcat "${DUMP}" | \
-psql -d postgres --user=postgres -c  "COPY $DESTINATION FROM STDIN with delimiter E'\t' escape '\' quote E'\b' csv" &> logs/psql-chunk-ratings.txt
+psql -d postgres --user=postgres -c  "COPY $DESTINATION FROM STDIN with delimiter E'\t' escape '\' quote E'\b' csv" &> logs/psql-simple-$DESTINATION.txt
