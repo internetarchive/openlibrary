8a2e2be6b37c123e68a07aa1bfd9ffb01cdc5673
Tom Morris <tfmorris@gmail.com>
2023-01-11 17:24:57 -0500

    WIP Fix 100q fuller_name support. Refs #7408
    Note that although this unbugs the import, I don't think the field
    is actually used (or visible?) anywhere.

--- openlibrary/catalog/utils/__init__.py
+++ openlibrary/catalog/utils/__init__.py
@@ -92,6 +92,7 @@ def remove_trailing_number_dot(date):
 
 
 def remove_trailing_dot(s):
+    # FIXME: Mr. Mrs. M. and probably other trailing destinations need to be handled
     if s.endswith(" Dept."):
         return s
     m = re_end_dot.search(s)
