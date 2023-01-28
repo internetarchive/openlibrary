13903595fdc00b6d6b028b3a9ca84dafdd96b312
RayBB <RayBB@users.noreply.github.com>
2023-01-28 02:38:26 +0100

    use sudo

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -4,6 +4,6 @@
 # Setup pre-commit hooks
 pyenv install 3.11 --skip-existing # must match python version in .pre-commit-config.yaml
 pyenv global 3.11
-python3 -m pip install pre-commit
+sudo python3 -m pip install pre-commit
 pre-commit install --install-hooks
 git commit
