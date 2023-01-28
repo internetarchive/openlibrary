dcb47ebdef0ed98ee265768255c374ac3ca06ef7
RayBB <RayBB@users.noreply.github.com>
2023-01-28 02:20:48 +0100

    try to install pre-commit without sudo

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -4,6 +4,6 @@
 # Setup pre-commit hooks
 pyenv install 3.11 --skip-existing # should match .pre-commit-config.yaml
 pyenv global 3.11
-sudo python3 -m pip install pre-commit
+python3 -m pip install pre-commit
 pre-commit install --install-hooks
 git commit
