51d98e1efd64823e3f4c771a28ab196e610db4f6
RayBB <RayBB@users.noreply.github.com>
2023-01-28 02:30:43 +0100

    better comment

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -2,7 +2,7 @@
 # This script does the setup needed for Gitpod
 
 # Setup pre-commit hooks
-pyenv install 3.11 --skip-existing # should match .pre-commit-config.yaml
+pyenv install 3.11 --skip-existing # must match python version in .pre-commit-config.yaml
 pyenv global 3.11
 python3 -m pip install pre-commit
 pre-commit install --install-hooks
