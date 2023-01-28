370b9c5283a1e5deea8d0705136bc06fd26af916
RayBB <RayBB@users.noreply.github.com>
2023-01-28 02:20:23 +0100

    don't use python docker image

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-# This script does the setup needed for gitpod
+# This script does the setup needed for Gitpod
 
 # Setup pre-commit hooks
-# This is in {} so the pyenv and pre-commit steps can run in the background while docker starts
-# A downside is that progress messages from docker and pre-commit may be mixed together.
-# {
 pyenv install 3.11 --skip-existing # should match .pre-commit-config.yaml
 pyenv global 3.11
 sudo python3 -m pip install pre-commit
-# PIP_USER false because https://github.com/gitpod-io/gitpod/issues/4886#issuecomment-963665656
-# env PIP_USER=false pre-commit install
-# env PIP_USER=false pre-commit run
 pre-commit install --install-hooks
 git commit
-# } &
-
-# # Builds the docker images so when user opens env this step is cached.
-# # We do this last because it occasionally fails on gitpod
-# docker-compose up --no-start
