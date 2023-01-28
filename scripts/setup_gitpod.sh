8b1297b007aa67cbb4310465772405cdc6caeb86
RayBB <RayBB@users.noreply.github.com>
2023-01-28 02:17:40 +0100

    --skip-existing

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -5,7 +5,7 @@
 # This is in {} so the pyenv and pre-commit steps can run in the background while docker starts
 # A downside is that progress messages from docker and pre-commit may be mixed together.
 # {
-pyenv install 3.11 # should match .pre-commit-config.yaml
+pyenv install 3.11 --skip-existing # should match .pre-commit-config.yaml
 pyenv global 3.11
 sudo python3 -m pip install pre-commit
 # PIP_USER false because https://github.com/gitpod-io/gitpod/issues/4886#issuecomment-963665656
