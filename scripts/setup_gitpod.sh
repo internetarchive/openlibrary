5af5d5e4cfdbb507c8cc3e98792849266af4367e
RayBB <RayBB@users.noreply.github.com>
2023-01-28 01:54:00 +0100

    pyenv 3.8

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -5,13 +5,14 @@
 # This is in {} so the pyenv and pre-commit steps can run in the background while docker starts
 # A downside is that progress messages from docker and pre-commit may be mixed together.
 # {
-pyenv install 3.11 # should match .pre-commit-config.yaml
-pyenv global 3.11
+pyenv install 3.8 # should match .pre-commit-config.yaml
+pyenv global 3.8
 pip install pre-commit
 # PIP_USER false because https://github.com/gitpod-io/gitpod/issues/4886#issuecomment-963665656
 # env PIP_USER=false pre-commit install
 # env PIP_USER=false pre-commit run
 pre-commit install --install-hooks
+git commit
 # } &
 
 # # Builds the docker images so when user opens env this step is cached.
