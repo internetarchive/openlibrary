134c6a2e81dc863a9762eba3aa61f0f859243bdc
RayBB <RayBB@users.noreply.github.com>
2023-01-28 01:51:55 +0100

    no pip_user

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -9,8 +9,9 @@ pyenv install 3.11 # should match .pre-commit-config.yaml
 pyenv global 3.11
 pip install pre-commit
 # PIP_USER false because https://github.com/gitpod-io/gitpod/issues/4886#issuecomment-963665656
-env PIP_USER=false pre-commit install
-env PIP_USER=false pre-commit run
+# env PIP_USER=false pre-commit install
+# env PIP_USER=false pre-commit run
+pre-commit install --install-hooks
 # } &
 
 # # Builds the docker images so when user opens env this step is cached.
