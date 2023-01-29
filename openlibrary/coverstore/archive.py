cb8c961a1c0a172c026abc7b4074c87c6f8d33e6
Mek <michael.karpeles@gmail.com>
2023-01-29 14:29:52 -0800

    clarify naming

--- openlibrary/coverstore/archive.py
+++ openlibrary/coverstore/archive.py
@@ -90,30 +90,30 @@ class TarManager:
 
 idx = id
 
-
 def is_uploaded(item, f):
-    command = fr'ia list {item} | grep "{f}\.[tar|index]" | wc -l'
+    command = f'ia list {item} | grep "{f}\.[tar|index]" | wc -l'
     result = subprocess.run(command, shell=True, capture_output=True)
     output = result.stdout.decode().strip()
     return int(output) == 2
 
 
-def audit(major, minor, upload=False, sizes=('', 's', 'm', 'l')):
-    """Checks the archive.org items pertaining to this `major` set (up to
-    1 million images and size specified, 4-digit e.g. 0008) and each
-    specified size, to verify that all `minor` chunks (up to the
-    specified max) and their indices + tars (of 10k images, 2-digit
+def audit(group_id, chunk_ids=(0,100), sizes=('', 's', 'm', 'l'), upload=False):
+    """Check which cover batches have been uploaded to archive.org.
+    
+    Checks the archive.org items pertaining to this `group` of up to
+    1 million images (4-digit e.g. 0008) for each specified size and verify
+    that all the chunks (within specified range) and their .indices + .tars (of 10k images, 2-digit
     e.g. 81) have been successfully uploaded.
-
-    {size}_covers_{major}_{minor}:
-    major - 4  digit, batches of 1M, 0000 to 9999M
-    minor - 2 digit, batches of 10k from _00 to _99
-
+                                                                                                             
+    {size}_covers_{group}_{chunk}:                                                                                           
+    group_id - 4 digit, batches of 1M, 0000 to 9999M                                                                                                              
+    chunk_ids - (min, max) chunk_id range or max_chunk_id; 2 digit, batch of 10k from [00, 99]                                                                                                             
+                                                                                                                                                                
     """
-    scope = minor if isinstance(minor, range) else range(0, minor)
+    scope = chunk_ids if isinstance(chunk_ids, range) else range(0, chunk_ids)
     for size in sizes:
         prefix = f"{size}_" if size else ''
-        item = f"{prefix}covers_{major}"
+        item = f"{prefix}covers_{primary}"
         files = [f"{prefix}covers_0008_{'%02i' % i}" for i in scope]
         missing_files = []
         sys.stdout.write(f"\n{size or 'full'}: ")
@@ -127,12 +127,9 @@ def audit(major, minor, upload=False, sizes=('', 's', 'm', 'l')):
         sys.stdout.write("\n")
         sys.stdout.flush()
         if missing_files:
-            print(
-                f"ia upload {item} {' '.join([f'{item}/{mf}*' for mf in missing_files])} --retries 10"
-            )
+            print(f"ia upload {item} {' '.join([f'{item}/{mf}*' for mf in missing_files])} --retries 10")
             if upload:
                 pass
-
 
 def archive(test=True):
     """Move files from local disk to tar files and update the paths in the db."""
