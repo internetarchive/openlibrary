f19d1b1330613adb31fa76a410abfadc111833cf
Christian Clauss <cclauss@me.com>
2023-02-09 10:35:18 +0100

    get_web_error(filename, hosts, docker_container)

--- scripts/get_web_error.py
+++ scripts/get_web_error.py
@@ -7,8 +7,8 @@ To see the contents of an Open Library user-reported like 2023-02-03/01155653562
 1. Copy this script into your user directory on any Open Library host.
 2. Type the command above substituting in the error code you are looking for.
 
-This script will ssh into ol-web1 and ol-web2 and if that file is found in the
-openlibrary_web_1 container, it will print its contents to stdout.
+This script will ssh into hosts ol-web1 and ol-web2 and if that file is found in the
+docker_container openlibrary_web_1, it will print its contents to stdout.
 
 Type `yes` if ssh prompts you to add the hosts to your known_hosts file.
 
@@ -17,27 +17,31 @@ If lynx is not yet installed, please type `sudo apt-get install lynx` to install
 
 import subprocess
 import sys
-from datetime import date
+from collections.abc import Iterable
 
 usage = f"""Usage: {sys.argv[0]} [filename] | lynx --stdin
 filename is like 2023-02-03/000008077313.html"""
 
 
-def get_web_error(filename: str = "2023-02-03/000008077313.html") -> str:
+def get_web_error(
+    filename: str,
+    hosts: Iterable[str] = ("ol-web1.us.archive.org", "ol-web2.us.archive.org"),
+    docker_container: str = "openlibrary_web_1",
+) -> str:
     """
-    ssh into ol-web1 and ol-web2 and if filename is found in the
-    openlibrary_web_1 Docker container then return its contents.
+    ssh into hosts ol-web1 and ol-web2 and if filename is found in the
+    docker_container openlibrary_web_1 then return its contents.
     """
     file_path = f"/var/log/openlibrary/ol-errors/{filename}"
-    for host in ("ol-web1.us.archive.org", "ol-web2.us.archive.org"):
-        command = f"ssh -A -t {host} 'docker exec -i openlibrary_web_1 cat {file_path}'"
+    for host in hosts:
+        cmd = f"ssh -A -t {host} 'docker exec -i {docker_container} cat {file_path}'"
         try:
-            if output := subprocess.check_output(command, shell=True, text=True):
+            if output := subprocess.check_output(cmd, shell=True, text=True):
                 return output
         except subprocess.CalledProcessError as e:
             print(f"Error: {e!r}")
             raise (e)
-    return f"Error: {file_path} was not found on either ol-web1 or ol-web2"
+    return f"Error: {file_path} was not found on {' or '.join(hosts)}."
 
 
 if __name__ == "__main__":
