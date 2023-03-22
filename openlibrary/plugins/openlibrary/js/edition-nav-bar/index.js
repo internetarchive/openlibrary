7dd3c826d9c179cf67bf3320b3aac38b5cee5164
James Champ <jameschamp@acm.org>
2023-03-22 08:37:51 -0700

    Fix scroll-aware nav item selection
    `navbarElem.scrollTo` call and scroll event handler were interfering
    with each other.  Added a lock that lasts for one second when `scrollTo`
    is called.  Scroll handler now removes lock and returns without further
    processing if lock is present.

--- openlibrary/plugins/openlibrary/js/edition-nav-bar/index.js
+++ openlibrary/plugins/openlibrary/js/edition-nav-bar/index.js
@@ -1,5 +1,3 @@
-import { debounce } from '../nonjquery_utils'
-
 /**
  * Adds navbar-related click and scroll listeners
  *
@@ -12,13 +10,14 @@ export function initNavbar(navbarWrapper) {
      * @type {HTMLElement}
      */
     const navbarElem = navbarWrapper.querySelector('.work-menu')
+
     /**
-     * Left and right navigation arrows.  These will only be present in
+     * Right navigation arrow.  This will only be present in
      * mobile views.
      *
-     * @type {HTMLCollection<HTMLElement>}
+     * @type {HTMLElement}
      */
-    const navArrows = navbarWrapper.querySelectorAll('.nav-arrow')
+    const navArrow = navbarWrapper.querySelector('.nav-arrow')
 
     /**
      * Each list item in the navbar.
@@ -35,6 +34,22 @@ export function initNavbar(navbarWrapper) {
     let selectedIndex = 0;
 
     /**
+     * Reference to the selected target element.
+     *
+     * @type {HTMLElement}
+     */
+    let selectedSection
+
+    /**
+     * Flag for ignoring navbar scroll events.  We want to disable
+     * scroll-aware nav item selection if navbarElem.scrollTo() was
+     * recently called.
+     *
+     * @type {boolean}
+     */
+    let ignoreScrollEvent = false
+
+    /**
      * The targets of the navbar links.
      *
      * Elements are ordered from highest position on the page to lowest.
@@ -47,30 +62,24 @@ export function initNavbar(navbarWrapper) {
     for (let i = 0; i < listItems.length; ++i) {
         listItems[i].addEventListener('click', function() {
             selectedIndex = i
-            debounce(selectElement(listItems[i]), 300, false)
+            selectElement(listItems[i], i, true)
         })
 
-        linkedSections.push(document.querySelector(listItems[i].children[0].hash))
+        linkedSections.push(document.getElementById(listItems[i].children[0].hash.substring(1)))
+        if (listItems[i].classList.contains('selected')) {
+            selectedSection = linkedSections[linkedSections.length - 1]
+        }
     }
 
-    // Initialize mobile-only navigation arrows
-    for (const arrow of navArrows) {
-        const decrementIndex = arrow.classList.contains('nav-arrow--left')
-
-        arrow.addEventListener('click', function() {
+    // Initialize mobile-only navigation arrow
+    if  (navArrow) {
+        navArrow.addEventListener('click', function() {
             let selectedElem
-            if (decrementIndex) {
-                if (selectedIndex > 0) {
-                    --selectedIndex
-                    selectedElem = listItems[selectedIndex]
-                    selectElement(selectedElem)
-                }
-            } else {
-                if (selectedIndex < listItems.length - 1) {
-                    ++selectedIndex;
-                    selectedElem = listItems[selectedIndex]
-                    selectElement(selectedElem)
-                }
+
+            if (selectedIndex < listItems.length - 1) {
+                ++selectedIndex;
+                selectedElem = listItems[selectedIndex]
+                selectElement(selectedElem, selectedIndex, true)
             }
             if (selectedElem) {
                 const anchor = selectedElem.querySelector('a')
@@ -79,6 +88,30 @@ export function initNavbar(navbarWrapper) {
         })
     }
 
+    // Add scroll listener that changes 'selected' navbar item based on page position:
+    document.addEventListener('scroll', function() {
+        if (ignoreScrollEvent) {
+            setTimeout(function() {
+                ignoreScrollEvent = false
+            }, 1000)
+            return
+        }
+
+        const navbarHeight = navbarWrapper.getBoundingClientRect().height
+        if (navbarHeight > 0) {
+            let i = listItems.length
+
+            // Find index of lowest element on the page that is positioned below the navbar:
+            // while(i < linkedSections.length && navbarWrapper.offsetTop < linkedSections[i++].offsetTop) {}
+            while (--i > 0 && navbarWrapper.offsetTop + navbarHeight < linkedSections[i].offsetTop) {}
+            if (i < linkedSections.length) {
+                if (linkedSections[i] !== selectedSection) {
+                    selectElement(listItems[i], i)
+                }
+            }
+        }
+    })
+
     /**
      * Adds 'selected' class to the given element.
      *
@@ -86,17 +119,21 @@ export function initNavbar(navbarWrapper) {
      * 'selected' to the newly selected link.
      *
      * @param {HTMLLIElement} selectedElem Element corresponding to the 'selected' navbar item.
+     * @param {Number} targetIndex The index of the target element
      */
-    function selectElement(selectedElem) {
+    function selectElement(selectedElem, targetIndex) {
         for (const li of listItems) {
             li.classList.remove('selected')
         }
         selectedElem.classList.add('selected')
+        selectedSection = linkedSections[targetIndex];
 
         // Scroll to / center the item
         // Note: We don't use the browser native scrollIntoView method because
         // that method scrolls _recursively_, so it also tries to scroll the
         // body to center the element on the screen, causing weird jitters.
+
+        ignoreScrollEvent = true
         navbarElem.scrollTo({
             left: selectedElem.offsetLeft - (navbarElem.clientWidth - selectedElem.offsetWidth) / 2,
             behavior: 'instant'
