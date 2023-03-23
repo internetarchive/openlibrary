1a5dac4bc50904d3ec75c4e11161a8be8af327ad
Scott Barnes <scottreidbarnes@gmail.com>
2023-03-22 22:12:18 -0700

    Replace Docker Compose per the Compose V2 spec recommendations
    This PR:
    - replaces references to "docker-compose" with "docker compose";
    - removes the "docker-" prefix from Compose files;
    - changes Compose file suffixes to "yaml"; and
    - updates any filenames. E.g. "docker-compose.yml" >
      "compose.yaml".
    
    Docker guidance prefers 'compose.yaml', and this PR follows that:
      https://docs.docker.com/compose/compose-file/03-compose-file/
    
    Co-authored-by: Scott Barnes <scottreidbarnes@gmail.com>

--- tests/test_docker_compose.py
+++ tests/test_docker_compose.py
@@ -10,18 +10,18 @@ def p(*paths):
 class TestDockerCompose:
     def test_all_root_services_must_be_in_prod(self):
         """
-        Each service in docker-compose.yml should also be in
-        docker-compose.production.yml with a profile. Services without profiles will
+        Each service in compose.yaml should also be in
+        compose.production.yaml with a profile. Services without profiles will
         match with any profile, meaning the service would get deployed everywhere!
         """
-        with open(p('..', 'docker-compose.yml')) as f:
+        with open(p('..', 'compose.yaml')) as f:
             root_dc: dict = yaml.safe_load(f)
-        with open(p('..', 'docker-compose.production.yml')) as f:
+        with open(p('..', 'compose.production.yaml')) as f:
             prod_dc: dict = yaml.safe_load(f)
         root_services = set(root_dc['services'])
         prod_services = set(prod_dc['services'])
         missing = root_services - prod_services
-        assert missing == set(), "docker-compose.production.yml missing services"
+        assert missing == set(), "compose.production.yaml missing services"
 
     def test_all_prod_services_need_profile(self):
         """
@@ -29,14 +29,14 @@ class TestDockerCompose:
         is not likely what you want. If that is what you want, add all server names to
         this service to make things explicit.
         """
-        with open(p('..', 'docker-compose.production.yml')) as f:
+        with open(p('..', 'compose.production.yaml')) as f:
             prod_dc: dict = yaml.safe_load(f)
         for serv, opts in prod_dc['services'].items():
             assert 'profiles' in opts, f"{serv} is missing 'profiles' field"
 
     def test_shared_constants(self):
-        # read the value in docker-compose.yml
-        with open(p('..', 'docker-compose.yml')) as f:
+        # read the value in compose.yaml
+        with open(p('..', 'compose.yaml')) as f:
             prod_dc: dict = yaml.safe_load(f)
         solr_service = prod_dc['services']['solr']
         solr_opts = next(
