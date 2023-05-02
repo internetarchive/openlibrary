cd2ed3190e902486de752d383426b989fc282d25
jimchamp <jameschamp@acm.org>
2023-05-01 20:04:51 -0700

    Fix selected nav item in Safari (#7793)
    * Add polyfill for `checkVisibility()`
    * Add padding to nav arrow
    * Reveal occluded navbar for tablet views
    * Remove conditional navbar hydratio
    * Updated selected nav item when navbar repositioned

--- openlibrary/plugins/openlibrary/js/edition-nav-bar/index.js
+++ openlibrary/plugins/openlibrary/js/edition-nav-bar/index.js
@@ -1,112 +1,33 @@
+import EdtionNavBar from './EditionNavBar';
+
 /**
- * Adds navbar-related click and scroll listeners
- *
- * @param {HTMLElement} navbarWrapper The wrapper that contains the navbar
+ * Holds references to each book page navbar.
+ * @type {Array<EditionNavBar>}
  */
-export function initNavbar(navbarWrapper) {
-    /**
-     * The book page navbar
-     *
-     * @type {HTMLElement}
-     */
-    const navbarElem = navbarWrapper.querySelector('.work-menu')
+const navbars = []
 
-    /**
-     * Right navigation arrow.  This will only be present in
-     * mobile views.
-     *
-     * @type {HTMLElement}
-     */
-    const navArrow = navbarWrapper.querySelector('.nav-arrow')
-
-    /**
-     * Each list item in the navbar.
-     *
-     * @type {HTMLCollection<HTMLLIElement}
-     */
-    const listItems = Array.from(navbarElem.querySelectorAll('li'));
-
-    /**
-     * The index of the currently selected item in the `listItems` array.
-     *
-     * @type {number}
-     */
-    let selectedIndex = 0;
-
-    /**
-     * The targets of the navbar links.
-     *
-     * Elements are ordered from highest position on the page to lowest.
-     *
-     * @type {Array<HTMLElement>}
-     */
-    const linkedSections = []
-
-    // Add click listeners to navbar items
-    for (let i = 0; i < listItems.length; ++i) {
-        listItems[i].addEventListener('click', function() {
-            selectedIndex = i
-            selectElement(listItems[i])
-        })
-
-        linkedSections.push(document.getElementById(listItems[i].children[0].hash.substring(1)))
-        if (listItems[i].classList.contains('selected')) {
-            selectedIndex = i
-        }
+/**
+ * Initializes and stores references to each book page navbar.
+ *
+ * @param {HTMLCollection<HTMLElement>} navbarWrappers Each navbar found on the book page
+ */
+export function initNavbars(navbarWrappers) {
+    for (const wrapper of navbarWrappers) {
+        const navbar = new EdtionNavBar(wrapper)
+        navbars.push(navbar)
     }
+}
 
-    // Initialize mobile-only navigation arrow
-    if (navArrow) {
-        navArrow.addEventListener('click', function() {
-            if (selectedIndex < listItems.length - 1) {
-                ++selectedIndex;
-                listItems[selectedIndex].children[0].click()
-            }
-        })
-    }
-
-    // Add scroll listener that changes 'selected' navbar item based on page position:
-    document.addEventListener('scroll', function() {
-
-        const navbarHeight = navbarWrapper.getBoundingClientRect().height
-        if (navbarHeight > 0) {
-            let i = listItems.length
-            while (--i > 0 && navbarWrapper.offsetTop + navbarHeight < linkedSections[i].offsetTop) {}
-            if (i !== selectedIndex) {
-                selectedIndex = i
-                selectElement(listItems[i])
-            }
-        }
-    })
-
-    /**
-     * Adds 'selected' class to the given element.
-     *
-     * Removes 'selected' class from all navbar links, then adds
-     * 'selected' to the newly selected link.
-     *
-     * @param {HTMLLIElement} selectedElem Element corresponding to the 'selected' navbar item.
-     */
-    function selectElement(selectedElem) {
-        for (const li of listItems) {
-            li.classList.remove('selected')
-        }
-        selectedElem.classList.add('selected')
-        scrollNavbar(selectedElem)
-    }
-
-    /**
-     * Centers given nav item in the navbar, if navbar has overflow (mobile views)
-     *
-     * @param {HTMLElement} selectedItem Newly selected nav item
-     */
-    function scrollNavbar(selectedItem) {
-        // Note: We don't use the browser native scrollIntoView method because
-        // that method scrolls _recursively_, so it also tries to scroll the
-        // body to center the element on the screen, causing weird jitters.
-
-        navbarElem.scrollTo({
-            left: selectedItem.offsetLeft - (navbarElem.clientWidth - selectedItem.offsetWidth) / 2,
-            behavior: 'instant'})
+/**
+ * Updates the "selected" nav item of each navbar.
+ *
+ * This should be used to update the book page navbars
+ * when they have been repositioned on the page by
+ * something other then a scroll event (e.g. when
+ * stickied to a new position).
+ */
+export function updateSelectedNavItem() {
+    for (const navbar of navbars) {
+        navbar.updateSelected()
     }
 }
