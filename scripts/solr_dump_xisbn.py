0dc14704dda0db4fdb118b14afe185797a8c1a23
Drini Cami <cdrini@gmail.com>
2023-05-30 13:46:43 -0400

    Expand solr xisbn script to optionally output lccn

--- scripts/solr_dump_xisbn.py
+++ scripts/solr_dump_xisbn.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
 """
-    Script for creating a file of similar ISBNs from Solr.
+    Script for creating a file of similar ISBNs or LCCNs from Solr.
 """
 import asyncio
 import sys
+from typing import Literal
 
 import httpx
 
-# EG http://localhost:8984/solr/openlibrary/export?editions.fl=key%2Cisbn&editions.q=(%7B!terms%20f%3D_root_%20v%3D%24row.key%7D)%20AND%20language%3Aeng%20AND%20isbn%3A*%20AND%20type%3Aedition&editions.rows=1000000&fl=key%2Ceditions%3A%5Bsubquery%5D&fq=type%3Awork&indent=true&q.op=OR&q=isbn%3A*%20AND%20NOT%20subject%3Atextbook%20AND%20_query_%3A(%7B!parent%20which%3Dtype%3Awork%20v%3D%22language%3Aeng%20AND%20ia_box_id%3A*%22%20filters%3D%22type%3Aedition%22%7D)&sort=key%20asc&wt=json
+# EG http://localhost:8984/solr/openlibrary/select?editions.fl=key%2Cisbn&editions.q=(%7B!terms%20f%3D_root_%20v%3D%24row.key%7D)%20AND%20language%3Aeng%20AND%20isbn%3A*%20AND%20type%3Aedition&editions.rows=1000000&fl=key%2Ceditions%3A%5Bsubquery%5D&fq=type%3Awork&indent=true&q=isbn%3A*%20AND%20NOT%20subject%3Atextbook%20AND%20_query_%3A(%7B!parent%20which%3Dtype%3Awork%20v%3D%22language%3Aeng%20AND%20ia_box_id%3A*%22%20filters%3D%22type%3Aedition%22%7D)&sort=key%20asc&wt=json
 
 
 async def fetch_docs(params: dict[str, str | int], solr_base: str, page_size=100):
@@ -63,27 +65,40 @@ async def stream_bounds(params: dict[str, str | int], solr_base: str, page_size=
                     await asyncio.sleep(2)
             data = response.json()
             numFound = data['response']['numFound']
-            seen += page_size
-            yield data['response']['docs'][0]['key'], data['response']['docs'][-1][
-                'key'
-            ]
-            if data['nextCursorMark'] == params['cursorMark']:
+
+            docs = data['response']['docs']
+            if docs:
+                seen += len(docs)
+                yield docs[0]['key'], docs[-1]['key']
+                params['cursorMark'] = data['nextCursorMark']
+            else:
                 break
-            params['cursorMark'] = data['nextCursorMark']
+
+            if params['cursorMark'] == data['nextCursorMark']:
+                break
 
 
 async def main(
     solr_base='http://localhost:8984/solr/openlibrary',
     workers=10,
     page_size=100,
+    id_field: Literal['isbn', 'lccn'] = 'isbn',
 ):
     """
     :param solr_base: Base URL of Solr instance
     :param workers: Number of workers to use
     :param page_size: Number of results to fetch per query
+    :param id_field: Which identifier to use for matching
     """
+    id_filter = f'{id_field}:*'
+    work_exclusions = 'subject:textbook'
     galloping_params = {
-        'q': 'isbn:* AND NOT subject:textbook AND _query_:({!parent which=type:work v="language:eng AND ia_box_id:*" filters="type:edition"})',
+        # Find works that have at least one edition with an IA box id
+        'q': f"""
+            {id_filter}
+            AND NOT ({work_exclusions})
+            AND _query_:({{!parent which=type:work v="language:eng AND ia_box_id:*" filters="type:edition"}})
+        """,
         'fq': 'type:work',
         'sort': 'key asc',
         'fl': 'key',
@@ -104,27 +119,27 @@ async def main(
                 'q': f'key:["{start}" TO "{end}"] AND {galloping_params["q"]}',
                 'fq': 'type:work',
                 'fl': 'key,editions:[subquery]',
-                'editions.q': '({!terms f=_root_ v=$row.key}) AND language:eng AND isbn:*',
+                'editions.q': f'({{!terms f=_root_ v=$row.key}}) AND language:eng AND {id_filter}',
                 'editions.fq': 'type:edition',
-                'editions.fl': 'key,isbn',
+                'editions.fl': f'key,{id_field}',
                 'editions.rows': 1_000_000,
                 'wt': 'json',
             },
             solr_base,
             page_size=page_size * 2,
         ):
-            isbns = {
-                isbn
+            identifiers = {
+                identifier
                 for ed in doc['editions']['docs']
-                for isbn in ed['isbn']
-                if len(isbn) == 13
+                for identifier in ed[id_field]
+                if (len(identifier) == 13 if id_field == 'isbn' else True)
             }
-            if len(isbns) > 1:
-                result += ' '.join(isbns) + '\n'
+            if len(identifiers) > 1:
+                result += ' '.join(identifiers) + '\n'
         print(f'[x] FETCH {bounds=}', file=sys.stderr)
         print(result, flush=True)
 
-    # now run 10 workers in async pool to process the bounds
+    # now run N workers in async pool to process the bounds
     running = set()
     async for bounds in stream_bounds(galloping_params, solr_base, page_size=page_size):
         if len(running) >= workers:
