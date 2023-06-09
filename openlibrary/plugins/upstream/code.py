a31e3c1a7d61eb720b0e483100f977a9026afa2c
Jayden Teoh <t3ohjingxiang@gmail.com>
2023-06-09 21:47:58 +0800

    remove addtag and add DDL for tags

--- openlibrary/plugins/upstream/code.py
+++ openlibrary/plugins/upstream/code.py
@@ -22,7 +22,7 @@ from infogami.utils.context import context
 
 from openlibrary import accounts
 
-from openlibrary.plugins.upstream import addbook, addtag, covers, models, utils
+from openlibrary.plugins.upstream import addbook, covers, models, utils
 from openlibrary.plugins.upstream import spamcheck
 from openlibrary.plugins.upstream import merge_authors
 from openlibrary.plugins.upstream import edits
@@ -384,7 +384,6 @@ def setup():
     models.setup()
     utils.setup()
     addbook.setup()
-    addtag.setup()
     covers.setup()
     merge_authors.setup()
     # merge_works.setup() # ILE code
