d1b67c1a7b6bcbda0fbeb04eb8864912a704aeb7
GustavoHenrique23 <gustavohenriqueprivado@gmail.com>
2023-06-19 22:22:36 -0300

    fix: fix test

--- tests/unit/js/editionsEditPage.test.js
+++ tests/unit/js/editionsEditPage.test.js
@@ -235,6 +235,13 @@ describe("initRoleValidation ", () => {
     initRoleValidation();
   });
 
+  it("Valida se o role e o name estão preenchido", () => {
+    $("#select-role").val("afterword");
+    $("#role-name").val("gustavo");
+    $(".repeat-add").trigger("click");
+    const cssDisplay = $("#role-errors").css("display");
+    expect(cssDisplay).toBe("none");
+  });
   it("Valida se o role e o name estão vazios", () => {
     $("#select-role").val("");
     $("#role-name").val("");
@@ -269,12 +276,5 @@ describe("initRoleValidation ", () => {
     $(".repeat-add").trigger("click");
     const cssDisplay = $("#role-errors").css("display");
     expect(cssDisplay).toBe("block");
-  });
-  it("Valida se o role e o name estão preenchido", () => {
-    $("#select-role").val("afterword");
-    $("#role-name").val("gustavo");
-    $(".repeat-add").trigger("click");
-    const cssDisplay = $("#role-errors").css("display");
-    expect(cssDisplay).toBe("none");
   });
 });
