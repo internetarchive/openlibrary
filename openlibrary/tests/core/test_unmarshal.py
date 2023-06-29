eba7330377de359cbaf229efa5ad8dd49a6a701e
Christian Clauss <cclauss@me.com>
2023-06-29 20:49:17 +0200

    @pytest.mark.parametrize

--- openlibrary/tests/core/test_unmarshal.py
+++ openlibrary/tests/core/test_unmarshal.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import re
 
+import pytest
+
 from openlibrary.api import unmarshal
 
 
@@ -30,81 +32,44 @@ def parse_datetime(value: datetime | str) -> datetime:
     return datetime(*(int(token) for token in tokens))  # type: ignore[arg-type]
 
 
-def test_ct1_unmarshal_list():
-    input_data = [
-        {"type": "/type/text", "value": "hello, world"},
-        {"type": "/type/datetime", "value": "2009-01-02T03:04:05.006789"},
-    ]
-    expected_output = [
-        Text("hello, world"),
-        parse_datetime("2009-01-02T03:04:05.006789"),
-    ]
-    assert unmarshal(input_data) == expected_output
+pytest.mark.parametrize(
+    "data,expected",
+    [
+        (
+            [
+                {"type": "/type/text", "value": "hello, world"},
+                {"type": "/type/datetime", "value": "2009-01-02T03:04:05.006789"},
+            ],
+            [
+                Text("hello, world"),
+                parse_datetime("2009-01-02T03:04:05.006789"),
+            ],
+        ),
+        ({"value": "hello, world", "type": "/type/text"}, Text("hello, world")),
+        (
+            {"value": "2009-01-02T03:04:05.006789", "type": "/type/datetime"},
+            parse_datetime("2009-01-02T03:04:05.006789"),
+        ),
+        ({}, {}),
+        ({"value": "hello, world"}, {"value": "hello, world"}),
+        (
+            {
+                "key1": "value1",
+                "key2": {"value2": "value2", "type": "/type/text"},
+                "key3": "2009-01-02T03:04:05.006789",
+            },
+            {
+                "key1": "value1",
+                "key2": {"value2": "value2", "type": "/type/text"},
+                "key3": "2009-01-02T03:04:05.006789",
+            },
+        ),
+        ([{"type": "/type/invalid", "value": "hello, world"}], ["hello, world"]),
+        ({"type": "/type/invalid", "value": "hello, world"}, "hello, world"),
+        ({"value": "", "type": "/type/text"}, ""),
+    ],
+)
 
 
-def test_ct2_unmarshal_dict_with_key():
-    input_data = {"value": "hello, world", "type": "/type/text"}
-    expected_output = Text("hello, world")
-    assert unmarshal(input_data) == expected_output
-
-
-def test_ct3_unmarshal_dict_with_value_and_type():
-    input_data = {"value": "hello, world", "type": "/type/text"}
-    expected_output = Text("hello, world")
-    assert unmarshal(input_data) == expected_output
-
-
-def test_ct4_unmarshal_dict_with_value_and_type_datetime():
-    input_data = {"value": "2009-01-02T03:04:05.006789", "type": "/type/datetime"}
-    expected_output = parse_datetime("2009-01-02T03:04:05.006789")
-    assert unmarshal(input_data) == expected_output
-
-
-def test_ct5_unmarshal_dict_with_no_key_or_value():
-    input_data = {}
-    expected_output = {}
-    assert unmarshal(input_data) == expected_output
-
-
-def test_ct6_unmarshal_dict_with_no_type_or_key():
-    input_data = {"value": "hello, world"}
-    expected_output = input_data
-    assert unmarshal(input_data) == expected_output
-
-
-def test_ct7_unmarshal_dict_with_complex_data():
-    input_data = {
-        "key1": "value1",
-        "key2": {"value2": "value2", "type": "/type/text"},
-        "key3": "2009-01-02T03:04:05.006789",
-    }
-    expected_output = {
-        "key1": "value1",
-        "key2": {"value2": "value2", "type": "/type/text"},
-        "key3": "2009-01-02T03:04:05.006789",
-    }
-    assert unmarshal(input_data) == expected_output
-
-
-def test_ct8_unmarshal_list_with_invalid_type():
-    input_data = [{"type": "/type/invalid", "value": "hello, world"}]
-    expected_output = ["hello, world"]
-    assert unmarshal(input_data) == expected_output
-
-
-def test_ct9_unmarshal_dict_with_invalid_type():
-    input_data = {"type": "/type/invalid", "value": "hello, world"}
-    expected_output = "hello, world"
-    assert unmarshal(input_data) == expected_output
-
-
-def test_ct10_unmarshal_dict_with_empty_value():
-    input_data = {"value": "", "type": "/type/text"}
-    expected_output = ""
-    assert unmarshal(input_data) == expected_output
-
-
-def test_ct11_unmarshal_dict_with_empty_dict():
-    input_data = {}
-    expected_output = {}
-    assert unmarshal(input_data) == expected_output
+def test_unmarshal(data, expected) -> None:
+    assert unmarshal(data) == expected
