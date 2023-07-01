743fd8e34a1389fc1cc359d6e123e656a5eec4e1
Christian Clauss <cclauss@me.com>
2023-07-01 07:50:23 +0200

    pytest.mark.parametrize

--- openlibrary/plugins/books/tests/test_readlinks.py
+++ openlibrary/plugins/books/tests/test_readlinks.py
@@ -1,100 +1,43 @@
-from openlibrary.plugins.books import readlinks
 import pytest
-
 import web
 
+from openlibrary.plugins.books import readlinks
 
-class TestReadProcessor:
-    def test_get_item_status_lending_library_true(self, mock_site):
-        collections = ['lendinglibrary']
-        subjects = ['Lending library']
 
-        status = readlinks.ReadProcessor.get_item_status(
-            self, 'ekey', 'iaid', collections, subjects
-        )
-        assert status == 'lendable'
+@pytest.mark.parametrize(
+    "collections,subjects,options,expected",
+    [
+        (['lendinglibrary'], ['Lending library'], {}, 'lendable'),
+        (['lendinglibrary'], ['Some other subject'], {}, 'restricted'),
+        (['inlibrary'], ['In library'], {}, 'restricted'),
+        (
+            ['inlibrary'],
+            ['In library'],
+            {'debug_items': True},
+            'restricted - not inlib',
+        ),
+        (['inlibrary'], ['In library'], {'show_inlibrary': True}, 'lendable'),
+        (['printdisabled'], [], {}, 'restricted'),
+        (['some other collection'], [], {}, 'full access'),
+    ],
+)
+def test_get_item_status(collections, subjects, options, expected, mock_site):
+    read_processor = readlinks.ReadProcessor(options=options)
+    status = read_processor.get_item_status('ekey', 'iaid', collections, subjects)
+    assert status == expected
 
-    def test_get_item_status_lending_library_false(self):
-        collections = ['lendinglibrary']
-        subjects = ['Some other subject']
 
-        status = readlinks.ReadProcessor.get_item_status(
-            self, 'ekey', 'iaid', collections, subjects
-        )
-        assert status == 'restricted'
-
-    def test_get_item_status_in_library_true(self):
-        collections = ['inlibrary']
-        subjects = ['In library']
-        readlinks.ReadProcessor.__init__(self, options={})
-
-        status = readlinks.ReadProcessor.get_item_status(
-            self, 'ekey', 'iaid', collections, subjects
-        )
-        assert status == 'restricted'
-
-    def test_get_item_status_in_library_debug_true(self):
-        collections = ['inlibrary']
-        subjects = ['In library']
-        readlinks.ReadProcessor.__init__(self, options={'debug_items': True})
-
-        status = readlinks.ReadProcessor.get_item_status(
-            self, 'ekey', 'iaid', collections, subjects
-        )
-        assert status == 'restricted - not inlib'
-
-    def test_get_item_status_in_library_show_true(self, mock_site):
-        collections = ['inlibrary']
-        subjects = ['In library']
-        readlinks.ReadProcessor.__init__(self, options={'show_inlibrary': True})
-
-        status = readlinks.ReadProcessor.get_item_status(
-            self, 'ekey', 'iaid', collections, subjects
-        )
-        assert status == 'lendable'
-
-    def test_get_item_status_print_disabled_true(self):
-        collections = ['printdisabled']
-        subjects = []
-
-        status = readlinks.ReadProcessor.get_item_status(
-            self, 'ekey', 'iaid', collections, subjects
-        )
-        assert status == 'restricted'
-
-    def test_get_item_status_print_disabled_false(self):
-        collections = ['some other collection']
-        subjects = []
-
-        status = readlinks.ReadProcessor.get_item_status(
-            self, 'ekey', 'iaid', collections, subjects
-        )
-        assert status == 'full access'
-
-    def test_get_item_status_checked_out_true(self, monkeypatch, mock_site):
-        collections = ['lendinglibrary']
-        subjects = ['Lending library']
-        readlinks.ReadProcessor.__init__(self, options={})
-
-        monkeypatch.setattr(
-            web.ctx.site.store, 'get', lambda _, __: {'borrowed': 'true'}
-        )
-
-        status = readlinks.ReadProcessor.get_item_status(
-            self, 'ekey', 'iaid', collections, subjects
-        )
-        assert status == 'checked out'
-
-    def test_get_item_status_checked_out_false(self, monkeypatch, mock_site):
-        collections = ['lendinglibrary']
-        subjects = ['Lending library']
-        readlinks.ReadProcessor.__init__(self, options={})
-
-        monkeypatch.setattr(
-            web.ctx.site.store, 'get', lambda _, __: {'borrowed': 'false'}
-        )
-
-        status = readlinks.ReadProcessor.get_item_status(
-            self, 'ekey', 'iaid', collections, subjects
-        )
-        assert status == 'lendable'
+@pytest.mark.parametrize(
+    "borrowed,expected",
+    [
+        ('true', 'checked out'),
+        ('false', 'lendable'),
+    ],
+)
+def test_get_item_status_monkeypatched(borrowed, expected, monkeypatch, mock_site):
+    read_processor = readlinks.ReadProcessor(options={})
+    monkeypatch.setattr(web.ctx.site.store, 'get', lambda _, __: {'borrowed': borrowed})
+    collections = ['lendinglibrary']
+    subjects = ['Lending library']
+    status = read_processor.get_item_status('ekey', 'iaid', collections, subjects)
+    assert status == expected
