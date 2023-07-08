8066a17b8b9561344cb0946ae208bd22ca64d0c1
Christian Clauss <cclauss@me.com>
2023-07-08 22:30:13 +0200

    BytesIO(code.encode('utf-8'))  # Babel wants bytes, not strings

--- openlibrary/i18n/__init__.py
+++ openlibrary/i18n/__init__.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import sys
 from collections.abc import Iterator
-from io import StringIO
+from io import BytesIO
 
 import web
 
@@ -132,7 +132,7 @@ def extract_templetor(fileobj, keywords, comment_tags, options):
         # Replace/remove inline js '\$' which interferes with the Babel python parser:
         cleaned_string = instring.replace(r'\$', '')
         code = web.template.Template.generate_code(cleaned_string, fileobj.name)
-        f = StringIO(code)
+        f = BytesIO(code.encode('utf-8'))  # Babel wants bytes, not strings
     except Exception as e:
         print('Failed to extract ' + fileobj.name + ':', repr(e), file=web.debug)
         return []
