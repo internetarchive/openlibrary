c5e909fc071e85db956e207aa534d040a1c3126a
Christian Clauss <cclauss@me.com>
2023-07-10 13:30:59 +0200

    DRAFT: Run bump-pydantic migration tool for Pydantic v2

--- openlibrary/plugins/importapi/import_validator.py
+++ openlibrary/plugins/importapi/import_validator.py
@@ -1,17 +1,18 @@
 from typing import Any
 
 
-from pydantic import BaseModel, ValidationError, validator
+from pydantic import field_validator, BaseModel, ValidationError, validator
 
 
 class Author(BaseModel):
     name: str
 
-    @validator("name")
+    @field_validator("name")
+    @classmethod
     def author_name_must_not_be_an_empty_string(cls, v):
-        if not v:
-            raise ValueError("name must not be an empty string")
-        return v
+        if v:
+            return v
+        raise ValueError("name must not be an empty string")
 
 
 class Book(BaseModel):
@@ -21,29 +22,34 @@ class Book(BaseModel):
     publishers: list[str]
     publish_date: str
 
-    @validator("source_records", "authors", "publishers")
+    @field_validator("source_records", "authors", "publishers")
+    @classmethod
     def list_must_not_be_empty(cls, v):
-        if not v:
-            raise ValueError("Lists must not be empty")
-        return v
+        if v:
+            return v
+        raise ValueError("Lists must not be empty")
 
+    # TODO[pydantic]: We couldn't refactor the `validator`, please replace it by `field_validator` manually.
+    # Check https://docs.pydantic.dev/dev-v2/migration/#changes-to-validators for more information.
     @validator("source_records", "publishers", each_item=True)
     def list_items_must_not_be_empty(cls, v):
-        assert v != '', 'Empty strings are not permitted'
-        return v
+        if v:
+            return v
+        raise ValueError("Empty strings are not permitted")
 
-    @validator('title', 'publish_date')
+    @field_validator('title', 'publish_date')
+    @classmethod
     def strings_must_not_be_empty(cls, v):
-        if not len(v):
-            raise ValueError("Field must have a non-empty string value")
-        return v
+        if v:
+            return v
+        raise ValueError("Field must have a non-empty string value")
 
 
 class import_validator:
     def validate(self, data: dict[str, Any]):
-        """Validates the given import data.
+        """Validate the given import data.
 
-        Returns True if the import object is valid.
+        Return True if the import object is valid.
         """
 
         try:
