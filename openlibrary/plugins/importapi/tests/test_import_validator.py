6d3431bcee1bca3bbded626871c3938c672f1409
Christian Clauss <cclauss@me.com>
2023-07-13 00:21:33 +0200

    Add more tests

--- openlibrary/plugins/importapi/tests/test_import_validator.py
+++ openlibrary/plugins/importapi/tests/test_import_validator.py
@@ -18,7 +18,27 @@ validator = import_validator()
 
 def test_validate():
     assert validator.validate(valid_values) is True
+
+
+@pytest.mark.parametrize('field', ['title', 'publish_date'])
+def test_validate_empty_string(field):
     invalid_values = valid_values.copy()
-    del invalid_values['authors']
+    invalid_values[field] = ""
+    with pytest.raises(ValidationError):
+        validator.validate(invalid_values)
+
+
+@pytest.mark.parametrize('field', ['source_records', 'authors', 'publishers'])
+def test_validate_empty_list(field):
+    invalid_values = valid_values.copy()
+    invalid_values[field] = []
+    with pytest.raises(ValidationError):
+        validator.validate(invalid_values)
+
+
+@pytest.mark.parametrize('field', ['source_records', 'publishers'])
+def test_validate_list_with_an_empty_string(field):
+    invalid_values = valid_values.copy()
+    invalid_values[field] = [""]
     with pytest.raises(ValidationError):
         validator.validate(invalid_values)
