afe851000162a1ebe0602300a0172dafa49b825e
Scott Barnes <scottreidbarnes@gmail.com>
2023-07-13 22:25:06 -0700

    Don't set edition LCCNs to None (#7999)
    * Don't set edition LCCNs to None
    * Add context to fix for invalid LCCNs

--- openlibrary/plugins/upstream/models.py
+++ openlibrary/plugins/upstream/models.py
@@ -331,7 +331,10 @@ class Edition(models.Edition):
             name, value = id['name'], id['value']
             if name == 'lccn':
                 value = normalize_lccn(value)
-            d.setdefault(name, []).append(value)
+            # `None` in this field causes errors. See #7999.
+            # We should surface to the patron that an invalid LCCN is dropped. See #8092.
+            if value is not None:
+                d.setdefault(name, []).append(value)
 
         # clear existing value first
         for name in names:
@@ -463,8 +466,8 @@ class Edition(models.Edition):
             }
         )
 
-        if self.lccn:
-            citation['lccn'] = normalize_lccn(self.lccn[0])
+        if self.lccn and (lccn := normalize_lccn(self.lccn[0])):
+            citation['lccn'] = lccn
         if self.get('oclc_numbers'):
             citation['oclc'] = self.oclc_numbers[0]
         citation['ol'] = str(self.get_olid())[2:]
