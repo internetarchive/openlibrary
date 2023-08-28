d3e30b400f3fc49bbe8a8bcfb8262c31a1c333fb
Samuel Whittenberger <sbw98978@gmail.com>
2023-08-28 10:27:24 -0400

    add lccn val to addbook and error msgs

--- openlibrary/plugins/openlibrary/js/add-book.js
+++ openlibrary/plugins/openlibrary/js/add-book.js
@@ -1,14 +1,17 @@
 import {
     parseIsbn,
+    parseLccn,
     isChecksumValidIsbn10,
     isChecksumValidIsbn13,
     isFormatValidIsbn10,
-    isFormatValidIsbn13
+    isFormatValidIsbn13,
+    isValidLccn
 } from './idValidation.js'
 
 let invalidChecksum;
 let invalidIsbn10;
 let invalidIsbn13;
+let invalidLccn;
 
 export function initAddBookImport () {
     $('.list-books a').on('click', function() {
@@ -22,11 +25,12 @@ export function initAddBookImport () {
     });
 
     const i18nStrings = JSON.parse(document.querySelector('#id-errors').dataset.i18n)
-    invalidChecksum = i18nStrings.invalid_checksum
-    invalidIsbn10 = i18nStrings.invalid_isbn10
-    invalidIsbn13 = i18nStrings.invalid_isbn13
+    invalidChecksum = i18nStrings.invalid_checksum;
+    invalidIsbn10 = i18nStrings.invalid_isbn10;
+    invalidIsbn13 = i18nStrings.invalid_isbn13;
+    invalidLccn = i18nStrings.invalid_lccn;
 
-    $('#addbook').on('submit', parseAndValidateIsbn);
+    $('#addbook').on('submit', parseAndValidateId);
     $('#id_value').on('input', clearIsbnError);
     $('#id_name').on('change', clearIsbnError);
 }
@@ -51,6 +55,14 @@ function displayIsbnError(event, errorMessage) {
     document.getElementById('id_value').value = parseIsbn(document.getElementById('id_value').value);
 }
 
+function displayLccnError(event, errorMessage) {
+    const errorDiv = document.getElementById('id-errors');
+    errorDiv.classList.remove('hidden');
+    errorDiv.textContent = errorMessage;
+    event.preventDefault();
+    return;
+}
+
 function clearIsbnError() {
     addBookWithIsbnErrors = false;
     const errorDiv = document.getElementById('id-errors');
@@ -59,27 +71,53 @@ function clearIsbnError() {
     confirm.classList.add('hidden');
 }
 
-function parseAndValidateIsbn(event) {
+function clearLccnError() {
+    const errorDiv = document.getElementById('id-errors');
+    errorDiv.classList.add('hidden');
+}
+
+function parseAndValidateId(event) {
     const fieldName = document.getElementById('id_name').value;
     let idValue = document.getElementById('id_value').value;
-    // parsing valid ISBN that passes checks
-    if (fieldName === 'isbn_10') {
-        idValue = parseIsbn(idValue);
-        if (!isFormatValidIsbn10(idValue)) {
-            return displayIsbnError(event, invalidIsbn10);
-        }
-        if (!isChecksumValidIsbn10(idValue)) {
-            return displayIsbnError(event, invalidChecksum);
-        }
+
+    if (fieldName == 'isbn_10') {
+        parseAndValidateIsbn10(event, idValue);
     }
-    else if (fieldName === 'isbn_13') {
-        idValue = parseIsbn(idValue);
-        if (!isFormatValidIsbn13(idValue)) {
-            return displayIsbnError(event, invalidIsbn13);
-        }
-        if (!isChecksumValidIsbn13(idValue)) {
-            return displayIsbnError(event, invalidChecksum);
-        }
+    else if (fieldName == 'isbn_13') {
+        parseAndValidateIsbn13(event, idValue);
+    }
+    else if (fieldName == 'lccn') {
+        parseAndValidateLccn(event, idValue);
+    }
+}
+
+function parseAndValidateIsbn10(event, idValue) {
+    // parsing valid ISBN that passes checks
+    idValue = parseIsbn(idValue);
+    if (!isFormatValidIsbn10(idValue)) {
+        return displayIsbnError(event, invalidIsbn10);
+    }
+    if (!isChecksumValidIsbn10(idValue)) {
+        return displayIsbnError(event, invalidChecksum);
+    }
+    document.getElementById('id_value').value = idValue;
+}
+
+function parseAndValidateIsbn13(event, idValue) {
+    idValue = parseIsbn(idValue);
+    if (!isFormatValidIsbn13(idValue)) {
+        return displayIsbnError(event, invalidIsbn13);
+    }
+    if (!isChecksumValidIsbn13(idValue)) {
+        return displayIsbnError(event, invalidChecksum);
+    }
+    document.getElementById('id_value').value = idValue;
+}
+
+function parseAndValidateLccn(event, idValue) {
+    idValue = parseLccn(idValue);
+    if (!isValidLccn(idValue)) {
+        return displayLccnError(event, invalidLccn);
     }
     document.getElementById('id_value').value = idValue;
 }
