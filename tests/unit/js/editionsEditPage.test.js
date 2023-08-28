0edb5f7691e499d67ae4d4c1fab7d5ee9aadab06
Samuel Whittenberger <sbw98978@gmail.com>
2023-08-28 10:41:18 -0400

    wip add tests for idValidation.js

--- tests/unit/js/editionsEditPage.test.js
+++ tests/unit/js/editionsEditPage.test.js
@@ -1,4 +1,3 @@
-import { isChecksumValidIsbn10, isChecksumValidIsbn13 } from '../../../openlibrary/plugins/openlibrary/js/idValidation.js';
 import { validateIdentifiers } from '../../../openlibrary/plugins/openlibrary/js/edit.js';
 import sinon from 'sinon';
 import * as testData from './html-test-data';
@@ -6,36 +5,6 @@ import { htmlquote } from '../../../openlibrary/plugins/openlibrary/js/jsdef';
 import jQueryRepeat from '../../../openlibrary/plugins/openlibrary/js/jquery.repeat';
 
 let sandbox;
-
-describe('isChecksumValidIsbn10', () => {
-    it('returns true with valid ISBN 10 (X check character)', () => {
-        expect(isChecksumValidIsbn10('080442957X')).toBe(true);
-    });
-    it('returns true with valid ISBN 10 (numerical check character, check 1)', () => {
-        expect(isChecksumValidIsbn10('1593279280')).toBe(true);
-    });
-    it('returns true with valid ISBN 10 (numerical check character, check 2)', () => {
-        expect(isChecksumValidIsbn10('1617295981')).toBe(true);
-    });
-    it('returns false with an invalid ISBN 10', () => {
-        expect(isChecksumValidIsbn10('1234567890')).toBe(false);
-    });
-})
-
-describe('isChecksumValidIsbn13', () => {
-    it('returns true with valid ISBN 13 (check 1)', () => {
-        expect(isChecksumValidIsbn13('9781789801217')).toBe(true);
-    });
-    it('returns true with valid ISBN 13 (check 2)', () => {
-        expect(isChecksumValidIsbn13('9798430918002')).toBe(true);
-    });
-    it('returns false with an invalid ISBN 13 (check 1)', () => {
-        expect(isChecksumValidIsbn13('1234567890123')).toBe(false);
-    });
-    it('returns false with an invalid ISBN 13 (check 2)', () => {
-        expect(isChecksumValidIsbn13('9790000000000')).toBe(false);
-    });
-})
 
 /**
  * Test various patterns with the editions identifier parsing function,
