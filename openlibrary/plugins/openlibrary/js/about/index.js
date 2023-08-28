40d0a24f7e9f114af154321028c01317cee9e535
Lasseignejk <jaye914@hotmail.com>
2023-08-28 11:40:27 -0400

    Got the filter working in the console
    Have it working using a script tag, and only in the console -- nothing is changing on the screen itself yet.

--- openlibrary/plugins/openlibrary/js/about/index.js
+++ openlibrary/plugins/openlibrary/js/about/index.js
@@ -1,1 +1,45 @@
 console.log("Hooked up")
+
+const team = [
+    {
+        name: "Drini",
+        role: "Staff",
+    },
+    {
+        name: "Jim",
+        role: "Staff",
+    },
+    {
+        name: "Aaron",
+        role: "Staff, Emeritus",
+    },
+    {
+        name: "Sam",
+        role: "Fellow",
+    },
+    {
+        name: "Abbey",
+        role: "Fellow",
+    },
+    {
+        name: "Jaye",
+        role: "Volunteer",
+    },
+    {
+        name: "Keita",
+        role: "Volunteer",
+    },
+];
+
+const staff = team.filter((person) => person.role.includes("Staff"));
+const fellows = team.filter((person) => person.role.includes("Fellow"));
+const volunteers = team.filter((person) =>
+    person.role.includes("Volunteer")
+);
+
+const staffButton = document.getElementById("staffSort");
+const fellowButton = document.getElementById("fellowSort");
+const volunteerButton = document.getElementById("volunteerSort");
+staffButton.addEventListener("click", () => console.log(staff));
+fellowButton.addEventListener("click", () => console.log(fellows));
+volunteerButton.addEventListener("click", () => console.log(volunteers));
