7bd2c969e903d383b486ebdeb2f11029e75d3900
Ray Berger <RayBB@users.noreply.github.com>
2023-09-01 02:30:22 +0000

    simplify renter template

--- openlibrary/core/schema.sql
+++ openlibrary/core/schema.sql
@@ -92,7 +92,7 @@ CREATE TABLE yearly_reading_goals (
 );
 
 CREATE TABLE wikidata (
-    id text not null  primary key,
+    id text not null primary key,
     data json,
     updated timestamp without time zone default (current_timestamp at time zone 'utc')
 )
