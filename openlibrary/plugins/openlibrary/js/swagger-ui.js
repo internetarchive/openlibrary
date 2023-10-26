ffe1c7bed6e69df10e560684a7dc47671b3268fc
imperial-chief <muditloya24@gmail.com>
2023-10-26 09:52:01 +0530

    Removed inline js from swagger-ui.html

---
+++ openlibrary/plugins/openlibrary/js/swagger-ui.js
@@ -0,0 +1,17 @@
+export function initializeSwaggerUI() {
+    const ui = SwaggerUIBundle({
+        url: "../../../static/openapi.json",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout",
+    });
+
+    window.ui = ui;
+}
