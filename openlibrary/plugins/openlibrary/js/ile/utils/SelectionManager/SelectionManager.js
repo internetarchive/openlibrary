68f517d0f60caefc0df4b533a2cc15dff9c0ca39
Mark Heiman <github@loomis-house.com>
2023-11-17 19:06:42 -0600

    rename getSiblingSet to getSelectableRange
    Co-authored-by: Drini Cami <cdrini@gmail.com>

--- openlibrary/plugins/openlibrary/js/ile/utils/SelectionManager/SelectionManager.js
+++ openlibrary/plugins/openlibrary/js/ile/utils/SelectionManager/SelectionManager.js
@@ -125,7 +125,7 @@ export default class SelectionManager {
      * @param {HTMLElement} clicked
      * @return {JQuery<HTMLElement>}
      */
-    getSiblingSet(clicked) {
+    getSelectableRange(clicked) {
         const lastParents = $(this.lastClicked).parents();
         const clickedParents = $(clicked).parents();
         let commonParent = undefined;
