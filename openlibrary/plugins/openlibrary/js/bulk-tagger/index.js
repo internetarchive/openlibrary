b5b40540d98f3f43d1343654f54ae37a71b46345
James Champ <jameschamp@acm.org>
2023-12-07 13:57:23 -0800

    Update submit button
    Adds new `cta-btn--primary` class, which has the same styling as our
    `available` buttons.  Adds styling for `disabled` states of these
    buttons.
    
    Submission button now begins in a disabled state.  On menu option click,
    or menu option creation, the disabled state is removed if any tag is
    staged for submission.

--- openlibrary/plugins/openlibrary/js/bulk-tagger/index.js
+++ openlibrary/plugins/openlibrary/js/bulk-tagger/index.js
@@ -32,7 +32,7 @@ export function renderBulkTagger() {
             </div>
         </div>
         <div class="submit-tags-section">
-            <button type="submit" class="bulk-tagging-submit">Submit</button>
+            <button type="submit" class="bulk-tagging-submit cta-btn cta-btn--primary" disabled>Submit</button>
         </div>
     </form>`
 }
