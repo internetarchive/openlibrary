1a5fb39d87d801d4bdd667d55d4a7b07d3c7766d
James Champ <jameschamp@acm.org>
2023-12-21 18:33:14 -0800

    Use localized strings

--- openlibrary/plugins/openlibrary/js/password-toggle.js
+++ openlibrary/plugins/openlibrary/js/password-toggle.js
@@ -4,15 +4,16 @@
  * @param {HTMLElement} elem Reference to affordance that toggles a password input's visibility
  */
 export function initPasswordToggling(elem) {
+    const i18nStrings = JSON.parse(elem.dataset.i18n)
     const passwordInput = document.querySelector('input[type=password]')
 
     elem.addEventListener('click', () => {
         if (passwordInput.type === 'password') {
             passwordInput.type = 'text'
-            elem.textContent = 'Hide password'
+            elem.textContent = i18nStrings['hide_password']
         } else {
             passwordInput.type = 'password'
-            elem.textContent = 'Show password'
+            elem.textContent = i18nStrings['show_password']
         }
     })
 }
