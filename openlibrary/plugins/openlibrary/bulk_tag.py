00c4841de1d54892150a5c8059067d84aeaa2b4e
xonx <119700621+xonx4l@users.noreply.github.com>
2023-12-26 22:25:24 +0530

    corrected indentation error on line 99

--- openlibrary/plugins/openlibrary/bulk_tag.py
+++ openlibrary/plugins/openlibrary/bulk_tag.py
@@ -1,6 +1,6 @@
 from infogami.utils import delegate
 
-from openlibrary.core import stats
+from openlibrary.core import stats  
 from openlibrary.utils import uniq
 from openlibrary.utils import get_original_subjects
 import web
@@ -8,98 +8,98 @@ import json
 
 
 class bulk_tag_works(delegate.page):
-   path = "/tags/bulk_tag_works"
+    path = "/tags/bulk_tag_works"
 
-   def POST(self):
-       i = web.input(work_ids='', tags_to_add='', tags_to_remove='')
+    def POST(self):
+        i = web.input(work_ids='', tags_to_add='', tags_to_remove='')
 
-       is_dry_run = i.dry_run == '1'
+        is_dry_run = i.dry_run == '1'
 
-       if is_dry_run:
-           original_subjects = get_original_subjects(works)
-           return delegate.RawText(render_template('diff.html',
-                               original=original_subjects,
-                               updated=docs_to_update))
-       else:
-           web.ctx.site.save_many(docs_to_update, comment="Bulk tagging works")
+        if is_dry_run:
+            original_subjects = get_original_subjects(works)
+            return delegate.RawText(render_template('diff.html',
+                                original=original_subjects,
+                                updated=docs_to_update))
+        else:
+            web.ctx.site.save_many(docs_to_update, comment="Bulk tagging works")
 
-           works = i.work_ids.split(',')
-           tags_to_add = json.loads(i.tags_to_add or '{}')
-           tags_to_remove = json.loads(i.tags_to_remove or '{}')
+            works = i.work_ids.split(',')
+            tags_to_add = json.loads(i.tags_to_add or '{}')
+            tags_to_remove = json.loads(i.tags_to_remove or '{}')
 
-           docs_to_update = []
-           # Number of tags added per work:
-           docs_adding = 0
-           # Number of tags removed per work:
-           docs_removing = 0
+            docs_to_update = []
+            # Number of tags added per work:
+            docs_adding = 0 
+            # Number of tags removed per work:
+            docs_removing = 0
 
-           for work in works:
-               w = web.ctx.site.get(f"/works/{work}")
+            for work in works:
+                w = web.ctx.site.get(f"/works/{work}")
 
-               current_subjects = {
-                   # XXX : Should an empty list be the default for these?
-                   'subjects': uniq(w.get('subjects', '')),
-                   'subject_people': uniq(w.get('subject_people', '')),
-                   'subject_places': uniq(w.get('subject_places', '')),
-                   'subject_times': uniq(w.get('subject_times', '')),
-               }
-               for subject_type, add_list in tags_to_add.items():
-                   if add_list:
-                       orig_len = len(current_subjects[subject_type])
-                       current_subjects[subject_type] = uniq(  # dedupe incoming subjects
-                           current_subjects[subject_type] + add_list
-                       )
-                       docs_adding += len(current_subjects[subject_type]) - orig_len
-                       w[subject_type] = current_subjects[subject_type]
+                current_subjects = {
+                    # XXX : Should an empty list be the default for these?
+                    'subjects': uniq(w.get('subjects', '')),
+                    'subject_people': uniq(w.get('subject_people', '')),
+                    'subject_places': uniq(w.get('subject_places', '')),
+                    'subject_times': uniq(w.get('subject_times', '')),
+                }
+                for subject_type, add_list in tags_to_add.items():
+                    if add_list:
+                        orig_len = len(current_subjects[subject_type])
+                        current_subjects[subject_type] = uniq(  # dedupe incoming subjects
+                            current_subjects[subject_type] + add_list
+                        )
+                        docs_adding += len(current_subjects[subject_type]) - orig_len
+                        w[subject_type] = current_subjects[subject_type]
 
-               for subject_type, remove_list in tags_to_remove.items():
-                   if remove_list:
-                       orig_len = len(current_subjects[subject_type])
-                       current_subjects[subject_type] = [
-                           item
-                           for item in current_subjects[subject_type]
-                           if item not in remove_list
-                       ]
-                       docs_removing += orig_len - len(current_subjects[subject_type])
-                       w[subject_type] = current_subjects[subject_type]
+                for subject_type, remove_list in tags_to_remove.items():
+                    if remove_list:
+                        orig_len = len(current_subjects[subject_type])
+                        current_subjects[subject_type] = [
+                            item
+                            for item in current_subjects[subject_type]
+                            if item not in remove_list
+                        ]
+                        docs_removing += orig_len - len(current_subjects[subject_type])
+                        w[subject_type] = current_subjects[subject_type]
 
-               docs_to_update.append(
-                   w.dict()
-               )  # need to convert class to raw dict in order for save_many to work
+                docs_to_update.append(
+                    w.dict()
+                )  # need to convert class to raw dict in order for save_many to work
 
-           web.ctx.site.save_many(docs_to_update, comment="Bulk tagging works")
+            web.ctx.site.save_many(docs_to_update, comment="Bulk tagging works")
 
-           def response(msg, status="success"):
-               return delegate.RawText(
-                   json.dumps({status: msg}), content_type="application/json"
-               )
+            def response(msg, status="success"):
+                return delegate.RawText(
+                    json.dumps({status: msg}), content_type="application/json"
+                )
 
-           # Number of times the handler was hit:
-           stats.increment('ol.tags.bulk_update')
-           stats.increment('ol.tags.bulk_update.add', n=docs_adding)
-           stats.increment('ol.tags.bulk_update.remove', n=docs_removing)
+            # Number of times the handler was hit:
+            stats.increment('ol.tags.bulk_update')
+            stats.increment('ol.tags.bulk_update.add', n=docs_adding)
+            stats.increment('ol.tags.bulk_update.remove', n=docs_removing)
 
-           return response('Tagged works successfully')
+            return response('Tagged works successfully')
 
-           def get_original_subjects(works):
+def get_original_subjects(works):
 
-             original = []
+    original = []
 
-             for work_id in works:
+    for work_id in works:
 
-              work = web.ctx.site.get("/works/" + work_id)
+      work = web.ctx.site.get("/works/" + work_id)
 
-              original_subjects = {
-                "subjects": work.get("subjects"),
-                "subject_people": work.get("subject_people"),
-                "subject_places": work.get("subject_places"),
-                "subject_times": work.get("subject_times")
-               }
+      original_subjects = {
+        "subjects": work.get("subjects"),
+        "subject_people": work.get("subject_people"),
+        "subject_places": work.get("subject_places"),
+        "subject_times": work.get("subject_times")
+      }
 
-               original.append(original_subjects)
+      original.append(original_subjects)
 
-             return original
+    return original
 
 
 def setup():
-   pass
+    pass
