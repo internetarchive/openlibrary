6d5d9b4b0b29cba1571b038939f1d8d81e833925
James Champ <jameschamp@acm.org>
2024-01-10 17:58:35 -0800

    Add more information to digest

--- scripts/gh_scripts/issue_comment_bot.py
+++ scripts/gh_scripts/issue_comment_bot.py
@@ -22,17 +22,29 @@ config_file = 'config/bot.ini'
 staff_usernames = ['scottbarnes', 'mekarpeles', 'jimchamp', 'cdrini']
 
 def fetch_issues(updated_since: str):
-    query = f'repo:internetarchive/openlibrary is:open is:issue comments:>1 updated:>{updated_since}'
+    """
+    Fetches all GitHub issues that have been updated since the given date string and have at least one comment.
+
+    GitHub results are paginated.  This functions appends each result to a list, and does so for all pages.
+    To keep API calls to a minimum, we request the maximum number of results per request (100 per page, as of writing).
+
+    Important: Updated issues need not have a recent comment. Update events include many other things, such as adding a
+    label to an issue, or moving an issue to a milestone.  Issues returned by this function will require additional
+    processing in order to determine if they have recent comments.
+    """
+    # Make initial query for updated issues:
+    query = f'repo:internetarchive/openlibrary is:open is:issue comments:>0 updated:>{updated_since}'
     response = requests.get(
-        'https://api.github.com/search/issues?per_page=100',
+        'https://api.github.com/search/issues',
         params={
             'q': query,
+            'per_page': 100,
         },
     )
     d = response.json()
     results = d['items']
 
-    # Pagination
+    # Fetch additional updated issues, if any exist
     def get_next_page(url: str):
         """Returns list of issues and optional url for next page"""
         resp = requests.get(url)
@@ -55,27 +67,63 @@ def fetch_issues(updated_since: str):
 
     return results
 
-def filter_issues(issues: list, since_date: str):
+def filter_issues(issues: list, since: datetime):
     """
     Returns list of issues that were not last responded to by staff.
     Requires fetching the most recent comments for the given issues.
     """
-    filtered_issues = []
+    results = []
 
     for i in issues:
+        # Fetch comments using URL from previous GitHub search results
         comments_url = i.get('comments_url')
-        resp = requests.get(f'{comments_url}?since{since_date}Z')
+        resp = requests.get(
+            comments_url,
+            params={
+                'per_page': 100
+            }
+        )
+
+        # Ensure that we have the last page of comments
+        links = resp.links
+        last = links.get('last', {})
+        last_url = last.get('url', '')
+
+        if last_url:
+            resp = requests.get(last_url)
+
+        # Get last comment
         comments = resp.json()
         last_comment = comments[-1]
-        last_commenter = last_comment['user']['login']
 
-        if last_commenter not in staff_usernames:
-            filtered_issues.append(i)
+        # Determine if last comment meets our criteria for Slack notifications
+        # First step: Ensure that the last comment was left after the given `since` datetime
+        created = datetime.fromisoformat(last_comment['created_at'])
+        # Removing timezone info to avoid TypeErrors, which occur when
+        # comparing a timezone-aware datetime with a timezone-naive datetime
+        created = created.replace(tzinfo=None)
+        if created > since:
+            # Next step: Determine if the last commenter is a staff member
+            last_commenter = last_comment['user']['login']
+            if last_commenter not in staff_usernames:
+                results.append({
+                    'comment_url': last_comment['html_url'],
+                    'commenter': last_commenter,
+                    'issue_title': i['title'],
+                })
 
-    return filtered_issues
+    return results
 
-def publish_digest(issues: list[str], slack_channel: str, slack_token: str):
-    parent_thread_msg = f'There are {len(issues)} issue(s) awaiting response.  More details in this thread.'
+def publish_digest(issues: list[str], slack_channel: str, slack_token: str, hours_passed: int):
+    """
+    Creates a threaded Slack messaged containing a digest of recently commented GitHub issues.
+
+    Parent Slack message will say how many comments were left, and the timeframe. Each reply
+    will include a link to the comment, as well as addiitonal information.
+    """
+    # Create the parent message
+    parent_thread_msg = f'At least {len(issues)} new comment(s) have been left by contributors in the past {hours_passed} hour(s)'
+
     response = requests.post(
         'https://slack.com/api/chat.postMessage',
         headers={
@@ -91,7 +139,7 @@ def publish_digest(issues: list[str], slack_channel: str, slack_token: str):
     if response.status_code != 200:
         # XXX : Log this
         print(f'Failed to send message to Slack.  Status code: {response.status_code}')
-        # XXX : Add retry logic
+        # XXX : Add retry logic?
         sys.exit(errno.ECOMM)
 
     d = response.json()
@@ -99,6 +147,9 @@ def publish_digest(issues: list[str], slack_channel: str, slack_token: str):
     ts = d.get('ts')
 
     def comment_on_thread(message: str):
+        """
+        Posts the given message as a reply to the parent message.
+        """
         response = requests.post(
             'https://slack.com/api/chat.postMessage',
             headers={
@@ -108,7 +159,7 @@ def publish_digest(issues: list[str], slack_channel: str, slack_token: str):
             json={
                 'channel': slack_channel,
                 'text': message,
-                'thread_ts': ts
+                'thread_ts': ts,
             },
         )
         if response.status_code != 200:
@@ -116,30 +167,34 @@ def publish_digest(issues: list[str], slack_channel: str, slack_token: str):
             print(f'Failed to POST slack message\n  Status code: {response.status_code}\n  Message: {message}')
             # XXX : Retry logic?
 
-    for issue in issues:
+    for i in issues:
         # Slack rate limit is roughly 1 request per second
         time.sleep(1)
 
-        issue_url = issue.get('html_url')
-        issue_title = issue.get('title')
-        message = f'<{issue_url}|*{issue_title}*>'
+        comment_url = i['comment_url']
+        issue_title = i['issue_title']
+        commenter = i['commenter']
+        message = f'<{comment_url}|Latest comment for: *{issue_title}*>\n'
+        message += f'Commenter: *{commenter}*'
         comment_on_thread(message)
 
-def create_date_string(hours):
+def time_since(hours):
+    """Returns datetime and string representations of the current time, minus the given hour"""
     now = datetime.now()
-    # XXX : Add a minute or two to the delta?
+    # XXX : Add a minute or two to the delta (to avoid dropping issues)?
     since = now - timedelta(hours=hours)
-    return since.strftime(f'%Y-%m-%dT%H:%M:%S')
+    return since, since.strftime(f'%Y-%m-%dT%H:%M:%S')
 
 def start_job(args: dict):
     """
     Starts the new comment digest job.
     """
-    date_string = create_date_string(args.hours)
+    since, date_string = time_since(args.hours)
     issues = fetch_issues(date_string)
-    filtered_issues = filter_issues(issues, date_string)
+    filtered_issues = filter_issues(issues, since)
+
     if filtered_issues:
-        publish_digest(filtered_issues, args.channel, args.token)
+        publish_digest(filtered_issues, args.channel, args.slack_token, args.hours)
     # XXX : Log this
     print('Digest POSTed to Slack')
 
@@ -165,6 +220,8 @@ def _get_parser() -> argparse.ArgumentParser:
 
         config = ConfigParser()
         config.read(config_path)
+        # XXX : Setting the defaults both here in the `get` calls and the config file
+        # is like wearing a belt with suspenders...
         return {
             'slack_token': config['tokens'].get('slack_token', ''),
             'hours': config['issue_digest'].getint('default_hours', 1),
@@ -188,8 +245,8 @@ def _get_parser() -> argparse.ArgumentParser:
         default=defaults['slack_channel'],
     )
     parser.add_argument(
-        '-t',
-        '--token',
+        '-s',
+        '--slack-token',
         help='Slack auth token',
         type=str,
         default=defaults['slack_token'],
@@ -198,6 +255,7 @@ def _get_parser() -> argparse.ArgumentParser:
     return parser
 
 if __name__ == '__main__':
+    # Process command-line arguments and starts the notification job
     parser = _get_parser()
     args = parser.parse_args()
     start_job(args)
