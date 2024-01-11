4fae3a36084c2ff3c32a3246ec24e089924e2d56
James Champ <jameschamp@acm.org>
2024-01-11 11:39:30 -0800

    Remove default configuration

--- scripts/gh_scripts/issue_comment_bot.py
+++ scripts/gh_scripts/issue_comment_bot.py
@@ -3,22 +3,17 @@
 Fetches Open Library GitHub issues that have been commented on 
 within some amount of time, in hours.
 
-Writes links to each issue to Slack channel #team-abc-plus
+Writes links to each issue to given Slack channel.
 """
 import argparse
 import errno
 import sys
 import time
 
-from configparser import ConfigParser
 from datetime import datetime, timedelta
-from pathlib import Path
 
 import requests
 
-config_file = 'config/bot.ini'
-
-# XXX : Configure?
 staff_usernames = ['scottbarnes', 'mekarpeles', 'jimchamp', 'cdrini']
 
 def fetch_issues(updated_since: str):
@@ -196,60 +191,29 @@ def start_job(args: dict):
     if filtered_issues:
         publish_digest(filtered_issues, args.channel, args.slack_token, args.hours)
     # XXX : Log this
-    print('Digest POSTed to Slack')
+    print('Digest posted to Slack')
 
 def _get_parser() -> argparse.ArgumentParser:
     """
     Creates and returns an ArgumentParser containing default values which were
     read from the config file.
     """
-    def get_defaults():
-        """
-        Reads the config file and returns a dict containing the default
-        values for this script's arguments.
-
-        Location of config file is expected to be a nested within this file's parent
-        directory.  The relative file location is stored as `config_file`.
-        """
-        this_dir = Path(__file__).parent.resolve()
-        config_path = this_dir / Path(config_file)
-        if not config_path.exists():
-            # XXX : Log to file:
-            print(f'{config_file} does not exist.')
-            sys.exit(errno.ENOENT)
-
-        config = ConfigParser()
-        config.read(config_path)
-        # XXX : Setting the defaults both here in the `get` calls and the config file
-        # is like wearing a belt with suspenders...
-        return {
-            'slack_token': config['tokens'].get('slack_token', ''),
-            'hours': config['issue_digest'].getint('default_hours', 1),
-            'slack_channel': config['issue_digest'].get('slack_channel', '#team-abc-plus'),
-        }
-
-    defaults = get_defaults()
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        '--hours',
-        metavar='N',
-        help='Fetch issues that have been updated since N hours ago',
+        'hours',
+        help='Fetch issues that have been updated since this many hours ago',
         type=int,
-        default=defaults['hours'],
     )
     parser.add_argument(
-        '-c',
-        '--channel',
+        'channel',
         help="Issues will be published to this Slack channel",
         type=str,
-        default=defaults['slack_channel'],
     )
     parser.add_argument(
-        '-s',
-        '--slack-token',
+        'slack_token',
+        metavar='slack-token',
         help='Slack auth token',
         type=str,
-        default=defaults['slack_token'],
     )
 
     return parser
