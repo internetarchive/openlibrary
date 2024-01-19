ba8f3354679d37951dccef90356c227f71f481cc
Drini Cami <cdrini@gmail.com>
2024-01-19 04:05:00 -0500

    Add tests for get_availability

--- openlibrary/tests/core/test_lending.py
+++ openlibrary/tests/core/test_lending.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock, patch
+
 from openlibrary.core import lending
 
 
@@ -40,3 +42,45 @@ class TestAddAvailability:
         r = f([{'ocaid': 'foo'}])
         print(r)
         assert r[0]['availability']['status'] == 'error'
+
+
+class TestGetAvailability:
+    def test_cache(self):
+        with patch("openlibrary.core.lending.requests.get") as mock_get:
+            mock_get.return_value = Mock()
+            mock_get.return_value.json.return_value = {
+                "responses": {"foo": {"status": "open"}}
+            }
+
+            foo_expected = {
+                "status": "open",
+                "identifier": "foo",
+                "is_restricted": False,
+                "is_browseable": False,
+                "__src__": 'core.models.lending.get_availability',
+            }
+            bar_expected = {
+                "status": "error",
+                "identifier": "bar",
+                "is_restricted": True,
+                "is_browseable": False,
+                "__src__": 'core.models.lending.get_availability',
+            }
+
+            r = lending.get_availability("identifier", ["foo"])
+            assert mock_get.call_count == 1
+            assert r == {"foo": foo_expected}
+
+            # Should not make a call to the API again
+            r2 = lending.get_availability("identifier", ["foo"])
+            assert mock_get.call_count == 1
+            assert r2 == {"foo": foo_expected}
+
+            # Now should make a call for just the new identifier
+            mock_get.return_value.json.return_value = {
+                "responses": {"bar": {"status": "error"}}
+            }
+            r3 = lending.get_availability("identifier", ["foo", "bar"])
+            assert mock_get.call_count == 2
+            assert mock_get.call_args[1]['params']['identifier'] == "bar"
+            assert r3 == {"foo": foo_expected, "bar": bar_expected}
