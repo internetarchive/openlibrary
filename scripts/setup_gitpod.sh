2d353a826e09d4df562fe910ca9543cddd80f560
Scott Barnes <scottreidbarnes@gmail.com>
2024-02-28 19:25:13 -0800

    Update containers to Python 3.12.2 + Bookworm
    This update is tied to a specific webpy commit because although the
    commit has been merged to master, there is not yet a release. Hence
    using the git hash in `requirements.txt`.

--- scripts/setup_gitpod.sh
+++ scripts/setup_gitpod.sh
@@ -2,7 +2,7 @@
 # This script does the setup needed for Gitpod
 
 # Setup pre-commit hooks
-pyenv install 3.11 --skip-existing # must match python version in .pre-commit-config.yaml
-pyenv global 3.11
+pyenv install 3.12 --skip-existing # must match python version in .pre-commit-config.yaml
+pyenv global 3.12
 sudo python3 -m pip install pre-commit
 pre-commit install --install-hooks
