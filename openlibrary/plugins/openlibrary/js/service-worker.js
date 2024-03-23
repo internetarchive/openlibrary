d42b20ccce3f71563737dfb58830859824e663a7
RayBB <RayBB@users.noreply.github.com>
2024-03-23 06:07:57 +0100

    dramatically rewrite caching setup

--- openlibrary/plugins/openlibrary/js/service-worker.js
+++ openlibrary/plugins/openlibrary/js/service-worker.js
@@ -18,6 +18,16 @@ offlineFallback({
     imageFallback: '/static/images/logo_OL-lg.png'
 });
 
+
+const daySeconds = 24 * 60 * 60;
+// only cache if it the request returns 0 or 200 status
+const cacheableResponses = new CacheableResponsePlugin({
+    statuses: [0, 200],
+});
+
+
+// TODO: Talk with Drini about what makes sense here
+// Which of these do we actually want to load network first vs show a cached page
 function matchFunction({ url }) {
     const pages = ['/', '/account/login', '/account', '/account/books', '/account/loans', '/account/books/already-read/stats'];
     return pages.includes(url.pathname);
@@ -30,75 +40,117 @@ registerRoute(
         cacheName: 'html-cache',
         plugins: [
             new ExpirationPlugin({
-                // Keep at most 50 entries.
                 maxEntries: 50,
-                // Don't keep any entries for more than 30 days.
                 maxAgeSeconds: 30 * 24 * 60 * 60,
-                // Automatically cleanup if quota is exceeded.
                 purgeOnQuotaError: true,
             }),
-            // only cache if it the request returns 0 or 200 status
-            new CacheableResponsePlugin({
-                statuses: [0, 200],
-            }),
+            cacheableResponses
         ],
     })
 );
 
-// covers png cache
+/*
+static/images, static/fonts, static/logos - should stay for a long time
+static/build/css(and js) - just for 5 minutes because I'm not sure about cache busting on deploy
+*/
+
 registerRoute(
-    new RegExp('https://covers.openlibrary.org/b/.+'),
+    /\/static\/(images|fonts|logos)/,
     new CacheFirst({
-        cacheName: 'covers-cache',
+        cacheName: 'static-cache-images-fonts-logos',
         plugins: [
             new ExpirationPlugin({
                 maxEntries: 150,
+                maxAgeSeconds: 7 * daySeconds,
+                purgeOnQuotaError: true,
             }),
-            new CacheableResponsePlugin({
-                statuses: [0, 200],
-            })
+            cacheableResponses
+        ],
+    })
+)
+
+registerRoute(
+    /\/static\/build/,
+    new CacheFirst({
+        cacheName: 'static-cache-build',
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 60 * 5,
+                purgeOnQuotaError: true,
+            }),
+            cacheableResponses
+        ],
+    })
+)
+
+/*
+Covers:
+S/M covers - cache 150 of them to take up no more than 2.25mb of space.
+L covers - cache 5 of them but with a very short timeout so if you're looking at a few they don't keep loading but they take up much of your storage.
+Original covers (with no letter) - save as above, just in case
+*/
+
+// Neither of these have the starting / because they don't come from the same origin
+// These urls could be domain agnostic if we want them to work locally but it would make them less readable
+const smallMediumCovers = new RegExp('.+(S|M).jpg');
+// This includes large covers and original size because of the ? in "L?"
+const largeCovers = new RegExp('.+L?.jpg');
+registerRoute(
+    smallMediumCovers,
+    new CacheFirst({
+        cacheName: 'covers-cache-small-medium',
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 150,
+                purgeOnQuotaError: true,
+            }),
+            cacheableResponses
         ],
     })
 );
 
-// assets cache
 registerRoute(
-    /\.(?:js|css|woff)/,
-    new NetworkFirst({
-        cacheName: 'assets-cache',
+    largeCovers,
+    new CacheFirst({
+        cacheName: 'covers-cache-large',
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 5,
+                maxAgeSeconds: daySeconds / 24,
+                purgeOnQuotaError: true,
+            }),
+            cacheableResponses
+        ],
     })
 );
 
-// images cache (precache)
+// cache all other images that happen to be on the page
+// This will only be same origin
 registerRoute(
     /\.(?:png|jpg|jpeg|svg|gif)/,
     new CacheFirst({
         cacheName: 'image-cache',
         plugins: [
             new ExpirationPlugin({
-                maxAgeSeconds: 7 * 24 * 60 * 60,
+                maxEntries: 50,
+                maxAgeSeconds: 7 * daySeconds,
+                purgeOnQuotaError: true
             })
         ],
     })
 );
 
-// others page html cache
+// cache all other requests on the same origin
 registerRoute(
-    new RegExp('/.+'),
+    /\/*/,
     new NetworkFirst({
         cacheName: 'other-html-cache',
         plugins: [
             new ExpirationPlugin({
-                // Due to redirect keep the value 2 times.
-                maxEntries: 10,
-                // Don't keep any entries for more than 30 days.
-                maxAgeSeconds: 30 * 24 * 60 * 60,
-                // Automatically cleanup if quota is exceeded.
+                maxEntries: 150,
                 purgeOnQuotaError: true,
             }),
-            new CacheableResponsePlugin({
-                statuses: [0, 200],
-            }),
+            cacheableResponses
         ],
     })
 );
