6d0b1c3c204a4c077dd2e2c5605f9e1134ff4f4d
QuantuM410 <kartikeys410@gmail.com>
2024-04-03 18:10:48 +0530

    Added: Debug Page redirect if user is librarian

--- openlibrary/plugins/openlibrary/code.py
+++ openlibrary/plugins/openlibrary/code.py
@@ -974,8 +974,7 @@ def internalerror():
 
     if sentry.enabled:
         sentry.capture_exception_webpy()
-
-    if i.debug.lower() == 'true':
+    if i.debug.lower() == 'true' or delegate.context.user.is_librarian():
         raise web.debugerror()
     else:
         msg = render.site(render.internalerror(name))
