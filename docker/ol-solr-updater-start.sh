e350c37a20e25e9c0a449e52773a48e72e036c27
Raymond Berger <RayBB@users.noreply.github.com>
2024-04-09 06:56:52 +0200

    Refactor ol-solr-updater-start.sh to use wget --no-clobber (#8921)

--- docker/ol-solr-updater-start.sh
+++ docker/ol-solr-updater-start.sh
@@ -2,13 +2,12 @@
 
 python --version
 OSP_DUMP_LOCATION="/solr-updater-data/osp_totals.db"
-# if the osp dump file does not exist, download it. Takes ~30s
-# When we update the python image we can use the --no-clobber option to avoid downloading the file again
-# https://github.com/internetarchive/openlibrary/pull/8790
-if [ ! -f "$OSP_DUMP_LOCATION" ]; then
-    # Keep link in sync with Makefile and Jenkinsfile
-    curl -L "https://archive.org/download/2023_openlibrary_osp_counts/osp_totals.db" --output "$OSP_DUMP_LOCATION"
-fi
+# If the osp dump file does not exist, download it. Takes ~30s
+# Keep link in sync with Makefile and Jenkinsfile
+wget --output-document=$OSP_DUMP_LOCATION \
+    --progress=dot:giga --no-clobber \
+    https://archive.org/download/2023_openlibrary_osp_counts/osp_totals.db
+
 ls -la /solr-updater-data/
 python scripts/solr_updater.py $OL_CONFIG \
     --state-file /solr-updater-data/$STATE_FILE \
