225f4de486cdd654449a77d7f112ab5489b0f5d8
RayBB <RayBB@users.noreply.github.com>
2024-04-15 11:56:56 +0200

    remove hidden class in jquery

--- openlibrary/plugins/openlibrary/js/waitlist.js
+++ openlibrary/plugins/openlibrary/js/waitlist.js
@@ -9,6 +9,8 @@ export function initLeaveWaitlist(leaveWaitlistLinks) {
             const $link = $(link)
             const title = $link.parents('tr').find('.book').text();
             $('#leave-waitinglist-dialog strong').text(title);
+            // We remove the hidden class here because otherwise it flashes for a moment on page load
+            $('#leave-waitinglist-dialog').removeClass('hidden');
             $('#leave-waitinglist-dialog')
                 .data('origin', $link)
                 .dialog('open');
