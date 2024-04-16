0683cef7479ab6fd2eff760eebd476d14bb19ca5
RayBB <RayBB@users.noreply.github.com>
2024-04-17 00:31:51 +0200

    todo comment

--- openlibrary/plugins/openlibrary/js/service-worker.js
+++ openlibrary/plugins/openlibrary/js/service-worker.js
@@ -89,6 +89,7 @@ registerRoute(
 
 registerRoute(
     // S/M covers - cache 150 of them. They take up no more than 2.25mb of space.
+    // TODO: UGH IDK WHY THIS ISN"T WORKING...
     /-[SM].jpg/,
     new CacheFirst({
         cacheName: 'covers-small-medium-cache',
