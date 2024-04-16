a62d47308705eadb7d6f076fa3531f9c1a4cdb5b
RayBB <RayBB@users.noreply.github.com>
2024-04-17 00:11:03 +0200

    fix S|M

--- openlibrary/plugins/openlibrary/js/service-worker.js
+++ openlibrary/plugins/openlibrary/js/service-worker.js
@@ -89,7 +89,7 @@ registerRoute(
 
 registerRoute(
     // S/M covers - cache 150 of them. They take up no more than 2.25mb of space.
-    /\/id\/\d+-[MS]\.jpg/,
+    /\d+-(S|M).jpg/,
     new CacheFirst({
         cacheName: 'covers-small-medium-cache',
         plugins: [
