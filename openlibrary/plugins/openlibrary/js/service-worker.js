81149ed4161d630e8ede56ae41d90ff75461a768
RayBB <RayBB@users.noreply.github.com>
2024-04-16 23:34:17 +0200

    improve covers cache and search json

--- openlibrary/plugins/openlibrary/js/service-worker.js
+++ openlibrary/plugins/openlibrary/js/service-worker.js
@@ -28,43 +28,11 @@ const cacheableResponses = new CacheableResponsePlugin({
 
 /*
 
-Lets make a nice little list of things to cache.
-
 // These only change on deploy
 https://openlibrary.org/static/css/
 // afaik it only contains https://testing.openlibrary.org/static/css/ajax-loader.gif
 // oh also fonts https://testing.openlibrary.org/static/css/fonts/slick.ttf
 // TODO we should move this to the static/images folder
-
-
-
-// These almost never change
-https://openlibrary.org/static/images/
-https://openlibrary.org/images/
-
-
-
-// covers
-https://covers.openlibrary.org/a/id/6257045-M.jpg // author covers
-https://covers.openlibrary.org/a/olid/OL2838765A-M.jpg // random dot images
-https://covers.openlibrary.org/b/id/1852327-M.jpg // book covers
-https://covers.openlibrary.org/w/id/14348537-M.jpg // redirects to IA with a 302
-
-// IA profile picture
-https://archive.org/services/img/@raybb
-https://archive.org/services/img/brand00ibse // these are covers
-
-
-
-// Done
-https://openlibrary.org/static/build/*.js
-https://openlibrary.org/static/build/*.css
-
-// Misc
-
-https://openlibrary.org/static/favicon.ico
-https://openlibrary.org/static/manifest.json
-https://testing.openlibrary.org/static/css/ajax-loader.gif
 
 */
 
@@ -119,20 +87,9 @@ registerRoute(
     })
 )
 
-/*
-Covers:
-S/M covers - cache 150 of them to take up no more than 2.25mb of space.
-L covers - cache 5 of them but with a very short timeout so if you're looking at a few they don't keep loading but they take up much of your storage.
-Original covers (with no letter) - save as above, just in case
-*/
-
-// Neither of these have the starting / because they don't come from the same origin
-// These urls could be domain agnostic if we want them to work locally but it would make them less readable
-const smallMediumCovers = new RegExp('.+(S|M).jpg');
-// This includes large covers and original size because of the ? in "L?"
-const largeCovers = new RegExp('.+L?.jpg');
 registerRoute(
-    smallMediumCovers,
+    // S/M covers - cache 150 of them. They take up no more than 2.25mb of space.
+    /\/id\/\d+-[MS]\.jpg/,
     new CacheFirst({
         cacheName: 'covers-small-medium-cache',
         plugins: [
@@ -146,7 +103,9 @@ registerRoute(
 );
 
 registerRoute(
-    largeCovers,
+    // L/Original covers - cache 5 of them but with a very short timeout so if you go to a few pages they stay.
+    // This includes "8775559-L.jpg" and "8775559.jpg" (original)
+    /\/id\/\d+(|-L)\\.jpg/,
     new CacheFirst({
         cacheName: 'covers-large-cache',
         plugins: [
@@ -171,6 +130,18 @@ registerRoute(
                 purgeOnQuotaError: true,
             }),
             cacheableResponses
+        ],
+    })
+);
+
+registerRoute(
+    /\/search.json/,
+    new CacheFirst({
+        cacheName: 'search-json-cache',
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 60,
+            }),
         ],
     })
 );
