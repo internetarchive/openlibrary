95f0f7af9e5ab0ac795ae39136e36ce8555d6d4c
RayBB <RayBB@users.noreply.github.com>
2024-04-17 16:21:10 +0200

    _u now

--- tests/unit/js/service-worker.test.js
+++ tests/unit/js/service-worker.test.js
@@ -1,77 +1,84 @@
 import {matchMiscFiles, matchSmallMediumCovers, matchLargeCovers, matchStaticImages, matchStaticBuild, matchArchiveOrgImage} from '../../../openlibrary/plugins/openlibrary/js/service-worker-matchers';
 
 
+// Make the expected input for our tests
+function _u(url){
+    return {url: new URL(url)}
+}
+
 test('matchMiscFiles', () => {
-    expect(matchMiscFiles({url: new URL('https://openlibrary.org/static/favicon.ico')})).toBe(true);
-    expect(matchMiscFiles({url: new URL('https://openlibrary.org/')})).toBe(false);
+    // Matches at least one from the list
+    expect(matchMiscFiles(_u('https://openlibrary.org/static/favicon.ico'))).toBe(true);
+    // Doesn't match homepage
+    expect(matchMiscFiles(_u('https://openlibrary.org/'))).toBe(false);
 })
 
 
 test('matchSmallMediumCovers', () => {
     // Test for author covers
-    expect(matchSmallMediumCovers({url: new URL('https://covers.openlibrary.org/a/id/6257045-M.jpg')})).toBe(true);
+    expect(matchSmallMediumCovers(_u('https://covers.openlibrary.org/a/id/6257045-M.jpg'))).toBe(true);
 
     // Test for random dot images
-    expect(matchSmallMediumCovers({url: new URL('https://covers.openlibrary.org/a/olid/OL2838765A-M.jpg')})).toBe(true);
+    expect(matchSmallMediumCovers(_u('https://covers.openlibrary.org/a/olid/OL2838765A-M.jpg'))).toBe(true);
 
     // Test for book covers
-    expect(matchSmallMediumCovers({url: new URL('https://covers.openlibrary.org/b/id/1852327-M.jpg')})).toBe(true);
+    expect(matchSmallMediumCovers(_u('https://covers.openlibrary.org/b/id/1852327-M.jpg'))).toBe(true);
 
     // Test for redirects to Internet Archive with a 302
     // Note: This test assumes you have a way to simulate or check for a 302 redirect.
     // If not, you might need to adjust this test based on your testing environment's capabilities.
-    expect(matchSmallMediumCovers({url: new URL('https://covers.openlibrary.org/w/id/14348537-M.jpg')})).toBe(true);
+    expect(matchSmallMediumCovers(_u('https://covers.openlibrary.org/w/id/14348537-M.jpg'))).toBe(true);
 
     // Test localhost case (will also apply to Gitpod and other domains)
-    expect(matchSmallMediumCovers({url: new URL('http://localhost:7075/a/id/1-M.jpg')})).toBe(true);
+    expect(matchSmallMediumCovers(_u('http://localhost:7075/a/id/1-M.jpg'))).toBe(true);
 
     // Negative cases
-    expect(matchSmallMediumCovers({url: new URL('https://openlibrary.org')})).toBe(false);
-    expect(matchSmallMediumCovers({url: new URL('http://localhost:7075/a/id/1-L.jpg')})).toBe(false);
-    expect(matchSmallMediumCovers({url: new URL('http://localhost:7075/a/id/1.jpg')})).toBe(false);
+    expect(matchSmallMediumCovers(_u('https://openlibrary.org'))).toBe(false);
+    expect(matchSmallMediumCovers(_u('http://localhost:7075/a/id/1-L.jpg'))).toBe(false);
+    expect(matchSmallMediumCovers(_u('http://localhost:7075/a/id/1.jpg'))).toBe(false);
 });
 
 
 test('matchLargeCovers', () => {
     // Test for author covers
-    expect(matchLargeCovers({url: new URL('https://covers.openlibrary.org/a/id/6257045-L.jpg')})).toBe(true);
+    expect(matchLargeCovers(_u('https://covers.openlibrary.org/a/id/6257045-L.jpg'))).toBe(true);
 
     // Test for random dot images
-    expect(matchLargeCovers({url: new URL('https://covers.openlibrary.org/a/olid/OL2838765A-L.jpg')})).toBe(true);
+    expect(matchLargeCovers(_u('https://covers.openlibrary.org/a/olid/OL2838765A-L.jpg'))).toBe(true);
 
     // Test for book covers
-    expect(matchLargeCovers({url: new URL('https://covers.openlibrary.org/b/id/1852327-L.jpg')})).toBe(true);
+    expect(matchLargeCovers(_u('https://covers.openlibrary.org/b/id/1852327-L.jpg'))).toBe(true);
 
     // Test for redirects to Internet Archive with a 302
     // Note: This test assumes you have a way to simulate or check for a 302 redirect.
     // If not, you might need to adjust this test based on your testing environment's capabilities.
-    expect(matchLargeCovers({url: new URL('https://covers.openlibrary.org/w/id/14348537-L.jpg')})).toBe(true);
+    expect(matchLargeCovers(_u('https://covers.openlibrary.org/w/id/14348537-L.jpg'))).toBe(true);
 
     // Test localhost case (will also apply to Gitpod and other domains)
-    expect(matchLargeCovers({url: new URL('http://localhost:7075/a/id/1-L.jpg')})).toBe(true);
+    expect(matchLargeCovers(_u('http://localhost:7075/a/id/1-L.jpg'))).toBe(true);
 
 
     // Negative cases
-    expect(matchLargeCovers({url: new URL('https://openlibrary.org')})).toBe(false);
-    expect(matchLargeCovers({url: new URL('http://localhost:7075/a/id/1-S.jpg')})).toBe(false);
-    expect(matchLargeCovers({url: new URL('http://localhost:7075/a/id/1-M.jpg')})).toBe(false);
-    expect(matchLargeCovers({url: new URL('http://localhost:7075/a/id/1.jpg')})).toBe(false);
+    expect(matchLargeCovers(_u('https://openlibrary.org'))).toBe(false);
+    expect(matchLargeCovers(_u('http://localhost:7075/a/id/1-S.jpg'))).toBe(false);
+    expect(matchLargeCovers(_u('http://localhost:7075/a/id/1-M.jpg'))).toBe(false);
+    expect(matchLargeCovers(_u('http://localhost:7075/a/id/1.jpg'))).toBe(false);
 });
 
 
 test('matchStaticImages', () => {
-    expect(matchStaticImages({url: new URL('https://openlibrary.org/static/images/down-arrow.png')})).toBe(true);
-    expect(matchStaticImages({url: new URL('https://testing.openlibrary.org/static/images/icons/barcode_scanner.svg')})).toBe(true);
-    expect(matchStaticImages({url: new URL('https://openlibrary.org/images/menu.png')})).toBe(true);
-    expect(matchStaticImages({url: new URL('http://localhost:8080/images/menu.png')})).toBe(true);
+    expect(matchStaticImages(_u('https://openlibrary.org/static/images/down-arrow.png'))).toBe(true);
+    expect(matchStaticImages(_u('https://testing.openlibrary.org/static/images/icons/barcode_scanner.svg'))).toBe(true);
+    expect(matchStaticImages(_u('https://openlibrary.org/images/menu.png'))).toBe(true);
+    expect(matchStaticImages(_u('http://localhost:8080/images/menu.png'))).toBe(true);
 
 
     // Negative cases
-    expect(matchStaticImages({url: new URL('https://openlibrary.org')})).toBe(false);
-    expect(matchStaticImages({url: new URL('https://openlibrary.org/stsaatic/images/down-arrow.png')})).toBe(false);
-    expect(matchStaticImages({url: new URL('https://covers.openlibrary.org/w/id/14348537-L.jpg')})).toBe(false);
-    expect(matchStaticImages({url: new URL('http://localhost:7075/a/id/1-M.jpg')})).toBe(false);
-    expect(matchStaticImages({url: new URL('http://localhost:7075/a/id/1.jpg')})).toBe(false);
+    expect(matchStaticImages(_u('https://openlibrary.org'))).toBe(false);
+    expect(matchStaticImages(_u('https://openlibrary.org/stsaatic/images/down-arrow.png'))).toBe(false);
+    expect(matchStaticImages(_u('https://covers.openlibrary.org/w/id/14348537-L.jpg'))).toBe(false);
+    expect(matchStaticImages(_u('http://localhost:7075/a/id/1-M.jpg'))).toBe(false);
+    expect(matchStaticImages(_u('http://localhost:7075/a/id/1.jpg'))).toBe(false);
 });
 
 
@@ -79,25 +86,25 @@ test('matchStaticBuild', () => {
 
     // Positive cases
     // It should work on js
-    expect(matchStaticBuild({url: new URL('https://openlibrary.org/static/build/4290.a0ae80aacde14696d322.js')})).toBe(true);
+    expect(matchStaticBuild(_u('https://openlibrary.org/static/build/4290.a0ae80aacde14696d322.js'))).toBe(true);
     // It should work on js with versions
-    expect(matchStaticBuild({url: new URL('https://openlibrary.org/static/build/all.js?v=e2544bd4947a7f4e8f5c34684df62659')})).toBe(true);
+    expect(matchStaticBuild(_u('https://openlibrary.org/static/build/all.js?v=e2544bd4947a7f4e8f5c34684df62659'))).toBe(true);
     // It should work on css (and testing)
-    expect(matchStaticBuild({url: new URL('https://testing.openlibrary.org/static/build/page-book.css?v=097b69dc350c972d96da0c70cebe7b75')})).toBe(true);
+    expect(matchStaticBuild(_u('https://testing.openlibrary.org/static/build/page-book.css?v=097b69dc350c972d96da0c70cebe7b75'))).toBe(true);
 
     // Negative cases
     // We don't want it caching localhost or gitpod
-    expect(matchStaticBuild({url: new URL('http://localhost:8080/static/build/4290.a0ae80aacde14696d322.js')})).toBe(false);
-    expect(matchStaticBuild({url: new URL('https://8080-internetarc-openlibrary-feliyig0grl.ws-eu110.gitpod.io/static/build/4290.a0ae80aacde14696d322.js')})).toBe(false);
-    expect(matchStaticBuild({url: new URL('https://openlibrary.org')})).toBe(false);
+    expect(matchStaticBuild(_u('http://localhost:8080/static/build/4290.a0ae80aacde14696d322.js'))).toBe(false);
+    expect(matchStaticBuild(_u('https://8080-internetarc-openlibrary-feliyig0grl.ws-eu110.gitpod.io/static/build/4290.a0ae80aacde14696d322.js'))).toBe(false);
+    expect(matchStaticBuild(_u('https://openlibrary.org'))).toBe(false);
 });
 
 test('matchArchiveOrgImage', () => {
     // test profile picture
-    expect(matchArchiveOrgImage({url: new URL('https://archive.org/services/img/@raybb')})).toBe(true);
+    expect(matchArchiveOrgImage(_u('https://archive.org/services/img/@raybb'))).toBe(true);
     // test cover
-    expect(matchArchiveOrgImage({url: new URL('https://archive.org/services/img/courtofmistfury0000maas')})).toBe(true);
+    expect(matchArchiveOrgImage(_u('https://archive.org/services/img/courtofmistfury0000maas'))).toBe(true);
 
     // not anything else
-    expect(matchArchiveOrgImage({url: new URL('https://archive.org/services/')})).toBe(false);
+    expect(matchArchiveOrgImage(_u('https://archive.org/services/'))).toBe(false);
 })
