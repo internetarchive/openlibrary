fdce4018269ee2d391706945b765110b3929742a
RayBB <RayBB@users.noreply.github.com>
2024-04-29 02:09:01 +0200

    remove inline.js from head.html and footer.html

--- openlibrary/plugins/openlibrary/js/index.js
+++ openlibrary/plugins/openlibrary/js/index.js
@@ -14,6 +14,7 @@ import jQueryRepeat from './jquery.repeat';
 import { enumerate, htmlquote, websafe, foreach, join, len, range, jsdef_get } from './jsdef';
 import initAnalytics from './ol.analytics';
 import init from './ol.js';
+import initServiceWorker from './service-worker-init.js'
 import * as Browser from './Browser';
 import { commify, urlencode, slice } from './python';
 import Template from './template.js';
@@ -54,6 +55,9 @@ window.jQuery = jQuery;
 window.$ = jQuery;
 
 window.Promise = Promise;
+
+// Init the service worker first since it does caching
+initServiceWorker();
 
 // This to the best of our knowledge needs to be run synchronously,
 // because it sends the initial pageview to analytics.
@@ -562,5 +566,11 @@ jQuery(function () {
     if (affiliateLinksSection.length) {
         import(/* webpackChunkName: "affiliate-links" */ './affiliate-links')
             .then(module => module.initAffiliateLinks(affiliateLinksSection))
+    }
+
+    // Setup analytics, depends on script loaded from CDN
+    if (typeof archive_analytics !== 'undefined') {
+        // eslint-disable-next-line no-undef
+        archive_analytics.set_up_event_tracking();
     }
 });
