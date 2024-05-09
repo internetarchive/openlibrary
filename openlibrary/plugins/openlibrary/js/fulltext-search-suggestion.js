653f6f4fd28863659ab3314333a61e840861e0c7
merwhite11 <meredith.white91@gmail.com>
2024-05-09 11:52:52 -0700

    Connected new js to template  via index.js Created macro, not inserting into work_search html Response from partial loading in macro
    Basic outline for card
    Added in pseucode for data call plan
    Partial querying db and sending back data
    Trying to get macro to render, not working
    Can't create FulltextSearchBox macro, 500 error
    
    Adding partial with data to DOM !
    Deleted some console.logs
    Author last name and covers loading in macro
    Attempting to get author name

---
+++ openlibrary/plugins/openlibrary/js/fulltext-search-suggestion.js
@@ -0,0 +1,19 @@
+export function initFulltextSearchSuggestion(fulltextSearchSuggestion) {
+    const query = fulltextSearchSuggestion.dataset.query
+    getPartials(fulltextSearchSuggestion, query)
+}
+
+async function getPartials(fulltextSearchSuggestion, query) {
+    const queryParam = encodeURIComponent(query)
+    return fetch(`/partials.json?_component=FulltextSearchSuggestion&data=${queryParam}`)
+        .then((resp) => {
+            if (resp.status !== 200) {
+                throw new Error(`Failed to fetch partials. Status code: ${resp.status}`)
+            }
+            return resp.json()
+        })
+        .then((data) => {
+            // console.log('DATA FROM PARTIAL', data['data'])
+            fulltextSearchSuggestion.innerHTML += data['partials']
+        })
+}
