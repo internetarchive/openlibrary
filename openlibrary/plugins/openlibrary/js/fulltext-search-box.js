f869f2ec9608ffcc890a747e9cbdd2238c69a5f1
merwhite11 <meredith.white91@gmail.com>
2024-05-10 12:39:44 -0700

    Response from partial loading in macro

--- openlibrary/plugins/openlibrary/js/fulltext-search-box.js
+++ openlibrary/plugins/openlibrary/js/fulltext-search-box.js
@@ -1,16 +1,19 @@
 export function initFulltextSearchBox(fulltextSearchBox) {
   console.log('MACRO LOADED')
+  getPartials(fulltextSearchBox)
 }
 
-// async function getPartials(fulltextSearchBox) {
+async function getPartials(fulltextSearchBox) {
 
-//   return fetch('/partials.json?_component=FulltextSearchBox')
-//       .then((resp) => {
-//           if (resp.status !== 200) {
-//               throw new Error(`Failed to fetch partials. Status code: ${resp.status}`)
-//           }
-//       })
-//       .then((data) => {
-//         console.log
-//       })
-// }
+  return fetch('/partials.json?_component=FulltextSearchBox')
+      .then((resp) => {
+          if (resp.status !== 200) {
+              throw new Error(`Failed to fetch partials. Status code: ${resp.status}`)
+          }
+          return resp.json()
+      })
+      .then((data) => {
+        console.log('DATA FROM PARTIAL CALL', data)
+        fulltextSearchBox.innerHTML = data['partials']
+      })
+}
