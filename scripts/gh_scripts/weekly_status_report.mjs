5418e2af96c98eea18507090aef4602a203d6a37
James Champ <jameschamp@acm.org>
2024-05-16 14:07:35 -0700

    Add configurations for each job

--- scripts/gh_scripts/weekly_status_report.mjs
+++ scripts/gh_scripts/weekly_status_report.mjs
@@ -1,4 +1,23 @@
+import fs from 'node:fs'
 import { Octokit } from "@octokit/action";
 
 const octokit = new Octokit()
-console.log(octokit)
+
+main()
+
+function main() {
+    const config = getConfig()
+    console.log(config)
+}
+
+function getConfig() {
+    if (process.argv.length < 3) {
+      throw new Error("Unexpected amount of arguments")
+    }
+    const configPath = process.argv[2]
+    try {
+        return JSON.parse(fs.readFileSync(configPath, 'utf8'))
+    } catch (err) {
+        throw err
+    }
+}
