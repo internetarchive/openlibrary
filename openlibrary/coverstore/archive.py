08ee830c5bf576e50899039de873b00c2c760dee
Mek <michael.karpeles@gmail.com>
2024-05-17 06:45:37 -0700

    Prepares cover archival to be run as cron #8278
    For closing #8278
    
    To be run with:
    ```
    PYTHONPATH=. python /openlibrary/openlibrary/coverstore/archive.py /olsystem/etc/openlibrary.yml /olsystem/etc/coverstore.yml
    ```

--- openlibrary/coverstore/archive.py
+++ openlibrary/coverstore/archive.py
@@ -14,6 +14,8 @@ from infogami.infobase import utils
 
 from openlibrary.coverstore import config, db
 from openlibrary.coverstore.coverlib import find_image_path
+from openlibrary.coverstore.server import load_config
+from scripts.solr_builder.solr_builder.fn_to_cli import FnToCLI
 
 
 ITEM_SIZE = 1_000_000
@@ -509,3 +511,12 @@ class ZipManager:
             file_list = zip_file.namelist()
             if file_list:
                 return max(file_list)
+
+def main(openlibrary_yml: str, coverstore_yml: str, dry_run: bool = False):
+    load_config(openlibrary_yml)
+    load_config(coverstore_yml)
+    archive.archive()
+    archive.Batch.process_pending(upload=True, finalize=True, test=not dry_run)
+
+if __name__ == '__main__':
+    FnToCLI(main).run()
