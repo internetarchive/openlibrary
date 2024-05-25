37efd19a8e533c55ac40c5ed088b3a6d760ad154
merwhite11 <meredith.white91@gmail.com>
2024-05-24 17:11:07 -0700

    changed config to point to production covers

--- openlibrary/plugins/openlibrary/js/fulltext-search-suggestion.js
+++ openlibrary/plugins/openlibrary/js/fulltext-search-suggestion.js
@@ -1,12 +1,10 @@
 export function initFulltextSearchSuggestion(fulltextSearchSuggestion) {
-    console.log('INIT CALLED')
     const query = fulltextSearchSuggestion.dataset.query
     getPartials(fulltextSearchSuggestion, query)
 }
 
 async function getPartials(fulltextSearchSuggestion, query) {
     const queryParam = encodeURIComponent(query)
-    console.log('GET PARTIAL CaLLED')
     return fetch(`/partials.json?_component=FulltextSearchSuggestion&data=${queryParam}`)
         .then((resp) => {
             if (resp.status !== 200) {
@@ -15,7 +13,6 @@ async function getPartials(fulltextSearchSuggestion, query) {
             return resp.json()
         })
         .then((data) => {
-            console.log('DATA FROM PARTIAL', data['data'])
             fulltextSearchSuggestion.innerHTML += data['partials']
         })
 }
