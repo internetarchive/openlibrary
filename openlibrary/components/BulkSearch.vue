ac57d4d64ffdd5f02cf838e6eafb59257b72df97
benbdeitch <benbdeitch@gmail.com>
2024-05-25 18:05:35 -0400

    Adding BulkSearch.Vue, which somehow got removed.

---
+++ openlibrary/components/BulkSearch.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <BulkSearchControls :bulkSearchState="bulkSearchState" />
+        <MatchTable  :bulkSearchState = "bulkSearchState" />
+    </div>
+</template>
+
+
+<script>
+import BulkSearchControls from './BulkSearch/components/BulkSearchControls.vue'
+import {BulkSearchState} from './BulkSearch/utils/classes.js';
+import MatchTable from './BulkSearch/components/MatchTable.vue';
+export default {
+    components: {
+        BulkSearchControls, MatchTable
+    },
+    data() {
+        return {
+            bulkSearchState: new BulkSearchState()
+        }
+    }
+
+}
+</script>
