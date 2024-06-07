47396db82fd99859c39b6f7fc8b75cdfa2ceac26
merwhite11 <meredith.white91@gmail.com>
2024-06-07 13:19:18 -0700

    Restructed html and less files w BEM
    Redesigned mobile style w flex, truncated text, quotes
    
    Resolved conflict-Book title blue and underlined
    
    Styling changes
    
    LoadingIndicator displaying which results fetching

--- openlibrary/plugins/openlibrary/js/fulltext-search-suggestion.js
+++ openlibrary/plugins/openlibrary/js/fulltext-search-suggestion.js
@@ -1,8 +1,21 @@
 export function initFulltextSearchSuggestion(fulltextSearchSuggestion) {
-    const query = fulltextSearchSuggestion.dataset.query
-    getPartials(fulltextSearchSuggestion, query)
+    const isLoading = showLoadingIndicators(fulltextSearchSuggestion)
+    if(isLoading) {
+        const query = fulltextSearchSuggestion.dataset.query
+        getPartials(fulltextSearchSuggestion, query)
+    }
 }
 
+function showLoadingIndicators(searchSuggestion) {
+    let isLoading = false
+    const loadingIndicator = searchSuggestion.querySelector('.loadingIndicator')
+        if (loadingIndicator) {
+            isLoading = true
+            loadingIndicator.classList.remove('hidden')
+        }
+
+    return isLoading
+}
 async function getPartials(fulltextSearchSuggestion, query) {
     const queryParam = encodeURIComponent(query)
     return fetch(`/partials.json?_component=FulltextSearchSuggestion&data=${queryParam}`)
@@ -15,5 +28,10 @@ async function getPartials(fulltextSearchSuggestion, query) {
         .then((data) => {
             // console.log('DATA FROM PARTIAL', data['data'])
             fulltextSearchSuggestion.innerHTML += data['partials']
+            const loadingIndicator = fulltextSearchSuggestion.querySelector('.loadingIndicator');
+            if (loadingIndicator) {
+                console.log('loadingindicator!')
+                loadingIndicator.classList.add('hidden')
+            }
         })
 }
