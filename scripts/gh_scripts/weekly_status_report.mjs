f68bb945648c5a3847aa9e04e6ae5656d23b8f5f
jimchamp <jameschamp@acm.org>
2024-06-11 05:28:06 -0700

    Use correct URL for untriaged issues

--- scripts/gh_scripts/weekly_status_report.mjs
+++ scripts/gh_scripts/weekly_status_report.mjs
@@ -213,7 +213,7 @@ async function prepareUntriagedIssues(leads) {
 
     let allIssuesTriaged = true
     for (const lead of leads) {
-        const searchResultsUrl = `https://github.com/internetarchive/openlibrary/issues?q=is%3Aissue+is%3Aopen+label%3A%22Needs%3A+Triage%22${encodeURIComponent('"' + lead.leadLabel + '"')}`
+        const searchResultsUrl = `https://github.com/internetarchive/openlibrary/issues?q=is%3Aissue+is%3Aopen+label%3A%22Needs%3A+Triage%22+label%3A${encodeURIComponent('"' + lead.leadLabel + '"')}`
         let issueCount = 0
 
         for (const issue of untriagedIssues) {
