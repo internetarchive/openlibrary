d636f70de86e2828c09b88a69bdd8e5a7749efb9
Chales Horn <charles.horn@gmail.com>
2024-06-16 09:20:08 +1200

    remove commented out method

--- openlibrary/plugins/upstream/models.py
+++ openlibrary/plugins/upstream/models.py
@@ -151,10 +151,6 @@ class Edition(models.Edition):
         v = meta_fields['collection']
         return 'printdisabled' in v or 'lendinglibrary' in v
 
-    #      def is_lending_library(self):
-    #         collections = self.get_ia_collections()
-    #         return 'lendinglibrary' in collections
-
     def get_lending_resources(self):
         """Returns the loan resource identifiers (in meta.xml format for ACS4 resources) for books hosted on archive.org
 
