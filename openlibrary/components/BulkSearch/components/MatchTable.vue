b8440122c79215ef80b737c2c85014e517e308ec
benbdeitch <benbdeitch@gmail.com>
2024-07-02 16:19:39 -0400

    Changed up listUrl, to function.

--- openlibrary/components/BulkSearch/components/MatchTable.vue
+++ openlibrary/components/BulkSearch/components/MatchTable.vue
@@ -18,7 +18,7 @@
           <td></td>
           <td></td>
           <td></td>
-          <td><a :href="listUrl" target="_blank" id="listMakerLink">Create list of first matches</a></td>
+          <td><a :href="bulkSearchState.listUrl" target="_blank" id="listMakerLink">Create list of first matches</a></td>
         </tr>
       </tfoot>
     </table>
@@ -34,7 +34,6 @@ export default {
     },
     props: {
         bulkSearchState: BulkSearchState,
-        listUrl: String
     }
 }</script>
 
