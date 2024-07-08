7b63deafa6e8386c68ab29b14cfb3d606b499925
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-07-08 16:04:59 -0400

    Replace hide/show password text with eye icons

--- openlibrary/plugins/openlibrary/js/password-toggle.js
+++ openlibrary/plugins/openlibrary/js/password-toggle.js
@@ -4,16 +4,15 @@
  * @param {HTMLElement} elem Reference to affordance that toggles a password input's visibility
  */
 export function initPasswordToggling(elem) {
-    const i18nStrings = JSON.parse(elem.dataset.i18n)
     const passwordInput = document.querySelector('input[type=password]')
 
     elem.addEventListener('click', () => {
         if (passwordInput.type === 'password') {
             passwordInput.type = 'text'
-            elem.textContent = i18nStrings['hide_password']
+            elem.src = '/static/images/icons/icon_eye-open.svg'
         } else {
             passwordInput.type = 'password'
-            elem.textContent = i18nStrings['show_password']
+            elem.src = '/static/images/icons/icon_eye-closed.svg'
         }
     })
 }
