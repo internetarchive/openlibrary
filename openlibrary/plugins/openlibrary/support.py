607ac2d27659b59b553c37e0710dee109c9654b3
Drini Cami <cdrini@gmail.com>
2024-07-23 15:45:42 +0200

    Use invisible recaptcha on /contact + always show

--- openlibrary/plugins/openlibrary/support.py
+++ openlibrary/plugins/openlibrary/support.py
@@ -1,5 +1,3 @@
-import hashlib
-
 import web
 import logging
 
@@ -9,9 +7,7 @@ from infogami.utils.view import render_template
 
 from openlibrary import accounts
 from openlibrary.core import stats
-from openlibrary.core.cache import get_memcache
 from openlibrary.plugins.upstream.addbook import get_recaptcha
-from openlibrary.utils.dateutil import MINUTE_SECS
 
 logger = logging.getLogger("openlibrary")
 
@@ -22,9 +18,7 @@ class contact(delegate.page):
         user = accounts.get_current_user()
         email = user and user.email
 
-        hashed_ip = hashlib.md5(web.ctx.ip.encode('utf-8')).hexdigest()
-        has_emailed_recently = get_memcache().get('contact-POST-%s' % hashed_ip)
-        recaptcha = has_emailed_recently and get_recaptcha()
+        recaptcha = get_recaptcha(invisible=True)
         return render_template("support", email=email, url=i.path, recaptcha=recaptcha)
 
     def POST(self):
@@ -40,19 +34,16 @@ class contact(delegate.page):
         if not all([email, description]):
             return ""
 
-        hashed_ip = hashlib.md5(web.ctx.ip.encode('utf-8')).hexdigest()
-        has_emailed_recently = get_memcache().get('contact-POST-%s' % hashed_ip)
-        if has_emailed_recently:
-            recap = get_recaptcha()
-            if recap and not recap.validate():
-                return render_template(
-                    "message.html",
-                    'Recaptcha solution was incorrect',
-                    (
-                        'Please <a href="javascript:history.back()">go back</a> and try '
-                        'again.'
-                    ),
-                )
+        recap = get_recaptcha(invisible=True)
+        if recap and not recap.validate():
+            return render_template(
+                "message.html",
+                'Recaptcha solution was incorrect',
+                (
+                    'Please <a href="javascript:history.back()">go back</a> and try '
+                    'again.'
+                ),
+            )
 
         default_assignees = config.get("support_default_assignees", {})
         if (topic_key := str(topic.replace(" ", "_").lower())) in default_assignees:
@@ -69,9 +60,6 @@ class contact(delegate.page):
         message = SUPPORT_EMAIL_TEMPLATE % locals()
         sendmail(email, assignee, subject, message)
 
-        get_memcache().set(
-            'contact-POST-%s' % hashed_ip, "true", expires=15 * MINUTE_SECS
-        )
         return render_template("email/case_created", assignee)
 
     def prepare_subject_line(self, subject, max_length=60):
