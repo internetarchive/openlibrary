a8f6dff8dc268a16ede79dea2de6d3ab9e773855
Benjamin Deitch <131627264+benbdeitch@users.noreply.github.com>
2024-07-23 16:54:27 -0400

    Bulk Search and List Creator (#9279)
    * Bulk Search History has been squashed and implemented, part 2.
    
    * Squishing history part 3
    
    * Squashing history part 4
    
    * Altered to support list building.
    
    * Adding BulkSearch.Vue, which somehow got removed.
    
    * Misc formatting fixes, and adding functionality to the list creator.
    
    * Adjusted styling
    
    * Adjusted styling
    
    * Renamed template
    
    * Renamed template
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Removed unnecessary class.
    
    * Assorted fixes
    
    * Replaced possibly copyrighted samples.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Adjusted location of sample data into a JSON object, rather than inside of html pre-tags.
    
    * Removed  now unnecessary codespell changes.
    
    * Removed leftover file.
    
    * Removed unnecessary argument
    
    * Misc fixes
    
    * Added title to component.
    
    * Adjusted samples and regexes
    
    * Removed unnecessary parameter in call
    
    * Apply suggestions from code review
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixed erroneous buildListUrl function, to actually do its job.
    
    * Added additional regex, to cover '<author> - <title>' case.
    
    * Changed up listUrl, to function.
    
    * Bulk Search History has been squashed and implemented, part 2.
    
    * Squishing history part 3
    
    * Squashing history part 4
    
    * Misc formatting fixes, and adding functionality to the list creator.
    
    * Replaced possibly copyrighted samples.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Removed  now unnecessary codespell changes.
    
    * Removed leftover file.
    
    * Moved the samplebar into the Bulk Search Controls, and added more information to the samples.
    
    * Adjusted watcher function to prevent selecting no sample to overwrite data in the input box.
    
    * Miscellaneous fixes
    
    * Misc fix
    
    * Added link to sample source
    
    * Remove uneeded solr fields fetched during Bulk Search
    
    ---------
    
    Co-authored-by: Drini Cami <cdrini@gmail.com>

---
+++ openlibrary/components/BulkSearch/components/BulkSearchControls.vue
@@ -0,0 +1,177 @@
+
+
+
+<template>
+    <details open class="bulk-search-controls">
+        <summary>Input</summary>
+        <div>
+            <textarea v-model="bulkSearchState.inputText"></textarea>
+            <br />
+            <label>Format: <select @change="selectAlgorithm">
+                    <option value="1">e.g. "The Wizard of Oz by L. Frank Baum"</option>
+                    <option value="2">e.g. "L. Frank Baum - The Wizard of Oz" </option>
+                    <option value="3">e.g. "The Wizard of Oz - L. Frank Baum" </option>
+                    <option value="4">e.g. "The Wizard of Oz (L. Frank Baum)"</option>
+                    <option value="5">Wikipedia Citation (e.g. Baum, Frank L. (1994). The Wizard of Oz)</option>
+                    <option value="6">✨ AI Extraction</option>
+                </select></label>
+            <label v-if="bulkSearchState.extractionOptions.use_gpt">OpenAI API Key:
+                <input v-if="showPassword" type="password" @click="togglePasswordVisibility()" v-model="bulkSearchState.extractionOptions.api_key" />
+
+                <input v-else type="text" @click="togglePasswordVisibility()" v-model="bulkSearchState.extractionOptions.api_key" />
+            </label>
+            <label>Sample Data:
+                <select v-model="selectedValue">
+                    <option v-for="sample in sampleData" :value = "sample.text" :key = "sample.source"> {{sample.name}} </option>
+                </select>
+            </label>
+            <label>
+                <input v-model="bulkSearchState.matchOptions.includeAuthor" type="checkbox" /> Use author in
+                search query
+            </label>
+            <br>
+            <button @click="extractBooks">Extract Books</button>
+            <button @click="matchBooks">Match Books</button>
+        </div>
+        <div v-if="bulkSearchState.errorMessage">
+            <p v-for="error in bulkSearchState.errorMessage" :key="error">
+                {{ error }}</p>
+        </div>
+    </details>
+</template>
+
+<script>
+import {sampleData} from '../utils/samples.js';
+import { BulkSearchState, ExtractedBook, BookMatch } from '../utils/classes.js';
+import { buildSearchUrl, buildListUrl } from '../utils/searchUtils.js'
+export default {
+
+    props: {
+        bulkSearchState: BulkSearchState
+    },
+    data() {
+        return {
+            selectedValue: '',
+            showPassword: true,
+            sampleData: sampleData,
+            regexDict: {
+                '': '',
+                1: new RegExp('(^|>)(?<title>[A-Za-z][\\p{L}0-9\\- ,]{1,250})\\s+(by|[-\u2013\u2014\\t])\\s+(?<author>[\\p{L}][\\p{L}\\.\\- ]{3,70})( \\(.*)?($|<\\/)', 'gmu'),
+                2: new RegExp('(^|>)(?<author>[A-Za-z][\\p{L}0-9\\- ,]{1,250})\\s+[,-\u2013\u2014\\t]\\s+(?<title>[\\p{L}][\\p{L}\\.\\- ]{3,70})( \\(.*)?($|<\\/)', 'gmu'),
+                3: new RegExp('(^|>)(?<title>[A-Za-z][\\p{L}0-9\\- ,]{1,250})\\s+[,-\u2013\u2014\\t]\\s+(?<author>[\\p{L}][\\p{L}\\.\\- ]{3,70})( \\(.*)?($|<\\/)', 'gmu'),
+                4: new RegExp('^(?<title>[\\p{L}].{1,250})\\s\\(?<author>(.{3,70})\\)$$', 'gmu'),
+                5: new RegExp('^(?<author>[^.()]+).*?\\)\\. (?<title>[^.]+)', 'gmu')
+            }
+        }
+    },
+    watch: {
+        selectedValue(newValue) {
+            if (newValue!==''){
+                this.bulkSearchState.inputText = newValue;
+            }
+        }
+    },
+    methods: {
+        togglePasswordVisibility(){
+            this.showPassword= !this.showPassword
+        },
+        selectAlgorithm(e) {
+            if (e.target.value === '6') {
+                this.bulkSearchState.extractionOptions.use_gpt = true
+            }
+            else {
+                this.bulkSearchState.extractionOptions.use_gpt = false
+                this.bulkSearchState.extractionOptions.regex = this.regexDict[e.target.value]
+            }
+        },
+        async extractBooks() {
+            this.bulkSearchState.errorMessage = []
+            if (this.bulkSearchState.extractionOptions.use_gpt) {
+                const request = {
+
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${this.bulkSearchState.extractionOptions.api_key}`
+                    },
+                    body: JSON.stringify({
+                        model: 'gpt-3.5-turbo',
+                        response_format: { type: 'json_object' },
+                        messages: [
+                            {
+                                role: 'system',
+                                content: 'You are a book extraction system. You will be given a free form passage of text containing references to books, and you will need to extract the book titles, author, and optionally ISBN in a JSON array.'
+                            },
+                            {
+                                role: 'user',
+                                content: `Please extract the books from the following text:\n\n${this.bulkSearchState.inputText}`,
+                            }
+                        ],
+                    })
+
+                }
+                try {
+                    const resp = await fetch('https://api.openai.com/v1/chat/completions', request)
+
+                    if (!resp.ok) {
+                        const status = resp.status
+                        if (status === 401) {
+
+                            this.bulkSearchState.errorMessage.push('Error: Incorrect Authorization key.')
+                        }
+                        throw new Error('Network response was not okay.')
+                    }
+                    const data = await resp.json()
+                    this.bulkSearchState.extractedBooks = JSON.parse(data.choices[0].message.content)['books'].map((entry) => new ExtractedBook(entry?.title, entry?.author))
+                    this.bulkSearchState.matchedBooks = JSON.parse(data.choices[0].message.content)['books'].map((entry) => new BookMatch(new ExtractedBook(entry?.title, entry?.author), {}))
+                }
+                catch (error) {
+
+                }
+
+            }
+            else {
+                const regex = this.bulkSearchState.extractionOptions.regex
+                if (regex && this.bulkSearchState.inputText) {
+                    const data = [...this.bulkSearchState.inputText.matchAll(regex)]
+
+                    this.bulkSearchState.extractedBooks = data.map((entry) => new ExtractedBook(entry.groups?.title, entry.groups?.author))
+                    this.bulkSearchState.matchedBooks = this.bulkSearchState.extractedBooks.map((entry) => new BookMatch(entry, []))
+                }
+            }
+        },
+        async matchBooks() {
+            const fetchSolrBook = async function (book, matchOptions) {
+
+                try {
+                    const data = await fetch(buildSearchUrl(book, matchOptions, true))
+                    return await data.json()
+                }
+                catch (error) {}
+            }
+            for (const bookMatch of this.bulkSearchState.matchedBooks) {
+                bookMatch.solrDocs = await fetchSolrBook(bookMatch.extractedBook, this.bulkSearchState.matchOptions)
+            }
+            this.bulkSearchState.listUrl = buildListUrl(this.bulkSearchState.matchedBooks)
+        },
+
+    }
+}
+</script>
+
+<style lang="less">
+
+.bulk-search-controls{
+    padding:20px;
+}
+label input {
+    flex: 1;
+}
+
+textarea {
+    width: 100%;
+    display: flex;
+    resize: vertical;
+    box-sizing: border-box;
+}
+</style>
---
+++ openlibrary/components/BulkSearch/utils/searchUtils.js
@@ -0,0 +1,40 @@
+
+/** @typedef {import('./classes.js').ExtractedBook} ExtractedBook */
+/** @typedef {import('./classes.js').MatchOptions} MatchOptions */
+/** @typedef {import('./classes.js').BookMatch} BookMatch */
+
+const OL_SEARCH_BASE = 'openlibrary.org'
+
+/**
+ * @param {ExtractedBook} extractedBook
+ * @param {MatchOptions} matchOptions
+ */
+export function buildSearchUrl(extractedBook, matchOptions, json = true) {
+    let title = extractedBook.title?.split(/[:(?]/)[0].replace(/’/g, '\'');
+    const author = extractedBook.author
+    title = title.replace(/^the\b/i, '').trim();
+    let query = `title:"${title}"`;
+    if (matchOptions.includeAuthor && author  && author.toLowerCase() !== 'null' && author.toLowerCase() !== 'unknown') {
+        const authorParts = author.replace(/^\S+\./, '').trim().split(/\s/);
+        const authorLastName = author.includes(',') ? author.replace(/,.*/, '') : authorParts[authorParts.length - 1];
+        query += ` author:${authorLastName}`;
+    }
+    let path = `https://${OL_SEARCH_BASE}/search`;
+    if (json) path += '.json';
+    const url = `${path}?${new URLSearchParams({
+        q: query,
+        mode: 'everything',
+        fields: 'key,title,author_name,cover_i,first_publish_year,edition_count,ebook_access',
+    })}`;
+    return url;
+}
+
+/**
+ * @param {BookMatch[]} bookMatches
+ */
+export function buildListUrl(bookMatches){
+    const seeds = bookMatches.filter(bookMatch => bookMatch.solrDocs.numFound>0).map((bookMatch) => bookMatch.solrDocs.docs[0].key.split('/')[2])
+    const url = `https://openlibrary.org/lists/add?seeds=${seeds.join(',')}`
+    return url
+}
+
