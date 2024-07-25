280834958fa92ef06d104fbf384fd49373fd21e6
benbdeitch <benbdeitch@gmail.com>
2024-07-24 20:48:54 -0400

    Altered BulkSearch to handle abstractors in their own class, rather than as a part of the vue component.

--- openlibrary/components/BulkSearch/components/BulkSearchControls.vue
+++ openlibrary/components/BulkSearch/components/BulkSearchControls.vue
@@ -27,8 +27,8 @@
                 search query
             </label>
             <br>
-            <button @click="extractBooks">Extract Books</button>
-            <button @click="matchBooks">Match Books</button>
+            <button v-on:click="extractBooks">Extract Books</button>
+            <button v-on:click="matchBooks">Match Books</button>
         </div>
         <div v-if="bulkSearchState.errorMessage">
             <p v-for="error in bulkSearchState.errorMessage" :key="error">
@@ -49,6 +49,7 @@ export default {
     data() {
         return {
             selectedValue: '',
+
             showPassword: true,
             sampleData: sampleData,
         }
