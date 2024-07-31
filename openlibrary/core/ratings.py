62d2243131a9c7e6aee00d1e9c5660fd5b594e89
Benjamin Deitch <131627264+benbdeitch@users.noreply.github.com>
2024-07-31 13:28:08 -0400

    Aggregate want to read counts by author (#9545)
    * first attempt at updating Solr's author updater to use the new facet API
    
    * Fixed divide by zero issue when there are no recorded ratings.
    
    * Adjusted test to reflect the new way in which solr handles the updating of authors.
    
    * Added default case to build ratings

--- openlibrary/core/ratings.py
+++ openlibrary/core/ratings.py
@@ -114,11 +114,13 @@ class Ratings(db.CommonExtras):
         cls, rating_counts: list[int]
     ) -> WorkRatingsSummary:
         total_count = sum(rating_counts, 0)
+        ratings_average = (
+            (sum((k * n_k for k, n_k in enumerate(rating_counts, 1)), 0) / total_count)
+            if total_count != 0
+            else 0
+        )
         return {
-            'ratings_average': sum(
-                (k * n_k for k, n_k in enumerate(rating_counts, 1)), 0
-            )
-            / total_count,
+            'ratings_average': ratings_average,
             'ratings_sortable': cls.compute_sortable_rating(rating_counts),
             'ratings_count': total_count,
             'ratings_count_1': rating_counts[0],
