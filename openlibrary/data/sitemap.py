c6fb3de70c4280337a753c59440d3427397afc57
Jash2606 <jash.23bcs10163@sst.scaler.com>
2024-07-31 12:01:37 +0530

    Add lang attribute to <html> tag for accessibility improvement.

--- openlibrary/data/sitemap.py
+++ openlibrary/data/sitemap.py
@@ -48,7 +48,7 @@ t_html_layout = t(
     """$def with (page)
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="robots" content="noindex,follow" />
