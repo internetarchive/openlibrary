66744efaa129386c8d2ac99817de286b0714510b
pidgezero-one <s.kischak@gmail.com>
2024-08-02 09:02:04 -0400

    unstash

--- scripts/providers/import_wikisource.py
+++ scripts/providers/import_wikisource.py
@@ -283,7 +283,7 @@ def update_record(book: BookRecord, new_data: dict):
 
     if book.description is None:
         try:
-            notes = wtp.plain_text(template.get("notes").value.strip())
+            notes = wtp.remove_markup(template.get("notes").value.strip().replace("\n", " "))
             if notes != "":
                 book.set_description(notes)
         except ValueError:
