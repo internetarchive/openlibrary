47b57c834e8e57d465c901f55c510a7feb7ec891
pidgezero-one <s.kischak@gmail.com>
2024-08-01 22:44:01 -0400

    uncomment imports

--- scripts/providers/import_wikisource.py
+++ scripts/providers/import_wikisource.py
@@ -20,9 +20,9 @@ import wikitextparser as wtp
 from nameparser import HumanName
 from nameparser.config import CONSTANTS
 
-# from infogami import config
-# from openlibrary.config import load_config
-# from openlibrary.core.imports import Batch
+from infogami import config
+from openlibrary.config import load_config
+from openlibrary.core.imports import Batch
 from scripts.solr_builder.solr_builder.fn_to_cli import FnToCLI
 
 logger = logging.getLogger("openlibrary.importer.pressbooks")
