bddd06055569f9048bf85a4437b8c092dd874cd4
Sandy Chu <sandy.chu.40@gmail.com>
2024-09-17 17:10:09 -0700

    Change initialization order of ILE and editions datatable for magic select

--- openlibrary/plugins/openlibrary/js/editions-table/index.js
+++ openlibrary/plugins/openlibrary/js/editions-table/index.js
@@ -6,7 +6,11 @@ const LS_RESULTS_LENGTH_KEY = 'editions-table.resultsLength';
 
 export function initEditionsTable() {
     var rowCount;
-
+    let currentLength;
+    // Prevent reinitialization of the editions datatable
+    if ($.fn.DataTable.isDataTable($('#editions'))) {
+        return;
+    }
     $('#editions th.title').on('mouseover', function(){
         if ($(this).hasClass('sorting_asc')) {
             $(this).attr('title','Sort latest to earliest');
@@ -42,7 +46,6 @@ export function initEditionsTable() {
     });
 
     $('#editions').on('length.dt', function(e, settings, length) {
-        togglePaginationVisibility(length);
         localStorage.setItem(LS_RESULTS_LENGTH_KEY, length);
     });
 
@@ -53,31 +56,36 @@ export function initEditionsTable() {
     })
 
     rowCount = $('#editions tbody tr').length;
-    const currentLength = Number(localStorage.getItem(LS_RESULTS_LENGTH_KEY)) || DEFAULT_LENGTH;
-
-    $('#editions').DataTable({
-        aoColumns: [{ sType: 'html' }, null],
-        order: [[1, 'asc']],
-        lengthMenu: [[3, 10, 25, 50, 100, -1], [3, 10, 25, 50, 100, 'All']],
-        bPaginate: true, // Always allow pagination initially
-        bInfo: true,
-        sPaginationType: 'full_numbers',
-        bFilter: true,
-        bStateSave: false,
-        bAutoWidth: false,
-        pageLength: currentLength,
-    });
-
-    // Toggle pagination visibility based on row count and selected length
-    function togglePaginationVisibility(selectedLength) {
-        const paginationElement = $('.dataTables_paginate.paging_full_numbers');
-        if (rowCount <= selectedLength || selectedLength === -1) {
-            paginationElement.hide();
-        } else {
-            paginationElement.show();
-        }
+    if (rowCount < 4) {
+        $('#editions').DataTable({
+            aoColumns: [{sType: 'html'},null],
+            order: [ [1,'asc'] ],
+            bPaginate: false,
+            bInfo: false,
+            bFilter: false,
+            bStateSave: false,
+            bAutoWidth: false
+        });
+    } else {
+        currentLength = Number(localStorage.getItem(LS_RESULTS_LENGTH_KEY));
+        $('#editions').DataTable({
+            aoColumns: [{sType: 'html'},null],
+            order: [ [1,'asc'] ],
+            lengthMenu: [ [3, 10, 25, 50, 100, -1], [3, 10, 25, 50, 100, 'All'] ],
+            bPaginate: true,
+            bInfo: true,
+            sPaginationType: 'full_numbers',
+            bFilter: true,
+            bStateSave: false,
+            bAutoWidth: false,
+            pageLength: currentLength ? currentLength : DEFAULT_LENGTH,
+            drawCallback: function() {
+                if ($('#ile-toolbar').css('display') === 'none') {
+                    for (const elem of $('.ile-selected')) {
+                        elem.classList.remove('ile-selected');
+                    }
+                }
+            }
+        })
     }
-
-    // Initial pagination visibility toggle
-    togglePaginationVisibility(currentLength);
 }
--- openlibrary/plugins/openlibrary/js/index.js
+++ openlibrary/plugins/openlibrary/js/index.js
@@ -79,11 +79,11 @@ jQuery(function () {
 
     init($);
 
-    // conditionally load functionality based on what's in the page
-    if (document.getElementsByClassName('editions-table--progressively-enhanced').length) {
-        import(/* webpackChunkName: "editions-table" */ './editions-table')
-            .then(module => module.initEditionsTable());
-    }
+    // // conditionally load functionality based on what's in the page
+    // if (document.getElementsByClassName('editions-table--progressively-enhanced').length) {
+    //     import(/* webpackChunkName: "editions-table" */ './editions-table')
+    //         .then(module => module.initEditionsTable());
+    // }
 
     const edition = document.getElementById('addWork');
     const autocompleteAuthor = document.querySelector('.multi-input-autocomplete--author');
@@ -329,8 +329,17 @@ jQuery(function () {
     if (document.getElementsByClassName('show-librarian-tools').length) {
         import(/* webpackChunkName: "ile" */ './ile')
             .then((module) => module.init());
+        // Import ile then the datatable to apply clickable classes to all listed editions
+        if (document.getElementsByClassName('editions-table--progressively-enhanced').length) {
+            import(/* webpackChunkName: "editions-table" */ './editions-table')
+                .then(module => module.initEditionsTable())
+        }
     }
-
+    // conditionally load functionality based on what's in the page
+    if (document.getElementsByClassName('editions-table--progressively-enhanced').length) {
+        import(/* webpackChunkName: "editions-table" */ './editions-table')
+            .then(module => module.initEditionsTable());
+    }
     if ($('#cboxPrevious').length) {
         $('#cboxPrevious').attr({'aria-label': 'Previous button', 'aria-hidden': 'true'});
     }
