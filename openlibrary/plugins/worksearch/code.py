c229e5daaa26a7dca79db4dfb28747076e95c5f5
Drini Cami <cdrini@gmail.com>
2024-09-23 16:22:46 +0200

    Sort highlights by most matches

--- openlibrary/plugins/worksearch/code.py
+++ openlibrary/plugins/worksearch/code.py
@@ -463,7 +463,7 @@ class SearchResponse:
         highlighting: dict[str, dict[str, list[str]]],
     ) -> dict[str, dict[str, list[str]]] | None:
         """
-        Remove highlight fragment that only contain stop words.
+        Remove highlight fragment that only contain stop words, and sort by most matches.
 
         :param highlighting: solr highlighting response. e.g. {'OL123W': {'title': ['<em>title</em>']}}
 
@@ -502,6 +502,13 @@ class SearchResponse:
                 highlights[field] = [
                     term for term in highlights[field] if get_matches(term) - stopwords
                 ]
+
+                # Sort by most matches
+                highlights[field].sort(
+                    key=lambda term: len(get_matches(term)), reverse=True
+                )
+
+                # Remove empty
                 if not highlights[field]:
                     del highlights[field]
 
