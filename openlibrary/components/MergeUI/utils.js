d3b4ae53b0f230b4596654f57170fad050496e84
DanielleInkster <danielle.inkster@gmail.com>
2024-10-01 14:13:22 +0100

    added get_author_names function

--- openlibrary/components/MergeUI/utils.js
+++ openlibrary/components/MergeUI/utils.js
@@ -230,6 +230,38 @@ function save_many(items, comment, action, data) {
     });
 }
 
+/**
+ * Fetches name associated with the author key
+ * @param {Object[]} works
+ * @returns {Promise<Record<string,object>} A response to the request
+ */
+export async function get_author_names(works) {
+    const authorIds = _.uniq(works).flatMap(record =>
+        record.authors.map(authorEntry => authorEntry.author.key)
+    )
+
+    const queryParams = new URLSearchParams({
+        q: `key:(${authorIds.join(' OR ')})`,
+        mode: 'everything',
+        fields: 'key,name',
+    })
+
+    const response = await fetch(`${CONFIGS.OL_BASE_BOOKS}/search/authors.json?${queryParams}`)
+
+    if (!response.ok) return {error: true};
+
+    const results = await response.json()
+
+    const authorDirectory = {}
+
+    for (const doc of results.docs) {
+        authorDirectory[doc.key] = doc.name;
+    }
+
+    return authorDirectory
+}
+
+
 // /**
 //  * @param {Object} record
 //  * @param {string} comment
