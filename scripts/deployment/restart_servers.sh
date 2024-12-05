5b95ce75520c44802b4b89a2283968e0e776181a
Drini Cami <cdrini@gmail.com>
2024-12-05 17:45:53 -0500

    Tweaks and improvements to deploy scripts
    * Still cleanup on error / ctrl-c exit
    * Stop executing if error occurs inside ssh code block
    * Fix Mac OS datetime parsing
    * Fix restart servers setting wrong hostname
    * Make restart servers output less noisy

--- scripts/deployment/restart_servers.sh
+++ scripts/deployment/restart_servers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -o xtrace
+set -e
+# set -o xtrace
 
 # Restart the Docker services on all specified hosts.
 # If no host is specified, then restart all servers.
@@ -20,6 +21,11 @@ SERVER_NAMES=${SERVERS:-"ol-home0 ol-covers0 ol-web0 ol-web1 ol-web2 ol-www0"}
 SERVERS=$(echo $SERVER_NAMES | sed "s/ /$SERVER_SUFFIX /g")$SERVER_SUFFIX
 
 for SERVER in $SERVERS; do
-    HOSTNAME=$(host $SERVER | cut -d " " -f 1)
-    ssh $SERVER "cd /opt/openlibrary; COMPOSE_FILE=$PRODUCTION HOSTNAME=$HOSTNAME OLIMAGE=$OLIMAGE docker compose --profile $(echo $SERVER | cut -f1 -d '.') up --build --no-deps -d"
+    echo "$SERVER ..."
+    ssh $SERVER "
+        set -e
+        HOSTNAME=\$(host $SERVER | cut -d ' ' -f 1)
+        cd /opt/openlibrary
+        COMPOSE_FILE=$PRODUCTION HOSTNAME=$HOSTNAME OLIMAGE=$OLIMAGE docker compose --profile $(echo $SERVER | cut -f1 -d '.') up --build --no-deps -d
+    "
 done
