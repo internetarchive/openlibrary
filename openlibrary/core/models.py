82b198c5bfef6050d87c55814bf67e62666c1104
pidgezero-one <s.kischak@gmail.com>
2024-12-05 17:56:14 -0500

    ?

--- openlibrary/core/models.py
+++ openlibrary/core/models.py
@@ -34,6 +34,7 @@ from openlibrary.core.vendors import get_amazon_metadata
 from openlibrary.core.wikidata import WikidataEntity, get_wikidata_entity
 from openlibrary.utils import extract_numeric_id_from_olid
 from openlibrary.utils.isbn import canonical, isbn_13_to_isbn_10, to_isbn_13
+from openlibrary.plugins.upstream.utils import get_author_config
 
 from ..accounts import OpenLibraryAccount  # noqa: F401 side effects may be needed
 from ..plugins.upstream.utils import get_coverstore_public_url, get_coverstore_url
@@ -806,7 +807,28 @@ class Author(Thing):
 
     def get_lists(self, limit=50, offset=0, sort=True):
         return self._get_lists(limit=limit, offset=offset, sort=sort)
-
+    
+    def merge_remote_ids(
+            self, incoming_ids: dict[str, str]
+        ) -> tuple[dict[str, str], int]:
+            output = {**self.remote_ids}
+            if len(incoming_ids.items()) == 0:
+                return output, -1
+            matches = 0
+            conflicts = 0
+            for id in get_author_config():
+                identifier: str = id.name
+                if identifier in output and identifier in incoming_ids:
+                    if output[identifier] != incoming_ids[identifier]:
+                        conflicts = conflicts + 1
+                    else:
+                        output[identifier] = incoming_ids[identifier]
+                        matches = matches + 1
+            if conflicts > matches:
+                # This means that the identifiers we already have for this author have too many conflicts with whichever identifiers we're trying to merge into it.
+                # TODO: Raise this to librarians, somehow.
+                return self.remote_ids, -1
+            return output, matches
 
 class User(Thing):
     DEFAULT_PREFERENCES = {
