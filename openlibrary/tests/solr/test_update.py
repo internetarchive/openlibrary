32b1aafeaee8597d5b85b6df7ca450e12d17eb74
benbdeitch <benbdeitch@gmail.com>
2024-12-16 14:53:12 -0500

    Removed residue

--- openlibrary/tests/solr/test_update.py
+++ openlibrary/tests/solr/test_update.py
@@ -110,7 +110,7 @@ class Test_update_keys:
     def setup_class(cls):
         update.data_provider = FakeDataProvider()
 
-    @pytest.mark.asyncio()
+    @pytest.mark.asyncio
     async def test_delete(self):
         update.data_provider.add_docs(
             [
@@ -134,7 +134,7 @@ class Test_update_keys:
         }
         assert update_state.adds == []
 
-    @pytest.mark.asyncio()
+    @pytest.mark.asyncio
     async def test_redirects(self):
         update.data_provider.add_docs(
             [
