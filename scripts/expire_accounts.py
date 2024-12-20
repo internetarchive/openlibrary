0b0fc0defc480077c295e01cb2b4ece998843991
jimchamp <28732543+jimchamp@users.noreply.github.com>
2024-12-20 13:45:27 -0800

    Update deprecated method

--- scripts/expire_accounts.py
+++ scripts/expire_accounts.py
@@ -1,12 +1,13 @@
-from datetime import datetime
-
+from datetime import datetime, UTC
 import web
 
 
 def delete_old_links():
     for doc in web.ctx.site.store.values(type="account-link"):
         expiry_date = datetime.strptime(doc["expires_on"], "%Y-%m-%dT%H:%M:%S.%f")
-        now = datetime.utcnow()
+        # Make expiry_date timezone-aware:
+        expiry_date = expiry_date.replace(tzinfo=UTC)
+        now = datetime.now(UTC)
         key = doc["_key"]
         if expiry_date > now:
             print("Deleting link %s" % (key))
