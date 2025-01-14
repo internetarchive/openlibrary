e213bd01571ce9e35e39e9a5aa6b17c280049349
RayBB <RayBB@users.noreply.github.com>
2025-01-13 21:45:52 -0500

    remove unused vite-plugin-css-injected-by-js

--- openlibrary/components/vite.config.mjs
+++ openlibrary/components/vite.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import fs from 'fs';
-// import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 
 const COMPONENT = process.env.COMPONENT?.replace('.vue', '');
