b4668689eacf638ee3d0a69b1bc00f5f61d286a0
RayBB <RayBB@users.noreply.github.com>
2025-01-15 12:04:14 -0500

    revert helloworld to old style

--- openlibrary/components/HelloWorld.vue
+++ openlibrary/components/HelloWorld.vue
@@ -1,145 +1,65 @@
-<script setup>
-import { ref } from 'vue';
-
-defineProps({
-    msg: String,
-});
-
-const count = ref(0);
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="https://vite.dev/logo.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img
-        src="https://raw.githubusercontent.com/vuejs/art/9f8ee218d32b52c2d150212f22798a7a61689361/logo.svg"
-        class="logo vue"
-        alt="Vue logo"
-      />
-    </a>
-  </div>
-  <h1>{{ msg }}</h1>
+    <div class="page">
+        <h1>Hello Vuorld!</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+        New Vue? Read the <a href="https://vuejs.org/v2/guide/">Getting Started Guide</a>. Here are some quick examples:
+        <hr />
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+        <p>Here's a list ({{sampleList.length}} items):</p>
+        <ol v-if="sampleList.length">
+            <li v-for="(item, i) in sampleList" :key="i">{{item}}</li>
+        </ol>
+        <p v-else>Your list is empty!</p>
+
+        <hr />
+
+        <p>Here's some logic:</p>
+        <!-- v-model is used to bind inputs to data fields. See https://vuejs.org/v2/guide/forms.html -->
+        <input type="number" v-model.number="num1"> + <input type="number" v-model.number="num2"> = {{num1 + num2}}
+    </div>
 </template>
 
-<style>
-.read-the-docs {
-  color: #888;
-}
+<script>
+export default {
+    // Props are for external options; if a subelement of this is modified,
+    // the view automatically re-renders
+    props: {
+        sampleList: {
+            type: Array,
+            default: () => ['A', 'B', 'C'],
+        },
+    },
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+    // Data is for internal stuff. This needs to be a function so that we get
+    // a fresh object every time this is initialized.
+    data: () => {
+        return {
+            num1: 31,
+            num2: 11,
+        }
+    },
 
-:root {
-  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-  line-height: 1.5;
-  font-weight: 400;
+    // Essentially getters
+    computed: {
 
-  color-scheme: light dark;
-  color: rgba(255, 255, 255, 0.87);
-  background-color: #242424;
+    },
 
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+    // Methods :)
+    methods: {
 
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
+    },
 }
-a:hover {
-  color: #535bf2;
-}
+</script>
 
-body {
-  margin: 0;
-  display: flex;
-  place-items: center;
-  min-width: 320px;
-  min-height: 100vh;
-}
-
-h1 {
-  font-size: 3.2em;
-  line-height: 1.1;
-}
-
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  background-color: #1a1a1a;
-  cursor: pointer;
-  transition: border-color 0.25s;
-}
-button:hover {
-  border-color: #646cff;
-}
-button:focus,
-button:focus-visible {
-  outline: 4px auto -webkit-focus-ring-color;
-}
-
-.card {
-  padding: 2em;
-}
-
-#app {
-  max-width: 1280px;
+<style lang="less">
+.page {
+  max-width: 800px;
   margin: 0 auto;
-  padding: 2rem;
-  text-align: center;
-}
+  margin-top: 60px;
+  padding: 10px;
 
-@media (prefers-color-scheme: light) {
-  :root {
-    color: #213547;
-    background-color: #ffffff;
-  }
-  a:hover {
-    color: #747bff;
-  }
-  button {
-    background-color: #f9f9f9;
+  input[type="number"] {
+    width: 3em;
   }
 }
 </style>
