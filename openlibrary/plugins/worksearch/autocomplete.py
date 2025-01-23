27e5914c4d374ac5117261a7427c6f293eff94de
Dorna Raj Gyawali <dronarajgyawali@gmail.com>
2025-01-23 06:47:57 +0000

    fixed type hint

--- openlibrary/plugins/worksearch/autocomplete.py
+++ openlibrary/plugins/worksearch/autocomplete.py
@@ -22,7 +22,7 @@ def to_json(d):
 
 class autocomplete(delegate.page):
     path = "/_autocomplete"
-    fq = '-type:edition'
+    fq = ('-type:edition',)
     fl = 'key,type,name,title,score'
     olid_suffix: str | None = None
     sort: str | None = None
@@ -46,7 +46,7 @@ class autocomplete(delegate.page):
     def GET(self):
         return self.direct_get()
 
-    def direct_get(self, fq: Iterable[str] | None):
+    def direct_get(self, fq: Iterable[str] | None = None):
         i = web.input(q="", limit=5)
         i.limit = safeint(i.limit, 5)
 
@@ -105,7 +105,7 @@ class languages_autocomplete(delegate.page):
 
 class works_autocomplete(autocomplete):
     path = "/works/_autocomplete"
-    fq = 'type:work'
+    fq = ('type:work',)
     fl = 'key,title,subtitle,cover_i,first_publish_year,author_name,edition_count'
     olid_suffix = 'W'
     query = 'title:"{q}"^2 OR title:({q}*)'
@@ -125,7 +125,7 @@ class works_autocomplete(autocomplete):
 
 class authors_autocomplete(autocomplete):
     path = "/authors/_autocomplete"
-    fq = 'type:author'
+    fq = ('type:author',)
     fl = 'key,name,alternate_names,birth_date,death_date,work_count,top_work,top_subjects'
     olid_suffix = 'A'
     query = 'name:({q}*) OR alternate_names:({q}*) OR name:"{q}"^2 OR alternate_names:"{q}"^2'
@@ -141,7 +141,7 @@ class authors_autocomplete(autocomplete):
 class subjects_autocomplete(autocomplete):
     # can't use /subjects/_autocomplete because the subjects endpoint = /subjects/[^/]+
     path = "/subjects_autocomplete"
-    fq = 'type:subject'
+    fq = ('type:subject',)
     fl = 'key,name,work_count'
     query = 'name:({q}*)'
     sort = 'work_count desc'
