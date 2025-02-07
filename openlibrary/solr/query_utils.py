d7124d6f16ea74ed1ec98303ddd0f6d93bd10f97
Tomas Rodriguez <tomasrodriguez@tomass-air.lan>
2025-02-06 18:35:42 -0800

    streamline code

--- openlibrary/solr/query_utils.py
+++ openlibrary/solr/query_utils.py
@@ -149,9 +149,8 @@ def fully_escape_query(query: str) -> str:
 def sanitize_solr_symbols(query: str, chars: list[str]) -> str:
     for c in chars:
         # Handles unclosed symbols (i.e. adding closing '"' for Solr Terms)
-        if c in ('"'):
-            if query.count(c) % 2 == 1:
-                query = query + f'{c}'
+        if c in ('"') and query.count(c) % 2 == 1:
+            query = query + f'{c}'
     return query
 
 def luqum_parser(query: str) -> Item:
