34e6e8863b1df3244ad0e0b57b8a8e7263c2edc8
Drini Cami <cdrini@gmail.com>
2025-02-28 19:52:28 -0500

    Add new monitoring service for prod services

---
+++ scripts/monitoring/monitor.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+"""
+Defines various monitoring jobs, that check the health of the system.
+"""
+
+import os
+
+from scripts.monitoring.utils import OlBlockingScheduler, bash_run, limit_server
+
+HOST = os.getenv("HOSTNAME")  # eg "ol-www0.us.archive.org"
+
+if not HOST:
+    raise ValueError("HOSTNAME environment variable not set.")
+
+SERVER = HOST.split(".")[0]  # eg "ol-www0"
+scheduler = OlBlockingScheduler()
+
+
+@limit_server(["ol-web*", "ol-covers0"], scheduler)
+@scheduler.scheduled_job('interval', seconds=60)
+def log_workers_cur_fn():
+    """Logs the state of the gunicorn workers."""
+    bash_run(f"log_workers_cur_fn stats.{SERVER}.workers.cur_fn", sources=["utils.sh"])
+
+
+@limit_server(["ol-www0", "ol-covers0"], scheduler)
+@scheduler.scheduled_job('interval', seconds=60)
+def log_recent_bot_traffic():
+    """Logs the state of the gunicorn workers."""
+    match SERVER:
+        case "ol-www0":
+            bucket = "ol"
+            container = "openlibrary-web_nginx-1"
+        case "ol-covers0":
+            bucket = "ol-covers"
+            container = "openlibrary-covers_nginx-1"
+        case _:
+            raise ValueError(f"Unknown server: {SERVER}")
+
+    bash_run(
+        f"log_recent_bot_traffic stats.{bucket}.bot_traffic {container}",
+        sources=["utils.sh"],
+    )
+
+
+@limit_server(["ol-www0", "ol-covers0"], scheduler)
+@scheduler.scheduled_job('interval', seconds=60)
+def log_recent_http_statuses():
+    """Logs the recent HTTP statuses."""
+    match SERVER:
+        case "ol-www0":
+            bucket = "ol"
+            container = "openlibrary-web_nginx-1"
+        case "ol-covers0":
+            bucket = "ol-covers"
+            container = "openlibrary-covers_nginx-1"
+        case _:
+            raise ValueError(f"Unknown server: {SERVER}")
+
+    bash_run(
+        f"log_recent_http_statuses stats.{bucket}.http_status {container}",
+        sources=["utils.sh"],
+    )
+
+
+# Print out all jobs
+jobs = scheduler.get_jobs()
+print(f"{len(jobs)} job(s) registered:", flush=True)
+for job in jobs:
+    print(job, flush=True)
+
+# Start the scheduler
+print(f"Monitoring started ({HOST})", flush=True)
+try:
+    scheduler.start()
+except (KeyboardInterrupt, SystemExit):
+    scheduler.shutdown()
