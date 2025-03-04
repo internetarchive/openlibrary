fbc00f6063770de7aae5e013ff6f73d493146562
Mek <michael.karpeles@gmail.com>
2025-03-04 10:31:48 -0800

    add back thrillers

--- openlibrary/plugins/openlibrary/tests/test_home.py
+++ openlibrary/plugins/openlibrary/tests/test_home.py
@@ -127,6 +127,7 @@ class TestHomeTemplates:
             "Books We Love",
             "Recently Returned",
             "Kids",
+            "Thrillers",
             "Romance",
             "Textbooks",
         ]
