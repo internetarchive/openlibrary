62ff96faa4d8a1428a2822be14413c4fa5745348
QuangPhung15 <106754129+QuangPhung15@users.noreply.github.com>
2025-03-12 15:10:46 -0400

    Initialize archive_book.py to capture 3K5 books to Wayback machine

---
+++ scripts/archive_book.py
@@ -0,0 +1,29 @@
+import requests
+from getpass import getpass
+import time
+
+# Prompt user for secrets securely
+access_key = getpass("Enter your access key: ")
+secret = getpass("Enter your secret key: ")
+
+# Define the API URL and headers
+api_url = "https://web.archive.org/save"
+headers = {
+    "Accept": "application/json",
+    "Authorization": f"LOW {access_key}:{secret}"
+}
+
+def archive_book(url, capture_all=True):
+    data = {
+      "url": url, # <-- our url here
+      "if_not_archived_within": "1m",
+    }
+
+    if capture_all and not url.lower().endswith(".pdf"):
+        # we don't want to waste a capture_all request if it's just 1 doc
+        data["capture_all"] = "1"
+
+    return requests.post(api_url, headers=headers, data=data)
+
+# response = archive_book("")
+# print(response.json())
