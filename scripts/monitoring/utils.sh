f828f4c94294c3a5ea37b584427fb1d822bc2925
Drini Cami <cdrini@gmail.com>
2025-03-18 07:01:10 -0400

    Add top ip monitoring

--- scripts/monitoring/utils.sh
+++ scripts/monitoring/utils.sh
@@ -108,3 +108,33 @@ log_recent_http_statuses() {
     done <<< "$HTTP_STATUS_COUNTS"
 }
 export -f log_recent_http_statuses
+
+log_top_ip_counts() {
+    BUCKET="$1"
+    CONTAINER_NAME="$2"
+
+    # Get top IP counts in the last 17500 requests
+    # (As of 2024-01-08, that's about how many requests we have a minute)
+    TOP_IP_COUNTS=$(
+        docker exec -i "$CONTAINER_NAME" tail -n 17500 /var/log/nginx/access.log | \
+        grep -oE '^[^ ]+' | \
+        sort | uniq -c | sort -rn | \
+        head -n 25 | \
+        awk '{print $1}'
+    )
+    # Output like this before the last awk:
+    #  182089 0.125.240.191
+    #  181874 0.36.168.144
+    #  181779 0.198.202.145
+    #  181093 0.233.200.251
+
+    # Iterate over, and log in grafana to bucket `ol.stats.top_ip_1`, `ol.stats.top_ip_2`, etc.
+    i=1
+    for count in $TOP_IP_COUNTS; do
+        graphite_event="$BUCKET.ip_$(printf "%02d" $i) $count $(date +%s)"
+        echo $graphite_event
+        echo $graphite_event | nc -q0 graphite.us.archive.org 2003
+        i=$((i+1))
+    done
+}
+export -f log_top_ip_counts
