202c853e0235934e3693cf35551bebcb7841221a
QuangPhung15 <106754129+QuangPhung15@users.noreply.github.com>
2025-04-12 12:59:45 -0400

    Initialize archive_books.py file to save fpb books' links to Wayback Machine using SPN2

---
+++ scripts/providers/EbookFoundationFPB/archive_fpb.py
@@ -0,0 +1,44 @@
+import os
+import requests
+from contextlib import redirect_stdout
+from openlibrary.config import load_config
+
+
+def format_archive_book_request(
+    url,
+    capture_all: bool = True,
+    capture_outlinks: bool = True,
+    capture_screenshot: bool = True,
+) -> dict[str, str]:
+    data = {
+        "url": url,  # <-- our url here
+        "if_not_archived_within": "1m",
+    }
+
+    if capture_all and not url.lower().endswith(".pdf"):
+        # we don't want to waste a capture_all request if it's just 1 doc
+        data["capture_all"] = "1"
+
+    if capture_outlinks:
+        data["capture_outlinks"] = "1"
+
+    if capture_screenshot:
+        data["capture_screenshot"] = "1"
+
+    return data
+
+
+def post_data(headers: dict[str, str], data: dict) -> dict:
+    api_url = "https://web.archive.org/save"
+    return requests.post(api_url, headers=headers, data=data)
+
+
+if __name__ == "__main__":
+    ol_config = os.getenv("OL_CONFIG")
+
+    if ol_config:
+        with open(os.devnull, 'w') as devnull, redirect_stdout(devnull):
+            load_config(ol_config)
+
+    data = format_archive_book_request("")
+    post_data({}, data)
