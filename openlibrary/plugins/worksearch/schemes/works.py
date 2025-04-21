36fcbe5894a65ba62ca795a8c7948bcdf27f15b9
ananyakaligal <ananyask.btech22@rvu.edu.in>
2025-04-21 23:22:39 +0530

    RUF012

--- openlibrary/plugins/worksearch/schemes/works.py
+++ openlibrary/plugins/worksearch/schemes/works.py
@@ -6,10 +6,10 @@ from copy import deepcopy
 from datetime import datetime
 from typing import Any, cast
 
+import infogami
 import luqum.tree
 import web
 
-import infogami
 from openlibrary.plugins.upstream.utils import convert_iso_to_marc
 from openlibrary.plugins.worksearch.schemes import SearchScheme
 from openlibrary.solr.query_utils import (
@@ -39,8 +39,10 @@ re_author_key = re.compile(r'(OL\d+A)')
 
 
 class WorkSearchScheme(SearchScheme):
-    universe = ['type:work']
-    all_fields = {
+    def __init__(self):
+        super().__init__()
+        self.universe = ['type:work']
+        self.all_fields = {
         "key",
         "redirects",
         "title",
@@ -96,11 +98,11 @@ class WorkSearchScheme(SearchScheme):
         "ddc_sort",
         "osp_count",
     }
-    non_solr_fields = {
+        self.non_solr_fields = {
         'description',
         'providers',
     }
-    facet_fields = {
+        self.facet_fields = {
         "has_fulltext",
         "author_facet",
         "language",
@@ -112,7 +114,7 @@ class WorkSearchScheme(SearchScheme):
         "time_facet",
         "public_scan_b",
     }
-    field_name_map = {
+        self.field_name_map = {
         'author': 'author_name',
         'authors': 'author_name',
         'by': 'author_name',
@@ -127,7 +129,7 @@ class WorkSearchScheme(SearchScheme):
         # plain string at the next opportunity, which will make it much more usable.
         '_ia_collection': 'ia_collection_s',
     }
-    sorts = {
+        self.sorts = {
         'editions': 'edition_count desc',
         'old': 'def(first_publish_year, 9999) asc',
         'new': 'first_publish_year desc',
@@ -166,7 +168,7 @@ class WorkSearchScheme(SearchScheme):
         'random.hourly': lambda: f'random_{datetime.now():%Y%m%dT%H} asc',
         'random.daily': lambda: f'random_{datetime.now():%Y%m%d} asc',
     }
-    default_fetched_fields = {
+        self.default_fetched_fields = {
         'key',
         'author_name',
         'author_key',
@@ -193,7 +195,7 @@ class WorkSearchScheme(SearchScheme):
         'id_cita_press',
         'id_wikisource',
     }
-    facet_rewrites = {
+        self.facet_rewrites = {
         ('public_scan', 'true'): 'ebook_access:public',
         ('public_scan', 'false'): '-ebook_access:public',
         ('print_disabled', 'true'): 'ebook_access:printdisabled',
