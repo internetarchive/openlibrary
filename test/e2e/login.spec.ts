565d2cb0769c5ee201a162531e7b609fb3d1810e
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-04-30 09:43:09 +0545

    add env sample file
    Signed-off-by: Ashim-Stha <ashimshrestha2384@gmail.com>

--- test/e2e/login.spec.ts
+++ test/e2e/login.spec.ts
@@ -4,11 +4,12 @@ import { base_url, invalidEmail, validEmail, validPassword } from "../helper-con
 
 test.beforeEach(async ({ page }) => {
   await page.goto(base_url);
+  const loginPage = new LoginPage({ page });
+  await loginPage.navigateToLoginPage();
 });
 
 test("login with valid credentials", async ({ page }) => {
   const loginPage = new LoginPage({ page });
-  await loginPage.navigateToLoginPage();
   await loginPage.login(validEmail, validPassword);
   await page.getByAltText("My account").click();
   await expect(page.getByRole("button", { name: "Log out" })).toBeVisible();
@@ -16,7 +17,6 @@ test("login with valid credentials", async ({ page }) => {
 
 test("login with invalid credentials", async ({ page }) => {
   const loginPage = new LoginPage({ page });
-  await loginPage.navigateToLoginPage();
   await loginPage.login(invalidEmail, "");
   const validationMessage = await page
     .locator("input#username")
@@ -24,5 +24,5 @@ test("login with invalid credentials", async ({ page }) => {
       const input = element as HTMLInputElement;
       return input.validationMessage;
     });
-  await expect(validationMessage).toContain(loginPage.errorMessage);
+  expect(validationMessage).toContain(loginPage.errorMessage);
 });
--- test/helper-config.ts
+++ test/helper-config.ts
@@ -1,11 +1,5 @@
-export const base_url: string = process.env.BASE_URL ?? " ";
-export const validEmail: string = process.env.EMAIL ?? " ";
-export const validPassword: string = process.env.PASSWORD
-  ? process.env.PASSWORD
-  : " ";
-export const invalidEmail: string = process.env.INVALID_EMAIL
-  ? process.env.INVALID_EMAIL
-  : " ";
-export const loginUrl: string = process.env.LOGIN_URL
-  ? process.env.LOGIN_URL
-  : " ";
+export const base_url: string = process.env.BASE_URL ?? "http://localhost:8080/";
+export const validEmail: string = process.env.EMAIL ?? "hello@gmail.com";
+export const validPassword: string = process.env.PASSWORD ?? "password";
+export const invalidEmail: string = process.env.INVALID_EMAIL ?? "invalid";
+export const loginUrl: string = process.env.LOGIN_URL ?? "http://localhost:8080/account/login";
