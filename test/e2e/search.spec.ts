d4aac2f4c950af0a61ec001dca76bf264a8887e6
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-05-06 15:37:19 +0545

    test: search book

---
+++ test/e2e/search.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+import { BASE_URL } from "../helper-config";
+import { SearchPage } from "../pages/SearchPage";
+
+const existingBook: string = "Luffy";
+const nonexistingBook: string = "hfur";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(BASE_URL);
+});
+
+test("search existing book", async ({ page }) => {
+  const searchPage = new SearchPage({ page });
+  await searchPage.search(existingBook);
+  await expect(searchPage.getResults()).toContainText(existingBook);
+});
+
+test("search non-existing book", async ({ page }) => {
+  const searchPage = new SearchPage({ page });
+  await searchPage.search(nonexistingBook);
+  await expect(
+   searchPage.getSearchNotMatchText()
+  ).toBeVisible();
+});
