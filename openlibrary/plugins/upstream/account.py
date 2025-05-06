6689260a2cdc3dab982071b54aa1e92aa62be712
jachamp <28732543+jimchamp@users.noreply.github.com>
2025-05-05 17:36:57 -0700

    Add informational flash message to `pd` requesters on initial login

--- openlibrary/plugins/upstream/account.py
+++ openlibrary/plugins/upstream/account.py
@@ -436,6 +436,7 @@ class account_login_json(delegate.page):
                         ol_account, get_pd_org(web.cookies().get("pda"))
                     )
                     _handle_pd_cookies(ol_account)
+                    add_flash_message("info", _("Thank you for registering an Open Library account and requesting special print disability access. You should receive an email detailing next steps in the process."))
 
         # Fallback to infogami user/pass
         else:
@@ -530,6 +531,7 @@ class account_login(delegate.page):
                     ol_account, get_pd_org(web.cookies().get("pda"))
                 )
                 _handle_pd_cookies(ol_account)
+                add_flash_message("info", _("Thank you for registering an Open Library account and requesting special print disability access. You should receive an email detailing next steps in the process."))
 
         blacklist = [
             "/account/login",
