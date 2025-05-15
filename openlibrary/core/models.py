8891d068b5acf93d1e3f1bf4e0985794dddd11c6
Raymond Berger <RayBB@users.noreply.github.com>
2025-05-15 14:40:50 -0700

    Update openlibrary/core/models.py

--- openlibrary/core/models.py
+++ openlibrary/core/models.py
@@ -76,7 +76,7 @@ class Image:
 
     def get_aspect_ratio(self) -> float | None:
         info = self.info()
-        if info and item.get('width') and item.get('height'):
+        if info and info.get('width') and info.get('height'):
             return info["width"] / info["height"]
         return None
 
