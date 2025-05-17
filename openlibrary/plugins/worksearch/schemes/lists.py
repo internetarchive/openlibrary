f815c9c143c56a19b2e3a38698cb3cc7044825a5
RayBB <RayBB@users.noreply.github.com>
2025-05-17 11:23:44 -0700

    fix a few new ruff rule breakages

--- openlibrary/plugins/worksearch/schemes/lists.py
+++ openlibrary/plugins/worksearch/schemes/lists.py
@@ -9,44 +9,45 @@ logger = logging.getLogger("openlibrary.worksearch")
 
 # define a search scheme for lists, similar to SubjectSearchScheme
 class ListSearchScheme(SearchScheme):
-    universe = ['type:list']  # this search only applies to list type documents
-    all_fields = {
-        'key',  # unique identifier for the list
-        'name',  # name/title of the list
-        'seed',
-        'subject',
-        'subject_key',
-        'person',
-        'person_key',
-        'place',
-        'place_key',
-        'time',
-        'time_key',
-    }
+    def __init__(self):
+        super().__init__()
+        self.universe = ['type:list']  # this search only applies to list type documents
+        self.all_fields = {
+            'key',  # unique identifier for the list
+            'name',  # name/title of the list
+            'seed',
+            'subject',
+            'subject_key',
+            'person',
+            'person_key',
+            'place',
+            'place_key',
+            'time',
+            'time_key',
+        }
 
-    # kept the same form SubjectSearchScheme
-    non_solr_fields: set[str] = {
-        'description',  # short description of the list
-    }
-    facet_fields: set[str] = set()
-    field_name_map: dict[str, str] = {}
+        self.non_solr_fields = {
+            'description',  # short description of the list
+        }
+        self.facet_fields = set()
+        self.field_name_map = {}
 
-    sorts = {
-        'name asc': 'name asc',  # sort alphabetically
-        # Random (kept from SubjectSearchScheme)
-        'random': 'random_1 asc',
-        'random asc': 'random_1 asc',
-        'random desc': 'random_1 desc',
-        'random.hourly': lambda: f'random_{datetime.now():%Y%m%dT%H} asc',
-        'random.daily': lambda: f'random_{datetime.now():%Y%m%d} asc',
-    }
-    default_fetched_fields = {
-        'key',
-        'name',
-    }
+        self.sorts = {
+            'name asc': 'name asc',  # sort alphabetically
+            # Random (kept from SubjectSearchScheme)
+            'random': 'random_1 asc',
+            'random asc': 'random_1 asc',
+            'random desc': 'random_1 desc',
+            'random.hourly': lambda: f'random_{datetime.now():%Y%m%dT%H} asc',
+            'random.daily': lambda: f'random_{datetime.now():%Y%m%d} asc',
+        }
+        self.default_fetched_fields = {
+            'key',
+            'name',
+        }
 
-    # kept from SubjectSearchScheme for rewriting facet values (not used in this case)
-    facet_rewrites: dict[tuple[str, str], str | Callable[[], str]] = {}
+        # kept from SubjectSearchScheme for rewriting facet values (not used in this case)
+        self.facet_rewrites: dict[tuple[str, str], str | Callable[[], str]] = {}
 
     # converts user search query into a Solr-compatible query
     def q_to_solr_params(
