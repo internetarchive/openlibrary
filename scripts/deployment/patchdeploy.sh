ce7e398cec5edbffc211fde4a10b32b38a267598
Michael E. Karpeles (Mek) <michael.karpeles@gmail.com>
2025-05-23 15:14:31 -0400

    variable servers

--- scripts/deployment/patchdeploy.sh
+++ scripts/deployment/patchdeploy.sh
@@ -12,46 +12,47 @@ PR_NUMBER=$1
 PATCH_URL="https://patch-diff.githubusercontent.com/raw/internetarchive/openlibrary/pull/${PR_NUMBER}.diff"
 echo "Note: Patch Deploys cannot rebuild js/css"
 
-read -p "Enter SSH username (or press Enter to use current user): " SSH_USER
-
-# Default to current logged-in user if no input is given
-SSH_USER=${SSH_USER:-$USER}
-
 # Iterate over hosts
-HOSTS=("ol-web0" "ol-web1" "ol-web2" "ol-www0")
-
-for host in "${HOSTS[@]}"; do
+SERVERS=${SERVERS:-"ol-web0 ol-web1 ol-web1 ol-www0"}
+PROXY="http://http-proxy.us.archive.org:8080"
+for host in $SERVERS; do
     echo "Applying patch on ${host}.us.archive.org..."
 
     # Determine container name based on host
     if [[ "$host" == "ol-www0" ]]; then
         CONTAINER="openlibrary-web_nginx-1"
+        ssh -T "${host}.us.archive.org" <<EOF
+        set-e  # Exit immediately if any command fails
+
+        cd /opt/openlibrary
+        export HTTPS_PROXY=${PROXY}
+
+        if curl -sSL --compressed "${PATCH_URL}" | tee >(cat) | tee >(git apply --check) | git apply; then
+          echo "Patch is applicable, applying..."
+          echo "Patch applied successfully, restarting ${CONTAINER} on ${host}."
+          docker restart ${CONTAINER}
+        else
+          echo "Patch already applied or not applicable cleanly — skipping patch on ${host}."
+        fi
+EOF
     else
         CONTAINER="openlibrary-web-1"
+        ssh ${host}.us.archive.org <<EOF
+        set -e  # Exit immediately if any command fails
+        PATCH_CMD='docker exec -i ${container} openlibrary-web-1 bash -c "HTTPS_PROXY=${PROXY} curl -sS ${PATCH_URL} | git apply"'
+        if eval "\$PATCH_CMD"; then
+            echo "Patch applied successfully on ${host}, restarting container..."
+            docker restart ${CONTAINER}
+        else
+            echo "Failed to apply patch on ${host}. Exiting..."
+            exit 1
+        fi
+EOF
     fi
 
-    ssh -T "${SSH_USER}@${host}.us.archive.org" <<EOF
-    set -e  # Exit immediately if any command fails
-
-    cd /opt/openlibrary
-    export HTTPS_PROXY=http://http-proxy.us.archive.org:8080
-
-    if curl -sSL --compressed "${PATCH_URL}" | tee >(cat) | tee >(git apply --check) | git apply; then
-        echo "Patch is applicable, applying..."
-        echo "Patch applied successfully, restarting ${CONTAINER} on ${host}."
-        docker restart ${CONTAINER}
-    else
-        echo "Patch already applied or not applicable cleanly — skipping patch on ${host}."
-    fi
     echo "# ================="
     echo "# FINISHING ${host}"
     echo "# ================="
-EOF
-
-    if [ $? -ne 0 ]; then
-        echo "Error encountered on ${host}. Stopping execution."
-        exit 1
-    fi
 
 done
 
