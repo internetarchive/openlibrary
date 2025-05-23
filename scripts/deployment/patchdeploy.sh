536854753d0721ee241159c9e300913633a15a6d
Mek <michael.karpeles@gmail.com>
2025-05-23 15:43:40 -0400

    updating patch deploy script (#10775)
    * updating patch deploy script

--- scripts/deployment/patchdeploy.sh
+++ scripts/deployment/patchdeploy.sh
@@ -11,25 +11,40 @@ fi
 PR_NUMBER=$1
 PATCH_URL="https://patch-diff.githubusercontent.com/raw/internetarchive/openlibrary/pull/${PR_NUMBER}.diff"
 echo "Note: Patch Deploys cannot rebuild js/css"
-# Iterate over webnode_id [0, 1, 2]
-for webnode_id in {0..2}; do
-    echo "Applying patch on ol-web${webnode_id}.us.archive.org..."
 
-    ssh mek@ol-web${webnode_id}.us.archive.org <<EOF
-        set -e  # Exit immediately if any command fails
-        PATCH_CMD='docker exec -i openlibrary-web-1 bash -c "HTTPS_PROXY=http://http-proxy.us.archive.org:8080 curl -sS ${PATCH_URL} | git apply"'
+# Iterate over hosts
+SERVERS=${SERVERS:-"ol-web0 ol-web1 ol-web1 ol-www0"}
 
-        if eval "\$PATCH_CMD"; then
-            echo "Patch applied successfully on ol-web${webnode_id}, restarting container..."
-            docker restart openlibrary-web-1
-        else
-            echo "Failed to apply patch on ol-web${webnode_id}. Exiting..."
-            exit 1
-        fi
+for host in $SERVERS; do
+    echo "Applying patch on ${host}.us.archive.org..."
+
+    # Determine container name based on host
+    if [[ "$host" == "ol-www0" ]]; then
+        CONTAINER="openlibrary-web_nginx-1"
+    else
+        CONTAINER="openlibrary-web-1"
+    fi
+
+    ssh -T "${host}.us.archive.org" <<EOF
+    set -e  # Exit immediately if any command fails
+
+    cd /opt/openlibrary
+    export HTTPS_PROXY=http://http-proxy.us.archive.org:8080
+
+    if curl -sSL --compressed "${PATCH_URL}" | tee >(cat) | tee >(git apply --check) | git apply; then
+        echo "Patch is applicable, applying..."
+        echo "Patch applied successfully, restarting ${CONTAINER} on ${host}."
+        docker restart ${CONTAINER}
+    else
+        echo "Patch already applied or not applicable cleanly — skipping patch on ${host}."
+    fi
+    echo "# ================="
+    echo "# FINISHING ${host}"
+    echo "# ================="
 EOF
 
     if [ $? -ne 0 ]; then
-        echo "Error encountered on ol-web${webnode_id}. Stopping execution."
+        echo "Error encountered on ${host}. Stopping execution."
         exit 1
     fi
 
