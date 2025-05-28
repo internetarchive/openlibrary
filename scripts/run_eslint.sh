8724f0928ec6a379c4b00c83ac8c2918c1a69623
RayBB <RayBB@users.noreply.github.com>
2025-05-27 23:57:52 -0700

    npm i what we need

--- scripts/run_eslint.sh
+++ scripts/run_eslint.sh
@@ -3,5 +3,5 @@
 # It seems running eslint from a bash script is the only way to have it work in pre-commit for v9.
 
 
-npm i
+npm i eslint@9.27.0 eslint-plugin-no-jquery@3.1.1 eslint-plugin-vue@10.1.0 @babel/eslint-parser@7.27.1 @babel/plugin-syntax-dynamic-import@7.8.3 @babel/preset-env@7.24.7
 npx eslint --fix $1
