09b6b5e1f0164073ff4628bf9dab9934dbca9a90
Drini Cami <cdrini@gmail.com>
2025-06-04 20:34:49 +0200

    add test to validate template files are compileable (#2900)

---
+++ openlibrary/tests/test_templates.py
@@ -0,0 +1,34 @@
+import glob
+from pathlib import Path
+from tokenize import TokenError
+
+import pytest
+from web.template import Template
+
+
+def get_template_filenames():
+    """
+    Returns a list of template filenames that are valid and can be parsed.
+    """
+
+    template_files = glob.glob('openlibrary/templates/**/*.html', recursive=True)
+    template_files += glob.glob('openlibrary/macros/**/*.html', recursive=True)
+    return map(Path, template_files)
+
+
+def try_parse_template(
+    template_text: str, filename: Path
+) -> tuple[bool, str | Exception | None]:
+    try:
+        Template(template_text, str(filename))
+        return True, None
+    except SyntaxError as e:
+        return False, f"{e.args[0]} - {e.lineno}:{e.offset}"
+    except TokenError as e:
+        return False, e
+
+
+@pytest.mark.parametrize('filename', get_template_filenames(), ids=str)
+def test_valid_template(filename: Path):
+    parsed, err = try_parse_template(filename.read_text(encoding='utf-8'), filename)
+    assert parsed, err
