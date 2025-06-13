e59c39e82799cf1cf7c6544c2b034bf1991c839d
jachamp <28732543+jimchamp@users.noreply.github.com>
2025-06-13 16:22:07 -0700

    Add script to delete `merge_authors_debug` store entries

---
+++ scripts/delete_merge_debug.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Deletes all store entries with type `merge-authors-debug`.
+"""
+import argparse
+import time
+import web
+from pathlib import Path
+
+import infogami
+from openlibrary.config import load_config
+
+DEFAULT_CONFIG_PATH = "/opt/olsystem/etc/openlibrary.yml"
+RECORD_TYPE = "merge-authors-debug"
+
+
+def setup(config_path):
+    if not Path(config_path).exists():
+        raise FileNotFoundError(f'no config file at {config_path}')
+    load_config(config_path)
+    infogami._setup()
+
+
+def delete_records():
+    while keys := web.ctx.site.store.keys(type=RECORD_TYPE):
+        for key in keys:
+            web.ctx.site.store.delete(key)
+            time.sleep(0.5)
+
+
+def main(args):
+    setup(args.config)
+    delete_records()
+
+
+def _parse_args():
+    _parser = argparse.ArgumentParser(description=__doc__)
+    _parser.add_argument(
+        "-c",
+        "--config",
+        default=DEFAULT_CONFIG_PATH,
+        help="Path to the `openlibrary.yml` configuration file",
+    )
+    _parser.set_defaults(func=main)
+    return _parser.parse_args()
+
+if __name__ == '__main__':
+    _args = _parse_args()
+    _args.func(_args)
