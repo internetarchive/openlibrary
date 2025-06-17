ec9e55804b538554429498c323c79cc1fc89d997
Drini Cami <cdrini@gmail.com>
2025-06-18 01:53:20 +0200

    Add trending sort to LibraryExplorer

--- openlibrary/components/LibraryExplorer/components/LibraryToolbar.vue
+++ openlibrary/components/LibraryExplorer/components/LibraryToolbar.vue
@@ -260,6 +260,20 @@
                   value="readinglog"
                 >Reading Log
               </label>
+              <label>
+                <input
+                  v-model="sortState.order"
+                  type="radio"
+                  value="trending"
+                >Trending
+              </label>
+              <label>
+                <input
+                  v-model="sortState.order"
+                  type="radio"
+                  value="daily"
+                >Daily Reading Log
+              </label>
               <label title="I.e. Classification order. Note some books maybe missing when sorting by shelf order–we're working on it.">
                 <input
                   v-model="sortState.order"
