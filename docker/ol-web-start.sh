dc818b85ce1962407ad2d55c3b0bf9fd743b45c7
RayBB <RayBB@users.noreply.github.com>
2025-06-19 17:23:26 -0700

    fix import issue

--- docker/ol-web-start.sh
+++ docker/ol-web-start.sh
@@ -7,7 +7,7 @@ if [ -n "$BEFORE_START" ] ; then
   $BEFORE_START
 fi
 
-exec scripts/openlibrary-server "$OL_CONFIG" \
+PYTHONPATH=. exec scripts/openlibrary-server "$OL_CONFIG" \
   --gunicorn \
   $GUNICORN_OPTS \
   --bind :8080
