932c819607482028f6d0168007f70f5aad597c5e
Drini Cami <cdrini@gmail.com>
2025-06-24 17:47:13 +0200

    Experiment with including page views in trending score calculation

--- scripts/solr_updater/trending_updater_hourly.py
+++ scripts/solr_updater/trending_updater_hourly.py
@@ -1,5 +1,13 @@
 import datetime
+import itertools
+import json
+import logging
+import subprocess
+from collections import Counter
+from dataclasses import dataclass
 from math import sqrt
+
+import requests
 
 from openlibrary.config import load_config
 from openlibrary.core import db
@@ -11,6 +19,8 @@ from scripts.solr_builder.solr_builder.fn_to_cli import FnToCLI
 # ignored, and the current count of bookshelves events in the last hour is the new trending_score_hourly[0]
 # value, with each other value incrementing up by one.
 
+logger = logging.getLogger("trending-updater")
+
 # Trending_score_daily values are all fetched in order to calculate their mean and standard deviation.
 # The formula for the new z-score is [ z = sum(trending_score_hourly_*) - mean(trending_score_daily_*) /  (standard_deviation(trending_score_daily)] + 1).
 # The incrementation of the denominator by 1 serves several purposes:
@@ -20,119 +30,276 @@ from scripts.solr_builder.solr_builder.fn_to_cli import FnToCLI
 #         0 events to 1 event.
 
 
-def fetch_works_trending_scores(current_hour: int, timestamp: datetime.datetime):
+@dataclass
+class LogLine:
+    ip: str
+    url: str
+    page_key: str
+
+    @staticmethod
+    def from_line(line: str) -> 'LogLine':
+        parts = line.split(' ')
+        return LogLine(
+            ip=parts[0],
+            url=parts[6],
+            page_key='/'.join(parts[6].split('/', 3)[0:3]),
+        )
+
+
+def get_logs_for_hour(dt: datetime.datetime, extra_grep: str | None = None):
+    start_ts = dt.replace(minute=0, second=0, microsecond=0).timestamp() * 1000
+    end_ts = start_ts + 3600 * 1000 - 1
+
+    if extra_grep:
+        extra_grep = f'| {extra_grep}'
+
+    # Note we use pretty aggressive filters to try to limit to non-automated traffic. Notably:
+    # - Exclude out known bots as defined in scripts/obfi.sh
+    # - Exclude HTTP/1.1 requests, which are likely from bots or automated scripts
+    # - Exclude requests that don't have a referrer, which is likely a bot
+    #
+    # This yields ~4-6k requests per hour, which is a reasonable number to process
+    with subprocess.Popen(
+        [
+            "bash",
+            "-c",
+            f"""
+                source scripts/obfi.sh && \
+                obfi_range {start_ts} {end_ts} \
+                    | obfi_grep_bots -v \
+                    | obfi_grep_secondary_reqs -v \
+                    | grep -E 'GET /(works|books)/OL' \
+                    | grep -F ' 200 ' \
+                    | grep -F 'HTTP/2.' \
+                    | grep -vF ' "-" ' \
+                    | grep -vE '\\.(opds|json)' \
+                    {extra_grep if extra_grep else ''}
+            """,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+        text=True,
+    ) as proc:
+        assert proc.stdout
+        for line in proc.stdout:
+            line = line.strip()
+            if not line:
+                continue
+
+            yield line
+
+
+def fetch_work_hour_pageviews(dt: datetime.datetime) -> Counter[str]:
+    """
+    Iterate over the nginx logs of the given hour and count the number of pageviews
+    """
+
+    # We will need to fetch the work keys for these
+    book_keys = set(get_logs_for_hour(dt, 'grep -oEF "GET /books/OL[0-9]+M"'))
+
+    logger.info(f"Found {len(book_keys)} /book pageviews in the last hour")
+
+    book_key_to_work_key: dict[str, str] = {}
+
+    session = requests.Session()
+    session.headers.update({'User-Agent': 'OpenLibrary Trending Updater'})
+    # The API doesn't support POST requests, so need to use smaller batches
+    # to avoid hitting the URL length limit.
+    for batch in itertools.batched(book_keys, 100):
+        resp = session.get(
+            'https://openlibrary.org/query.json',
+            params={
+                'query': json.dumps(
+                    {
+                        'type': '/type/edition',
+                        'key': batch,
+                        'works': None,
+                    }
+                ),
+            },
+        )
+        resp.raise_for_status()
+        data = resp.json()
+        for edition in data:
+            book_key_to_work_key[edition['key']] = edition['works'][0]['key']
+
+    # Will want to dedupe by IP and work key, so we can count unique pageviews per work.
+    # Each tuple is (ip, work_key).
+    req_set: set[tuple[str, str]] = set()
+
+    for line in get_logs_for_hour(dt):
+        line = LogLine.from_line(line)
+        page_key = line.page_key
+        if page_key.startswith('/books/'):
+            work_key = book_key_to_work_key.get(page_key, '')
+            if not work_key:
+                logger.warning(
+                    f"Could not find work key for book page {page_key}, skipping"
+                )
+                continue
+
+            req_set.add((line.ip, work_key))
+        else:
+            req_set.add((line.ip, page_key))
+
+    return Counter(work_key for _, work_key in req_set)
+
+
+def fetch_work_hour_book_events(dt: datetime.datetime) -> dict[str, int]:
+    start = dt.replace(minute=0, second=0, microsecond=0)
+    end_ts = start + datetime.timedelta(hours=1)
+
     ol_db = db.get_db()
-    # Use the provided timestamp for the time window
     query = f"""
         SELECT work_id, COUNT(updated)
         FROM bookshelves_books
-        WHERE updated >= '{timestamp - datetime.timedelta(hours=1)}' AND updated < '{timestamp}'
+        WHERE updated >= '{start.isoformat()}' AND updated < '{end_ts.isoformat()}'
         GROUP BY work_id
     """
-    db_data = {
-        f'/works/OL{storage.work_id}W': storage.count
-        for storage in list(ol_db.query(query))
+    result = {
+        f'/works/OL{storage.work_id}W': storage.count for storage in ol_db.query(query)
     }
-    print(f"{len(db_data)} works with bookshelf books in the last hour")
+    logger.info(f"{len(result)} works with bookshelf books in the last hour")
+    return result
 
+
+def fetch_solr_trending_data(hour_slot: int, work_keys: set[str]) -> list[dict]:
+    solr_fields = (
+        "key",
+        "trending_score_hourly_sum",
+        f'trending_score_hourly_{hour_slot}',
+        "trending_score_daily_*",
+    )
+
+    # Docs with outdated values in the hour slot:
+    docs = []
     resp = execute_solr_query(
         '/export',
         {
-            "q": (
-                f'trending_score_hourly_{current_hour}:[1 TO *]'
-                + (" OR " if db_data != {} else "")
-                + (" OR ".join(["key:\"" + key + "\"" for key in db_data]))
-            ),
-            "fl": ",".join(
-                [
-                    "key",
-                    "trending_score_hourly_sum",
-                    f'trending_score_hourly_{current_hour}',
-                    "trending_score_daily_*",
-                ]
-            ),
+            "q": f'trending_score_hourly_{hour_slot}:[1 TO *]',
+            "fl": ",".join(solr_fields),
             "sort": "key asc",
         },
         _timeout=10_000,  # Increase timeout for large datasets
     )
-
     assert resp
-
     data = resp.json()
-    for doc in data["response"]["docs"]:
+    docs.extend(data["response"]["docs"])
+    old_work_keys = {doc["key"] for doc in docs}
+
+    if keys_to_fetch := work_keys - old_work_keys:
+        logger.info(f"Fetching {len(keys_to_fetch)} new works from Solr")
+        resp = execute_solr_query(
+            '/export',
+            {
+                "q": ' OR '.join(f'key:"{key}"' for key in keys_to_fetch),
+                "fl": ",".join(solr_fields),
+                "sort": "key asc",
+            },
+            _timeout=10_000,  # Increase timeout for large datasets
+        )
+        assert resp
+        data = resp.json()
+        docs.extend(data["response"]["docs"])
+
+    for doc in docs:
         # Ensure all expected fields are present, defaulting to 0 if missing
         for i in range(7):
             doc.setdefault(f"trending_score_daily_{i}", 0)
         doc.setdefault("trending_score_hourly_sum", 0)
-        doc.setdefault(f"trending_score_hourly_{current_hour}", 0)
-    return {
-        doc["key"]: {"count": db_data.get(doc["key"], 0), "solr_doc": doc}
-        for doc in data["response"]["docs"]
-    }
+        doc.setdefault(f"trending_score_hourly_{hour_slot}", 0)
+
+    return docs
 
 
 # A book must have at least this many events in the past week to be considered for trending.
-MIN_DAILY_EVENTS = 5
+MIN_PAST_SUM = 50
 
 
-def get_trending_z_score(events_today: int, past_events: list[int]) -> float:
+def compute_trending_value(pageviews_today: int, book_events_today: int) -> int:
+    return pageviews_today + book_events_today * 10
+
+
+def compute_trending_z_score(value_today: int, past_values: list[int]) -> float:
     """
     Calculate the trending z-score for the last 24h compared to the past events.
     I.e. How many standard deviations the current number of events is away from the mean
     of the past events.
     """
-    past_sum = sum(past_events)
-    if (past_sum + events_today) < MIN_DAILY_EVENTS:
+    past_sum = sum(past_values)
+    if (past_sum + value_today) < MIN_PAST_SUM:
         return 0.0
-    N = len(past_events)
+    N = len(past_values)
     past_mean = past_sum / N
-    std_dev = sqrt(sum((x - past_mean) ** 2 for x in past_events) / N)
-    return (events_today - past_mean) / (std_dev + 1.0)
+    std_dev = sqrt(sum((x - past_mean) ** 2 for x in past_values) / N)
+    return (value_today - past_mean) / (std_dev + 1.0)
 
 
-def form_inplace_updates(work_key: str, count: int, solr_doc: dict, current_hour: int):
+def form_inplace_trending_update(
+    solr_doc: dict,
+    hour_slot: int,
+    hour_value: int,
+):
     new_hourly_sum = (
         solr_doc["trending_score_hourly_sum"]
-        - solr_doc[f'trending_score_hourly_{current_hour}']
-        + count
+        - solr_doc[f'trending_score_hourly_{hour_slot}']
+        + hour_value
     )
+    new_z_score = compute_trending_z_score(
+        new_hourly_sum,
+        [solr_doc.get(f'trending_score_daily_{i}', 0) for i in range(7)],
+    )
+
     request_body = {
-        "key": work_key,
-        f'trending_score_hourly_{current_hour}': {"set": count},
+        "key": solr_doc["key"],
+        f'trending_score_hourly_{hour_slot}': {"set": hour_value},
         "trending_score_hourly_sum": {"set": new_hourly_sum},
-        "trending_z_score": {
-            "set": get_trending_z_score(
-                new_hourly_sum,
-                [solr_doc.get(f'trending_score_daily_{i}', 0) for i in range(7)],
-            )
-        },
+        "trending_z_score": {"set": new_z_score},
     }
 
     return request_body
 
 
-def main(openlibrary_yml: str, timestamp: str | None = None):
+def main(openlibrary_yml: str, timestamp: str | None = None, dry_run: bool = False):
     load_config(openlibrary_yml)
 
     if timestamp:
         ts = datetime.datetime.fromisoformat(timestamp)
     else:
-        ts = datetime.datetime.now()
-    current_hour = ts.hour
-    if work_data := fetch_works_trending_scores(current_hour, ts):
-        request_body = [
-            form_inplace_updates(
-                work_id,
-                work_data[work_id]["count"],
-                work_data[work_id]["solr_doc"],
-                current_hour,
-            )
-            for work_id in work_data
-        ]
-        print(f"{len(request_body)} works to update")
+        # Use previous hour if no timestamp is provided
+        ts = datetime.datetime.now() - datetime.timedelta(hours=1)
+
+    # The slot in solr that corresponds to the given hour
+    hour_slot = ts.hour
+
+    work_pageview = fetch_work_hour_pageviews(ts)
+    work_book_events = fetch_work_hour_book_events(ts)
+    keys_to_fetch = set(itertools.chain(work_pageview.keys(), work_book_events.keys()))
+    solr_docs = fetch_solr_trending_data(hour_slot, keys_to_fetch)
+
+    if not solr_docs:
+        logger.info("No new trending data found for the current hour.")
+        return
+
+    request_body = [
+        form_inplace_trending_update(
+            doc,
+            hour_slot,
+            compute_trending_value(
+                work_pageview.get(doc["key"], 0),
+                work_book_events.get(doc["key"], 0),
+            ),
+        )
+        for doc in solr_docs
+    ]
+    logger.info(f"{len(request_body)} works to update")
+    if dry_run:
+        logger.info("Dry run mode enabled, not sending updates to Solr.")
+    else:
         resp = get_solr().update_in_place(request_body, commit=True)
-        print(resp)
-        print("Hourly update completed.")
+        logger.info(resp)
+    logger.info("Hourly update completed.")
 
 
 if __name__ == '__main__':
