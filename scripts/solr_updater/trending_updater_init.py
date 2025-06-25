554dab6501115e486bd9054c542a338f31086674
Drini Cami <cdrini@gmail.com>
2025-06-26 01:11:57 +0200

    Add trending_update_init start timestamp option

--- scripts/solr_updater/trending_updater_init.py
+++ scripts/solr_updater/trending_updater_init.py
@@ -5,10 +5,19 @@ from scripts.solr_updater.trending_updater_daily import main as daily_main
 from scripts.solr_updater.trending_updater_hourly import main as hourly_main
 
 
-def main(openlibrary_yml: str, dry_run: bool = False):
-    now = datetime.datetime.now()
-    start = now - datetime.timedelta(days=7)
-    for day in range(8):
+def main(
+    openlibrary_yml: str,
+    timestamp: str | None = None,
+    dry_run: bool = False,
+):
+    end = datetime.datetime.now()
+    if timestamp:
+        start = datetime.datetime.fromisoformat(timestamp)
+    else:
+        start = end - datetime.timedelta(days=7)
+    days = (end - start).days
+
+    for day in range(days + 1):
         day_dt = start + datetime.timedelta(days=day)
         for hour in range(24):
             ts = day_dt.replace(hour=hour, minute=5, second=0, microsecond=0)
