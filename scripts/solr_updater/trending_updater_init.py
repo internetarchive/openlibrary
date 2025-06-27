8d10c004ca8eec31266c4db226e3527d07d53713
Drini Cami <cdrini@gmail.com>
2025-06-27 17:34:08 +0200

    [trending] fix trending init start/edge cases

--- scripts/solr_updater/trending_updater_init.py
+++ scripts/solr_updater/trending_updater_init.py
@@ -10,25 +10,48 @@ def main(
     timestamp: str | None = None,
     dry_run: bool = False,
 ):
+    """
+    Script to initialize the trending data in Solr.
+
+    Environment variables:
+
+        OL_SOLR_BASE_URL: The base URL for the Solr instance.
+
+    To run over the last 7 days:
+
+        PYTHONPATH=. python scripts/solr_updater/trending_updater_init.py /olsystem/etc/openlibrary.yml
+
+    To run starting from a specific timestamp to present:
+
+        PYTHONPATH=. python scripts/solr_updater/trending_updater_init.py /olsystem/etc/openlibrary.yml --timestamp 2025-06-24T00:05:00
+
+    :param openlibrary_yml: Path to the Open Library configuration file.
+    :param timestamp: Optional ISO format timestamp to start from. Default is 7 days ago.
+    :param dry_run: If True, will not send updates to Solr, just runs the logic and print the number of updates.
+    """
     end = datetime.datetime.now()
     if timestamp:
         start = datetime.datetime.fromisoformat(timestamp)
+        start = start.replace(minute=5, second=0, microsecond=0)
     else:
         start = end - datetime.timedelta(days=7)
-    days = (end - start).days
+        # Ensure we start at the beginning of the day
+        start = start.replace(hour=0, minute=5, second=0, microsecond=0)
 
-    for day in range(days + 1):
-        day_dt = start + datetime.timedelta(days=day)
-        for hour in range(24):
-            ts = day_dt.replace(hour=hour, minute=5, second=0, microsecond=0)
-            if ts > datetime.datetime.now():
-                print("Caught up to current time, stopping.")
-                return
-            print(f"Running hourly trending for {ts.isoformat()}")
-            hourly_main(openlibrary_yml, timestamp=ts.isoformat(), dry_run=dry_run)
+    cur_dt = start
+    while cur_dt < end:
         # After 24 hours, run daily trending for this day
-        print(f"Running daily trending for {day_dt.isoformat()}")
-        daily_main(openlibrary_yml, timestamp=day_dt.isoformat(), dry_run=dry_run)
+        if cur_dt.hour == 0:
+            day_dt = cur_dt.replace(hour=0, minute=0, second=0, microsecond=0)
+            print(f"Running daily trending for {day_dt.isoformat()}")
+            daily_main(openlibrary_yml, timestamp=day_dt.isoformat(), dry_run=dry_run)
+
+        print(f"Running hourly trending for {cur_dt.isoformat()}")
+        hourly_main(openlibrary_yml, timestamp=cur_dt.isoformat(), dry_run=dry_run)
+
+        cur_dt += datetime.timedelta(hours=1)
+
+    print("Caught up to current time, stopping.")
 
 
 if __name__ == '__main__':
