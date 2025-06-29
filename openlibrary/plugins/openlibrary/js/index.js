ddbe22cbb99c0dbfae6e9b34bae32fa226edcdf6
RayBB <RayBB@users.noreply.github.com>
2025-06-28 22:15:21 -0700

    create loading gradient file

--- openlibrary/plugins/openlibrary/js/index.js
+++ openlibrary/plugins/openlibrary/js/index.js
@@ -171,28 +171,10 @@ jQuery(function () {
             .then(module => module.initLoginForm());
     }
 
-    // TODO: put this new code somewhere
     // this creates the loading gradient for all elements with this tag
     if (document.querySelector('.loading-gradient')) {
-        document.querySelectorAll('.loading-gradient').forEach((el) => {
-            // The image element can be nested inside the loading gradient, or it can be the loading gradient itself;
-            const imgEl = el.querySelector('img') || el;
-            imgEl.addEventListener('load', () => {
-                if (imgEl.classList.contains('opacity-0')) {
-                    const now = performance.now(); // more precise than Date.now()
-                    const delay = Math.ceil(now / 500) * 500 - now; // so that images transition together every 500ms
-                    // The idea of this is to make it so they don't all start jumping into existance in close succession
-                    // If this code is overcomplicated, we can remove it. In local testing it seems a worthwhile improvement
-                    setTimeout(() => {
-                        imgEl.classList.remove('opacity-0');
-                        el.classList.remove('loading-gradient');
-                    }, delay);
-                } else {
-                    // This is the case where images don't fade in because there's no opacity-0 class
-                    el.classList.remove('loading-gradient');
-                }
-            });
-        });
+        import(/* webpackChunkName: "loading-gradient" */'./loading-gradient.js')
+            .then(module => module.initLoadingGradient());
     }
 
     // conditionally load clamping components
