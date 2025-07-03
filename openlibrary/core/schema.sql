30d94b8981c4e3d22ca1a4aebb740f82739c0b8a
pidgezero_one <s.kischak@gmail.com>
2025-07-03 13:06:10 -0400

    add semicolon

--- openlibrary/core/schema.sql
+++ openlibrary/core/schema.sql
@@ -110,7 +110,7 @@ CREATE TABLE wikidata (
     id text not null primary key,
     data json,
     updated timestamp without time zone default (current_timestamp at time zone 'utc')
-)
+);
 
 CREATE TABLE bestbooks (
     award_id serial not null primary key,
