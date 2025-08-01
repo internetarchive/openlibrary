9d52b32711eeae36aac4c43fbcbfe73c81e3288e
ragipidavid <ragipidavid@gmail.com>
2025-08-01 11:37:06 -0700

    Testing

--- openlibrary/plugins/openlibrary/js/book-page-lists.js
+++ openlibrary/plugins/openlibrary/js/book-page-lists.js
@@ -35,7 +35,7 @@ export function initListsSection(elem) {
                         }
 
                         listSection.replaceChildren(fragment)
-
+                        // console.log(listSection)
                         // Show "See All" link
                         if (data.hasLists) {
                             const showAllLink = elem.querySelector('.lists-heading a')
@@ -43,6 +43,65 @@ export function initListsSection(elem) {
                                 showAllLink.classList.remove('hidden')
                             }
                         }
+                        const followButtons = listSection.querySelectorAll('.follow-form');
+                        // console.log(followButtons)
+                        followButtons.forEach(form => {
+                            form.addEventListener('submit', async e => {
+                                e.preventDefault()
+                                // console.log('submit clicked')
+                                // const formData = new FormData(form)
+                                const stateField = elem.querySelector('input[name=state]');
+                                const state = stateField.value
+                                const publisherField = elem.querySelector('input[name=publisher]');
+                                const publisher = publisherField.value
+                                const redir_urlField = elem.querySelector('input[name=redir_url]');
+                                const redir_url = redir_urlField.value
+                                const url = elem.querySelector('form').action
+
+                                // console.log(publisher)
+                                // console.log(state)
+                                // console.log(redir_url)
+                                // console.log(url)
+
+                                const data = {
+                                    state: state,
+                                    publisher: publisher,
+                                    redir_url: redir_url,
+                                }
+                                $.ajax({
+                                    type: 'POST',
+                                    url: url,
+                                    contentType: 'application/json',
+                                    data: JSON.stringify(data),
+                                    dataType: 'json',
+                                    beforeSend: function (xhr) {
+                                        xhr.setRequestHeader('Content-Type', 'application/json');
+                                        xhr.setRequestHeader('Accept', 'application/json');
+                                    },
+                                    success: function () {
+                                        // console.log('successs!!!!')
+                                    },
+                                    error: function () {
+                                        // console.log('failed')
+                                    },
+                                    complete: function () {
+                                        // console.log('completed')
+                                    }
+                                });
+
+                            })
+                        })
+                        // followButtons.forEach(button => {
+                        //     button.addEventListener('click', () => {
+                        //         console.log(button.classList)
+                        //         if (button.classList[2].includes('cta-btn--delete')){
+                        //             console.log('contains delete')
+                        //             button.classList.remove('cta-btn--delete')
+                        //             button.classList.add('cta-btn--primary')
+                        //         }
+                        //     });
+                        // });
+
                     })
             }
         })
