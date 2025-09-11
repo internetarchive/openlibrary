de0a3c30b8b8de9e007d22de474968d650d196aa
Drini Cami <cdrini@gmail.com>
2025-09-11 22:12:03 +0200

    Add solr to py-spy monitoring

--- scripts/monitoring/utils.sh
+++ scripts/monitoring/utils.sh
@@ -62,8 +62,12 @@ log_workers_cur_fn() {
     for pid in $(ps aux | grep -E 'openlibrary-server|coverstore-server' | grep -v 'grep' | awk '{print $2}'); do
         echo "$pid $(py_spy_cur_fn $pid)";
     done 2>/dev/null \
-        | awk '{print $2}' \
-        | awk '{if ($1 ~ /^(connect|sleep|wait|getaddrinfo|create_connection|get_availability|get_api_response)$/) print $1; else print "other"}' \
+        | awk '{$1=""; print}' \
+        | awk '{
+            if ($2 ~ /solr\.py/) print "solr_py";
+            else if ($1 ~ /^(connect|sleep|wait|getaddrinfo|create_connection|get_availability|get_api_response)$/) print $1;
+            else print "other"
+        }' \
         | sort \
         | uniq -c \
         | awk -v date=$(date +%s) -v bucket="$BUCKET" '{print bucket "." $2 " " $1 " " date}' \
