c9b9ce80a946334dd2117af19fdd0a5f67950210
unknown <minhsunnybear@gmail.com>
2025-10-07 19:48:40 +0700

    complete all features for global book filter

--- openlibrary/plugins/openlibrary/js/carousel/Carousel.js
+++ openlibrary/plugins/openlibrary/js/carousel/Carousel.js
@@ -2,6 +2,7 @@
 import 'slick-carousel';
 import '../../../../../static/css/components/carousel--js.css';
 import { buildPartialsUrl } from  '../utils.js';
+import { getGlobalPreferences, mapPreferencesToBackend } from '../../../../../../openlibrary/static/js/preferences.js'
 
 /**
  * @typedef {Object} CarouselConfig
