afdeb0ed4cbbc7fa8ee2e9731f4feb8d2a5c170c
SK Akram <skcodewizard786@gmail.com>
2025-11-04 16:35:05 +0000

    refactor: update cache headers and image expiration for better caching

--- openlibrary/coverstore/code.py
+++ openlibrary/coverstore/code.py
@@ -274,8 +274,7 @@ class cover:
                 return web.notmodified()
 
             web.header('Cache-Control', 'public, max-age=31536000, immutable')
-            # this image is not going to expire in next 100 years.
-            web.expires(100 * 365 * 24 * 3600)
+            web.expires(365 * 24 * 3600)
         else:
             web.header('Cache-Control', 'public, max-age=2592000')
             web.expires(30 * 24 * 3600)
