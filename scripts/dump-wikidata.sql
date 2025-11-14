555743aee37aeef5023c42ad58458f7789d62041
krishnaShuk <shuklakrishna.kris@gmail.com>
2025-11-14 09:43:59 +0530

    fix(data): Correct wikidata dump format to raw TSV

--- scripts/dump-wikidata.sql
+++ scripts/dump-wikidata.sql
@@ -1,6 +1,6 @@
 -- Script to export Wikidata table from the Open Library database
 -- When called, this will export the entire wikidata table as a TSV file
--- Usage: psql $PSQL_PARAMS -f dump-wikidata.sql | gzip -c > ol_dump_wikidata_YYYY-MM-DD.txt.gz
+-- Usage: psql $PSQL_PARAMS -f dump-wikidata.sql | sed 's/\\\\"/\\"/g' | gzip -c > ol_dump_wikidata_YYYY-MM-DD.txt.gz
 
 -- Export all rows from the wikidata table as tab-separated values
-COPY wikidata TO STDOUT WITH (FORMAT csv, DELIMITER E'\t');
+COPY wikidata (id, updated, data) TO STDOUT;
