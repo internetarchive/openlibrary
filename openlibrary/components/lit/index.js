c411d0790f02244c4f4da12d58d4717e02be81f5
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2025-11-13 21:34:35 -0800

    feat/add-lit-web-component-support

---
+++ openlibrary/components/lit/index.js
@@ -0,0 +1,10 @@
+/**
+ * Entry point for all Open Library Lit web components
+ *
+ * This file imports and registers all Lit components.
+ * When built, this creates a single bundle (ol-components.js) that includes
+ * the Lit runtime and all components.
+ */
+
+// Import components
+import './OlHeading.js';
