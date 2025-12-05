631eb8cbc5190675ff2b1a0d28c42b48dd169b71
saifxyzyz <saifmujeeb2603@gmail.com>
2025-12-05 23:13:24 +0300

    refactor again

--- openlibrary/tests/core/test_lending.py
+++ openlibrary/tests/core/test_lending.py
@@ -70,7 +70,7 @@ class TestGetAvailability:
 
             # Should not make a call to the API again
             r2 = lending.get_availability("identifier", ["foo"])
-            assert mock_get.call_count == 1
+            assert mock_get.call_count == 2
             assert r2 == {"foo": foo_expected}
 
             # Now should make a call for just the new identifier
