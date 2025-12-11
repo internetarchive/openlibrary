98669e4be58376e3103a70eb355b97e8f0da4268
RayBB <RayBB@users.noreply.github.com>
2025-12-11 11:15:26 -0800

    feat: Add a Pydantic Language model and specify it as the response model for the /languages.json endpoint.

--- openlibrary/fastapi/languages.py
+++ openlibrary/fastapi/languages.py
@@ -3,13 +3,23 @@ from __future__ import annotations
 from typing import Literal
 
 from fastapi import APIRouter, Query, Request
+from pydantic import BaseModel
 
 from openlibrary.plugins.worksearch.languages import get_top_languages
+
+
+class Language(BaseModel):
+    name: str
+    key: str
+    marc_code: str
+    count: int
+    ebook_edition_count: int
+
 
 router = APIRouter()
 
 
-@router.get("/languages.json")
+@router.get("/languages.json", response_model=list[Language])
 async def list_languages(
     request: Request,
     sort: Literal["count", "name", "ebook_edition_count"] = Query(
