54eceac5b6b488c9a6509d786c27b912f1badd84
RayBB <RayBB@users.noreply.github.com>
2025-12-11 11:04:34 -0800

    refactor: remove redundant JSONResponse class and clarify limit parameter description.

--- openlibrary/fastapi/languages.py
+++ openlibrary/fastapi/languages.py
@@ -3,20 +3,19 @@ from __future__ import annotations
 from typing import Literal
 
 from fastapi import APIRouter, Query, Request
-from fastapi.responses import JSONResponse
 
 from openlibrary.plugins.worksearch.languages import get_top_languages
 
 router = APIRouter()
 
 
-@router.get("/languages.json", response_class=JSONResponse)
+@router.get("/languages.json")
 async def list_languages(
     request: Request,
     sort: Literal["count", "name", "ebook_edition_count"] = Query(
         "count", description="The field to sort by."
     ),
-    limit: int = Query(15, gt=0, description="The number of results to return."),
+    limit: int = Query(15, gt=0, description="The max number of results to return."),
 ):
     """
     Get a list of the top languages, sorted by the specified criteria.
