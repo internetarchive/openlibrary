9014bce1551f4e0a262ff9b5e59f2fa186a04f21
Arpit Tripathi <arpittripathiayo@gmail.com>
2026-01-07 22:04:26 +0530

    Address Copilot feedback
    - Update comment to accurately reflect trending_z_score as defined Solr field
    - Simplify redundant condition in trending badge display logic

--- openlibrary/plugins/upstream/models.py
+++ openlibrary/plugins/upstream/models.py
@@ -600,7 +600,7 @@ class Work(models.Work):
             "has_fulltext",
             "lending_edition_s",
             "public_scan_b",
-            "trending_z_score",  # Safe to add - Solr ignores unknown fields
+            "trending_z_score",  # Defined Solr field; in general, Solr ignores unknown fields
         ] + get_solr_keys()
         solr = get_solr()
         stats.begin("solr", get=self.key, fields=fields)
