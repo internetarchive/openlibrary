561be3b4beb70766922188ae4869d7361c915c0d
RayBB <RayBB@users.noreply.github.com>
2026-01-27 14:03:32 -0800

    remove outdated deprecation

--- openlibrary/plugins/worksearch/languages.py
+++ openlibrary/plugins/worksearch/languages.py
@@ -27,7 +27,6 @@ class languages(subjects.subjects):
         return "languages" in web.ctx.features
 
 
-@deprecated("migrated to fastapi")
 class languages_json(subjects.subjects_json):
     path = '(/languages/[^_][^/]*)'
     encoding = "json"
