2e8e9dfab620cc7955a601494bc9297a3733ee17
Mek <michael.karpeles@gmail.com>
2026-02-04 00:25:11 +0000

    Remove redundant tests for expensive search detection

--- openlibrary/plugins/worksearch/tests/test_verification.py
+++ openlibrary/plugins/worksearch/tests/test_verification.py
@@ -17,45 +17,6 @@ class TestSearchVerification:
         i = web.storage(q='language:eng')
         assert self.search_handler._is_expensive_search(i) is True
     
-    def test_is_expensive_search_with_wildcard(self):
-        """Test detection of wildcard queries."""
-        i = web.storage(q='*:*')
-        assert self.search_handler._is_expensive_search(i) is True
-    
-    def test_is_expensive_search_with_field_syntax(self):
-        """Test detection of field-specific syntax."""
-        i = web.storage(q='title:hobbit')
-        assert self.search_handler._is_expensive_search(i) is True
-    
-    def test_is_expensive_search_with_boolean_operator(self):
-        """Test detection of boolean operators."""
-        i = web.storage(q='test AND query')
-        assert self.search_handler._is_expensive_search(i) is True
-        
-        i = web.storage(q='test OR query')
-        assert self.search_handler._is_expensive_search(i) is True
-        
-        i = web.storage(q='test NOT query')
-        assert self.search_handler._is_expensive_search(i) is True
-    
-    def test_is_expensive_search_with_language_filter(self):
-        """Test detection of language filter parameter."""
-        i = web.storage(q='test', language=['eng'])
-        assert self.search_handler._is_expensive_search(i) is True
-    
-    def test_is_expensive_search_with_range_query(self):
-        """Test detection of range queries."""
-        i = web.storage(q='year:[1900 TO 2000]')
-        assert self.search_handler._is_expensive_search(i) is True
-        
-        i = web.storage(q='year:{1900 TO 2000}')
-        assert self.search_handler._is_expensive_search(i) is True
-    
-    def test_is_expensive_search_simple_query(self):
-        """Test that simple queries are not marked as expensive."""
-        i = web.storage(q='the hobbit')
-        assert self.search_handler._is_expensive_search(i) is False
-    
     def test_is_expensive_search_empty_query(self):
         """Test that empty queries are not marked as expensive."""
         i = web.storage(q='')
