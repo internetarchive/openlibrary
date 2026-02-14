f7e4682d25b8a65a53b1dd8ea5a6d03e5d55aa27
Alex McKee <alex@agmckee.co.uk>
2026-02-14 19:12:51 +0000

    Restore "clamper" functionality for e.g. subjects, without using jQuery

---
+++ openlibrary/plugins/openlibrary/js/clampers.js
@@ -0,0 +1,35 @@
+/**
+ * @param {NodeListOf<Element>} clampers
+ * 
+ */
+export function initClampers(clampers) {
+    for(const clamper of clampers) {
+        if (clamper.clientHeight === clamper.scrollHeight) {
+            clamper.classList.remove('.clamp');
+        } else {
+
+            /*
+                Clamper used to collapse category list by toggling `hidden`
+                style on parent element
+            */
+
+            clamper.addEventListener("click", (event) => {
+                const up = event.target;
+
+                if (event.target instanceof HTMLAnchorElement) {
+                    return;
+                }
+
+                if (up.classList.contains('clamp')) {
+                    clamper.style.display = clamper.style.display === '-webkit-box' || clamper.style.display === '' ? 'unset' : '-webkit-box'
+                    
+                    if (up.getAttribute('data-before') === '\u25BE ') {
+                        up.setAttribute('data-before', '\u25B8 ')
+                    } else {
+                        up.setAttribute('data-before', '\u25BE ')
+                    }
+                }
+            })
+        }
+    }
+}
