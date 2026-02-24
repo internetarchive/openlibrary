6db10c4a1500bca9f5727042e076ced17068c901
Drini Cami <cdrini@gmail.com>
2026-02-24 00:26:23 -0500

    Make solr start script only run in compose.override

--- docker/ol-local-solr-start.sh
+++ docker/ol-local-solr-start.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # Then runs solr-precreate which creates new core or uses existing
 # Only runs in dev mode (LOCAL_DEV=true)
 
-PREFIX="[ol-solr-start]"
+PREFIX="[ol-local-solr-start]"
 
 # This directory is volume mounted in and contains the latest/greatest
 CONFIG_PATH="/opt/solr/server/solr/configsets/olconfig"
@@ -19,9 +19,13 @@ SOLR_SCHEMA_FILE="/var/solr/data/${CORE_NAME}/conf/managed-schema.xml"
 
 echo "${PREFIX} Running"
 
-# If local-dev and schema files exist in solr + ol
-if [ "$LOCAL_DEV" = "true" ] && [ -f "$OL_SCHEMA_FILE" ] && [ -f "$SOLR_SCHEMA_FILE" ]; then
+if [ "$LOCAL_DEV" != "true" ]; then
+    echo "${PREFIX} Not in local dev mode; this script should not be running. Exiting with error."
+    exit 1
+fi
 
+# If local-dev and schema files exist in solr + ol
+if [ -f "$OL_SCHEMA_FILE" ] && [ -f "$SOLR_SCHEMA_FILE" ]; then
     # If the schema files differ
     if ! diff -q "$OL_SCHEMA_FILE" "$SOLR_SCHEMA_FILE" >/dev/null 2>&1; then
         echo "${PREFIX} Schema files has been updated. Attempting to resolve by deleting and recreating Solr core..."
