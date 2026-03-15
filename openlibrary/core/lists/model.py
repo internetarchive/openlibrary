ecf70dac2d5e1b42c8a51462caaccd5ee5e51a62
Ivan Escobar <pisquiy22@gmail.com>
2026-03-15 13:53:32 -0600

    Refactor on the code added for better practices and better structured

--- openlibrary/core/lists/model.py
+++ openlibrary/core/lists/model.py
@@ -657,6 +657,7 @@ class SeriesDict(ListDict):
 
 
 class Series(List):
+    SEED_LIMIT = 100
     @cached_property
     @typing.override
     def seeds(  # type: ignore[override]
@@ -671,7 +672,7 @@ class Series(List):
             {
                 'type': '/type/work',
                 'series': {'series': {'key': self.key}},
-                'limit': 100,
+                'limit': self.SEED_LIMIT,
             },
         )
         works = cast(list[Work], web.ctx.site.get_many(work_keys))
