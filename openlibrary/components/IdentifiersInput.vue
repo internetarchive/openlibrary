fc2da8d941e0526a4c5e38a2615b3b8c1957587e
muralimadhava96-ui <muralimadhava96@gmail.com>
2026-03-30 21:56:40 +0530

    Add inline preview button for identifiers in edit page
    Add a lightweight inline preview feature for identifiers in the edit interface.
    
    This change introduces a small preview button (🔗) next to each identifier,
    allowing users to verify the resolved link without saving or leaving the page.
    
    The preview uses existing identifier configuration (identifierConfigsByKey)
    to construct URLs and opens them in a new tab to preserve unsaved edits.
    
    This approach follows the suggested minimal implementation and avoids adding
    additional UI complexity such as modals.
    
    Signed-off-by: Madhava Murali <muralimadhava96@gmail.com>

--- openlibrary/components/IdentifiersInput.vue
+++ openlibrary/components/IdentifiersInput.vue
@@ -1,24 +1,9 @@
 <template>
-  <div
-    class="identifiers wrapper"
-    role="table"
-  >
-    <div
-      id="identifiers-form"
-      class="identifiers-table"
-      role="row"
-    >
-      <select
-        v-model="selectedIdentifier"
-        class="form-control cell1"
-        name="name"
-      >
-        <option
-          disabled
-          value=""
-        >
-          Select one
-        </option>
+  <div class="identifiers wrapper" role="table">
+    <div id="identifiers-form" class="identifiers-table" role="row">
+      <select v-model="selectedIdentifier" class="form-control cell1" name="name">
+        <option disabled value="">Select one</option>
+
         <template v-if="hasPopularIds">
           <option
             v-for="entry in popularIds.filter(e => isAdmin || e.name !== 'ocaid')"
@@ -27,13 +12,9 @@
           >
             {{ entry.label }}
           </option>
-          <option
-            disabled
-            value=""
-          >
-            ---
-          </option>
+          <option disabled value="">---</option>
         </template>
+
         <option
           v-for="idConfig in identifierConfigsByKey"
           :key="idConfig.name"
@@ -44,10 +25,8 @@
           {{ idConfig.label }}
         </option>
       </select>
-      <div
-        class="cell2"
-        role="cell"
-      >
+
+      <div class="cell2" role="cell">
         <input
           id="id-value"
           v-model.trim="inputValue"
@@ -57,10 +36,8 @@
           @keyup.enter="setIdentifier"
         >
       </div>
-      <div
-        class="cell3"
-        role="cell"
-      >
+
+      <div class="cell3" role="cell">
         <button
           class="form-control"
           name="set"
@@ -79,23 +56,29 @@
         class="assigned-identifiers-table"
         role="row"
       >
-        <div
-          class="identifier-name"
-          role="rowheader"
-        >
+        <div class="identifier-name" role="rowheader">
           {{ identifierConfigsByKey[name]?.label ?? name }}
         </div>
-        <div
-          class="identifier-value"
-          role="cell"
-        >
+
+        <div class="identifier-value" role="cell">
           {{ item }}
         </div>
-        <div
-          class="remove-button"
-          role="cell"
-        >
+
+        <div class="remove-button" role="cell">
+          <!-- 🔗 Preview Button -->
           <button
+            type="button"
+            class="form-control"
+            @click="previewIdentifier(name, item)"
+            aria-label="Preview identifier"
+            title="Preview identifier link"
+          >
+            🔗
+          </button>
+
+          <!-- Remove Button -->
+          <button
+            type="button"
             class="form-control"
             :disabled="!isAdmin && name === 'ocaid'"
             :title="!isAdmin && name === 'ocaid' ? 'Only librarians can edit this identifier' : ''"
@@ -111,281 +94,206 @@
 
 <script>
 import { errorDisplay, validateIdentifiers } from './IdentifiersInput/utils/utils.js';
-const identifierPatterns  = {
-    wikidata: /^Q[1-9]\d*$/i,
-    isni: /^[0]{4} ?[0-9]{4} ?[0-9]{4} ?[0-9]{3}[0-9X]$/i,
-    lc_naf: /^n[bors]?[0-9]+$/,
-    amazon: /^B[0-9A-Za-z]{9}$/,
-    youtube: /^@[A-Za-z0-9_\-.]{3,30}/,
-}
+
+const identifierPatterns = {
+  wikidata: /^Q[1-9]\d*$/i,
+  isni: /^[0]{4} ?[0-9]{4} ?[0-9]{4} ?[0-9]{3}[0-9X]$/i,
+  lc_naf: /^n[bors]?[0-9]+$/,
+  amazon: /^B[0-9A-Za-z]{9}$/,
+  youtube: /^@[A-Za-z0-9_\-.]{3,30}/,
+};
 
 export default {
-    // Props are for external options; if a subelement of this is modified,
-    // the view automatically re-renders
-    props: {
-        /** The list of ids currently associated with the entity in the database in string form */
-        assigned_ids_string: {
-            type: String,
-            //default: () =>  "{'wikidata': 'Q10000'}"
-        },
-        /** everything from https://openlibrary.org/config/author
-         * Most importantly:
-         * {"identifiers": [{"label": "ISNI", "name": "isni", "notes": "", "url": "http://www.isni.org/@@@", "website": "http://www.isni.org/"}, ... ]}
-         */
-        id_config_string: {
-            type: String,
-            required: true
-        },
-        /** see createHiddenInputs function for usage
-         * #hiddenEditionIdentifiers, #hiddenWorkIdentifiers
-         */
-        output_selector: {
-            type: String,
-            required: true
-        },
-        /*
-        Where to save; eg author--remote_ids-- for authors or
-        edition--identifiers-- for editions
-        */
-        input_prefix: {
-            type: String
-        },
-        /** Editions and works can have multiple identifiers in the form of a list
-         * Not applicable to author identifiers
-         */
-        multiple: {
-            type: String,
-            default: 'false',
-        },
-        admin: {
-            type: String,
-            default: 'false',
-        },
-        /** Maintains identifier order in the dropdown list
-         * Expects a list containing the name field from the
-         * respective identifiers.yaml file
-        */
-        popular_ids: {
-            type: String,
-            default: '',
-        },
+  props: {
+    assigned_ids_string: { type: String },
+    id_config_string: { type: String, required: true },
+    output_selector: { type: String, required: true },
+    input_prefix: { type: String },
+    multiple: { type: String, default: 'false' },
+    admin: { type: String, default: 'false' },
+    popular_ids: { type: String, default: '' },
+  },
+
+  data: () => ({
+    selectedIdentifier: '',
+    inputValue: '',
+    assignedIdentifiers: {}
+  }),
+
+  computed: {
+    idConfigs() {
+      return JSON.parse(decodeURIComponent(this.id_config_string));
     },
-
-    // Data is for internal stuff. This needs to be a function so that we get
-    // a fresh object every time this is initialized.
-    data: () => {
-        return {
-            selectedIdentifier: '', // Which identifier is selected in dropdown
-            inputValue: '', // What user put into input
-            assignedIdentifiers: {} // IDs assigned to the entity Ex: {'viaf': '12632978'} or {'abaa': ['123456','789012']}
-        }
+    popularIds() {
+      if (this.popular_ids) {
+        const popularIdNames = JSON.parse(decodeURIComponent(this.popular_ids));
+        return this.idConfigs.filter(config => popularIdNames.includes(config.name));
+      }
+      return [];
     },
-
-    computed: {
-        idConfigs: function() {
-            return JSON.parse(decodeURIComponent(this.id_config_string))
-        },
-        popularIds: function() {
-            if (this.popular_ids) {
-                const popularIdNames = JSON.parse(decodeURIComponent(this.popular_ids));
-                return this.idConfigs.filter((config) => popularIdNames.includes(config.name));
-            }
-            return [];
-        },
-        identifierConfigsByKey: function() {
-            return Object.fromEntries(this.idConfigs.map(e => [e.name, e]));
-        },
-        saveIdentifiersAsList: function() {
-            return this.multiple.toLowerCase() === 'true';
-        },
-        setButtonEnabled: function(){
-            return this.selectedIdentifier !== '' && this.inputValue !== '' && (this.isAdmin || this.selectedIdentifier !== 'ocaid');
-        },
-        hasPopularIds: function() {
-            return Object.keys(this.popularIds).length !== 0;
-        },
-        isAdmin: function() {
-            return this.admin.toLowerCase() === 'true';
-        }
+    identifierConfigsByKey() {
+      return Object.fromEntries(this.idConfigs.map(e => [e.name, e]));
     },
-    watch: {
-        assignedIdentifiers:
-            {
-                handler: function(){this.createHiddenInputs()},
-                deep: true
-            },
-        inputValue:
-            {
-                handler: function(){this.selectIdentifierByInputValue()},
-            },
+    saveIdentifiersAsList() {
+      return this.multiple.toLowerCase() === 'true';
     },
-    created: function(){
-        this.assignedIdentifiers = JSON.parse(decodeURIComponent(this.assigned_ids_string));
-        if (this.assignedIdentifiers.length === 0) {
-            this.assignedIdentifiers = {}
-            return;
-        }
-        if (this.saveIdentifiersAsList) {
-            const edition_identifiers = {};
-            this.assignedIdentifiers.forEach(entry => {
-                if (!edition_identifiers[entry.name]) {
-                    edition_identifiers[entry.name] = [entry.value];
-                } else {
-                    edition_identifiers[entry.name].push(entry.value)
-                }
-            })
-            this.assignedIdentifiers = edition_identifiers;
-        }
+    setButtonEnabled() {
+      return this.selectedIdentifier !== '' && this.inputValue !== '' && (this.isAdmin || this.selectedIdentifier !== 'ocaid');
     },
-
-    methods: {
-        setIdentifier: function(){
-            // if no identifier selected don't execute
-            if (!this.setButtonEnabled) return
-
-            if (this.selectedIdentifier === 'isni') {
-                this.inputValue = this.inputValue.replace(/\s/g, '')
-            }
-            if (this.saveIdentifiersAsList) {
-                // collect id values of matching type, or empty array if none present
-                const existingIds = this.assignedIdentifiers[this.selectedIdentifier] ?? [];
-                // Only one Internet Archive ID (ocaid) is allowed per edition
-                if (this.selectedIdentifier === 'ocaid' && existingIds.length > 0) {
-                    errorDisplay('Only one Internet Archive ID is allowed per edition.', this.output_selector);
-                    return;
-                }
-                const validEditionId = validateIdentifiers(this.selectedIdentifier, this.inputValue, existingIds, this.output_selector);
-                if (validEditionId) {
-                    if (!this.assignedIdentifiers[this.selectedIdentifier]) {
-                        this.inputValue = [this.inputValue];
-                    } else {
-                        const updateIdentifiers = this.assignedIdentifiers[this.selectedIdentifier]
-                        updateIdentifiers.push(this.inputValue);
-                        this.inputValue = updateIdentifiers;
-                    }
-                } else {
-                    return;
-                }
-            } else if (this.assignedIdentifiers[this.selectedIdentifier]) {
-                errorDisplay(`An identifier for ${this.identifierConfigsByKey[this.selectedIdentifier].label} already exists.`, this.output_selector)
-                return;
-            } else { errorDisplay('', this.output_selector) }
-
-            this.assignedIdentifiers[this.selectedIdentifier] = this.inputValue;
-            this.inputValue = '';
-            this.selectedIdentifier = '';
-        },
-        /** Removes an identifier with value from memory and it will be deleted from database on save */
-        removeIdentifier: function(identifierName, idx = 0) {
-            if (this.saveIdentifiersAsList) {
-                this.assignedIdentifiers[identifierName].splice(idx, 1);
-            } else {
-                this.assignedIdentifiers[identifierName] = '';
-            }
-        },
-        createHiddenInputs: function(){
-            /** Right now, we have a vue component embedded as a small part of a larger form
-              * There is no way for that parent form to automatically detect the inputs in a component without JS
-              * This is because the vue component is in a shadow dom
-              * So for now this just drops the hidden inputs into the the parent form anytime there is a change
-              */
-            let html = '';
-            // should save a list of ids for work + edition identifiers
-            if (this.saveIdentifiersAsList) {
-                let num = 0;
-                for (const [key, value] of Object.entries(this.assignedIdentifiers)) {
-                    for (const idx in value) {
-                        html += `<input type="hidden" name="${this.input_prefix}--${num}--name" value="${key}"/>`
-                        html += `<input type="hidden" name="${this.input_prefix}--${num}--value" value="${value[idx]}"/>`
-                        num += 1;
-                    }
-                }
-            }
-            else {
-                html = Object.entries(this.assignedIdentifiers)
-                    .map(([name, value]) => `<input type="hidden" name="${this.input_prefix}--${name}" value="${value}"/>`).join('');
-            }
-            document.querySelector(this.output_selector).innerHTML = html;
-        },
-        selectIdentifierByInputValue: function() {
-            // Ignore for edition identifiers
-            if (this.saveIdentifiersAsList) {
-                return;
-            }
-            // Selects the dropdown identifier based on the input value when possible
-            for (const idtype in identifierPatterns) {
-                if (this.inputValue.match(identifierPatterns[idtype])){
-                    this.selectedIdentifier = idtype;
-                    break;
-                }
-            }
-
-        }
+    hasPopularIds() {
+      return Object.keys(this.popularIds).length !== 0;
+    },
+    isAdmin() {
+      return this.admin.toLowerCase() === 'true';
     }
+  },
+
+  watch: {
+    assignedIdentifiers: {
+      handler() { this.createHiddenInputs(); },
+      deep: true
+    },
+    inputValue: {
+      handler() { this.selectIdentifierByInputValue(); }
+    }
+  },
+
+  created() {
+    this.assignedIdentifiers = JSON.parse(decodeURIComponent(this.assigned_ids_string));
+
+    if (this.assignedIdentifiers.length === 0) {
+      this.assignedIdentifiers = {};
+      return;
+    }
+
+    if (this.saveIdentifiersAsList) {
+      const edition_identifiers = {};
+      this.assignedIdentifiers.forEach(entry => {
+        if (!edition_identifiers[entry.name]) {
+          edition_identifiers[entry.name] = [entry.value];
+        } else {
+          edition_identifiers[entry.name].push(entry.value);
+        }
+      });
+      this.assignedIdentifiers = edition_identifiers;
+    }
+  },
+
+  methods: {
+    setIdentifier() {
+      if (!this.setButtonEnabled) return;
+
+      if (this.selectedIdentifier === 'isni') {
+        this.inputValue = this.inputValue.replace(/\s/g, '');
+      }
+
+      if (this.saveIdentifiersAsList) {
+        const existingIds = this.assignedIdentifiers[this.selectedIdentifier] ?? [];
+
+        if (this.selectedIdentifier === 'ocaid' && existingIds.length > 0) {
+          errorDisplay('Only one Internet Archive ID is allowed per edition.', this.output_selector);
+          return;
+        }
+
+        const validEditionId = validateIdentifiers(
+          this.selectedIdentifier,
+          this.inputValue,
+          existingIds,
+          this.output_selector
+        );
+
+        if (validEditionId) {
+          if (!this.assignedIdentifiers[this.selectedIdentifier]) {
+            this.inputValue = [this.inputValue];
+          } else {
+            const updateIdentifiers = this.assignedIdentifiers[this.selectedIdentifier];
+            updateIdentifiers.push(this.inputValue);
+            this.inputValue = updateIdentifiers;
+          }
+        } else return;
+
+      } else if (this.assignedIdentifiers[this.selectedIdentifier]) {
+        errorDisplay(
+          `An identifier for ${this.identifierConfigsByKey[this.selectedIdentifier].label} already exists.`,
+          this.output_selector
+        );
+        return;
+      } else {
+        errorDisplay('', this.output_selector);
+      }
+
+      this.assignedIdentifiers[this.selectedIdentifier] = this.inputValue;
+      this.inputValue = '';
+      this.selectedIdentifier = '';
+    },
+
+    removeIdentifier(identifierName, idx = 0) {
+      if (this.saveIdentifiersAsList) {
+        this.assignedIdentifiers[identifierName].splice(idx, 1);
+      } else {
+        this.assignedIdentifiers[identifierName] = '';
+      }
+    },
+
+    previewIdentifier(name, value) {
+      if (!value) return;
+
+      const url = this.resolveIdentifierUrl(name, value);
+      if (url) {
+        window.open(url, '_blank');
+      }
+    },
+
+    resolveIdentifierUrl(name, value) {
+      const config = this.identifierConfigsByKey[name];
+
+      if (config && config.url) {
+        return config.url.replace('@@@', value);
+      }
+
+      if (name === 'wikidata') {
+        return `https://www.wikidata.org/wiki/${value}`;
+      }
+
+      if (name === 'isbn_10' || name === 'isbn_13') {
+        return `https://openlibrary.org/isbn/${value}`;
+      }
+
+      return null;
+    },
+
+    createHiddenInputs() {
+      let html = '';
+
+      if (this.saveIdentifiersAsList) {
+        let num = 0;
+        for (const [key, value] of Object.entries(this.assignedIdentifiers)) {
+          for (const idx in value) {
+            html += `<input type="hidden" name="${this.input_prefix}--${num}--name" value="${key}"/>`;
+            html += `<input type="hidden" name="${this.input_prefix}--${num}--value" value="${value[idx]}"/>`;
+            num += 1;
+          }
+        }
+      } else {
+        html = Object.entries(this.assignedIdentifiers)
+          .map(([name, value]) =>
+            `<input type="hidden" name="${this.input_prefix}--${name}" value="${value}"/>`
+          ).join('');
+      }
+
+      document.querySelector(this.output_selector).innerHTML = html;
+    },
+
+    selectIdentifierByInputValue() {
+      if (this.saveIdentifiersAsList) return;
+
+      for (const idtype in identifierPatterns) {
+        if (this.inputValue.match(identifierPatterns[idtype])) {
+          this.selectedIdentifier = idtype;
+          break;
+        }
+      }
+    }
+  }
 }
 </script>
-
-<style>
-/* This and .form-control ensure that select, input, and buttons are the same height */
-select.form-control {
-  height: calc(2.25rem + 2px);
-}
-.wrapper {
-  display: inline-block;
-  background-color: #f6f5ee;
-  padding: .25rem;
-}
-.identifiers-table {
-  display: grid;
-  grid-template-columns: auto auto auto;
-  gap: 4px;
-}
-.cell3 {
-  justify-self: end;
-}
-.assigned-identifiers-table {
-  display: grid;
-  grid-template-columns: 60% auto auto;
-  gap: 4px;
-  align-items: center;
-  margin-top: 4px;
-  border-top: 1px solid #ddd;
-  padding: .25rem 0;
-}
-.identifier-value, .identifier-name {
-  white-space: normal;
-  overflow-wrap: break-word;
-  word-break: break-word;
-  min-width: 0;
-}
-.remove-button {
-  justify-self: end;
-}
-/* The <select> element will cause the IdentifiersInput area to expand past its boundaries at around 850px which is why the media query targets 855px and not the standard tablet breakpoint of 768. */
-@media (max-width: 855px) {
-  .identifiers-table {
-    grid-template-columns: 1fr auto;
-    grid-template-areas:
-      "cell1 cell1"
-      "cell2 cell3";
-  }
-  .cell1 { grid-area: cell1; }
-  .cell2 { grid-area: cell2; }
-  .cell3 { grid-area: cell3; }
-  .assigned-identifiers-table {
-    grid-template-columns: minmax(0, 1fr) auto;
-    grid-template-areas:
-      "identifier-name remove-button"
-      "identifier-value remove-button";
-  }
-  .identifier-name { grid-area: identifier-name; font-weight: bold; }
-  .identifier-value { grid-area: identifier-value; }
-  .remove-button { grid-area: remove-button; }
-}
-.form-control {
-  padding: .375rem .75rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  border: 1px solid #ced4da;
-}
-</style>
