e34190b7d7ee654b4b98da26f85f6b9f55107f47
RayBB <RayBB@users.noreply.github.com>
2026-04-08 11:23:22 -0700

    remove extra line

--- openlibrary/plugins/upstream/addbook.py
+++ openlibrary/plugins/upstream/addbook.py
@@ -536,7 +536,6 @@ def trim_value(value):
     [1, 2]
     >>> trim_value({'x': 'a', 'y': ''})
     {'x': 'a'}
-    >>> trim_value({'x': ['']})
     >>> print(trim_value({'x': ['']}))
     None
     """
