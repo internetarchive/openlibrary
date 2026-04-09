55da02ee9166cb3fbd1f0fa4a176f193e0aead9e
Numericals <98039774+numericals-org@users.noreply.github.com>
2026-04-09 23:56:27 +0530

    Replace postgres wait loop with Docker healthcheck (#12339)
    Co-authored-by: RayBB <RayBB@users.noreply.github.com>

--- docker/ol-home-start.sh
+++ docker/ol-home-start.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# quick method to start all ol services from one script
-# inside an container, bypass all upstart/services
-
-python --version
-
-echo "Waiting for postgres..."
-until pg_isready --host db; do sleep 5; done
-
-
 make reindex-solr
