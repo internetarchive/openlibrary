beb6b9cf4df6c534dca4ed26f1b621cff9c974b1
SK Akram <skcodewizard786@gmail.com>
2026-04-12 12:10:37 +0000

    feat: auto-sync docs wiki into docs/wiki/ on container start

--- docker/ol-home-start.sh
+++ docker/ol-home-start.sh
@@ -4,7 +4,9 @@
 # documentation locally without switching to the browser.
 if [ -d "/openlibrary/docs/wiki/.git" ]; then
     echo "Updating docs wiki..."
-    cd /openlibrary/docs/wiki && git pull
+    cd /openlibrary/docs/wiki && git pull --ff-only
+elif [ -d "/openlibrary/docs/wiki" ]; then
+    echo "docs/wiki exists but is not a git repo, skipping..."
 else
     echo "Cloning docs wiki..."
     git clone https://github.com/internetarchive/openlibrary.wiki.git /openlibrary/docs/wiki
