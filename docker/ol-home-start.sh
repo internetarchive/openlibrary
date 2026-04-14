927e3db43382f6dc997325b049437d0b38921e15
SK Akram <skcodewizard786@gmail.com>
2026-04-14 04:51:49 +0000

    address RayBB feedback: remove skipped directory case

--- docker/ol-home-start.sh
+++ docker/ol-home-start.sh
@@ -5,8 +5,6 @@
 if [ -d "/openlibrary/docs/wiki/.git" ]; then
     echo "Updating docs wiki..."
     cd /openlibrary/docs/wiki && git pull --ff-only
-elif [ -d "/openlibrary/docs/wiki" ]; then
-    echo "docs/wiki exists but is not a git repo, skipping..."
 else
     echo "Cloning docs wiki..."
     git clone https://github.com/internetarchive/openlibrary.wiki.git /openlibrary/docs/wiki
