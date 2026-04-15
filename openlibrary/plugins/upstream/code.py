9331025291b8e3a12cad010119675d18bc90b1a0
jimchamp <28732543+jimchamp@users.noreply.github.com>
2026-04-15 10:27:16 -0700

    Update actions where needed (#12226)
    * Update actions where needed
    We may need an update for addbook.py (savemany w/ bulk-update); currently saves authors, editions, and works (summary in UI should probably be augmented by transaction_details). Transaction_details for transactions we fetch should almost certainly be cached -- i.e. synthesized commit msgs from `transaction_details` will go in cache... It would be nice if we had a cached function that could accept a list of transaction IDs and fetch all details or synthesized commit messages in one call -- i.e. `cached_transaction_details(list_of_txids) -> dict of commit_msgs.
    
    * we *hope* this should track series and list updates distinctly
    Co-authored-by: Mek <michael.karpeles@gmail.com>

--- openlibrary/plugins/upstream/code.py
+++ openlibrary/plugins/upstream/code.py
@@ -334,7 +334,9 @@ class revert(delegate.mode):
                 if prev.type.key in ["/type/delete", "/type/redirect"]:
                     return revert(prev)
                 else:
-                    prev._save("revert to revision %d" % prev.revision)
+                    prev._save(
+                        "revert to revision %d" % prev.revision, action="revert-version"
+                    )
                     return prev
             elif thing.type.key == "/type/redirect":
                 redirect = web.ctx.site.get(thing.location)
@@ -370,7 +372,7 @@ class revert(delegate.mode):
             thing[k] = process(thing[k])
 
         comment = i._comment or "reverted to revision %d" % v
-        thing._save(comment)
+        thing._save(comment, action="revert-version")
         raise web.seeother(key)
 
 
