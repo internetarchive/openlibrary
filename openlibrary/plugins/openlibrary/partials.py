a78909a1cee9133d6357ea25621a3e03583e8f1b
Sanket Srivastava <sanket17052006@gmail.com>
2026-04-18 00:54:59 +0530

    move get_betterworldbooks_metadata and get_amazon_metadata out of AffiliateLinks template (#12393)
    Co-authored-by: RayBB <RayBB@users.noreply.github.com>

--- openlibrary/plugins/openlibrary/partials.py
+++ openlibrary/plugins/openlibrary/partials.py
@@ -12,7 +12,9 @@ from openlibrary.accounts import get_current_user
 from openlibrary.core import cache
 from openlibrary.core.fulltext import fulltext_search_async
 from openlibrary.core.lending import compose_ia_url, get_available
+from openlibrary.core.vendors import get_amazon_metadata, get_betterworldbooks_metadata
 from openlibrary.i18n import gettext as _
+from openlibrary.plugins.openlibrary.code import is_bot
 from openlibrary.plugins.openlibrary.lists import get_lists_async, get_user_lists
 from openlibrary.plugins.upstream.utils import render_macro
 from openlibrary.plugins.upstream.yearly_reading_goals import get_reading_goals
@@ -211,7 +213,23 @@ class AffiliateLinksPartial(PartialDataHandler):
         if len(args) < 2:
             raise ValueError("Unexpected amount of arguments")
 
-        macro = web.template.Template.globals['macros'].AffiliateLinks(args[0], args[1])
+        title, opts = args[0], args[1]
+        isbn = opts.get('isbn', '')
+
+        bwb_metadata = None
+        amz_metadata = None
+        if not is_bot() and opts.get('prices') and isbn:
+            bwb_metadata = get_betterworldbooks_metadata(isbn)
+            if not (bwb_metadata and bwb_metadata.get('market_price')):
+                amz_metadata = get_amazon_metadata(isbn, resources='prices')
+
+        macro = web.template.Template.globals['macros'].AffiliateLinks(
+            title,
+            opts,
+            async_load=False,
+            bwb_metadata=bwb_metadata,
+            amz_metadata=amz_metadata,
+        )
         return {"partials": str(macro)}
 
 
