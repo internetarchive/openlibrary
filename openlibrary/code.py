12f20bf0c0b9bb91d239be86751b27d1d5b3d329
Drini Cami <cdrini@gmail.com>
2026-04-17 12:51:54 -0400

    Support non .json paths in fastapi deprecated handler

--- openlibrary/code.py
+++ openlibrary/code.py
@@ -34,15 +34,18 @@ def setup():
     # override the deprecated ones
     # This is only temporary while we move to fastapi
 
-    for path in deprecated_handler.DEPRECATED_PATHS:
+    for path, encoding in deprecated_handler.DEPRECATED_PATHS:
         if path not in pages:
             pages[path] = {}
-        old_handler = pages[path].get("json")
+        old_handler = pages[path].get(encoding)
         print(
             f"DEBUG [openlibrary/code.py]: Registering deprecated handler for {path}, old handler was: {old_handler}",
             file=sys.stderr,
         )
-        pages[path]["json"] = deprecated_handler.DeprecatedEndpointHandler
+        if encoding == "json":
+            pages[path]["json"] = deprecated_handler.DeprecatedJSONEndpointHandler
+        else:
+            pages[path][None] = deprecated_handler.DeprecatedEndpointHandler
 
     load_views()
 
--- openlibrary/plugins/openlibrary/deprecated_handler.py
+++ openlibrary/plugins/openlibrary/deprecated_handler.py
@@ -68,8 +68,6 @@ def proxy_to_fastapi():
 class DeprecatedEndpointHandler(delegate.page):
     """Catches all deprecated endpoints and redirects them."""
 
-    encoding = "json"
-
     def GET(self, *args):
         return handle_deprecated_request()
 
@@ -80,18 +78,25 @@ class DeprecatedEndpointHandler(delegate.page):
         return handle_deprecated_request()
 
 
-# List of deprecated paths (all json)
-DEPRECATED_PATHS = [
-    "/search",
-    "/search/lists",
-    "/search/subjects",
-    "/search/authors",
-    "/search/inside",
-    "/languages",
-    "/reading-goal",
-    r"(/subjects/[^/]+)",
-    r"(/publishers/[^/]+)",
-    r"(/partials/[^/]+)",
-    r"/api/books.*",
-    r"/api/volumes.*",
+class DeprecatedJSONEndpointHandler(DeprecatedEndpointHandler):
+    encoding = "json"
+
+
+# List of deprecated paths and encodings
+DEPRECATED_PATHS: list[tuple[str, str | None]] = [
+    (r"/search", "json"),
+    (r"/search/lists", "json"),
+    (r"/search/subjects", "json"),
+    (r"/search/authors", "json"),
+    (r"/search/inside", "json"),
+    (r"/languages", "json"),
+    (r"/reading-goal", "json"),
+    (r"(/subjects/[^/]+)", "json"),
+    (r"(/publishers/[^/]+)", "json"),
+    (r"(/partials/[^/]+)", "json"),
+    (r"/api/books", "json"),
+    (r"/api/books", None),
+    # Simplified regex
+    (r"/api/volumes/(.+)", "json"),
+    (r"/api/volumes/(.+)", None),
 ]
