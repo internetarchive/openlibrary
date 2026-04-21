a0e554765c8622237cfe862f5ed87da379d9def4
Sanket Srivastava <sanket17052006@gmail.com>
2026-04-21 22:10:23 +0000

    Add asyncio support

--- openlibrary/utils/sentry.py
+++ openlibrary/utils/sentry.py
@@ -5,6 +5,7 @@ from os import getenv
 
 import sentry_sdk
 import web
+from sentry_sdk.integrations.asyncio import AsyncioIntegration
 from sentry_sdk.tracing import Transaction, TransactionSource
 from sentry_sdk.utils import capture_internal_exceptions
 
@@ -72,6 +73,7 @@ class Sentry:
             profiles_sample_rate=self.config.get("profiles_sample_rate", 0.0),
             release=get_software_version(),
             enable_logs=self.config.get("enable_logs", True),
+            integrations=[AsyncioIntegration()],
         )
 
     def bind_to_webpy_app(self, app: web.application) -> None:
