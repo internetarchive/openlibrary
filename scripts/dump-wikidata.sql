ed3fb7947966446a70f42e73568ec58dbb62ff8a
Tom Morris <tfmorris@gmail.com>
2026-04-30 15:26:56 -0400

    Simplify Wikidata dump. Fixes #11439 - dump as raw TSV instead of weird CSV format - simplify to use in-line command instead of separate script - remove column reordering so that dump matches schema

--- scripts/dump-wikidata.sql
+++
@@ -1,6 +0,0 @@
--- Script to export Wikidata table from the Open Library database
--- When called, this will export the entire wikidata table as a TSV file
--- Usage: psql $PSQL_PARAMS -f dump-wikidata.sql | sed 's/\\\\"/\\"/g' | gzip -c > ol_dump_wikidata_YYYY-MM-DD.txt.gz
-
--- Export all rows from the wikidata table as tab-separated values
-COPY wikidata (id, updated, data) TO STDOUT;
