31c244df69b72932b458865ffa75adf88a27355a
Mek <michael.karpeles@gmail.com>
2026-05-04 17:56:18 -0600

    modsecurity

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -14,6 +14,11 @@ curl -fsSL https://nginx.org/keys/nginx_signing.key | tee /usr/share/keyrings/ng
 echo "deb [signed-by=/usr/share/keyrings/nginx-keyring.asc] http://nginx.org/packages/debian $(lsb_release -cs) nginx" \
     > /etc/apt/sources.list.d/nginx.list
 
-# Install nginx and the NJS module
 apt-get update
+# Install nginx and the NJS module
 apt-get install -y --no-install-recommends nginx nginx-module-njs letsencrypt
+# Install modsecurity
+apt-get install -y libmodsecurity3 libmodsecurity-dev
+
+# Consider pulling modsecurity rules from:
+#wget https://github.com/coreruleset/coreruleset/archive/refs/tags/v4.25.0.tar.gz
