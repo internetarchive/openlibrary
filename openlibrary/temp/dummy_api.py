3ed71fae99a83e2c7edb5ae52fc0ccf6e01fa07c
benbdeitch <benbdeitch@gmail.com>
2026-06-02 11:48:26 -0400

    Temporary commit for switching laptops

---
+++ openlibrary/temp/dummy_api.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI 
+
+
+app = FastAPI() 
+
+@app.get("/")
+async def return_json():
+    return {
+            "status": "OK",
+            "latest_uid": 3,
+            "rows": [
+            {
+                "time": "2026-05-01 16:50:29",
+                "identifier": "completeworksofm21twai",
+                "username": "@XXX",
+                "loan_id": "5bb9565114ff108cff50d2417f4b85c14ae67c42",
+                "event_type": "expire_browse",
+                "extra": "{\"userid\":\"@XXX\",\"listname\":\"loan\",\"identifier\":\"completeworksofm21twai\",\"updatedate\":\"2026-05-01 14:42:43\",\"created\":\"2026-05-01 14:42:43\",\"id\":\"3888614424\",\"type\":\"SESSION_LOAN\",\"until\":\"2026-06-03 15:42:43\"}",
+                "uid": 3
+            }
+            ]
+            }
