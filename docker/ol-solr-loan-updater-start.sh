07598483acd119656866c4153523ac3a942e405f
benbdeitch <benbdeitch@gmail.com>
2026-06-16 14:55:11 -0400

    in-progress update. Schema updates, and updating in-place updates to use _root_ and _version_

--- docker/ol-solr-loan-updater-start.sh
+++ docker/ol-solr-loan-updater-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #Currently, this script is used just for testing with a mock on local. If this makes it through with 'reset=True' and 'use_ia = False', something has gone wrong. 
-python3 scripts/solr_updater/loan_availability_updater.py reset=True use_ia=False
+python3 scripts/solr_updater/loan_availability_updater.py "$OL_CONFIG"\
+    --state-file /solr-updater-data/"$STATE_FILE" 
