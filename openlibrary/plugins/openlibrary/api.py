e5b6d22c8efe99714efa974acdf7d8c77b165c1c
CipherZ3r0 <nilax.pln@gmail.com>
2026-09-06 15:35:58 +0530

    perf: batch edition lookups with get_many in openlibrary/plugins/openlibrary/api.py

--- openlibrary/plugins/openlibrary/api.py
+++ openlibrary/plugins/openlibrary/api.py
@@ -681,7 +681,7 @@ class unlink_ia_ol(delegate.page):
         if not edition_keys:
             raise web.HTTPError("404 Not Found", {"Content-Type": "application/json"})
 
-        editions = [web.ctx.site.get(key) for key in edition_keys]
+        editions = web.ctx.site.get_many(edition_keys)
         logger.info(f"Disassociating {ocaid} from the following editions: {', '.join(edition_keys)}")
 
         # Update records
